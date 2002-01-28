46359bfabbe58c9caebc9432ab3b2afc2a8d25d9
mastcu <mast@colorado.edu>
2002-01-28 16:17:27 +0000

    Fixed report of illegal object number

--- imodutil/imodextract.c
+++ imodutil/imodextract.c
@@ -1,4 +1,39 @@
-/* Simple program to extract a list of objects from a model */
+/*  IMOD VERSION 2.67
+ *
+ *  imodextract.c  -  Program to extract a list of objects from a model
+ *
+ *  Author: David Mastronarde   email: mast@colorado.edu
+ */
+
+/*****************************************************************************
+ *   Copyright (C) 1995-2001 by Boulder Laboratory for 3-Dimensional Fine    *
+ *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
+ *                                                                           *
+ *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
+ *   distributing copies for sale, lease or lending and displaying this      *
+ *   software and documentation.                                             *
+ *   Users may reproduce the software and documentation as long as the       *
+ *   copyright notice and other notices are preserved.                       *
+ *   Neither the software nor the documentation may be distributed for       *
+ *   profit, either in original form or in derivative works.                 *
+ *                                                                           *
+ *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
+ *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
+ *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
+ *                                                                           *
+ *   This work is supported by NIH biotechnology grant #RR00592,             *
+ *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
+ *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
+ *****************************************************************************/
+/*  $Author$
+
+    $Date$
+
+    $Revision$
+
+    $Log$
+*/
+
 
 #include <imodel.h>
 
@@ -53,7 +88,7 @@ int main(int argc, char **argv)
     for (i = 0; i < nlist; i++) {
       ob = list[i] - 1;
       if (ob < 0 || ob >= origsize) {
-	fprintf(stderr, "imodextract: Invalid object number %d\n", ob);
+	fprintf(stderr, "imodextract: Invalid object number %d\n", ob + 1);
 	exit (1);
       }
       imodObjectCopy(&inModel->obj[ob + nlist], &inModel->obj[i]);
