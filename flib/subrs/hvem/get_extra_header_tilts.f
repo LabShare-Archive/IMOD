6d72877c84aace3754968886d7dc16cad5a7956d
mastcu <mast@colorado.edu>
2002-02-26 23:09:22 +0000

    Added test for whether tilt angles are packed as shorts or reals, and ability to retrieve them if they are the first real

--- flib/subrs/hvem/get_extra_header_tilts.f
+++ flib/subrs/hvem/get_extra_header_tilts.f
@@ -10,15 +10,45 @@ c	  NTILT = # of tilt angles returned (or highest # if there are gaps)
 c	  MAXTILTS = size of TILT array
 c	  IZPIECE = Z value of each section in file
 c
+c	  $Author$
+c
+c	  $Date$
+c
+c	  $Revision$
+c
+c	  $Log$
+c
 	subroutine get_extra_header_tilts
      &	    (array,nbsym,nbyte,iflags,nz,tilt,ntilt,maxtilts,izpiece)
+	implicit none
 	byte array(*)
 	real*4 tilt(*)
 	integer*4 izpiece(*)
+	integer*4 nbsym,nbyte,iflags,nz,ntilt,maxtilts
 	integer*2 temp
+	logical nbytes_and_flags,shorts
+	integer*4 i,ind,nbskip,itilt
+c
 	ntilt =0
-	if(mod(iflags,2).eq.0.or.nbyte.eq.0.or.nbsym.eq.0) return
-	ind=1
+	if(nbsym.eq.0) return
+	shorts=nbytes_and_flags(nbyte,iflags)
+	if(shorts) then
+c	    
+c	    if data are packed as shorts, then the tilt angle is the first
+c	    set of data
+c
+	  if(mod(iflags,2).eq.0.or.nbyte.eq.0) return
+	  nbskip=nbyte
+	  ind=1
+	else
+c	    
+c	    otherwise, tilt angle is the first float; need to skip over ints
+c
+	  if(iflags.eq.0)return
+	  nbskip=4*(nbyte+iflags)
+	  ind=1+4*nbyte
+	endif
+c
 	do i=1,nz
 	  itilt=izpiece(i)+1
 	  if(itilt.lt.1)stop
@@ -26,9 +56,13 @@ c
 	  if(itilt.gt.maxtilts)stop
      &	      ' - ARRAY NOT BIG ENOUGH FOR TILT DATA'
 	  ntilt=max(ntilt,itilt)
-	  call move(temp,array(ind),2)
-	  tilt(itilt)=temp/100.
-	  ind=ind+nbyte
+	  if(shorts)then
+	    call move(temp,array(ind),2)
+	    tilt(itilt)=temp/100.
+	  else
+	    call move(tilt(itilt),array(ind),4)
+	  endif
+	  ind=ind+nbskip
 	  if(ind.gt.nbsym)return
 	enddo
 	return
