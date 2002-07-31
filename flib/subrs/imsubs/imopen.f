a0065be945c7226b2a34ee464313c679d171a9fe
mastcu <mast@colorado.edu>
2002-07-31 17:57:41 +0000

    Finished standardizing error output, added implicit none, changed line spacing for output, used lnblnk instead of len to truncate the filename output

--- flib/subrs/imsubs/imopen.f
+++ flib/subrs/imsubs/imopen.f
@@ -12,14 +12,19 @@ c
 c	  $Revision$
 c
 c	  $Log$
+c	  Revision 3.2  2002/07/21 19:17:31  mast
+c	  Standardized error output to ERROR: ROUTINE
+c	
 c	  Revision 3.1  2002/06/26 00:23:44  mast
 c	  Changed STOP statements to print and call exit(1)
 c	
 c
 	SUBROUTINE IMOPEN(ISTREAM,NAME,ATBUTE)
+	implicit none
 	CHARACTER*(*) NAME,ATBUTE
 	character*7 at2
-	CHARACTER*60 FULLNAME
+	CHARACTER*120 FULLNAME
+	integer*4 istream
 	include 'imsubs.inc'
 	DATA NBHDR/1024/, NBW/4/, NBW3/12/, NB/1,2,4,4,8/, NBL/800/
 	DATA FLAG/maxunit*.TRUE./, NOCON/maxunit*.FALSE./, numopen/0/
@@ -29,12 +34,14 @@ c
 	integer*4 intbuf(3)
 	equivalence (buf,intbuf)
 	logical mrctyp,spityp
+	integer*4 j,i,nsam,lenrec,nfilsz,ier,intflip
+	integer*4 lnblnk
 C
 C   Check for valid unit number
 C
 	IF (ISTREAM .GT. maxstream) THEN
 	  WRITE(6,1000)
-1000	  FORMAT(//' IMOPEN: Invalid STREAM number,',
+1000	  FORMAT(//' ERROR: IMOPEN - Invalid STREAM number,',
      &	      ' cannot open file!!!'//)
 	  call exit(1)
 	END IF
@@ -53,6 +60,10 @@ C
 	J = LSTREAM(ISTREAM)
 	FLAG(J) = .TRUE.
 	NOCON(J) = .FALSE.
+c	  
+c	  DNM 7/30/02: initialize flip here for all open units
+c
+	mrcflip(j) = .false.
 	spider(j)=at2(1:2).eq.'RO'.or.at2(1:3).eq.'OLD'
 c	  
 c	  check if it's a SPIDER file or a flipped file
@@ -88,7 +99,7 @@ c
 	  if(.not.(mrctyp.or.spityp))then
 	    print *
 	    print *, 'ERROR: IMOPEN - THIS FILE IS NOT ',
-     &		'RECOGNIZABLE AS MR OR SPIDER IMAGE FILE'
+     &		'RECOGNIZABLE AS MRC OR SPIDER IMAGE FILE'
 	    call exit(1)
 	  endif
 	endif
@@ -115,19 +126,19 @@ C  DNM: for unix, change len(at2) to len(atbute)
 	CALL QINQUIRE(J,FULLNAME,NFILSZ)
 	IF (AT2 .EQ. 'NEW' .OR. AT2 .EQ. 'SCRATCH') THEN
 	  if (print)WRITE(6,2000) AT2(1:len(atbute)),ISTREAM,
-     .	  FULLNAME(1:len(fullname))
+     .	  FULLNAME(1:lnblnk(fullname))
 	ELSE
 	  if (print)WRITE(6,2100) AT2(1:len(atbute)),ISTREAM,
-     .	  FULLNAME(1:len(fullname)),nfilsz
+     .	  FULLNAME(1:lnblnk(fullname)),nfilsz
 	ENDIF
 	endif
-2000	FORMAT(/,1x,A,' image file on unit',I4,' : ',A/)
+2000	FORMAT(/,1x,A,' image file on unit',I4,' : ',A)
 2100	FORMAT(/,1x,A,' image file on unit',I4,' : ',A,
-     .	'     Size= ',I12,/)
+     .	'     Size= ',I12)
 	if(spider(j).and.print)write(6,2200)
-2200	format(20x,'This is a SPIDER file.',/)
+2200	format(/,20x,'This is a SPIDER file.')
 	if(mrcflip(j).and.print)write(6,2300)
-2300	format(20x,'This is a byte-swapped file.',/)
+2300	format(/,20x,'This is a byte-swapped file.')
 C	  
 	RETURN
 C
