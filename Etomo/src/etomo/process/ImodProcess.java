dd3e2c2867ab4dae8febe28f08f36ec9fcfade54
rickg <devnull@localhost>
2002-09-13 21:28:44 +0000

    initial entry

---
+++ Etomo/src/etomo/process/ImodProcess.java
@@ -0,0 +1,20 @@
+package etomo.process;
+
+/*
+ * <p>Description: </p>
+ *
+ * <p>Copyright: Copyright (c) 2002</p>
+ *
+ * <p>Organization: Boulder Laboratory for 3D Fine Structure,
+ * University of Colorado</p>
+ *
+ * @author $Author$
+ *
+ * @version $Revision$
+ *
+ * <p> $Log$
+ * <p> </p>
+ */
+public class ImodProcess {
+	public static final String rcsid = "$Id$";
+}
