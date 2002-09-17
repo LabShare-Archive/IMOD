18b47dd92347bea9e6b2172ad0f6b451b9e8345a
rickg <devnull@localhost>
2002-09-17 23:39:38 +0000

    ImodProcess based, in progress

--- Etomo/src/etomo/process/ImodManager.java
+++ Etomo/src/etomo/process/ImodManager.java
@@ -18,75 +18,65 @@ import java.io.File;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 1.1  2002/09/13 21:28:31  rickg
+ * <p> initial entry
+ * <p>
  * <p> </p>
  */
 public class ImodManager {
-  public static final String rcsid = "$Id$";
-  
+  public static final String rcsid =
+    "$Id$";
+
   private ApplicationManager appManager;
-	private AxisType axisType;
+  private AxisType axisType;
 
-	private int wIDRawStackA = 0;
-	private int wIDRawStackB = 0;
-	private int wIDCoarseAlignedA = 0;
-	private int wIDCoarseAlignedB = 0;
-	private int wIDFineAlignedA = 0;
-	private int wIDFineAlignedB = 0;
-	private int wIDSampleA = 0;
-	private int wIDSampleB = 0;
-	private int wIDTomgramA = 0;
-	private int wIDTomogramB = 0;
-	private int wIDCombined = 0;
-	
-	/**
-	 * Default constructor, this only manages a single imod instance and will
-	 * throw an exception if the AxisID is not AxisID.ONLY when specified.
-	 * AxisType can also be respecified by the setAxisType method.
-	 */
-	public ImodManager(ApplicationManager appMgr) { 
-		appManager = appMgr;
-		axisType = AxisType.SINGLE_AXIS;
-	}
-	
+  private ImodProcess wIDRawStackA;
+  private ImodProcess wIDRawStackB;
+  private ImodProcess wIDCoarseAlignedA;
+  private ImodProcess wIDCoarseAlignedB;
+  private ImodProcess wIDFineAlignedA;
+  private ImodProcess wIDFineAlignedB;
+  private ImodProcess wIDSampleA;
+  private ImodProcess wIDSampleB;
+  private ImodProcess wIDTomgramA;
+  private ImodProcess wIDTomogramB;
+  private ImodProcess wIDCombined;
 
-	/**
-	 * Construction with AxisType specification.  An axisType of
-	 * AxisType.DUAL_AXIS will manage two instances of imod and requires the
-	 * specifiation of which AxisID each method call is operating on.
-	 */
-	public ImodManager(ApplicationManager appMgr, AxisType createAxisType) {
-		appManager = appMgr;
-		axisType = createAxisType;		
-	}
-	
-
-	/**
-	 * Set the axis type of this ImodManager
-	 */
-	public void setAxisType(AxisType newAxisType) {
-		// FIXME how else does the flag state need to be modified
-		axisType = newAxisType;
-	}
-	
-	/**
-	 * Open an imod window with the specified model.  If imod could not open the
-	 * image file and Exception is thrown containing the requested image filename.
-	 */
-	public void openImod(String imageFile) throws Exception {
-		//
-		//  
-	}
-	
-  private void startSystemProgramThread(String command) {
-    SystemProgram sysProgram = new SystemProgram(command);
-    sysProgram.setWorkingDirectory(new File(appManager.getWorkingDirectory()));
-
-    //  Start the system program thread
-    Thread sysProgThread = new Thread(sysProgram);
-    sysProgThread.start();
-    System.out.println("Started " + command);
-    System.out.println("  working directory: " +
-      appManager.getWorkingDirectory());
+  /**
+   * Default constructor, this only manages a single imod instance and will
+   * throw an exception if the AxisID is not AxisID.ONLY when specified.
+   * AxisType can also be respecified by the setAxisType method.
+   */
+  public ImodManager(ApplicationManager appMgr) {
+    appManager = appMgr;
+    axisType = AxisType.SINGLE_AXIS;
   }
-	
+
+  /**
+   * Construction with AxisType specification.  An axisType of
+   * AxisType.DUAL_AXIS will manage two instances of imod and requires the
+   * specifiation of which AxisID each method call is operating on.
+   */
+  public ImodManager(ApplicationManager appMgr, AxisType createAxisType) {
+    appManager = appMgr;
+    axisType = createAxisType;
+  }
+
+  /**
+   * Set the axis type of this ImodManager
+   */
+  public void setAxisType(AxisType newAxisType) {
+    // FIXME how else does the flag state need to be modified
+    axisType = newAxisType;
+  }
+
+  /**
+   * Open an imod window with the specified model.  If imod could not open the
+   * image file and Exception is thrown containing the requested image filename.
+   */
+  public void openImod(String imageFile) throws Exception {
+    //
+    //  
+  }
+
 }
