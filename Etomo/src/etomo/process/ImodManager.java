90ee40a24c471d9a0d4730c03d2a4717046aa8dc
rickg <devnull@localhost>
2002-09-19 23:11:26 +0000

    Completed initial vesion to work with ImodProcess

--- Etomo/src/etomo/process/ImodManager.java
+++ Etomo/src/etomo/process/ImodManager.java
@@ -2,11 +2,14 @@ package etomo.process;
 import etomo.ApplicationManager;
 import etomo.type.AxisID;
 import etomo.type.AxisType;
+import etomo.type.ConstMetaData;
 
 import java.io.File;
 /*
  * <p>Description: This class manages the opening, closing and sending of 
- * messages to the appropriate imod processes.</p>
+ * messages to the appropriate imod processes. This class is state based in the
+ * sense that is initialized with MetaData information and uses that information
+ * to know which data sets to work with.  Thus if the </p>
  *
  * <p>Copyright: Copyright (c) 2002</p>
  *
@@ -18,6 +21,9 @@ import java.io.File;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 1.2  2002/09/17 23:39:38  rickg
+ * <p> ImodProcess based, in progress
+ * <p>
  * <p> Revision 1.1  2002/09/13 21:28:31  rickg
  * <p> initial entry
  * <p>
@@ -29,54 +35,180 @@ public class ImodManager {
 
   private ApplicationManager appManager;
   private AxisType axisType;
+  private String filesetName;
 
-  private ImodProcess wIDRawStackA;
-  private ImodProcess wIDRawStackB;
-  private ImodProcess wIDCoarseAlignedA;
-  private ImodProcess wIDCoarseAlignedB;
-  private ImodProcess wIDFineAlignedA;
-  private ImodProcess wIDFineAlignedB;
-  private ImodProcess wIDSampleA;
-  private ImodProcess wIDSampleB;
-  private ImodProcess wIDTomgramA;
-  private ImodProcess wIDTomogramB;
-  private ImodProcess wIDCombined;
+  private ImodProcess rawStackA;
+  private ImodProcess rawStackB;
+  private ImodProcess coarseAlignedA;
+  private ImodProcess coarseAlignedB;
+  private ImodProcess fineAlignedA;
+  private ImodProcess fineAlignedB;
+  private ImodProcess sampleA;
+  private ImodProcess sampleB;
+  private ImodProcess tomogramA;
+  private ImodProcess tomogramB;
+  private ImodProcess combinedTomogram;
 
   /**
-   * Default constructor, this only manages a single imod instance and will
-   * throw an exception if the AxisID is not AxisID.ONLY when specified.
-   * AxisType can also be respecified by the setAxisType method.
+   * Default constructor
+   * @param metaData this class is used to initialize the
+   * fileset name and axisType of the data to used in imod.
    */
-  public ImodManager(ApplicationManager appMgr) {
-    appManager = appMgr;
-    axisType = AxisType.SINGLE_AXIS;
+  public ImodManager(ConstMetaData metaData) {
+    axisType = metaData.getAxisType();
+    filesetName = metaData.getFilesetName();
+    
+    //  Initialize the necessary ImodProcesses
+    if(axisType == AxisType.SINGLE_AXIS) {
+      rawStackA = new ImodProcess(filesetName + ".st");
+      coarseAlignedA = new ImodProcess(filesetName + ".preali");
+      fineAlignedA = new ImodProcess(filesetName + ".ali");
+      sampleA = new ImodProcess("top.rec mid.rec bot.rec", "tomopitch.mod");
+      tomogramA = new ImodProcess(filesetName + ".rec");
+      tomogramA.setSwapYZ(true);
+    }
+    else {
+      rawStackA = new ImodProcess(filesetName + "a.st");
+      rawStackB = new ImodProcess(filesetName + "b.st");
+      coarseAlignedA = new ImodProcess(filesetName + "a.preali");
+      coarseAlignedB = new ImodProcess(filesetName + "b.preali");
+      fineAlignedA = new ImodProcess(filesetName + "a.ali");
+      fineAlignedB = new ImodProcess(filesetName + "b.ali");
+      sampleA = new ImodProcess("topa.rec mida.rec bota.rec", "tomopitcha.mod");
+      sampleB = new ImodProcess("topb.rec midb.rec botb.rec", "tomopitchb.mod");
+      tomogramA = new ImodProcess(filesetName + "a.rec");
+      tomogramA.setSwapYZ(true);
+      tomogramB = new ImodProcess(filesetName + "b.rec");
+      tomogramB.setSwapYZ(true);
+    }
+      
+  }
+  
+
+  /**
+   * Open the specified raw data stack in imod if it is not already open
+   * @param axisID the AxisID of the desired axis.
+   */
+  public void openRawStack(AxisID axisID) throws Exception {
+    checkAxisID(axisID);
+    if(axisID == AxisID.SECOND) {
+      rawStackB.open();
+    }
+    else {
+      rawStackA.open(); 
+    }
+  }
+
+
+  /**
+   * Open the specified model with the course aligned imod
+   */
+  public void modelRawStack(String modelName, AxisID axisID)
+  throws Exception {
+    // Make sure there is an imod with right course aligned data set that
+    // is already open
+    openRawStack(axisID);
+    if(axisID == AxisID.SECOND) {
+      rawStackB.openModel(modelName);
+    }
+    else {
+      rawStackA.openModel(modelName);
+    }    
+  }
+
+
+  /**
+   * Open the specified coarse aligned stack in imod if it is not already open
+   * @param axisID the AxisID of the desired axis.
+   */
+  public void openCoarseAligned(AxisID axisID) throws Exception {
+    checkAxisID(axisID);
+    if(axisID == AxisID.SECOND) {
+      coarseAlignedB.open();
+    }
+    else {
+      coarseAlignedA.open(); 
+    }
+  }
+
+
+  /**
+   * Open the specified model with the course aligned imod
+   */
+  public void modelCoarseAligned(String modelName, AxisID axisID)
+  throws Exception {
+    // Make sure there is an imod with right coarse aligned data set that
+    // is already open
+    openCoarseAligned(axisID);
+    if(axisID == AxisID.SECOND) {
+      coarseAlignedB.openModel(modelName);
+    }
+    else {
+      coarseAlignedA.openModel(modelName);
+    }    
+  }
+  
+
+  /**
+   * Open the specified fine aligned stack in imod if it is not already open
+   * @param axisID the AxisID of the desired axis.
+   */
+  public void openFineAligned(AxisID axisID) throws Exception {
+    checkAxisID(axisID);
+    if(axisID == AxisID.SECOND) {
+      fineAlignedB.open();
+    }
+    else {
+      fineAlignedA.open(); 
+    }
+  }
+
+
+  /**
+   * Open the specified tomograph samples in imod if they are not already open
+   * @param axisID the AxisID of the desired axis.
+   */
+  public void openSample(AxisID axisID) throws Exception {
+    checkAxisID(axisID);
+    if(axisID == AxisID.SECOND) {
+      sampleB.open();
+    }
+    else {
+      sampleA.open(); 
+    }
   }
 
   /**
-   * Construction with AxisType specification.  An axisType of
-   * AxisType.DUAL_AXIS will manage two instances of imod and requires the
-   * specifiation of which AxisID each method call is operating on.
+   * Open the specified tomogram in imod if it is not already open
+   * @param axisID the AxisID of the desired axis.
    */
-  public ImodManager(ApplicationManager appMgr, AxisType createAxisType) {
-    appManager = appMgr;
-    axisType = createAxisType;
+  public void openTomogram(AxisID axisID) throws Exception {
+    checkAxisID(axisID);
+    if(axisID == AxisID.SECOND) {
+      tomogramB.open();
+    }
+    else {
+      tomogramA.open(); 
+    }
   }
 
   /**
-   * Set the axis type of this ImodManager
+   * Open the specified tomogram in imod if it is not already open
+   * @param axisID the AxisID of the desired axis.
    */
-  public void setAxisType(AxisType newAxisType) {
-    // FIXME how else does the flag state need to be modified
-    axisType = newAxisType;
+  public void openCombinedTomogram(AxisID axisID) throws Exception {
+    checkAxisID(axisID);
+    if(axisID == AxisID.SECOND) {
+      combinedTomogram.open();
+    }
+    else {
+      combinedTomogram.open(); 
+    }
   }
 
-  /**
-   * Open an imod window with the specified model.  If imod could not open the
-   * image file and Exception is thrown containing the requested image filename.
-   */
-  public void openImod(String imageFile) throws Exception {
-    //
-    //  
-  }
-
+  private void checkAxisID(AxisID axisID) throws Exception {
+    if(axisType == AxisType.SINGLE_AXIS  && axisID == AxisID.SECOND) {
+      throw new Exception("Second axis requested in a single axis data set");
+    }
+  }  
 }
