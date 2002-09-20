2c2893a0e95f0450980cc4506fdcc6f03e0704a2
rickg <devnull@localhost>
2002-09-20 16:59:50 +0000

    Initial revision

---
+++ Etomo/src/etomo/process/SystemProcessException.java
@@ -0,0 +1,23 @@
+package etomo.process;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright (c) 2002</p>
+ * 
+ * <p>Organization: Boulder Laboratory for 3D Fine Structure,
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+public class SystemProcessException extends Exception {
+  public static final String rcsid = "$Id$";
+  
+  public SystemProcessException(String message) {
+    super(message);
+  }
+}
