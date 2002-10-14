269e13c1c65af4ec413249dbfc676a9f381678ae
rickg <devnull@localhost>
2002-10-14 19:03:59 +0000

    vmstocsh and csh -ef are executed directly and separately now.

--- Etomo/src/etomo/process/RunComScript.java
+++ Etomo/src/etomo/process/RunComScript.java
@@ -1,6 +1,7 @@
 package etomo.process;
 
 import java.io.*;
+import java.util.ArrayList;
 
 /*
  * <p>Description: Provides a threadable class to execute IMOD com scripts.</p>
@@ -15,6 +16,9 @@ import java.io.*;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 1.4  2002/10/11 23:32:30  rickg
+ * <p> Implementing individual execution of vmstocsh and csh
+ * <p>
  * <p> Revision 1.3  2002/10/10 18:53:29  rickg
  * <p> Enabled SystemProgram debugging and remove local
  * <p> writing to stdout.
@@ -30,14 +34,16 @@ public class RunComScript extends Thread {
   public static final String rcsid =
     "$Id$";
 
-  private String comBaseName = null;
-  private String command = null;
+  private String comScript = null;
   private File workingDirectory = null;
   private ProcessManager processManager;
   private boolean isDemo = false;
+  private boolean enableDebug = false;
+  private String message = "";
+  
 
   public RunComScript(String comScript, ProcessManager processManager) {
-    comBaseName = parseBaseName(comScript, ".com");
+    this.comScript = comScript;
     this.processManager = processManager;
   }
 
@@ -55,32 +61,15 @@ public class RunComScript extends Thread {
   public void run() {
 
     String IMODDIR = processManager.getIMODDirectory();
-    //
-    // FIXME need to include the script below in the code or find a work around
-    // for the exisisting scripts
-    command =
-      "bash "
-        + IMODDIR
-        + "/bin/vmstocsh.sh "
-        + comBaseName
-        + ".com "
-        + comBaseName
-        + ".log ";
 
-    command =
-      "csh -c vmstocsh " + comBaseName + ".log < " + comBaseName + ".com";
-
-    command = "bash -c \"/bin/cat <file.txt \"\" ";
-
-    SystemProgram systemProgram = new SystemProgram(command);
-    systemProgram.setWorkingDirectory(workingDirectory);
-
+    //  Start a progress bar to keep the user informed
     ProcessProgressDialog progress =
-      new ProcessProgressDialog(comBaseName, this);
+      new ProcessProgressDialog(parseBaseName(comScript, ".com"), this);
     progress.stepTimeMS = 200;
     progress.progressBar.setIndeterminate(true);
     Thread progressBarThread = new Thread(progress);
     progressBarThread.start();
+
 
     int exitValue = 0;
     if (isDemo) {
@@ -94,24 +83,47 @@ public class RunComScript extends Thread {
     }
     else {
       try {
-        systemProgram.enableDebug(true);
-        systemProgram.run();
-        exitValue = systemProgram.getExitValue();
+        //  Covert the com script to a sequence of csh command
+        String[] commands = vmsToCsh();
+        execCsh(commands);
       }
       catch (Exception excep) {
         excep.printStackTrace();
-        System.exit(-1);
+        message = excep.getMessage();        
       }
 
     }
-    progressBarThread.interrupt();
 
     //
-    //  Send a message back to the ProcessManager that this thread is done.
+    // Stop the progress bar and send a message back to the ProcessManager
+    // that this thread is done.
     //
-    processManager.msgComScriptDone(exitValue, command);
+    progressBarThread.interrupt();
+    processManager.msgComScriptDone(this, exitValue, comScript);
   }
 
+
+  /**
+   * Get the error message, if any from the run method.
+   */
+  public String getMessage() {
+    return message;
+  }
+  
+  /**
+   * Get the debug state.
+   */
+  public boolean getEnableDebug() {
+    return enableDebug;
+  }
+  
+  /**
+   * Set the debug state.
+   */
+  public void setEnableDebug(boolean state) {
+    enableDebug = state;
+  }
+  
   /**
    * Extract the basename from a filename given the filename and the expected
    * extension.
@@ -123,21 +135,75 @@ public class RunComScript extends Thread {
       base = filename.substring(0, idxExtension);
     return base;
   }
+
+  /**
+   * Execute the csh commands.
+   */
+  private void execCsh(String[] commands) throws SystemProcessException {
+    SystemProgram csh = new SystemProgram("csh -ef");
+    csh.setWorkingDirectory(workingDirectory);
+    csh.setStdInput(commands);
+    csh.enableDebug(enableDebug);
+    csh.run();
+
+    if (csh.getExitValue() != 0) {
+      String[] stderr = csh.getStdError();
+      String message = "";
+      for (int i = 0; i < stderr.length; i++) {
+        message += stderr[i] + "\n";
+      }
+      throw new SystemProcessException(message);
+    }
+  }
+
   /**
    * Convert the com script to a sequence of csh command commands
    * @return A string array containing the csh command sequence
    */
-  //private String[]
-  //  Redirecting stdin for the command does not work even when a shell is called,
+  private String[] vmsToCsh() throws IOException, SystemProcessException {
+    // Redirecting stdin for the command does not work even when a shell is
+    // called, need to pump the com file directly into the stdin of vmstocsh
+    String[] comSequence = loadFile();
+
+
+    SystemProgram vmstocsh = new SystemProgram("vmstocsh "  + 
+      parseBaseName(comScript, ".com") + ".log");
+    vmstocsh.setWorkingDirectory(workingDirectory);
+    vmstocsh.setStdInput(comSequence);
+    vmstocsh.enableDebug(enableDebug);
+    vmstocsh.run();
+
+    if (vmstocsh.getExitValue() != 0) {
+      String[] stderr = vmstocsh.getStdError();
+      String message = "";
+      for (int i = 0; i < stderr.length; i++) {
+        message += stderr[i] + "\n";
+      }
+      throw new SystemProcessException(message);
+    }
+
+    return vmstocsh.getStdOutput();
+  }
 
   /**
-   * Load the file into a StringArray
+   * Load the comscript into a String[]
+   * @return a String[] with each element containing a line of the com script
    */
-  private String[] loadFile() {
+  private String[] loadFile() throws IOException {
 
+    //  Open the file as a 
     InputStream fileStream =
-      new FileInputStream(workingDirectory.getAbsolutePath() + command);
-      
-    
+      new FileInputStream(workingDirectory.getAbsolutePath() + 
+        "/" + comScript);
+
+    BufferedReader fileBuffer =
+      new BufferedReader(new InputStreamReader(fileStream));
+
+    ArrayList lines = new ArrayList();
+    String line;
+    while ((line = fileBuffer.readLine()) != null) {
+      lines.add(line);
+    }
+    return (String[]) lines.toArray(new String[lines.size()]);
   }
 }
