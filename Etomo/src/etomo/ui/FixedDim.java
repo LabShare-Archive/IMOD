2b3dd749f888f24b4cb12fe3abf41cb3a8be9a61
rickg <devnull@localhost>
2002-10-18 17:46:46 +0000

    Added new dimension constansts

--- Etomo/src/etomo/ui/FixedDim.java
+++ Etomo/src/etomo/ui/FixedDim.java
@@ -14,17 +14,23 @@ import java.awt.Dimension;
  *
  * @version $Revision$
  *
- * <p> $Log$ </p>
+ * <p> $Log$
+ * <p> Revision 1.1  2002/09/09 22:57:02  rickg
+ * <p> Initial CVS entry, basic functionality not including combining
+ * <p> </p>
  */
 public class FixedDim {
-  public static final String rcsid = "$Id$";
+  public static final String rcsid =
+    "$Id$";
 
   public final static Dimension x5_y0 = new Dimension(5, 0);
   public final static Dimension x10_y0 = new Dimension(10, 0);
   public final static Dimension x20_y0 = new Dimension(20, 0);
   public final static Dimension x0_y5 = new Dimension(0, 5);
   public final static Dimension x0_y10 = new Dimension(0, 10);
+  public final static Dimension x0_y20 = new Dimension(0, 20);
+  public final static Dimension x0_y40 = new Dimension(0, 40);
   public final static Dimension fileChooser = new Dimension(400, 400);
-  public final static Dimension folderButton = new Dimension(32,32);
-  public final static Dimension button2Line = new Dimension(150,50);
+  public final static Dimension folderButton = new Dimension(32, 32);
+  public final static Dimension button2Line = new Dimension(150, 50);
 }
