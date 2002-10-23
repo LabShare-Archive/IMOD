5ce03447695c731041ba61197f4fcd8037b733aa
mastcu <mast@colorado.edu>
2002-10-23 21:27:02 +0000

    Added check for existence of old file, and err= clauses to open statements, to get a good error output and exit with error

--- flib/subrs/hvem/dopen.f
+++ flib/subrs/hvem/dopen.f
@@ -15,12 +15,20 @@ C  22/6/00 replace implicit sting join (//) with call to CONCAT  CER
 C  23/6/00 remove 'SHARED'... not supported by g77 CER
 C  23/6/00 remove 'CARRIAGECONTROL='LIST'... not supported by g77 CER
 C  23/6/00 remove READONLY... not supproted by g77 CER
+c
+c	  $Author$
+c
+c	  $Date$
+c
+c	  $Revision$
+c
+c	  $Log$
 C------------------------------------------------------
 	SUBROUTINE DOPEN(IUNIT,FNAME,ITYPE,IFORM)
 	CHARACTER*(*) FNAME,ITYPE
 	CHARACTER*1 IFORM
 	CHARACTER*11 FORMAT
-	CHARACTER*50 FULLNAM
+	CHARACTER*120 FULLNAM
 c       add definition of concat subroutine from hvem directory
         character*80 concat
 c	  
@@ -40,6 +48,19 @@ c	    ierr=rename(fname,fname(1:namlen)//'~')
 	  endif
 	endif
 c
+c	  check existence of old file to get a nice error message
+c	  
+	if(itype.eq.'OLD'.or.itype.eq.'old'.or.
+     &	    itype.eq.'RO'.or.itype.eq.'ro')then
+	  inquire(file=fname,exist=exist)
+	  if(.not.exist)then
+	    print *
+	    print *,'ERROR: DOPEN - FILE ',fname(1:lnblnk(fname)),
+     &		' DOES NOT EXIST'
+	    call exit(1)
+	  endif
+	endif
+c
 	FORMAT = 'FORMATTED'
 	IF (IFORM .EQ. 'U' .OR. IFORM .EQ. 'u') FORMAT = 'UNFORMATTED'
 	IF (IFORM .EQ. 'P' .OR. IFORM .EQ. 'p') GOTO 10
@@ -49,27 +70,30 @@ C
 	IF (ITYPE .EQ. 'RO' .OR. ITYPE .EQ. 'ro') GOTO 5
 C	OPEN(UNIT=IUNIT,NAME=FNAME,CARRIAGECONTROL='LIST',
 C    .	STATUS=ITYPE,FORM=FORMAT)
-       	OPEN(UNIT=IUNIT,NAME=FNAME,STATUS=ITYPE,FORM=FORMAT)
+       	OPEN(UNIT=IUNIT,NAME=FNAME,STATUS=ITYPE,FORM=FORMAT,ERR=30)
 	GOTO 20
 C5	OPEN(UNIT=IUNIT,NAME=FNAME,CARRIAGECONTROL='LIST',
 C     .	STATUS='OLD',FORM=FORMAT,SHARED,READONLY)
-5	OPEN(UNIT=IUNIT,NAME=FNAME,STATUS='OLD',FORM=FORMAT)
+5	OPEN(UNIT=IUNIT,NAME=FNAME,STATUS='OLD',FORM=FORMAT,ERR=30)
 	GOTO 20
 C
 C  HERE FOR FORMATTED, PRINTING (CARRIAGECONTROL=FORTRAN) ONLY
 C
 10	IF (ITYPE .EQ. 'RO' .OR. ITYPE .EQ. 'ro') GOTO 15
-	OPEN(UNIT=IUNIT,NAME=FNAME,STATUS=ITYPE,FORM=FORMAT)
+	OPEN(UNIT=IUNIT,NAME=FNAME,STATUS=ITYPE,FORM=FORMAT,ERR=30)
 	GOTO 20
 C15	OPEN(UNIT=IUNIT,NAME=FNAME,STATUS='OLD',FORM=FORMAT,SHARED,
 C     .	READONLY)
-15	OPEN(UNIT=IUNIT,NAME=FNAME,STATUS='OLD',FORM=FORMAT)
+15	OPEN(UNIT=IUNIT,NAME=FNAME,STATUS='OLD',FORM=FORMAT,ERR=30)
 C
 C  NOW WRITE OUT FILE INFO
 C
 20	INQUIRE (FILE=FNAME,NAME=FULLNAM)
-	WRITE(6,1000) FORMAT,ITYPE,IUNIT,FULLNAM
+	WRITE(6,1000) FORMAT,ITYPE,IUNIT,FULLNAM(1:lnblnk(fullnam))
 1000	FORMAT(/,5x,A,3X,A,'  file opened on unit # ',I2,
      .	/'  Name= ',A,/)
 	RETURN
+30	print *
+	print *,'ERROR: DOPEN - CANNOT OPEN FILE ',fname(1:lnblnk(fname))
+	call exit(1)
 	END
