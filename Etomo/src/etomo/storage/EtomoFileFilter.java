49f61343877bb255fdab7aa4ff3fc1b31ad263ea
rickg <devnull@localhost>
2002-12-09 04:14:23 +0000

    Initial revision

---
+++ Etomo/src/etomo/storage/EtomoFileFilter.java
@@ -0,0 +1,41 @@
+package etomo.storage;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+/**
+ * <p>Description: </p>
+ *
+ * <p>Copyright: Copyright (c) 2002</p>
+ *
+ * <p>Organization: Boulder Laboratory for 3D Fine Structure,
+ * University of Colorado</p>
+ *
+ * @author $Author$
+ *
+ * @version $Revision$
+ *
+ * <p> $Log$
+ */
+public class EtomoFileFilter extends FileFilter {
+
+  /**
+   * @see javax.swing.filechooser.FileFilter#accept(File)
+   */
+  public boolean accept(File f) {
+    //  If this is a file test its extension, all others should return true
+    if(f.isFile() &&  ! f.getAbsolutePath().endsWith(".edf")) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * @see javax.swing.filechooser.FileFilter#getDescription()
+   */
+  public String getDescription() {
+    return "Etomo data file";
+  }
+
+}
