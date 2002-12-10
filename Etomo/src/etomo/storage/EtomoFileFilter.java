059b1f6314780fee0c68ae7bf1f46bc7c0f492cc
rickg <devnull@localhost>
2002-12-10 23:47:37 +0000

    Added rcsid

--- Etomo/src/etomo/storage/EtomoFileFilter.java
+++ Etomo/src/etomo/storage/EtomoFileFilter.java
@@ -17,15 +17,19 @@ import javax.swing.filechooser.FileFilter;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 1.1  2002/12/09 04:14:08  rickg
+ * <p> Initial revision
+ * <p>
  */
 public class EtomoFileFilter extends FileFilter {
+  public static final String rcsid = "$Id:";
 
   /**
    * @see javax.swing.filechooser.FileFilter#accept(File)
    */
   public boolean accept(File f) {
     //  If this is a file test its extension, all others should return true
-    if(f.isFile() &&  ! f.getAbsolutePath().endsWith(".edf")) {
+    if (f.isFile() && !f.getAbsolutePath().endsWith(".edf")) {
       return false;
     }
     return true;
--- Etomo/src/etomo/storage/StackFileFilter.java
+++ Etomo/src/etomo/storage/StackFileFilter.java
@@ -17,15 +17,19 @@ import javax.swing.filechooser.FileFilter;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 1.1  2002/12/09 04:14:23  rickg
+ * <p> Initial revision
+ * <p>
  */
 public class StackFileFilter extends FileFilter {
-
+  public static final String rcsid = "$Id:";
+  
   /**
    * @see javax.swing.filechooser.FileFilter#accept(File)
    */
   public boolean accept(File f) {
     //  If this is a file test its extension, all others should return true
-    if(f.isFile() &&  ! f.getAbsolutePath().endsWith(".st")) {
+    if (f.isFile() && !f.getAbsolutePath().endsWith(".st")) {
       return false;
     }
     return true;
