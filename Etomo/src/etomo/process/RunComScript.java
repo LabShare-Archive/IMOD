fab6be53b466306770828f04b33f24a4cb773d49
rickg <devnull@localhost>
2002-12-30 20:31:59 +0000

    Added interface to get process output Program execution correctly calls done message of process manager

--- Etomo/src/etomo/process/RunComScript.java
+++ Etomo/src/etomo/process/RunComScript.java
@@ -16,6 +16,10 @@ import java.util.ArrayList;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 1.6  2002/10/22 21:38:40  rickg
+ * <p> ApplicationManager now controls both demo and debug
+ * <p> modes
+ * <p>
  * <p> Revision 1.5  2002/10/14 19:03:59  rickg
  * <p> vmstocsh and csh -ef are executed directly and separately now.
  * <p>
@@ -36,17 +40,19 @@ import java.util.ArrayList;
 public class RunComScript extends Thread {
   public static final String rcsid =
     "$Id$";
-
-  private String comScript = null;
+    
+  private String name = null;
   private File workingDirectory = null;
   private ProcessManager processManager;
   private boolean demoMode = false;
   private boolean enableDebug = false;
-  private String message = "";
-  
+  private String[] errorMessage;
+  private String[] warningMessage;
+  private SystemProgram vmstocsh;
+  private SystemProgram csh;
 
   public RunComScript(String comScript, ProcessManager processManager) {
-    this.comScript = comScript;
+    this.name = comScript;
     this.processManager = processManager;
   }
 
@@ -67,14 +73,12 @@ public class RunComScript extends Thread {
 
     //  Start a progress bar to keep the user informed
     ProcessProgressDialog progress =
-      new ProcessProgressDialog(parseBaseName(comScript, ".com"), this);
+      new ProcessProgressDialog(parseBaseName(name, ".com"), this);
     progress.stepTimeMS = 200;
     progress.progressBar.setIndeterminate(true);
     Thread progressBarThread = new Thread(progress);
     progressBarThread.start();
 
-
-    int exitValue = 0;
     if (demoMode) {
       try {
         sleep(3000);
@@ -85,48 +89,117 @@ public class RunComScript extends Thread {
       }
     }
     else {
+      //  Covert the com script to a sequence of csh command
+      String[] commands;
       try {
-        //  Covert the com script to a sequence of csh command
-        String[] commands = vmsToCsh();
+        commands = vmsToCsh();
+      }
+      catch(SystemProcessException except) {
+        progressBarThread.interrupt();
+        processManager.msgComScriptDone(this, csh.getExitValue());
+        return;
+      }
+      catch(IOException except) {
+        progressBarThread.interrupt();
+        errorMessage = new String[1];
+        errorMessage[0] = except.getMessage();
+        processManager.msgComScriptDone(this, csh.getExitValue());
+        return;
+      }      
+
+      // Execute the csh commands
+      try {
         execCsh(commands);
       }
-      catch (Exception excep) {
-        excep.printStackTrace();
-        message = excep.getMessage();        
+      catch (SystemProcessException except) {
+        try {
+          errorMessage = parseError();
+        }
+        catch (IOException except2) {
+          errorMessage = new String[1];
+          errorMessage[0] = except2.getMessage();
+        }
       }
-
+      catch (IOException except) {
+        errorMessage = new String[1];
+        errorMessage[0] = except.getMessage();
+      }
+   
+      // Get any warnings from the log file
+      try { 
+        warningMessage = parseWarning();
+      }
+      catch(IOException except2) {
+        errorMessage = new String[1];
+        errorMessage[0] = except2.getMessage();
+      }
     }
 
-    //
     // Stop the progress bar and send a message back to the ProcessManager
     // that this thread is done.
-    //
     progressBarThread.interrupt();
-    processManager.msgComScriptDone(this, exitValue, comScript);
+    processManager.msgComScriptDone(this, csh.getExitValue());
   }
 
+  /**
+   * Get the name of the com script
+   */
+  public String getScriptName() {
+    return name;
+  }
 
   /**
    * Get the error message, if any from the run method.
    */
-  public String getMessage() {
-    return message;
+  public String[] getErrorMessage() {
+    return errorMessage;
   }
-  
+
+  /**
+   * Get the warning message, if any from the run method.
+   */
+  public String[] getWarningMessage() {
+    return warningMessage;
+  }
+
+  /**
+   * Get the standard output
+   */
+  public String[] getStdOutput() {
+    if (csh != null) {
+      return csh.getStdOutput();
+    }
+    else {
+      return null;
+    }
+  }
+
+  /**
+   * Get the standard error output
+   */
+  public String[] getStdError() {
+    if (csh != null) {
+      return csh.getStdError();
+    }
+    else {
+      return null;
+    }
+  }
+
   /**
    * Get the debug state.
    */
   public boolean getEnableDebug() {
     return enableDebug;
   }
-  
+
   /**
    * Set the debug state.
    */
   public void setEnableDebug(boolean state) {
     enableDebug = state;
   }
-  
+
   /**
    * Extract the basename from a filename given the filename and the expected
    * extension.
@@ -142,20 +215,18 @@ public class RunComScript extends Thread {
   /**
    * Execute the csh commands.
    */
-  private void execCsh(String[] commands) throws SystemProcessException {
-    SystemProgram csh = new SystemProgram("csh -ef");
+  private void execCsh(String[] commands)
+    throws IOException, SystemProcessException {
+    csh = new SystemProgram("csh -ef");
     csh.setWorkingDirectory(workingDirectory);
     csh.setStdInput(commands);
     csh.enableDebug(enableDebug);
     csh.run();
 
+    // Check the exit value, if it is non zero, parse the warnings and errors
+    // from the log file.
     if (csh.getExitValue() != 0) {
-      String[] stderr = csh.getStdError();
-      String message = "";
-      for (int i = 0; i < stderr.length; i++) {
-        message += stderr[i] + "\n";
-      }
-      throw new SystemProcessException(message);
+      throw new SystemProcessException("");
     }
   }
 
@@ -168,21 +239,16 @@ public class RunComScript extends Thread {
     // called, need to pump the com file directly into the stdin of vmstocsh
     String[] comSequence = loadFile();
 
-
-    SystemProgram vmstocsh = new SystemProgram("vmstocsh "  + 
-      parseBaseName(comScript, ".com") + ".log");
+    vmstocsh = new SystemProgram("vmstocsh " + parseBaseName(name, ".com") + ".log");
     vmstocsh.setWorkingDirectory(workingDirectory);
     vmstocsh.setStdInput(comSequence);
     vmstocsh.enableDebug(enableDebug);
     vmstocsh.run();
 
     if (vmstocsh.getExitValue() != 0) {
-      String[] stderr = vmstocsh.getStdError();
-      String message = "";
-      for (int i = 0; i < stderr.length; i++) {
-        message += stderr[i] + "\n";
-      }
-      throw new SystemProcessException(message);
+      errorMessage = new String[1];
+      errorMessage[0] = "Running vmstocsh against " + name + " failed";
+      throw new SystemProcessException("");
     }
 
     return vmstocsh.getStdOutput();
@@ -194,10 +260,9 @@ public class RunComScript extends Thread {
    */
   private String[] loadFile() throws IOException {
 
-    //  Open the file as a 
+    //  Open the file as a stream
     InputStream fileStream =
-      new FileInputStream(workingDirectory.getAbsolutePath() + 
-        "/" + comScript);
+      new FileInputStream(workingDirectory.getAbsolutePath() + "/" + name);
 
     BufferedReader fileBuffer =
       new BufferedReader(new InputStreamReader(fileStream));
@@ -223,6 +288,68 @@ public class RunComScript extends Thread {
    */
   public void setDemoMode(boolean demoMode) {
     this.demoMode = demoMode;
+  }
+
+  /**
+   * Parse the log file for errors.  Since the fortran code is no smart enough
+   * handle formatted output we need find ERROR: in the middle of the output
+   * stream.  The error report starts with the ERROR: text instead of the whole
+   * line.
+   */
+  private String[] parseError() throws IOException {
+    //  Open the file as a stream
+    InputStream fileStream =
+      new FileInputStream(
+        workingDirectory.getAbsolutePath()
+          + "/"
+          + parseBaseName(name, ".com")
+          + ".log");
+
+    BufferedReader fileBuffer =
+      new BufferedReader(new InputStreamReader(fileStream));
+
+    ArrayList errors = new ArrayList();
+    String line;
+    while ((line = fileBuffer.readLine()) != null) {
+      int index = line.indexOf("ERROR:");
+      if (index != -1) {
+        errors.add(line.substring(index));
+      }
+    }
+    return (String[]) errors.toArray(new String[errors.size()]);
+  }
+
+  /**
+   * Parse the log file for warnings.  Since the fortran code is no smart enough
+   * handle formatted output we need find WARNING: in the middle of the output
+   * stream.  The error report starts with the WARNING: text instead of the
+   * whole line.
+   * 
+   * @return A String[] containing any errors.  If the com script has not been
+   * run then null is returned.  If the com script ran with no warnings then
+   * zero length array will be returned.
+   */
+  private String[] parseWarning() throws IOException {
+    //  Open the file as a stream
+    InputStream fileStream =
+      new FileInputStream(
+        workingDirectory.getAbsolutePath()
+          + "/"
+          + parseBaseName(name, ".com")
+          + ".log");
+
+    BufferedReader fileBuffer =
+      new BufferedReader(new InputStreamReader(fileStream));
+
+    ArrayList errors = new ArrayList();
+    String line;
+    while ((line = fileBuffer.readLine()) != null) {
+      int index = line.indexOf("WARNING:");
+      if (index != -1) {
+        errors.add(line.substring(index));
+      }
+    }
+    return (String[]) errors.toArray(new String[errors.size()]);
   }
 
 }
