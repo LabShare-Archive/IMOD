fbfea74cd0a805c676b8ddc6ec2e19ad1933cdec
rickg <devnull@localhost>
2003-01-04 00:40:08 +0000

    Added UI items for center view, search direction

--- Etomo/src/etomo/ui/TransferfidPanel.java
+++ Etomo/src/etomo/ui/TransferfidPanel.java
@@ -1,8 +1,10 @@
 package etomo.ui;
 
 import java.awt.Container;
+import java.awt.Color;
 import javax.swing.*;
 import javax.swing.JCheckBox;
+import javax.swing.border.TitledBorder;
 
 import etomo.comscript.TransferfidParam;
 
@@ -15,39 +17,70 @@ import etomo.comscript.TransferfidParam;
  * Window>Preferences>Java>Code Generation.
  */
 public class TransferfidPanel {
-  public static final String rcsid = "$Id$";
+  public static final String rcsid =
+    "$Id$";
 
+  // FIXME: these should be gotten from the app some how
+  private final Color highlight = new Color(248, 254, 255);
+  private final Color shadow = new Color(121, 124, 136);
 
-/*  String inputImageFile = "";
-  String outputImageFile = "";
-  String inputModelFile = "";
-  String outputModelFile = "";
-*/
-  JPanel  panelTransferfid = new JPanel();
-  
-  JCheckBox chkRunMidas = new JCheckBox("Run midas");
+  private JPanel panelTransferfid = new JPanel();
 
-  String logSuffix;  
+  private JCheckBox chkRunMidas = new JCheckBox("Run midas");
+  private LabeledTextField ltfCenterView = new LabeledTextField("Center view: ");
+
+  private JPanel panelSearchDirection = new JPanel();
+  private ButtonGroup bgSearchDirection = new ButtonGroup();
+  private JRadioButton rbSearchBoth = new JRadioButton("Both directions");
+  private JRadioButton rbSearchPlus90 = new JRadioButton("+90 (CCW) only");
+  private JRadioButton rbSearchMinus90 = new JRadioButton("-90 (CW) only");
+
+  private String logSuffix;
 
   public TransferfidPanel(String suffix) {
-    logSuffix = suffix;
-    
-    panelTransferfid.setLayout(new BoxLayout(panelTransferfid, BoxLayout.Y_AXIS));
-    panelTransferfid.add(chkRunMidas);
 
+    logSuffix = suffix;
+
+    panelTransferfid.setLayout(
+      new BoxLayout(panelTransferfid, BoxLayout.Y_AXIS));
+    panelTransferfid.setBorder(new TitledBorder(
+        BorderFactory.createEtchedBorder(highlight, shadow),
+        "Transferfid Parameters"));
+    panelTransferfid.add(chkRunMidas);
+    panelTransferfid.add(ltfCenterView.getContainer());
+
+    bgSearchDirection.add(rbSearchBoth);
+    bgSearchDirection.add(rbSearchPlus90);
+    bgSearchDirection.add(rbSearchMinus90);
+    panelSearchDirection.setLayout(
+      new BoxLayout(panelSearchDirection, BoxLayout.Y_AXIS));
+    panelSearchDirection.setBorder(new TitledBorder(
+        BorderFactory.createEtchedBorder(highlight, shadow),
+        "Search Direction"));
+    panelSearchDirection.add(rbSearchBoth);
+    panelSearchDirection.add(rbSearchPlus90);
+    panelSearchDirection.add(rbSearchMinus90);
+    panelTransferfid.add(panelSearchDirection);
   }
-  
-  public void getParameters(TransferfidParam params) {
+
+  /**
+   * Set the values of the panel using a TransferfidParam parameter
+   * object
+   */
+  public void setParameters(TransferfidParam params) {
     chkRunMidas.setSelected(params.isRunMidas());
   }
-  
-  public void setParameter(TransferfidParam params) {
+
+  /**
+   * Get the values from the panel filling in the TransferfidParam object
+   */
+  public void getParameters(TransferfidParam params) {
     params.setRunMidas(chkRunMidas.isSelected());
+
   }
-  
+
   public Container getContainer() {
     return panelTransferfid;
   }
-  
-    
+
 }
