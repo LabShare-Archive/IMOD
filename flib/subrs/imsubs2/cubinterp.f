3591c5bbe969d9885ea1fff4912733f6f9f669f2
mastcu <mast@colorado.edu>
2003-03-14 02:08:05 +0000

    Added a linear interpolation option and implemented implicit none

--- flib/subrs/imsubs2/cubinterp.f
+++ flib/subrs/imsubs2/cubinterp.f
@@ -19,6 +19,7 @@ C		  center of the output array is normally taken as
 C		  NXB/2, NYB/2
 C	SCALE	- A multiplicative scale actor for the intensities
 C       DMEAN   - Mean intensity of image- dnm added to use this instead of 0
+C	LINEAR  - Set non-zero to do linear interpolation
 C	
 C	Xo = a11(Xi - Xc) + a12(Yi - Yc) + NXB/2. + XT
 C	Yo = a21(Xi - Xc) + a22(Yi - Yc) + NYB/2. + YT
@@ -32,10 +33,23 @@ c
 c	  $Revision$
 c
 c	  $Log$
+c	  Revision 3.1  2002/07/31 01:09:22  mast
+c	  Changed margins for computing xlft and xrt from 2.0 and 1.001 to
+c	  2.01 and 1.01 because of crash on SGI, probably due to rounding
+c	  error
+c	
 c
 	SUBROUTINE CUBINTERP(ARRAY,BRAY,NXA,NYA,NXB,NYB,AMAT,
-     .	XC,YC,XT,YT,SCALE,DMEAN)
-	DIMENSION ARRAY(NXA,NYA),BRAY(NXB,NYB),AMAT(2,2)
+     &	    XC,YC,XT,YT,SCALE,DMEAN, linear)
+	implicit none
+	integer*4 NXA,NYA,NXB,NYB,linear
+	real*4 ARRAY(NXA,NYA),BRAY(NXB,NYB),AMAT(2,2)
+	real*4 XC,YC,XT,YT,SCALE,dmean
+	real*4 xcen,ycen,xco,yco,denom,a11,a12,a22,a21,dyo,xbase,ybase
+	real*4 xst,xnd,xlft,xrt,xp,yp,dennew,dx,dy,v2,v4,v6,v8,v5,a,b,c,d
+	real*4 dxm1,dxdxm1,fx1,fx2,fx3,fx4,dym1,dydym1,v1,v3,vmin,vmax
+	integer*4 iy,ix,ixp,ixpp1,iyp,iypp1,iypp2,ixpp2,ixpm1,iypm1,linefb
+	integer*4 ixnd,ixst,ixfbst,ixfbnd,iqst,iqnd,ifall
 C
 C   Calc inverse transformation
 C
@@ -51,7 +65,7 @@ C
 C
 C Loop over output image
 C
-	DO 200 IY = 1,NYB
+	DO IY = 1,NYB
 	  DYO = IY - YCEN
 	  xbase = a12*dyo +xco - a11*xcen
 	  ybase = a22*dyo + yco - a21*xcen
@@ -111,97 +125,146 @@ c
 	    bray(ix,iy)=dmean
 	  enddo
 c	    
-c	    do fallback to quadratic
+c	    do fallback to quadratic with tests
 c	    
 	  iqst=ixfbst
 	  iqnd=ixst-1
 	  do ifall=1,2
-	    do 100 ix=iqst,iqnd
+	    if (linear .eq. 0) then
+C		    
+C		    Do quadratic interpolation
+C		    
+	      do ix=iqst,iqnd
+		xp = a11*ix + xbase
+		yp = a21*ix + ybase
+		IXP = NINT(XP)
+		IYP = NINT(YP)
+		dennew = DMEAN
+		IF (IXP .ge. 1 .and. IXP .le. NXA .and. IYP .ge. 1 .and.
+     &		    IYP .le. NYA) then
+		  DX = XP - IXP
+		  DY = YP - IYP
+		  IXPP1 = IXP + 1
+		  IXPM1 = IXP - 1
+		  IYPP1 = IYP + 1
+		  IYPM1 = IYP - 1
+		  IF (IXPM1 .LT. 1) IXPM1 = 1
+		  IF (IYPM1 .LT. 1) IYPM1 = 1
+		  IF (IXPP1 .GT. NXA) IXPP1 = NXA
+		  IF (IYPP1 .GT. NYA) IYPP1 = NYA
+C		    
+C		    Set up terms for quadratic interpolation
+C		    
+		  V2 = ARRAY(IXP, IYPM1)
+		  V4 = ARRAY(IXPM1, IYP)
+		  V5 = ARRAY(IXP, IYP)
+		  V6 = ARRAY(IXPP1, IYP)
+		  V8 = ARRAY(IXP, IYPP1)
+		  vmax=max(v2,v4,v5,v6,v8)
+		  vmin=min(v2,v4,v5,v6,v8)
+C		    
+		  A = (V6 + V4)*.5 - V5
+		  B = (V8 + V2)*.5 - V5
+		  C = (V6 - V4)*.5
+		  D = (V8 - V2)*.5
+C		    
+		  dennew = SCALE*(A*DX*DX + B*DY*DY + C*DX + D*DY + V5)
+		  if(dennew.gt.vmax)dennew=vmax
+		  if(dennew.lt.vmin)dennew=vmin
+		endif
+		bray(ix,iy)=dennew
+C		  
+	      enddo
+
+	    else
+c		
+c		fallback to linear
+c
+	      do ix=iqst,iqnd
+		xp = a11*ix + xbase
+		yp = a21*ix + ybase
+		IXP = XP
+		IYP = YP
+		bray(ix,iy)=dmean
+		IF (IXP .ge. 1 .and. IXP .lt. NXA .and. IYP .ge. 1 .and.
+     &		    IYP .lt. NYA) then
+		  DX = XP - IXP
+		  DY = YP - IYP
+		  IXPP1 = IXP + 1
+		  IYPP1 = IYP + 1
+		  bray(ix, iy) = (1. - dy) * ((1. - dx) * array(ixp, iyp) +
+     &		      dx * array(ixpp1, iyp)) +
+     &		      dy * ((1. - dx) * array(ixp, iypp1) +
+     &		      dx * array(ixpp1, iypp1))
+		  
+		endif
+	      enddo
+	    endif
+	    iqst=ixnd+1
+	    iqnd=ixfbnd
+	  enddo
+
+	  if (linear .eq. 0) then
+C
+C   Do cubic interpolation on the central region
+C
+	    DO IX = ixst,ixnd
 	      xp = a11*ix + xbase
 	      yp = a21*ix + ybase
-	      IXP = NINT(XP)
-	      IYP = NINT(YP)
-	      BRAY(IX,IY) = DMEAN
-	      IF (IXP .LT. 1 .OR. IXP .GT. NXA) GOTO 100
-	      IF (IYP .LT. 1 .OR. IYP .GT. NYA) GOTO 100
-C
-C		Do quadratic interpolation
-C		
+	      IXP = XP
+	      IYP = YP
 	      DX = XP - IXP
 	      DY = YP - IYP
 	      IXPP1 = IXP + 1
 	      IXPM1 = IXP - 1
 	      IYPP1 = IYP + 1
 	      IYPM1 = IYP - 1
-	      IF (IXPM1 .LT. 1) IXPM1 = 1
-	      IF (IYPM1 .LT. 1) IYPM1 = 1
-	      IF (IXPP1 .GT. NXA) IXPP1 = NXA
-	      IF (IYPP1 .GT. NYA) IYPP1 = NYA
-C		
-C		Set up terms for quadratic interpolation
-C		
-	      V2 = ARRAY(IXP, IYPM1)
-	      V4 = ARRAY(IXPM1, IYP)
-	      V5 = ARRAY(IXP, IYP)
-	      V6 = ARRAY(IXPP1, IYP)
-	      V8 = ARRAY(IXP, IYPP1)
-	      vmax=max(v2,v4,v5,v6,v8)
-	      vmin=min(v2,v4,v5,v6,v8)
-C		
-	      A = (V6 + V4)*.5 - V5
-	      B = (V8 + V2)*.5 - V5
-	      C = (V6 - V4)*.5
-	      D = (V8 - V2)*.5
-C		
-	      dennew = SCALE*(A*DX*DX + B*DY*DY + C*DX + D*DY + V5)
-	      if(dennew.gt.vmax)dennew=vmax
-	      if(dennew.lt.vmin)dennew=vmin
-	      bray(ix,iy)=dennew
-C
-100	    CONTINUE
-	    iqst=ixnd+1
-	    iqnd=ixfbnd
-	  enddo
-C
-C   Do cubic interpolation on the central region
-C
-	  DO 120 IX = ixst,ixnd
-	    xp = a11*ix + xbase
-	    yp = a21*ix + ybase
-	    IXP = XP
-	    IYP = YP
-	    DX = XP - IXP
-	    DY = YP - IYP
-	    IXPP1 = IXP + 1
-	    IXPM1 = IXP - 1
-	    IYPP1 = IYP + 1
-	    IYPM1 = IYP - 1
-	    ixpp2 = ixp + 2
-	    iypp2 = iyp + 2
+	      ixpp2 = ixp + 2
+	      iypp2 = iyp + 2
+	      
+	      dxm1 = dx-1.
+	      dxdxm1=dx*dxm1
+	      fx1=-dxm1*dxdxm1
+	      fx4=dx*dxdxm1
+	      fx2=1+dx**2*(dx-2.)
+	      fx3=dx*(1.-dxdxm1)
+	      
+	      dym1 = dy-1.
+	      dydym1=dy*dym1
+	      
+	      v1=fx1*array(ixpm1,iypm1)+fx2*array(ixp,iypm1)+
+     &		  fx3*array(ixpp1,iypm1)+fx4*array(ixpp2,iypm1)
+	      v2=fx1*array(ixpm1,iyp)+fx2*array(ixp,iyp)+
+     &		  fx3*array(ixpp1,iyp)+fx4*array(ixpp2,iyp)
+	      v3=fx1*array(ixpm1,iypp1)+fx2*array(ixp,iypp1)+
+     &		  fx3*array(ixpp1,iypp1)+fx4*array(ixpp2,iypp1)
+	      v4=fx1*array(ixpm1,iypp2)+fx2*array(ixp,iypp2)+
+     &		  fx3*array(ixpp1,iypp2)+fx4*array(ixpp2,iypp2)
+	      bray(ix,iy)=-dym1*dydym1*v1+(1.+dy**2*(dy-2.))*v2+
+     &		  dy*(1.-dydym1)*v3 +dy*dydym1*v4
+c		
+	    enddo
 
-	    dxm1 = dx-1.
-	    dxdxm1=dx*dxm1
-	    fx1=-dxm1*dxdxm1
-	    fx4=dx*dxdxm1
-	    fx2=1+dx**2*(dx-2.)
-	    fx3=dx*(1.-dxdxm1)
-
-	    dym1 = dy-1.
-	    dydym1=dy*dym1
-
-	    v1=fx1*array(ixpm1,iypm1)+fx2*array(ixp,iypm1)+
-     &		fx3*array(ixpp1,iypm1)+fx4*array(ixpp2,iypm1)
-	    v2=fx1*array(ixpm1,iyp)+fx2*array(ixp,iyp)+
-     &		fx3*array(ixpp1,iyp)+fx4*array(ixpp2,iyp)
-	    v3=fx1*array(ixpm1,iypp1)+fx2*array(ixp,iypp1)+
-     &		fx3*array(ixpp1,iypp1)+fx4*array(ixpp2,iypp1)
-	    v4=fx1*array(ixpm1,iypp2)+fx2*array(ixp,iypp2)+
-     &		fx3*array(ixpp1,iypp2)+fx4*array(ixpp2,iypp2)
-	    bray(ix,iy)=-dym1*dydym1*v1+(1.+dy**2*(dy-2.))*v2+
-     &		dy*(1.-dydym1)*v3 +dy*dydym1*v4
-c
-120	  CONTINUE
-200	CONTINUE
+	  else
+c	      
+c	      do linear interpolation
+c	      
+	    DO IX = ixst,ixnd
+	      xp = a11*ix + xbase
+	      yp = a21*ix + ybase
+	      IXP = XP
+	      IYP = YP
+	      DX = XP - IXP
+	      DY = YP - IYP
+	      IXPP1 = IXP + 1
+	      IYPP1 = IYP + 1
+	      bray(ix, iy) = (1. - dy) * ((1. - dx) * array(ixp, iyp) +
+     &		  dx * array(ixpp1, iyp)) +
+     &		  dy * ((1. - dx) * array(ixp, iypp1) +
+     &		  dx * array(ixpp1, iypp1))
+	    enddo
+	  endif
+	enddo
 C
 	RETURN
 	END
