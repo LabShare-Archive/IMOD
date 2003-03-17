37fb9b0f0705f6936dd4cf2e4f9fd95ede021f86
rickg <devnull@localhost>
2003-03-17 06:46:30 +0000

    in progress

--- Etomo/src/etomo/comscript/ConstSolvematchmodParam.java
+++ Etomo/src/etomo/comscript/ConstSolvematchmodParam.java
@@ -12,10 +12,21 @@ package etomo.comscript;
  * 
  * @version $Revision$
  * 
- * <p> $Log$ </p>
+ * <p> $Log$
+ * <p> Revision 2.1  2003/03/02 23:30:41  rickg
+ * <p> Combine layout in progress
+ * <p> </p>
  */
 public class ConstSolvematchmodParam {
   public static final String rcsid = "$Id$";
+  protected String tofiducialCoordinatesFile;
+  protected String fromfiducialCoordinatesFile;
+  protected StringList fiducialMatchListA = new StringList(0);
+  protected StringList fiducialMatchListB = new StringList(0);
+  protected FortranInputString xAxistTilt = new FortranInputString(2);
+  protected double residualThreshold;
+  protected int nSurfaces;
+  protected String outputTransformationFile;
   
 }
 
--- Etomo/src/etomo/comscript/ConstSolvematchshiftParam.java
+++ Etomo/src/etomo/comscript/ConstSolvematchshiftParam.java
@@ -12,6 +12,9 @@ package etomo.comscript;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 2.3  2003/03/07 07:22:50  rickg
+ * <p> combine layout in progress
+ * <p>
  * <p> Revision 2.2  2003/03/06 05:53:28  rickg
  * <p> Combine interface in progress
  * <p>
@@ -21,75 +24,75 @@ package etomo.comscript;
  */
 
 public class ConstSolvematchshiftParam {
-  public static final String rcsid =
-    "$Id$";
+	public static final String rcsid =
+		"$Id$";
 
-  protected String tofiducialCoordinatesFile;
-  protected String fromfiducialCoordinatesFile;
-  protected StringList fiducialMatchListA = new StringList(0);
-  protected StringList fiducialMatchListB = new StringList(0);
-  protected FortranInputString xAxistTilt =new FortranInputString(2);
-  protected double residualThreshold;
-  protected int nSurfaces;
-  protected String outputTransformationFile;
+	protected String tofiducialCoordinatesFile;
+	protected String fromfiducialCoordinatesFile;
+	protected StringList fiducialMatchListA = new StringList(0);
+	protected StringList fiducialMatchListB = new StringList(0);
+	protected FortranInputString xAxistTilt = new FortranInputString(2);
+	protected double residualThreshold;
+	protected int nSurfaces;
+	protected String outputTransformationFile;
 
-  public ConstSolvematchshiftParam() {
-  }
+	public ConstSolvematchshiftParam() {
+	}
 
-  /**
-   * @return StringList
-   */
-  public StringList getFiducialMatchListA() {
-    return fiducialMatchListA;
-  }
+	/**
+	 * @return StringList
+	 */
+	public StringList getFiducialMatchListA() {
+		return fiducialMatchListA;
+	}
 
-  /**
-   * @return StringList
-   */
-  public StringList getFiducialMatchListB() {
-    return fiducialMatchListB;
-  }
+	/**
+	 * @return StringList
+	 */
+	public StringList getFiducialMatchListB() {
+		return fiducialMatchListB;
+	}
 
-  /**
-   * @return String
-   */
-  public String getFromfiducialCoordinatesFile() {
-    return fromfiducialCoordinatesFile;
-  }
+	/**
+	 * @return String
+	 */
+	public String getFromfiducialCoordinatesFile() {
+		return fromfiducialCoordinatesFile;
+	}
 
-  /**
-   * @return int
-   */
-  public int getNSurfaces() {
-    return nSurfaces;
-  }
+	/**
+	 * @return int
+	 */
+	public int getNSurfaces() {
+		return nSurfaces;
+	}
 
-  /**
-   * @return String
-   */
-  public String getOutputTransformationFile() {
-    return outputTransformationFile;
-  }
+	/**
+	 * @return String
+	 */
+	public String getOutputTransformationFile() {
+		return outputTransformationFile;
+	}
 
-  /**
-   * @return double
-   */
-  public double getResidualThreshold() {
-    return residualThreshold;
-  }
+	/**
+	 * @return double
+	 */
+	public double getResidualThreshold() {
+		return residualThreshold;
+	}
 
-  /**
-   * @return String
-   */
-  public String getTofiducialCoordinatesFile() {
-    return tofiducialCoordinatesFile;
-  }
+	/**
+	 * @return String
+	 */
+	public String getTofiducialCoordinatesFile() {
+		return tofiducialCoordinatesFile;
+	}
 
-  /**
-   * @return FortranInputString
-   */
-  public FortranInputString getXAxistTilt() {
-    return xAxistTilt;
-  }
+	/**
+	 * @return FortranInputString
+	 */
+	public FortranInputString getXAxistTilt() {
+		return xAxistTilt;
+	}
 
 }
