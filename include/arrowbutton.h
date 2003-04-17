90234988d93fe5cef5befb870d67099a8bc57f9a
cvs2hg <devnull@localhost>
2003-04-17 23:12:01 +0000

    fixup commit for tag 'rel-0-4_initial_full_functionality'

--- include/arrowbutton.h
+++
@@ -1,9 +0,0 @@
-#include <qtoolbutton.h>
-class ArrowButton : public QToolButton
-{
-  Q_OBJECT
-    public:
-
-  ArrowButton ( ArrowType type, QWidget * parent, const char * name = 0 );
-  ~ArrowButton();
-};
