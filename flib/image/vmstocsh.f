b4473e80937a3e4ab109f12ee25ab2781efb53c2
mastcu <mast@colorado.edu>
2003-05-12 18:55:31 +0000

    imod->3dmod name change, and other changes for cygwin/etomo trials

--- flib/image/vmstocsh.f
+++ flib/image/vmstocsh.f
@@ -23,20 +23,29 @@ c	  log file stuff.log will be created, and the bell will ring when the
 c	  job is completed.
 c
 	logical reading
-	integer*4 iffirst,lenin,lencom
+	integer*4 iffirst,lenin,lencom,indarrow
 	character*1024 linein,linecom
 	character*80 logfile
 	character*10 herestring/'HERESTRING'/
 c	  
 	logfile=' '
 	lenlog=1
+c	  
+c	  make indarrow 8 for piping to cat, 2 otherwise
+c
+	indarrow = 2
 	if(iargc().ne.0)then
 	  call getarg(1,logfile)
 	  lenlog=lnblnk(logfile)
 	  write(6,101)'if (-e '//logfile(1:lenlog)//') \\mv -f '
      &	      //logfile(1:lenlog)//' '//logfile(1:lenlog)//'~'
+c	    
+c	    The alternatives for no piping to cat or piping to cat
+c
 	  logfile='  > '//logfile(1:lenlog)
-	  lenlog=lenlog+4
+c	  logfile=' | cat  > '//logfile(1:lenlog)
+
+	  lenlog=lenlog+indarrow+2
 	endif
 
 	write(6,101)'if ($?IMOD_DIR) set path = ($IMOD_DIR/bin $path)'
@@ -50,6 +59,14 @@ c
 101	  format(a)
 	  reading=.true.
 10	  lenin=lnblnk(linein)
+c	    
+c	    For Cygwin/windows, if the line is not properly stripped of
+c	    Return, replace it now
+c
+	  if (lenin.gt.0 .and. linein(lenin:lenin).eq.char(13)) then
+	    linein(lenin:lenin) = ' '
+	    lenin = lenin - 1
+	  endif
 	  if(linein(1:1).ne.'#' .and. linein(1:2).ne.'$!')then
 	    if(iffirst.eq.0.and.linecom(lencom:lencom).eq.'\\')then
 c		
@@ -68,7 +85,7 @@ c
 	      if(iffirst.ge.0)then
 		if(iffirst.eq.0)then
 		  write(6,101)linecom(2:lencom)//logfile(1:lenlog)
-		  logfile(2:2)='>'
+		  logfile(indarrow:indarrow)='>'
 		else
 		  write(6,101)herestring
 		endif
@@ -85,8 +102,8 @@ c
 	      if(iffirst.ge.0)then
 		if(iffirst.eq.0)then
 		  write(6,101)linecom(2:lencom)
-     &		      //logfile(1:lenlog)//' << '//herestring
-		  logfile(2:2)='>'
+     &		      //' << '//herestring//logfile(1:lenlog)
+		  logfile(indarrow:indarrow)='>'
 		  iffirst=1
 		endif
 		write(6,101)linein(1:lenin)
