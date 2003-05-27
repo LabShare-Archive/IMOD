bb7813828aee28345c1aa0c4934bca4ab1988cd7
rickg <devnull@localhost>
2003-05-27 08:47:24 +0000

    Made technique to wait for log file more robust Added estimated time to completion

--- Etomo/src/etomo/process/XcorrProcessWatcher.java
+++ Etomo/src/etomo/process/XcorrProcessWatcher.java
@@ -19,7 +19,15 @@ public class XcorrProcessWatcher implements Runnable {
   SystemProcessInterface systemProcess;
   ApplicationManager applicationManager;
   AxisID axisID;
+  long processStartTime;
+  BufferedReader fileBuffer;
 
+  /**
+   * Construct a proce
+   * @param process
+   * @param appMgr
+   * @param id
+   */
   public XcorrProcessWatcher(
     SystemProcessInterface process,
     ApplicationManager appMgr,
@@ -30,54 +38,21 @@ public class XcorrProcessWatcher implements Runnable {
   }
 
   public void run() {
-    while(! systemProcess.isStarted()) {
-      try {
-        Thread.sleep(100);
-      }
-      catch (InterruptedException excption) {
-        return;
-      }
-      
-    }
-    //  Wait for the process to start signified by the existance of the log file
-    File logFile =
-      new File(
-        System.getProperty(
-          "user.dir"),
-          "xcorr" + axisID.getExtension() + ".log");
-
-    while (!logFile.exists()) {
-      try {
-        Thread.sleep(100);
-      }
-      catch (InterruptedException excption) {
-        return;
-      }
-    }
-
-    //  Open the log file
-    BufferedReader fileBuffer;
-    try {
-      fileBuffer = new BufferedReader(new FileReader(logFile));
-    }
-    catch (FileNotFoundException e1) {
-      return;
-    }
-
+  
+    fileBuffer = waitForLogFile("xcorr");
+    
     //  Search for the number of sections, we should see a header ouput first
     boolean foundNSections = false;
     String line;
     try {
+      int nSections = -1;
       while (!foundNSections) {
-
         Thread.sleep(100);
         while ((line = fileBuffer.readLine()) != null) {
           if (line.startsWith(" Number of columns, rows, sections")) {
             String[] fields = line.split("\\s+");
             if (fields.length > 9) {
-             
-              int nSections = Integer.parseInt(fields[9]);
-               System.err.println(nSections);
+              nSections = Integer.parseInt(fields[9]);
               applicationManager.setProgressBar(
                 "Creating coarse stack",
                 nSections,
@@ -92,16 +67,39 @@ public class XcorrProcessWatcher implements Runnable {
       while (!systemProcess.isDone()) {
         Thread.sleep(100);
         while ((line = fileBuffer.readLine()) != null) {
-          System.err.println(line);
           if (line.startsWith("View")) {
             String[] fields = line.split("\\s+");
             if (fields.length > 1) {
-              System.err.println (fields[1]);
               String number = fields[1];
-              int idxSection = Integer.parseInt(number.substring(0,number.length()-1));
-              System.err.println(idxSection);
-              applicationManager.setProgressBarValue(idxSection, axisID);
-              
+              int idxSection =
+                Integer.parseInt(number.substring(0, number.length() - 1));
+
+              double fractionDone = (double) idxSection / nSections;
+              int percentage = (int) Math.round(fractionDone * 100);
+
+              long elapsedTime = System.currentTimeMillis() - processStartTime;
+              double remainingTime = elapsedTime / fractionDone - elapsedTime;
+              int minutes = (int) remainingTime / 60000;
+              int seconds =
+                (int) Math.round((remainingTime - minutes * 60000) / 1000.0);
+
+              String message =
+                String.valueOf(percentage)
+                  + "%   ETC: "
+                  + String.valueOf(minutes)
+                  + ":";
+              if (seconds < 10) {
+                message = message + "0" + String.valueOf(seconds);
+              }
+              else {
+                message = message + String.valueOf(seconds);
+              }
+
+              applicationManager.setProgressBarValue(
+                idxSection,
+                message,
+                axisID);
+
             }
           }
         }
@@ -110,15 +108,65 @@ public class XcorrProcessWatcher implements Runnable {
     }
     catch (InterruptedException e) {
       applicationManager.progressBarDone(axisID);
-    } 
+    }
     catch (NumberFormatException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
-    } 
+    }
     catch (IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
 
+  }
+  
+  /**
+   * Wait for the process to start and the appropriate log file to be created 
+   * @param logFileBasename
+   * @return a buffered reader of the log file
+   */
+  private BufferedReader waitForLogFile(String logFileBasename) {
+    try {
+
+      //  Wait for the process to start
+      while (!systemProcess.isStarted()) {
+        Thread.sleep(100);
+      }
+
+      processStartTime = System.currentTimeMillis();
+
+      //  Don't do anything until the (possibly new) logfile is created
+      File logFile = null;
+      boolean newLogFile = false;
+      while (!newLogFile) {
+        // Instantiate a new log file object
+        logFile =
+          new File(
+            System.getProperty("user.dir"),
+            logFileBasename + axisID.getExtension() + ".log");
+
+        // Check to see if the log file exists and if it recent enough
+        // Since this thread and the shell thread are asynchronous we want to
+        // make sure that processStartTime estimate is not greater than the log
+        // log file modification time by 5 second
+        if (logFile.exists()) {
+          if (processStartTime - logFile.lastModified() > 5000) {
+            Thread.sleep(100);
+          }
+          else {
+            newLogFile = true;
+          }
+        }
+      }
+      //  Open the log file
+      fileBuffer = new BufferedReader(new FileReader(logFile));
+    }
+    catch (InterruptedException excption) {
+      return null;
+    }
+    catch (FileNotFoundException e1) {
+      return null;
+    }
+    return fileBuffer;
   }
 }
