345b1d5eb924260400a3efd1f8d0639a47ef64cc
mastcu <mast@colorado.edu>
2003-06-20 23:11:39 +0000

    Added capability to process options all in one string

--- flib/subrs/hvem/parse_input_params.f
+++ flib/subrs/hvem/parse_input_params.f
@@ -1,10 +1,13 @@
 c	  PipParseInput provides a simple front end to the "pip" package
 c	  for parsing input parameters
 c	  
-c	  options is an array of character strings, one for each option,
+c	  "options" is an array of character strings, one for each option,
 c	  listing the short name, long name, type code, and help string,
-c	  separated by commas.
-c	  numOptions is the number of options in the array.
+c	  separated by commas.  In this case "separator" should be a space.
+c	  Alternatively, options can be an array consisting of one long
+c	  character string, with each option separated from the next by
+c	  the character in "separator"
+c	  "numOptions" is the number of options in the array or string
 c	  The routine returns numOptArg, the number of option arguments
 c	  on the command line; and numNonOptArg, the number of non-option
 c	  arguments.
@@ -16,27 +19,56 @@ c
 c	  $Revision$
 c
 c	  $Log$
+c	  Revision 3.1  2003/06/05 00:14:22  mast
+c	  Addition to package
+c	
 
-	integer function PipParseInput(options, numOptions, numOptArg,
-     &	    numNonOptArg)
+	integer function PipParseInput(options, numOptions, separator,
+     &	    numOptArg, numNonOptArg)
 	implicit none
 	integer bufferSize
 	parameter (bufferSize = 1024)
 	character*(*) options(*)
+	character separator
 	integer*4 numOptArg, numOptions, numNonOptArg
 	integer*4 PipInitialize, PipAddOption
 	integer*4 PipNextArg
-	integer*4 iargc, i, lnblnk
+	integer*4 iargc, i, lnblnk, j, indStr, indEnd, lenAll
 	character*(bufferSize) string
 c	  
 c	  initialize then pass the options one by one
 c
 	PipParseInput = PipInitialize(numOptions)
 	if (PipParseInput .ne. 0) return
-	do i = 1, numOptions
-	  PipParseInput = PipAddOption(options(i))
-	  if (PipParseInput .ne. 0) return
-	enddo
+	if (separator .eq. ' ') then
+	  do i = 1, numOptions
+	    PipParseInput = PipAddOption(options(i))
+	    if (PipParseInput .ne. 0) return
+	  enddo
+	else
+c	    
+c	    if options are all in one string with a separator
+c
+	  indStr = 1
+	  lenAll = lnblnk(options(1))
+	  do i = 1, numOptions
+	    j = indStr
+	    indEnd = 0
+	    do while (j .le. lenAll .and. indEnd .eq. 0)
+	      if (options(1)(j:j) .eq. separator) indEnd = j - 1
+	      j = j + 1
+	    enddo
+	    if (j .gt. lenAll) indEnd = lenAll
+	    if (indStr .gt. indEnd) then
+	      call PipSetError('Too few options in string')
+	      PipParseInput = -1
+	      return
+	    endif
+	    PipParseInput = PipAddOption(options(1)(indStr:indEnd))
+	    if (PipParseInput .ne. 0) return
+	    indStr = j
+	  enddo
+	endif
 c	  
 c	  pass the arguments in one by one
 c
