046f6524fe7db2308cb420c6ac6a9a417ad5a481
rickg <devnull@localhost>
2003-07-01 19:30:06 +0000

    Added mode bytes handling Get input and output filenames from tilt?.com

--- Etomo/src/etomo/process/TiltProcessMonitor.java
+++ Etomo/src/etomo/process/TiltProcessMonitor.java
@@ -1,11 +1,13 @@
 package etomo.process;
 
+import java.io.File;
+import java.io.IOException;
+
 import etomo.ApplicationManager;
 import etomo.comscript.ComScriptManager;
-import etomo.comscript.ConstTiltParam;
 import etomo.comscript.TiltParam;
-import etomo.comscript.TiltalignParam;
 import etomo.type.AxisID;
+import etomo.util.InvalidParameterException;
 import etomo.util.MRCHeader;
 
 /**
@@ -20,11 +22,15 @@ import etomo.util.MRCHeader;
  * 
  * @version $Revision$
  * 
- * <p> $Log$ </p>
+ * <p> $Log$
+ * <p> Revision 1.1  2003/06/27 20:29:25  rickg
+ * <p> Initial rev (in progress)
+ * <p> </p>
  */
 
 public class TiltProcessMonitor extends FileSizeProcessMonitor {
-  public static final String rcsid = "$Id$";
+  public static final String rcsid =
+    "$Id$";
 
   public TiltProcessMonitor(ApplicationManager appMgr, AxisID id) {
     super(appMgr, id);
@@ -33,33 +39,77 @@ public class TiltProcessMonitor extends FileSizeProcessMonitor {
   /* (non-Javadoc)
    * @see etomo.process.FileSizeProcessMonitor#calcFileSize()
    */
-  long calcFileSize() {
+  void calcFileSize() throws InvalidParameterException, IOException {
     int nX;
     int nY;
     int nZ;
     int modeBytes = 4;
 
-    // Get the header from the aligned aligned stack to use as default nX and
+    // Get the depth, mode, any mods to the X and Y size from the tilt 
+    // command script and the input and output filenames. 
+    ComScriptManager comScriptManager =
+      applicationManager.getComScriptManager();
+    comScriptManager.loadTilt(axisID);
+    TiltParam tiltParam = comScriptManager.getTiltParam(axisID);
+
+    // Get the header from the aligned stack to use as default nX and
     // nY parameters
     String alignedFilename =
-      System.getProperty("user.dir")
-        + applicationManager.getDatasetName()
-        + axisID.getExtension()
-        + "ali";
+      System.getProperty("user.dir") + "/" + tiltParam.getInputFile();
 
     MRCHeader alignedStack = new MRCHeader(alignedFilename);
+    alignedStack.read();
+
     nX = alignedStack.getNRows();
     nY = alignedStack.getNColumns();
 
-    // Get the depth, mode and any mods to the X and Y size from the tilt 
-    // command script
-    ComScriptManager comScriptManager =
-      applicationManager.getComScriptManager();
-
-    //  FIXME: can we assume that the latest tilt param is loaded?
-    TiltParam tiltParam = comScriptManager.getTiltParam(axisID);
     nZ = tiltParam.getThickness();
-    if(tiltParam.has)
-    return 1024 + nX * nY * modeBytes;
+    if (tiltParam.hasWidth()) {
+      nX = tiltParam.getWidth();
+    }
+    if (tiltParam.hasSlice()) {
+      nY =
+        (tiltParam.getIdxSliceStop() - tiltParam.getIdxSliceStart())
+          / tiltParam.getIdxSliceIncr();
+    }
+    if (tiltParam.hasMode()) {
+      switch (tiltParam.getMode()) {
+        case 0 :
+          modeBytes = 1;
+          break;
+        case 1 :
+          modeBytes = 2;
+          break;
+
+        case 2 :
+          modeBytes = 4;
+          break;
+
+        case 3 :
+          modeBytes = 4;
+          break;
+
+        case 4 :
+          modeBytes = 8;
+          break;
+
+        case 16 :
+          modeBytes = 3;
+          break;
+
+        default :
+          throw new InvalidParameterException("Unknown mode parameter");
+      }
+    }
+    long fileSize = 1024 + nX * nY * nZ * modeBytes;
+    nKBytes = (int) (fileSize / 1024);
+
+    applicationManager.setProgressBar("Calculating tomogram", nKBytes, axisID);
+
+    // Create a file object describing the file to be monitored
+    watchedFile =
+      new File(
+        System.getProperty("user.dir"),
+        tiltParam.getOutputFile());
   }
 }
