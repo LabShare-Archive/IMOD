8c65dbee8de9988d471b12a8802cff7691859c56
mastcu <mast@colorado.edu>
2003-07-31 21:42:46 +0000

    Extract object views for the particular objects being extracted

--- imodutil/imodextract.c
+++ imodutil/imodextract.c
@@ -27,13 +27,16 @@
  *****************************************************************************/
 /*  $Author$
 
-    $Date$
+$Date$
 
-    $Revision$
+$Revision$
 
-    $Log$
-    Revision 3.1  2002/01/28 16:17:27  mast
-    Fixed report of illegal object number
+$Log$
+Revision 3.2  2003/02/21 23:18:44  mast
+Open output file in binary mode
+
+Revision 3.1  2002/01/28 16:17:27  mast
+Fixed report of illegal object number
 
 */
 
@@ -42,74 +45,81 @@
 
 static void usage()
 {
-    fprintf(stderr, "Usage: imodextract <list of objects> <input model> <output model>\n");
-    fprintf(stderr, "       The list of objects can include ranges, e.g. 1-3,6,9,13-15\n");
-    exit(-1);
+  fprintf(stderr, "Usage: imodextract <list of objects> <input model> <output model>\n");
+  fprintf(stderr, "       The list of objects can include ranges, e.g. 1-3,6,9,13-15\n");
+  exit(-1);
 }
 
 int *parselist (char *line, int *nlist);
 
 int main(int argc, char **argv)
 {
-    Imod *inModel;
-    int ob, nob;
-    int *list;
-    int i, nlist, origsize;
-    char backname[257];
+  Imod *inModel;
+  int ob, nob, iview;
+  int *list;
+  int i, nlist, origsize;
+  char backname[257];
     
-    if (argc != 4) usage();
+  if (argc != 4) usage();
     
-    list = parselist(argv[1], &nlist);
-    if (!list) {
-      fprintf(stderr, "imodextract: Error parsing object list\n");
-      exit(1);
-    }
+  list = parselist(argv[1], &nlist);
+  if (!list) {
+    fprintf(stderr, "imodextract: Error parsing object list\n");
+    exit(1);
+  }
 
-    inModel = imodRead(argv[2]);
-    if (!inModel) {
-      fprintf(stderr, "imodextract: Fatal error reading model\n");
-      exit(1);
-    }
+  inModel = imodRead(argv[2]);
+  if (!inModel) {
+    fprintf(stderr, "imodextract: Fatal error reading model\n");
+    exit(1);
+  }
 
-/*    for(ob = inModel->objsize - 1; ob >= 0; ob--){
-      found = 0;
-      for (i = 0; i < nlist; i++) {
-	if (list[i] == ob + 1) found = 1;
-      }
-      if(!found)
-	imodDeleteObject(inModel, ob);
-    } */
 
-/* Make nlist new objects, then shift all existing objects to top */
-    origsize = inModel->objsize;
-    for (i = 0; i < nlist; i++)
-      imodNewObject(inModel);
-    for (ob = origsize - 1; ob >= 0; ob--)
-      imodObjectCopy(&inModel->obj[ob], &inModel->obj[ob+nlist]);
+  /* Make nlist new objects */
+  origsize = inModel->objsize;
+  for (i = 0; i < nlist; i++)
+    imodNewObject(inModel);
 
-    /* Now copy all of the selected ones into place */
-    for (i = 0; i < nlist; i++) {
-      ob = list[i] - 1;
-      if (ob < 0 || ob >= origsize) {
-	fprintf(stderr, "imodextract: Invalid object number %d\n", ob + 1);
-	exit (1);
-      }
-      imodObjectCopy(&inModel->obj[ob + nlist], &inModel->obj[i]);
-    }
+  /* Extent the object views then shift all existing objects and their views
+     to top */
+  imodObjviewComplete(inModel);
+  for (ob = origsize - 1; ob >= 0; ob--) {
+    imodObjectCopy(&inModel->obj[ob], &inModel->obj[ob+nlist]);
+    for (iview = 1; iview < inModel->viewsize; iview++)
+      inModel->view[iview].objview[ob + nlist] = 
+        inModel->view[iview].objview[ob];
+  }
 
-    /* Delete extra objects by just setting objsize, set current indexes to -1
-       to avoid problems */
-    inModel->objsize = nlist;
-    inModel->cindex.point  = -1;
-    inModel->cindex.contour = -1;
-    inModel->cindex.object = 0;
-
-    sprintf(backname, "%s~", argv[argc - 1]);
-    rename (argv[argc - 1], backname);
-    if (imodOpenFile(argv[argc - 1], "wb", inModel)) {
-      fprintf(stderr, "imodextract: Fatal error opening new model\n");
+  /* Now copy all of the selected ones into place, copying the view down 
+     too */
+  for (i = 0; i < nlist; i++) {
+    ob = list[i] - 1;
+    if (ob < 0 || ob >= origsize) {
+      fprintf(stderr, "imodextract: Invalid object number %d\n", ob + 1);
       exit (1);
     }
-    imodWriteFile(inModel);
-    exit(0);
+    imodObjectCopy(&inModel->obj[ob + nlist], &inModel->obj[i]);
+    for (iview = 1; iview < inModel->viewsize; iview++)
+      inModel->view[iview].objview[i] = 
+        inModel->view[iview].objview[ob + nlist];
+  }
+
+  /* Delete extra objects by just setting objsize, and the object view sizes,
+     set current indexes to -1 to avoid problems */
+  inModel->objsize = nlist;
+  inModel->cindex.point  = -1;
+  inModel->cindex.contour = -1;
+  inModel->cindex.object = 0;
+  for (iview = 1; iview < inModel->viewsize; iview++)
+    inModel->view[iview].objvsize = nlist;
+      
+
+  sprintf(backname, "%s~", argv[argc - 1]);
+  rename (argv[argc - 1], backname);
+  if (imodOpenFile(argv[argc - 1], "wb", inModel)) {
+    fprintf(stderr, "imodextract: Fatal error opening new model\n");
+    exit (1);
+  }
+  imodWriteFile(inModel);
+  exit(0);
 }
