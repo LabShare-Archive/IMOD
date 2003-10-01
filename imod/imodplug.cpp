a646b1e81ffa811403558724157e910b3e01fbd5
mastcu <mast@colorado.edu>
2003-10-01 05:13:28 +0000

    Changes to use special internal modules

--- imod/imodplug.cpp
+++ imod/imodplug.cpp
@@ -1,3 +1,10 @@
+/*   imodplug.cpp  -  load and communicate with plugins or special modules
+ *
+ *   Copyright (C) 1995-2002 by Boulder Laboratory for 3-Dimensional Electron
+ *   Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *   Colorado.  See implementation file for full copyright notice.
+ */                                                                           
+
 /*  $Author$
 
 $Date$
@@ -5,6 +12,9 @@ $Date$
 $Revision$
 
 $Log$
+Revision 4.4  2003/04/17 19:09:33  mast
+plugs OK on Mac now
+
 Revision 4.3  2003/03/28 05:49:16  mast
 No plugs for Mac either
 
@@ -35,69 +45,136 @@ Changes to get clean compilation with g++
 #include <sys/types.h>
 #include <dlfcn.h>
 #include <dirent.h>
-#include <string.h>
 #endif
+#include <string.h>
 
 #include <qpopupmenu.h>
 #include "imod.h"
 #include "imodplug.h"
+#include "special_module.h"
+#include "beadfix.h"
+
+static int imodPlugLoadDir(char *plugdir);
+static int imodPlugLoad(char *plugpath);
+static int addInternalModules();
+
+static Ilist *plugList = NULL;
 
 
-extern int Imod_debug;
-Ilist *plugList = NULL;
-
-static int maxPlug = 0;
 typedef struct
 {
   char *name;
   void *handle;
+  SpecialModule *module;
   int   type;
 }PlugData;
 
-/*char **plugNames;*/
-
-void imodPlugOpen(int item)
+/* Load all plugins that we can use. */
+/* Return the number we have loaded. */
+int imodPlugInit(void)
 {
-#ifndef NOPLUGS
-  void (*fptr)(ImodView *);
-  void (*nfptr)(ImodView *, int, int);
-  PlugData *pd = (PlugData *)ilistItem(plugList, item);
-  if (!pd) return;
+  PlugData thePlug;
 
+  char *defdir3 = "/usr/freeware/lib/imodplugs/";
+  char *defdir2 = "/usr/local/IMOD/plugins";
+  char *defdir1 = "/usr/IMOD/plugins";
+#ifndef NO_PLUGS  
+  char *envdir = getenv("IMOD_PLUGIN_DIR");
 
-  /* find address of function and data objects */
-  fptr = (void (*)(ImodView *))dlsym
-    (pd->handle, "imodPlugExecute");
-  if (!fptr){
-    nfptr = (void (*)(ImodView *, int, int))dlsym
-      (pd->handle, "imodPlugExecuteType");
-    if (!nfptr){
-      wprint("\nError invoking %s\n", pd->name);
-      return;
-    }
-    (*nfptr)(App->cvi, 1 /*IMOD_PLUG_MENU*/, 0);
-    return;
-  }
-  /* invoke function */
-  (*fptr)(App->cvi);
+  DIR *dirp;
+  struct dirent *dp;
 #endif
-  return;
+
+  int len, type;
+  char *ext;
+  int maxPlug = 0;
+
+  plugList = ilistNew(sizeof(PlugData), 8);
+
+  /* Add internal modules to list first */
+  addInternalModules();
+
+#ifndef NO_PLUGS  
+  /* load system plugins. */
+  imodPlugLoadDir(envdir);
+
+  /* load plugs in environment varialble. */
+  imodPlugLoadDir(defdir1);
+  imodPlugLoadDir(defdir2);
+  imodPlugLoadDir(defdir3);
+#endif
+
+  maxPlug = ilistSize(plugList);
+  return(maxPlug);
 }
 
-
-void imodPlugClean(void)
+/*
+ * Add special modules to the list
+ */
+static int addInternalModules()
 {
-  imodPlugCall(App->cvi, 0, IMOD_REASON_CLEANUP);
-  return;
+  PlugData thePlug, *lplug;
+  int mi, i;
+
+  thePlug.module = new BeadFixerModule;
+  ilistAppend(plugList, &thePlug);
+
+  // After all are added, go get the information from them
+  mi = ilistSize(plugList);
+  for (i = 0; i < mi; i++){
+    lplug = (PlugData *)ilistItem(plugList, i);
+    lplug->name = (*lplug->module->mInfo)(&(lplug->type));
+    lplug->handle = NULL;
+  }
+  return mi;
 }
 
 
 #ifndef NOPLUGS
+/*
+ * Load all the plugins in a given directory 
+ */
+static int imodPlugLoadDir(char *plugdir)
+{
+  char soname[1024];
+
+  DIR *dirp;
+  struct dirent *dp;
+  int len, type;
+  char *ext;
+
+  int pload = 0; /* Return the number of plugs loaded. */
+
+  if (!plugdir) 
+    return pload;
+
+  dirp = opendir(plugdir);
+  if (!dirp)
+    return pload;
+     
+  while ((dp = readdir(dirp)) != NULL) {
+    len = strlen(dp->d_name);
+    ext = dp->d_name + len - 3;
+
+    if (!(strcmp(ext, ".so") == 0))
+      continue;
+
+    /* try and load plug */
+    sprintf(soname, "%s/%s", plugdir, dp->d_name);
+          
+    if (imodPlugLoad(soname))
+      continue;
+  }
+  closedir(dirp);
+
+  return(pload);
+}
+
 /* 
  * Loads the plugin given by the plugpath in to the
  * global pluglist.
  */
-int imodPlugLoad(char *plugpath)
+static int imodPlugLoad(char *plugpath)
 {
   PlugData thePlug, *lplug;
   char *(*fptr)(int *);
@@ -153,83 +230,51 @@ int imodPlugLoad(char *plugpath)
     
   return 0;
 }
+#endif
 
-int imodPlugLoadDir(char *plugdir)
+/*
+ * Open a plugin when selected from Special menu 
+ */
+void imodPlugOpen(int item)
 {
-  char soname[1024];
+  void (*fptr)(ImodView *) = NULL;
+  void (*nfptr)(ImodView *, int, int) = NULL;
+  PlugData *pd = (PlugData *)ilistItem(plugList, item);
+  if (!pd) 
+    return;
 
-  DIR *dirp;
-  struct dirent *dp;
-  char **names;
-  int len, type;
-  char *ext;
+  /* Get function from internal module */
+  if (!pd->handle) {
+    fptr = pd->module->mExecute;
+    if (!fptr)
+      nfptr = pd->module->mExecuteType;
 
-  int pload = 0; /* Return the number of plugs loaded. */
+#ifndef NO_PLUGS
+  } else {
 
-  if (!plugdir) 
-    return pload;
-
-  dirp = opendir(plugdir);
-  if (!dirp)
-    return pload;
-     
-  while ((dp = readdir(dirp)) != NULL) {
-    len = strlen(dp->d_name);
-    ext = dp->d_name + len - 3;
-
-    if (!(strcmp(ext, ".so") == 0))
-      continue;
-
-    /* try and load plug */
-    sprintf(soname, "%s/%s", plugdir, dp->d_name);
-          
-    if (imodPlugLoad(soname))
-      continue;
+    /* or find address of function and data objects */
+    fptr = (void (*)(ImodView *))dlsym(pd->handle, "imodPlugExecute");
+    if (!fptr)
+      nfptr = (void (*)(ImodView *, int, int))dlsym
+        (pd->handle, "imodPlugExecuteType");
+#endif
   }
-  closedir(dirp);
-
-  return(pload);
-}
-#endif
-
-/* Load all plugins that we can use. */
-/* Return the number we have loaded. */
-int imodPlugInit(void)
-{
-#ifdef NOPLUGS
-  return(0);
-#else
-  PlugData thePlug;
-
-  char *defdir3 = "/usr/freeware/lib/imodplugs/";
-  char *defdir2 = "/usr/local/IMOD/plugins";
-  char *defdir1 = "/usr/IMOD/plugins";
-  char *envdir = getenv("IMOD_PLUGIN_DIR");
-
-
-  DIR *dirp;
-  struct dirent *dp;
-  char **names;
-  int len, type;
-  char *ext;
-
-  maxPlug = 0;
-  plugList = ilistNew(sizeof(PlugData), 8);
-
-  /* load system plugins. */
-  imodPlugLoadDir(envdir);
-
-  /* load plugs in environment varialble. */
-  imodPlugLoadDir(defdir1);
-  imodPlugLoadDir(defdir2);
-  imodPlugLoadDir(defdir3);
-
-  maxPlug = ilistSize(plugList);
-  return(maxPlug);
-#endif
+    
+  /* invoke function */
+  if (fptr) {
+    (*fptr)(App->cvi);
+    return;
+  }
+  if (!nfptr){
+    wprint("\nError invoking %s\n", pd->name);
+    return;
+  }
+  (*nfptr)(App->cvi, 1 /*IMOD_PLUG_MENU*/, 0);
 }
 
-/* returns the number of plugins of a given type that loaded. */
+/*
+ * returns the number of plugins of a given type that loaded. 
+ */
 int imodPlugLoaded(int type)
 {
   PlugData *pd;
@@ -245,66 +290,11 @@ int imodPlugLoaded(int type)
   return(plugs);
 }
 
-int imodPlugCall(ImodView *vw, int type, int reason)
-{
-#ifndef NOPLUGS
-  PlugData *pd;
-  int keyhandled,i,mi = ilistSize(plugList);
-  void (*fptr)(ImodView *, int, int);
-    
-  if (!mi) return 0;
-  pd = (PlugData *)ilistFirst(plugList);
-  for(i = 0; i < mi; i++){
-        
-    pd = (PlugData *)ilistItem(plugList, i);
-    if (!pd) continue;
-        
-    fptr = (void (*)(ImodView *, int, int))dlsym
-      (pd->handle, "imodPlugExecuteType");
-    if (fptr){
-      (*fptr)(vw,type,reason);
-    }
-  }
-  return 1;
-#else
-  return 0;
-#endif
-
-}
-
-int imodPlugHandleKey(ImodView *vw, QKeyEvent *event)
-{
-#ifndef NOPLUGS
-  PlugData *pd;
-  int keyhandled,i,mi = ilistSize(plugList);
-  int (*fptr)(ImodView *, QKeyEvent *);
-
-  if (!mi) return 0;
-  pd = (PlugData *)ilistFirst(plugList);
-  for(i = 0; i < mi; i++){
-          
-    pd = (PlugData *)ilistItem(plugList, i);
-    if (!pd) continue;
-          
-    if (pd->type & IMOD_PLUG_KEYS){
-      fptr = (int (*)(ImodView *, QKeyEvent *))dlsym
-	(pd->handle, "imodPlugKeys");
-      if (fptr){
-	keyhandled = (*fptr)(vw, event);
-	if (keyhandled) return 1;
-      }
-
-    }
-  }
-#endif
-  return 0;
-}
-
-
+/*
+ * Populates the menu with the plugin names
+ */
 void imodPlugMenu(QPopupMenu *parent)
 {
-#ifndef NOPLUGS
-     
   PlugData *pd;
   int    i, mi;
   QString str;
@@ -325,20 +315,81 @@ void imodPlugMenu(QPopupMenu *parent)
 	parent->insertItem(str, i);
       }
     }
-
-
   }
+}
 
+
+/*
+ * Calls all the plugins of a given type that have an imodPlugExecute
+ * function with the given reason
+ */
+int imodPlugCall(ImodView *vw, int type, int reason)
+{
+  PlugData *pd;
+  int i, mi = ilistSize(plugList);
+  void (*fptr)(ImodView *, int, int);
+  int retval = 0;
+    
+  if (!mi) 
+    return 0;
+  pd = (PlugData *)ilistFirst(plugList);
+  for (i = 0; i < mi; i++){
+        
+    pd = (PlugData *)ilistItem(plugList, i);
+    if (!pd)
+      continue;
+         
+    if (!pd->handle)
+      fptr = pd->module->mExecuteType;
+#ifndef NOPLUGS
+    else
+      fptr = (void (*)(ImodView *, int, int))dlsym
+        (pd->handle, "imodPlugExecuteType");
 #endif
-#ifdef OPEN_PLUG_WORKING
-    item = XtVaCreateManagedWidget
-      ("separator", xmSeparatorWidgetClass,  plugMenu, NULL);
-  item = XtVaCreateManagedWidget
-    ("Open Plugin...", xmPushButtonWidgetClass, plugMenu,
-     XmNmnemonic, 'O',
-     NULL);
-  XtAddCallback(item, XmNactivateCallback, file_open_cb, App);
+    if (fptr){
+      (*fptr)(vw, type, reason);
+      retval++;
+    }
+  }
+  return retval;
+}
+
+/*
+ * Asks plugins to handle a key 
+ */
+int imodPlugHandleKey(ImodView *vw, QKeyEvent *event)
+{
+  PlugData *pd;
+  int keyhandled, i, mi = ilistSize(plugList);
+  SpecialKeys fptr;
+
+  if (!mi) return 0;
+  pd = (PlugData *)ilistFirst(plugList);
+  for(i = 0; i < mi; i++){
+          
+    pd = (PlugData *)ilistItem(plugList, i);
+    if (!pd) continue;
+          
+    if (pd->type & IMOD_PLUG_KEYS){
+      if (!pd->handle)
+        fptr = pd->module->mKeys;
+#ifndef NOPLUGS
+      else
+        fptr = (SpecialKeys)dlsym(pd->handle, "imodPlugKeys");
 #endif
+      if (fptr){
+	keyhandled = (*fptr)(vw, event);
+	if (keyhandled) return 1;
+      }
+
+    }
+  }
+  return 0;
+}
+
+void imodPlugClean(void)
+{
+  imodPlugCall(App->cvi, 0, IMOD_REASON_CLEANUP);
   return;
 }
 
