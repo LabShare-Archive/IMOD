aaaa8f2558694673006f7436e005e808a978911f
cvs2hg <devnull@localhost>
2003-10-06 22:35:19 +0000

    fixup commit for tag 'IMOD_3-0-21A'

--- flib/subrs/hvem/mypause.f
+++
@@ -1,9 +0,0 @@
-	subroutine mypause(message)
-	character*(*) message
-	character ichar
-	write(*,101)message
-101     format(' PAUSE: ',a,'   (return to continue)',$)
-	read(5,'(a)')ichar
-	return
-	end
-
