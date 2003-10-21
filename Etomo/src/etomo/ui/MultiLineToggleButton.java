6a7c3823994b22119dc1576c480df6d13b27ea13
SueHeld <sueh@colorado.edu>
2003-10-21 02:33:38 +0000

    Bug325 Pulled out common button functionality and placed it in ButtonHelper.

--- Etomo/src/etomo/ui/MultiLineToggleButton.java
+++ Etomo/src/etomo/ui/MultiLineToggleButton.java
@@ -6,8 +6,6 @@ import javax.swing.JToggleButton;
 import javax.swing.plaf.ColorUIResource;
 import java.lang.String;
 
-import etomo.ApplicationManager;
-
 /**
 * <p>Description: MultiLineToggleButton is a JToggleButton which can
 * contain a multi-line label.  It can be disabled correctly and will
@@ -30,6 +28,9 @@ import etomo.ApplicationManager;
 * @version $Revision$
 *
 * <p> $Log$
+* <p> Revision 1.6  2003/10/20 17:07:52  sueh
+* <p> bug317 removing platform dependent methods
+* <p>
 * <p> Revision 1.5  2003/10/20 17:06:11  sueh
 * <p> bug317
 * <p>
@@ -54,139 +55,135 @@ import etomo.ApplicationManager;
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
 final public class MultiLineToggleButton extends JToggleButton {
-  public static final String rcsid = "$$Id$$";
+  public static final String rcsid =
+    "$$Id$$";
 
-  public static final String ENABLED_UNSELECTED_TEXT_COLOR_PROPERTY = "ToggleButton.foreground";
-  public static final String ENABLED_SELECTED_TEXT_COLOR_PROPERTY = "ToggleButton.foreground";
-  public static final String DISABLED_UNSELECTED_TEXT_COLOR_PROPERTY = "ToggleButton.disabledText";
-  public static final String DISABLED_SELECTED_TEXT_COLOR_PROPERTY =  "ToggleButton.disabledSelectedText";
+  public static final String ENABLED_UNSELECTED_TEXT_COLOR_PROPERTY =
+    "ToggleButton.foreground";
+  public static final String ENABLED_SELECTED_TEXT_COLOR_PROPERTY =
+    "ToggleButton.foreground";
+  public static final String DISABLED_UNSELECTED_TEXT_COLOR_PROPERTY =
+    "ToggleButton.disabledText";
+  public static final String DISABLED_SELECTED_TEXT_COLOR_PROPERTY =
+    "ToggleButton.disabledSelectedText";
 
-
-  
-  
   public MultiLineToggleButton() {
     this(null, null, false);
   }
-  
+
   public MultiLineToggleButton(Icon icon) {
     this(null, icon, false);
   }
-  
+
   public MultiLineToggleButton(Icon icon, boolean selected) {
     this(null, icon, selected);
   }
-  
+
   public MultiLineToggleButton(String text) {
     this(text, null, false);
   }
-  
+
   public MultiLineToggleButton(String text, boolean selected) {
     this(text, null, selected);
   }
-  
+
   public MultiLineToggleButton(Action a) {
     super(a);
     init();
   }
-  
+
   public MultiLineToggleButton(String text, Icon icon) {
     this(text, icon, false);
   }
-  
+
   public MultiLineToggleButton(String text, Icon icon, boolean selected) {
-    super(format(text), icon, selected);
+    super(ButtonHelper.format(text), icon, selected);
     init();
   }
-  
+
   public void setEnabled(boolean isEnabled) {
     super.setEnabled(isEnabled);
     if (super.isSelected()) {
-      super.setForeground(isEnabled ? enabledSelectedTextColor : disabledSelectedTextColor);
+      super.setForeground(
+        isEnabled ? enabledSelectedTextColor : disabledSelectedTextColor);
     }
     else {
-      super.setForeground(isEnabled ? enabledUnselectedTextColor : disabledUnselectedTextColor);
+      super.setForeground(
+        isEnabled ? enabledUnselectedTextColor : disabledUnselectedTextColor);
     }
-    System.out.println(paramString());
   }
-  
+
   public void setText(String text) {
-    super.setText(format(text));
+    super.setText(ButtonHelper.format(text));
   }
-  
 
   public String toString() {
     return getClass().getName() + "[" + paramString() + "]";
   }
-  
-  
-//protected methods
+
+  //protected methods
 
   protected String paramString() {
-    return ",enabledUnselectedTextColor=" + enabledUnselectedTextColor
-      + ", enabledSelectedTextColor =" + enabledSelectedTextColor
-      + ", disabledUnselectedTextColor =" + disabledUnselectedTextColor
-      + ", disabledSelectedTextColor =" + disabledSelectedTextColor   
+    return ",enabledUnselectedTextColor="
+      + enabledUnselectedTextColor
+      + ", enabledSelectedTextColor ="
+      + enabledSelectedTextColor
+      + ", disabledUnselectedTextColor ="
+      + disabledUnselectedTextColor
+      + ", disabledSelectedTextColor ="
+      + disabledSelectedTextColor
       + super.paramString();
   }
-  
+
   protected void init(String text, Icon icon) {
-    super.init(format(text), icon);
+    super.init(ButtonHelper.format(text), icon);
   }
 
-
-//private implementation
+  //private implementation
 
   private static ColorUIResource enabledUnselectedTextColor = null;
   private static ColorUIResource enabledSelectedTextColor = null;
   private static ColorUIResource disabledUnselectedTextColor = null;
   private static ColorUIResource disabledSelectedTextColor = null;
 
-
-  private static String format(String text) {
-    if (text.substring(0,6).equalsIgnoreCase("<html>")) {
-      return text;
-    }
-    text = "<html><b>".concat(text).concat("</b>");
-    return text;
-  }
-  
-
   //if changing this class to inheritable, make this method protected
   private void init() {
-    enabledUnselectedTextColor = getDefaultUIToggleButtonTextColor(ENABLED_UNSELECTED_TEXT_COLOR_PROPERTY);
-    enabledSelectedTextColor = getDefaultUIToggleButtonTextColor(ENABLED_SELECTED_TEXT_COLOR_PROPERTY);
-    disabledUnselectedTextColor = getDefaultUIToggleButtonTextColor(DISABLED_UNSELECTED_TEXT_COLOR_PROPERTY);
-    disabledSelectedTextColor = getDefaultUIToggleButtonTextColor(DISABLED_SELECTED_TEXT_COLOR_PROPERTY);
+    enabledUnselectedTextColor =
+      getDefaultUIColor(ENABLED_UNSELECTED_TEXT_COLOR_PROPERTY);
+    enabledSelectedTextColor =
+      getDefaultUIColor(ENABLED_SELECTED_TEXT_COLOR_PROPERTY);
+    disabledUnselectedTextColor =
+      getDefaultUIColor(DISABLED_UNSELECTED_TEXT_COLOR_PROPERTY);
+    disabledSelectedTextColor =
+      getDefaultUIColor(DISABLED_SELECTED_TEXT_COLOR_PROPERTY);
   }
-  
-  //if changing this class to inheritable, make this method protected
-  private static ColorUIResource getDefaultUIToggleButtonTextColor(String property) {
-    ColorUIResource color = new ColorUIResource(0, 0, 0);
-    color = (ColorUIResource) ApplicationManager.getDefaultUIResource(color, property);
+
+  private ColorUIResource getDefaultUIColor(String property) {
+    ColorUIResource color = ButtonHelper.getDefaultUIColor(property);
     if (color == null) {
-      return createDefaultColor(property);
+      color = createDefaultColor(property);
     }
     return color;
   }
 
   private static ColorUIResource createDefaultColor(String property) {
-    System.err.println("Warning: Cannot retrieve default UI property: " + property);
+    System.err.println(
+      "Warning: Cannot retrieve default UI property: " + property);
     if (property == ENABLED_UNSELECTED_TEXT_COLOR_PROPERTY) {
-      return new ColorUIResource(0,0,0);
+      return new ColorUIResource(0, 0, 0);
     }
     else if (property == ENABLED_SELECTED_TEXT_COLOR_PROPERTY) {
-      return new ColorUIResource(0,0,0);
+      return new ColorUIResource(0, 0, 0);
     }
     else if (property == DISABLED_UNSELECTED_TEXT_COLOR_PROPERTY) {
-      return new ColorUIResource(153,153,153);
+      return new ColorUIResource(153, 153, 153);
     }
     else if (property == DISABLED_SELECTED_TEXT_COLOR_PROPERTY) {
-      return new ColorUIResource(102,102,102);
+      return new ColorUIResource(102, 102, 102);
     }
     else {
       throw new IllegalArgumentException(property);
     }
   }
-    
-}
 
+}
