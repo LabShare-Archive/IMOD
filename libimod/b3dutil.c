8c888703e735183d83fc92b5d119e20329fe38b8
mastcu <mast@colorado.edu>
2003-10-24 03:01:34 +0000

    initial creation, consolidating routines from elsewhere

---
+++ libimod/b3dutil.c
@@ -0,0 +1,171 @@
+/*   b3dutil.c   - utility functions for getting version and copyright, 
+ *                      trimming program name
+ *
+ *   Copyright (C) 1995-2003 by Boulder Laboratory for 3-Dimensional Electron
+ *   Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *   Colorado.
+ */                                                                           
+
+/*  $Author$
+
+$Date$
+
+$Revision$
+
+$Log$
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "imodconfig.h"
+#include "b3dutil.h"
+
+/* DNM 2/26/03: These need to be printf instead of fprintf(stderr) to not crash 
+   imod under Windows */
+int imodVersion(char *pname)
+{
+  if (pname)
+    printf("%s Version %s %s %s\n",
+           pname, VERSION_NAME, __DATE__, __TIME__);
+  return(VERSION);
+}
+
+void imodCopyright(void)
+{
+  char *uofc =   "the Regents of the University of Colorado";
+  printf("Copyright (C) %s by %s\n%s & %s\n", COPYRIGHT_YEARS,
+         LAB_NAME1, LAB_NAME2, uofc);
+  return;
+}
+
+/* imodProgName returns a program name stripped of directories and .exe,
+ * given the full name (argv[0]) in fullname
+ * It returns a pointer internal to argv[0], unless the name ends in .exe,
+ * in which case it tries to return a duplicate copy.  Do not free it */
+char *imodProgName(char *fullname)
+{
+  char *tail, *tailback, *exe;
+  int indexe;
+  tail = strrchr(fullname, '/');
+  tailback = strrchr(fullname, '\\');
+  if (tailback > tail)
+    tail = tailback;
+
+  if (!tail)
+    return fullname;
+  tail++;
+  exe = strstr(tail, ".exe");
+  indexe = strlen(tail) - 4;
+  if (!exe || exe != tail + indexe)
+    return tail;
+  exe = strdup(tail);
+  if (!exe)
+    return tail;
+  exe[indexe] = 0x00;
+  return exe;
+}
+
+
+/* imodBackupFile renames an existing file named filename to filename~ and
+   deletes filename~ first if necessary */
+int imodBackupFile(char *filename)
+{
+  struct stat buf;
+  int len;
+  char *backname;
+
+  /* If file does not exist, return */
+  if (stat(filename, &buf))
+    return 0;
+
+  /* Get backup name */
+  backname = (char *)malloc(strlen(filename) + 3);
+  if (!backname)
+    return -2;
+  sprintf(backname, "%s~", filename);
+
+  /* If the backup file exists, try to remove it first (Windows/Intel) */
+  if (!stat(backname, &buf) && remove(backname))
+    return -1;
+
+  /* finally, rename file */
+  return rename(filename, backname);
+}
+
+/* A fortran wrapper for the function to make backup file */
+#ifdef F77FUNCAP
+#define imodbackupfile_ IMODBACKUPFILE
+#define imodgetenv_ IMODGETENV
+#endif
+
+int imodbackupfile_(char *filename, int strlen)
+{
+  int err;
+  char *cstr = f2cString(filename, strlen);
+  if (!cstr)
+    return -2;
+  err = imodBackupFile(cstr);
+  free(cstr);
+  return err;
+}
+
+/* Get an environment variable for a fortran routine */
+int imodgetenv_(char *var, char *value, int varSize, int valueSize)
+{
+  char *valPtr;
+  char *cstr = f2cString(var, varSize);
+  if (!cstr)
+    return -1;
+  valPtr = getenv(cstr);
+  free(cstr);
+  if (!valPtr)
+    return -1;
+  return c2fString(valPtr, value, valueSize);
+}
+
+
+/* Create a C string with a copy of a Fortran string */
+char *f2cString(char *str, int strSize)
+{
+  int i;
+  char *newStr;
+
+  /* find last non-blank character */
+  for (i = strSize - 1; i >= 0; i--)
+    if (str[i] != ' ')
+      break;
+
+  newStr = (char *)malloc(i + 2);
+  if (!newStr) {
+    return NULL;
+  }
+
+  /* copy string if non-null, then put terminator at end */
+  if (i >= 0)
+    strncpy(newStr, str, i + 1);
+  newStr[i + 1] = 0x00;
+  return newStr;
+}
+
+/* Return a C string into a Fortran string, return error if it won't fit */
+int c2fString(char *cStr, char *fStr, int fSize)
+{
+  int i;
+  while (*cStr && fSize > 0) {
+    *fStr++ = *cStr++;
+    fSize--;
+  }
+
+  /* Return error if there is still a non-null character */
+  if (*cStr)
+    return -1;
+
+  /* Blank-pad */
+  while (fSize > 0) {
+    *fStr++ = ' ';
+    fSize--;
+  }
+  return 0;
+}
