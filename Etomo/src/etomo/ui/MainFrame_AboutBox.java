ad879ceef3d91e75a30fd1fe9284cbc6f2c619bb
rickg <devnull@localhost>
2003-10-31 00:00:39 +0000

    Bug# 260

--- Etomo/src/etomo/ui/MainFrame_AboutBox.java
+++ Etomo/src/etomo/ui/MainFrame_AboutBox.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import etomo.process.SystemProgram;
+
 /**
  * <p>Description: </p>
  *
@@ -17,6 +19,9 @@ import javax.swing.*;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 2.4  2003/09/09 17:15:45  rickg
+ * <p> Upped version number to 0.95
+ * <p>
  * <p> Revision 2.3  2003/08/20 21:59:30  rickg
  * <p> Changed version number to 0.9
  * <p>
@@ -40,72 +45,62 @@ import javax.swing.*;
  * <p> Initial CVS entry, basic functionality not including combining
  * <p> </p>
  */
-public class MainFrame_AboutBox extends JDialog implements ActionListener {
+public class MainFrame_AboutBox extends JDialog {
   public static final String rcsid =
     "$Id$";
 
-  JPanel panel1 = new JPanel();
-  JPanel panel2 = new JPanel();
-  JPanel insetsPanel1 = new JPanel();
-  JPanel insetsPanel2 = new JPanel();
-  JPanel insetsPanel3 = new JPanel();
-  JButton button1 = new JButton();
-  JLabel imageLabel = new JLabel();
-  JLabel label1 = new JLabel();
-  JLabel label2 = new JLabel();
-  JLabel label3 = new JLabel();
-  JLabel label4 = new JLabel();
-  BorderLayout borderLayout1 = new BorderLayout();
-  BorderLayout borderLayout2 = new BorderLayout();
-  FlowLayout flowLayout1 = new FlowLayout();
-  GridLayout gridLayout1 = new GridLayout();
-  String product = "eTomo: The IMOD Tomography GUI";
-  String version = "Version: 0.95";
-  String copyright = "Copyright (c) 2002,2003 University of Colorado";
-  String comments = "";
+  String vers3dmod = "unknown";
+  String cpyrght3dmodLine1 = "";
+  String cpyrght3dmodLine2 = "";
+  JPanel pnlAbout = new JPanel();
+
+  JButton btnOK = new JButton("OK");
+
   public MainFrame_AboutBox(Frame parent) {
     super(parent);
-    enableEvents(AWTEvent.WINDOW_EVENT_MASK);
-    try {
-      jbInit();
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
+    get3dmodVersion();
+    JPanel pnlRoot = (JPanel) getContentPane();
+    JPanel pnlText = new JPanel();
+		JPanel pnlButton = new JPanel();
+    pnlRoot.setLayout(new BorderLayout());
+    setTitle("About");
+    setResizable(false);
+
+		pnlText.setLayout(new BoxLayout(pnlText, BoxLayout.Y_AXIS));
+    pnlAbout.setLayout(new BoxLayout(pnlAbout, BoxLayout.Y_AXIS));
+		
+    JLabel lblEtomo = new JLabel("eTomo: The IMOD Tomography GUI");
+    JLabel lblVersion = new JLabel("Version 1.0.0");
+		JLabel lblAuthors = new JLabel("Written by: Rick Gaudette & Sue Held");
+    JLabel lbl3dmodVersion = new JLabel("IMOD Version: " + vers3dmod);
+    JLabel lblCopyright1 = new JLabel(cpyrght3dmodLine1);
+    JLabel lblCopyright2 = new JLabel(cpyrght3dmodLine2);
+    
+    btnOK.addActionListener(new AboutActionListener(this));
+    
+    pnlAbout.add(Box.createRigidArea(FixedDim.x0_y10));
+    pnlText.add(lblEtomo);
+    pnlText.add(Box.createRigidArea(FixedDim.x0_y5));
+    pnlText.add(lblVersion);
+		pnlText.add(Box.createRigidArea(FixedDim.x0_y10));
+		pnlText.add(lblCopyright1);
+		pnlText.add(Box.createRigidArea(FixedDim.x0_y5));
+		pnlText.add(lblCopyright2);
+		pnlText.add(Box.createRigidArea(FixedDim.x0_y10));
+		pnlText.add(lblAuthors);
+    pnlText.add(Box.createRigidArea(FixedDim.x0_y20));
+    pnlText.add(lbl3dmodVersion);
+    pnlText.add(Box.createRigidArea(FixedDim.x0_y20));
+    pnlButton.add(btnOK);
+
+		pnlAbout.add(pnlText);
+		pnlAbout.add(pnlButton);
+    pnlRoot.add(pnlAbout, BorderLayout.CENTER);
+    pnlRoot.add(Box.createRigidArea(FixedDim.x20_y0), BorderLayout.WEST);
+    pnlRoot.add(Box.createRigidArea(FixedDim.x20_y0), BorderLayout.EAST);
     pack();
   }
-  /**Component initialization*/
-  private void jbInit() throws Exception {
-    //imageLabel.setIcon(new ImageIcon(MainFrame_AboutBox.class.getResource("[Your Image]")));
-    this.setTitle("About");
-    setResizable(false);
-    panel1.setLayout(borderLayout1);
-    panel2.setLayout(borderLayout2);
-    insetsPanel1.setLayout(flowLayout1);
-    insetsPanel2.setLayout(flowLayout1);
-    insetsPanel2.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-    gridLayout1.setRows(4);
-    gridLayout1.setColumns(1);
-    label1.setText(product);
-    label2.setText(version);
-    label3.setText(copyright);
-    label4.setText(comments);
-    insetsPanel3.setLayout(gridLayout1);
-    insetsPanel3.setBorder(BorderFactory.createEmptyBorder(10, 60, 10, 10));
-    button1.setText("Ok");
-    button1.addActionListener(this);
-    insetsPanel2.add(imageLabel, null);
-    panel2.add(insetsPanel2, BorderLayout.WEST);
-    this.getContentPane().add(panel1, null);
-    insetsPanel3.add(label1, null);
-    insetsPanel3.add(label2, null);
-    insetsPanel3.add(label3, null);
-    insetsPanel3.add(label4, null);
-    panel2.add(insetsPanel3, BorderLayout.CENTER);
-    insetsPanel1.add(button1, null);
-    panel1.add(insetsPanel1, BorderLayout.SOUTH);
-    panel1.add(panel2, BorderLayout.NORTH);
-  }
+
   /**Overridden so we can exit when window is closed*/
   protected void processWindowEvent(WindowEvent e) {
     if (e.getID() == WindowEvent.WINDOW_CLOSING) {
@@ -118,9 +113,47 @@ public class MainFrame_AboutBox extends JDialog implements ActionListener {
     dispose();
   }
   /**Close the dialog on a button event*/
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == button1) {
+  public void buttonAction(ActionEvent e) {
+    if (e.getSource() == btnOK) {
       cancel();
     }
   }
+
+  /**
+   * Run 3dmod -h to version and copyright information.
+   */
+  private void get3dmodVersion() {
+    SystemProgram threeDmod_h = new SystemProgram("3dmod -h");
+
+    threeDmod_h.run();
+
+    String[] stdout = threeDmod_h.getStdOutput();
+    if (stdout.length >= 1) {
+      int idxVersion = stdout[0].indexOf("Version");
+      if (idxVersion > 0) {
+        String noPath = stdout[0].substring(idxVersion);
+        String[] tokens = noPath.split(" ");
+        if (tokens.length > 1) {
+          vers3dmod = tokens[1];
+        }
+      }
+    }
+    if (stdout.length > 3) {
+      cpyrght3dmodLine1 = stdout[1];
+      cpyrght3dmodLine2 = stdout[2];
+    }
+  }
+  
+	class AboutActionListener implements ActionListener {
+
+		MainFrame_AboutBox adaptee;
+
+		AboutActionListener(MainFrame_AboutBox adaptee) {
+			this.adaptee = adaptee;
+		}
+
+		public void actionPerformed(ActionEvent event) {
+			adaptee.buttonAction(event);
+		}
+	}
 }
