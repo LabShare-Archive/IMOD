34138c47e86f4c43612d14c161891c6838f1f7b7
mastcu <mast@colorado.edu>
2003-12-12 20:53:22 +0000

    Preliminary addition to repository

---
+++ flib/distort/matinvd.f
@@ -0,0 +1,89 @@
+c	subroutine matinv
+c	from Cooley and Lohnes - Multivariate Procedures for the
+c	Behavioral Sciences, Wiley, 1962 (!).
+c	inverts matrix a of order n, computes determinant of a
+c	replaces b with a solution vector if called for by m>0
+c	  12/25/90: converted to fortran 77, indented, put in message
+c	  for singularity, verified basically same program as GAUSSJ from
+c	  Press et al 1986 for Gauss-Jordan elimination
+c
+	subroutine matinvd(a,msiz,n,b,m,determ)
+c	parameter (msiz = 1000)
+	parameter (linsiz = 10000)
+	implicit double precision (a-h,o-z)
+	dimension a(msiz,msiz), b(msiz), pivot(linsiz), ipivot(linsiz),
+     1 index(linsiz,2)
+	equivalence (irow,jrow), (icolum,jcolum), (amax, t, swap)
+	determ=1.0
+	do 20 j=1,n
+	  ipivot(j)=0
+20	continue 
+	do 550 i=1,n
+	  amax=0.
+	  do 105 j=1,n
+	    if(ipivot(j).ne.1)then
+	      do 100 k=1,n
+		if(ipivot(k).eq.0)then
+		  if(abs(amax).lt.abs(a(j,k)))then
+		    irow=j
+		    icolum=k
+		    amax=a(j,k)
+		  endif
+		elseif(ipivot(k).gt.1)then
+		  write(*,*) 'Singular matrix'
+		  return
+		endif
+100	      continue
+	    endif
+105	  continue
+	  ipivot(icolum)=ipivot(icolum)+1
+	  if(irow.ne.icolum)then
+	    determ=-determ
+	    do 200 l=1,n
+	      swap=a(irow,l)
+	      a(irow,l)=a(icolum,l)
+	      a(icolum,l)=swap
+200	    continue 
+	    if(m.ne.0)then
+	      swap=b(irow)
+	      b(irow)=b(icolum)
+	      b(icolum)=swap
+	    endif
+	  endif
+	  index(i,1)=irow
+	  index(i,2)=icolum
+	  pivotmp=a(icolum,icolum)
+	  if(abs(pivotmp).lt.1.e-30) write(*,*) 'small pivot',pivotmp
+	  pivot(i)=pivotmp
+	  determ=determ*pivotmp
+	  a(icolum,icolum)=1.
+c	    worried about that step!
+	  do 350 l=1,n
+	    a(icolum,l)=a(icolum,l)/pivotmp
+350	  continue
+	  if(m.gt.0)b(icolum)=b(icolum)/pivotmp
+	  do 545 l1=1,n
+	    if(l1.ne.icolum)then
+	      t=a(l1,icolum)
+	      a(l1,icolum)=0.
+	      do 450 l=1,n
+		a(l1,l)=a(l1,l)-a(icolum,l)*t
+450	      continue
+	      if(m.gt.0)b(l1)=b(l1)-b(icolum)*t
+	    endif
+545	  continue
+550	continue
+	do 710 i=1,n
+	  l=n+1-i
+	  if(index(l,1).ne.index(l,2))then
+	    jrow=index(l,1)
+	    jcolum=index(l,2)
+	    do 705 k=1,n
+	      swap=a(k,jrow)
+	      a(k,jrow)=a(k,jcolum)
+	      a(k,jcolum)=swap
+705	    continue
+	  endif
+710	continue
+	return
+	end
---
+++ flib/distort/multrd.f
@@ -0,0 +1,120 @@
+c	subroutine multr
+c	from Cooley and Lohnes - Multivariate Procedures for the
+c	Behavioral Sciences, Wiley, 1962 (!).
+c	computes multiple regression coefficients for an input data matrix
+c	with the dependent variable in the last column
+	subroutine multrd(x,msiz,mp,ng,sx,ss,xm,sd,b,b1,rsq,f)
+c	x = data matrix, msiz columns by any # of rows
+c	mp = # of columns (parameters),  ng = # of rows (subjects)
+c	sx, xm, sd = sum, mean and sd of each column
+c	ss, ssd = raw and deviation sums of squares and cross products
+c	d, r = dispersion and correlation matrices
+c	b, b1 = beta and b weights,  c = constant term
+c	rsq is r squared, f is anova f with mp-1 and ng-mp degrees of freedom
+c	pass mpin as negative if there are weights in column mp+1
+	parameter (linsiz = 10000)
+	real*4 x(msiz,*)
+	real*8 sx(msiz), xm(msiz), sd(msiz), ss(msiz,msiz),
+     &	    r12(linsiz), b(msiz), b1(msiz), determ
+	call correld(x,msiz,mp,ng,sx,ss,r12,xm,sd)
+	m=mp-1
+	do i=1,m
+	  b(i)=ss(i,mp) / (sd(i) * sd(mp))
+	  do j=1,m
+	    ss(i,j)=ss(i,j) / (sd(i) * sd(j))
+	  enddo
+	enddo
+	call matinvd(ss, msiz, m, b, 1, determ)
+	rsq=0.
+	do i=1,m
+	  rsq=rsq+b(i)*r12(i)
+	  b1(i)=b(i)*sd(mp)/sd(i)
+	enddo
+	if(rsq.lt.1.)then
+	  f=rsq*(ng-mp)/(m*(1.-rsq))
+	else
+c	  write(*,*) 'rsquared is 1.0'
+	endif
+	return
+	end
+
+
+c	subroutine correl
+c	from Cooley and Lohnes - Multivariate Procedures for the
+c	Behavioral Sciences, Wiley, 1962 (!).
+c	computes basic matrices - means, sd's, variances, covariances,
+c	correlations - for an input data matrix
+	subroutine correld(x,msiz,m,ng,sx,ss,r12,xm,sd)
+c	x = data matrix, msiz columns by any # of rows
+c	m = # of columns (parameters),  ng = # of rows (subjects)
+c	or ng = 10000*(starting row #) + (ending row #)
+c	sx, xm, sd = sum, mean and sd of each column
+c	ss, ssd = raw and deviation sums of squares and cross products
+c	d, r = dispersion and correlation matrices, compute if ifdisp>0
+c	IF ifdisp <0, then compute ssd with weights in column m+1
+c	  
+c	  4/30/92: rewrote to calculate means first then form SSD from the
+c	  deviations from means; this is slower, but the old formula was
+c	  too inaccurate when fitting high-order polynomials.
+c	  Also indented loops and consolidated some loops
+c
+	real*4 x(msiz,*)
+	real*8 sx(msiz), xm(msiz), sd(msiz), ss(msiz,msiz), r12(*)
+	ns=1
+	eng=ng+1-ns
+c	write(*,*) 'doing correl on cells',ns,' to',ng
+	do 232 i=1,m
+	  sx(i)=0.
+	  do 230 j=1,m
+	    ss(i,j)=0.
+230	  continue
+232	continue
+	do 242 i=1,m
+	  do 240 k=ns,ng
+	    sx(i)=sx(i)+x(i,k)
+240	  continue
+	  xm(i)=sx(i)/eng
+242	continue
+	do 264 k=ns,ng
+c	    write(*,'(8f9.4)')(x(i,k),i=1,m)
+	  do 262 i=1,m
+	    do 260 j=1,m
+	      ss(i,j)=ss(i,j)+(x(i,k)-xm(i))*(x(j,k)-xm(j))
+260	    continue
+262	  continue
+264	continue
+	do i = 1,m
+	  sd(i)=sqrt(ss(i,i)/(eng-1.))
+	enddo
+	do i = 1,m
+	  r12(i) = 0.
+	  den=sd(i)*sd(m)
+	  if(den.gt.1.e-30)r12(i)=(ss(i,m)/(eng-1.))/(sd(i)*sd(m))
+	enddo
+	do 288 i=1,m
+	  do 286 j=1,m
+	    ss(i,j)=ss(i,j)+sx(i)*sx(j)/eng
+286	  continue
+288	continue
+c	call mprint(ss,m,'ss ')
+c	call mprint(ssd,m,'ssd')
+c	write(*,*)'xm '
+c	write(*,105)(xm(i),i=1,m)
+c105	format(5f15.7)
+c	write(*,*)'sd '
+c	write(*,105)(sd(i),i=1,m)
+c	call mprint(d,m,'d  ')
+c	call mprint(r,m,'r  ')
+700	return
+	end
+
+c	subroutine mprint(x,m,string)
+c       include 'statsize.inc'
+c	dimension x(msiz,msiz)
+c	character*3 string
+c	write(*,*)string
+c	do 10 i=1,m
+c10	write(*,20)(x(i,j),j=1,m)
+c20	format(5g15.7)
+c	return
+c	end
