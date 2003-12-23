f7b90b8a74ae915f7af106f00f32406804f4b41d
SueHeld <sueh@colorado.edu>
2003-12-23 21:34:17 +0000

    bug# 372 Reformatting.  Fixing test function.

--- Etomo/src/etomo/ui/PrimativeTokenizer.java
+++ Etomo/src/etomo/ui/PrimativeTokenizer.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.StreamTokenizer;
 import java.io.FileNotFoundException;
 
-
 /**
 * <p>Description:</p>
 *
@@ -16,20 +15,25 @@ import java.io.FileNotFoundException;
 *
 * <p>Organization:
 * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
-* Univeristy of Colorado</p>
+* University of Colorado</p>
 *
 * @author $$Author$$
 *
 * @version $$Revision$$
 *
-* <p> $$Log$$ </p>
+* <p> $$Log$
+* <p> $Revision 1.1  2003/12/22 23:50:52  sueh
+* <p> $bug# 372 creates primative tokens
+* <p> $$ </p>
 */
 public class PrimativeTokenizer {
-  public static final String rcsid = "$$Id$$";
-  
+  public static final String rcsid =
+    "$$Id$$";
+
   private File file;
   private StreamTokenizer tokenizer = null;
-  public static final String SYMBOLS = new String("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+  public static final String SYMBOLS =
+    new String("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
   Token token = new Token();
   boolean nextTokenFound = false;
   Token nextToken = new Token();
@@ -37,13 +41,17 @@ public class PrimativeTokenizer {
   PrimativeTokenizer(File file) {
     this.file = file;
   }
-    
+
   public final Token getToken() {
     return token;
   }
-    
+
   public void initialize() throws FileNotFoundException, IOException {
-    tokenizer = null;
+    initializeStreamTokenizer();
+    tokenizer.nextToken();
+  }
+
+  private void initializeStreamTokenizer() throws FileNotFoundException {
     FileReader reader = new FileReader(file);
     tokenizer = new StreamTokenizer(reader);
     tokenizer.resetSyntax();
@@ -51,7 +59,6 @@ public class PrimativeTokenizer {
     tokenizer.wordChars('A', 'Z');
     tokenizer.wordChars('0', '9');
     tokenizer.eolIsSignificant(true);
-    tokenizer.nextToken();
   }
 
   public Token next() throws IOException {
@@ -60,7 +67,7 @@ public class PrimativeTokenizer {
     token.reset();
     boolean whitespaceFound = false;
     StringBuffer whitespaceBuffer = null;
-    
+
     if (nextTokenFound) {
       found = true;
       token.set(nextToken);
@@ -94,7 +101,7 @@ public class PrimativeTokenizer {
           whitespaceBuffer.append((char) tokenizer.ttype);
         }
       }
-      
+
       if (found) {
         if (whitespaceFound) {
           nextTokenFound = true;
@@ -104,42 +111,49 @@ public class PrimativeTokenizer {
           whitespaceFound = false;
         }
       }
-      if (!nextTokenFound) {     
+      if (!nextTokenFound) {
         tokenizer.nextToken();
       }
     }
     return token;
   }
-  
+
   public void testStreamTokenizer(boolean tokens) throws IOException {
-    initialize();
-    System.out.println("in Primative.testStreamTokenizer");
+    initializeStreamTokenizer();
     if (tokens) {
-      System.out.println("TT_EOL=" + StreamTokenizer.TT_EOL + ",TT_WORD=" + StreamTokenizer.TT_WORD + ",TT_NUMBER=" + StreamTokenizer.TT_NUMBER);
+      System.out.println(
+        "TT_EOL="
+          + StreamTokenizer.TT_EOL
+          + ",TT_WORD="
+          + StreamTokenizer.TT_WORD
+          + ",TT_NUMBER="
+          + StreamTokenizer.TT_NUMBER);
     }
-    while (tokenizer.ttype != StreamTokenizer.TT_EOF) {
+    do {
+      tokenizer.nextToken();
       if (tokens) {
-        System.out.println("ttype=" + tokenizer.ttype + ",sval=" + tokenizer.sval + ",nval=" + tokenizer.nval);
+        System.out.println(
+          "ttype="
+            + tokenizer.ttype
+            + ",sval="
+            + tokenizer.sval
+            + ",nval="
+            + tokenizer.nval);
       }
-      if (tokenizer.ttype == StreamTokenizer.TT_EOL) {
-        if (!tokens) {
-          System.out.println();
-        }
+      else if (tokenizer.ttype == StreamTokenizer.TT_EOL) {
+        System.out.println();
       }
-      else {
+      else if (tokenizer.ttype != StreamTokenizer.TT_EOF) {
         if (tokenizer.ttype == StreamTokenizer.TT_WORD) {
-          if (!tokens) {
-            System.out.print(tokenizer.sval);
-          }
+          System.out.print(tokenizer.sval);
         }
         else {
-          if (!tokens) {
-            System.out.print((char) tokenizer.ttype);
-          }
+          System.out.print((char) tokenizer.ttype);
         }
       }
-      tokenizer.nextToken();
+
     }
+    while (tokenizer.ttype != StreamTokenizer.TT_EOF);
   }
 
 }
