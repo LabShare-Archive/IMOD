e856c91dcc65f043e6051edc02902c641dab61c8
mastcu <mast@colorado.edu>
2003-12-27 19:45:21 +0000

    *** empty log message ***

--- flib/distort/finddistort.f
+++ flib/distort/finddistort.f
@@ -1004,43 +1004,6 @@ c	  Then IW contains the I values, the starting index for each row
 	return
 	end
 
-
-	subroutine interpolateGrid(x, y, dxGrid, dyGrid, ixgDim,
-     &	    nxGrid, nyGrid, ixGridStrt, xGridIntrv, iyGridStrt,
-     &	    yGridIntrv, dx, dy)
-	implicit none
-	integer*4 ixgDim, iygDim, nxGrid, nyGrid, ixGridStrt, iyGridStrt
-	real*4 dxGrid(ixgDim, *), dyGrid(ixgDim, *)
-	real*4 xGridIntrv, yGridIntrv, x, y, dx, dy
-	real*4 xgrid,ygrid,fx1,fx,fy1,fy,c00,c10,c01,c11
-	integer*4 ixg,iyg,ixg1,iyg1
-
-	xgrid = 1. + (x - ixGridStrt) / xGridIntrv
-	ixg=xgrid
-	ixg=max(1,min(nxGrid-1,ixg))
-	fx1=max(0.,min(1.,xgrid-ixg))		!NO EXTRAPOLATIONS ALLOWED
-	fx=1.-fx1
-	ixg1=ixg+1
-	ygrid = 1. + (y - iyGridStrt) / yGridIntrv
-	iyg=ygrid
-	iyg=max(1,min(nyGrid-1,iyg))
-	fy1=max(0.,min(1.,ygrid-iyg))
-	fy=1.-fy1
-	iyg1=iyg+1
-	c00=fx*fy
-	c10=fx1*fy
-	c01=fx*fy1
-	c11=fx1*fy1
-c	  
-c	  interpolate
-c
-	dx = c00*dxGrid(ixg,iyg) + c10*dxGrid(ixg1,iyg)
-     &	    + c01*dxGrid(ixg,iyg1) + c11*dxGrid(ixg1,iyg1)
-	dy = c00*dyGrid(ixg,iyg) + c10*dyGrid(ixg1,iyg)
-     &	    + c01*dyGrid(ixg,iyg1) + c11*dyGrid(ixg1,iyg1)
-	return
-	end
-
 	subroutine errorexit(message)
 	implicit none
 	character*(*) message
