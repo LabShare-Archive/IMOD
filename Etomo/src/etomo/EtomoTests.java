4066996dbcb991726009b8a4f46aa87284f0b77f
rickg <devnull@localhost>
2004-01-13 23:03:31 +0000

    Fixed header Added etomo.util test suite

--- Etomo/src/etomo/EtomoTests.java
+++ Etomo/src/etomo/EtomoTests.java
@@ -1,8 +1,17 @@
-/*
- * Created on Oct 28, 2003
+/**
+ * <p>Description: Test suite for etomo.util </p>
+ * 
+ * <p>Copyright: Copyright (c) 2002, 2003</p>
  *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ *<p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
  */
 package etomo;
 
@@ -14,13 +23,9 @@ import java.util.Enumeration;
 import etomo.comscript.ComScriptTests;
 import etomo.process.ProcessTests;
 import etomo.type.TypeTests;
+import etomo.util.UtilTests;
 
-/**
- * @author sueh
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
+
 public class EtomoTests {
   public static Test suite() {
     TestSuite suite = new TestSuite("Test for etomo");
@@ -31,33 +36,40 @@ public class EtomoTests {
     Enumeration tests = testSuite.tests();
     Object test;
     while (tests.hasMoreElements()) {
-      test = tests.nextElement();   
-      if (test instanceof Test) {
-        suite.addTest((Test) test);
-      }
-    }
-    
-    testSuite = (TestSuite) ComScriptTests.suite();
-    tests = testSuite.tests();
-    while (tests.hasMoreElements()) {
-      test = tests.nextElement();   
-      if (test instanceof Test) {
-        suite.addTest((Test) test);
-      }
-    }
-    
-    testSuite = (TestSuite) ProcessTests.suite();
-    tests = testSuite.tests();
-    while (tests.hasMoreElements()) {
-      test = tests.nextElement();   
+      test = tests.nextElement();
       if (test instanceof Test) {
         suite.addTest((Test) test);
       }
     }
 
-    
+    testSuite = (TestSuite) ComScriptTests.suite();
+    tests = testSuite.tests();
+    while (tests.hasMoreElements()) {
+      test = tests.nextElement();
+      if (test instanceof Test) {
+        suite.addTest((Test) test);
+      }
+    }
+
+    testSuite = (TestSuite) ProcessTests.suite();
+    tests = testSuite.tests();
+    while (tests.hasMoreElements()) {
+      test = tests.nextElement();
+      if (test instanceof Test) {
+        suite.addTest((Test) test);
+      }
+    }
+
+    testSuite = (TestSuite) UtilTests.suite();
+    tests = testSuite.tests();
+    while (tests.hasMoreElements()) {
+      test = tests.nextElement();
+      if (test instanceof Test) {
+        suite.addTest((Test) test);
+      }
+    }
+
     return suite;
   }
-  
-  
+
 }
