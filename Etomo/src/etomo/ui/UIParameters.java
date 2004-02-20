3b5cb833213d07da5c2a368af1f28a2c983f7a56
SueHeld <sueh@colorado.edu>
2004-02-20 23:52:10 +0000

    bug# 386 added FileField dimension and spinner dimension

--- Etomo/src/etomo/ui/UIParameters.java
+++ Etomo/src/etomo/ui/UIParameters.java
@@ -12,6 +12,9 @@
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 3.0  2003/11/07 23:19:01  rickg
+ * <p> Version 1.0.0
+ * <p>
  * <p> Revision 1.1  2003/10/16 21:49:05  rickg
  * <p> Initial revision
  * <p> </p>
@@ -26,6 +29,8 @@ public class UIParameters {
 	public static final String rcsid = "$Id$";
 	
 	static Dimension dimButton = new Dimension();
+  static Dimension dimSpinner = new Dimension();
+  static Dimension dimFileField = new Dimension();
 	
 	/**
 	 * Default constructor
@@ -42,6 +47,15 @@ public class UIParameters {
 	  //  Return a safe copy of the Dimension
 	  return new Dimension(dimButton);
 	}
+  
+  public static Dimension getSpinnerDimension() {
+    return new Dimension(dimSpinner);
+  }
+  
+  public static Dimension getFileFieldDimension() {
+    return new Dimension(dimFileField);
+  }
+
 	
 	/**
 	 * Recalculate the size of objects given the current UI state.
@@ -52,5 +66,7 @@ public class UIParameters {
 	  JCheckBox temp = new JCheckBox();
 		double height = temp.getPreferredSize().getHeight();
 	  dimButton.setSize(7 * height, 2 * height);
+    dimSpinner.setSize(5 * height, 2 * height);
+    dimFileField.setSize(20 * height, 2 * height);
 	}
 }
