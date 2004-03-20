42d62328068da03bb3316bc4e3d1afad231b7831
mastcu <mast@colorado.edu>
2004-03-20 15:36:09 +0000

    Added readMagGradients

--- flib/subrs/xfsubs/readdistortions.f
+++ flib/subrs/xfsubs/readdistortions.f
@@ -1,3 +1,16 @@
+c	  This file contains modules for reading distortion field files
+c	  or mag gradient files
+c
+c	  $Author$
+c
+c	  $Date$
+c
+c	  $Revision$
+c
+c	  $Log$
+c	  Revision 1.1  2003/12/27 19:23:55  mast
+c	  Addition to library
+c	
 c	  readDistortions will read a distortion field from a file
 c	  idfFile  specifies name of file with field
 c	  fieldDx and fieldDy are arrays in which to place the grid, 
@@ -9,17 +22,10 @@ c	  ixGridStrt, iyGridStrt are coordinates as which grid starts
 c	  xGridIntrv, yGridIntrv are the spacing between grid points in X & Y
 c	  nxGrid, nyGrid are the number of grid points in X & Y
 c
-c	  $Author$
-c
-c	  $Date$
-c
-c	  $Revision$
-c
-c	  $Log$
-c
 	subroutine readDistortions(idfFile, fieldDx, fieldDy, lmGrid, idfNx,
      &	    idfNy, idfBinning, pixelIdf, ixGridStrt, xGridIntrv, nxGrid,
      &	    iyGridStrt, yGridIntrv, nyGrid)
+	implicit none
 	character*(*) idfFile
 	integer*4  idfBinning, idfNx, idfNy, lmGrid
 	integer*4 ixGridStrt, iyGridStrt, nxGrid, nyGrid
@@ -46,6 +52,48 @@ c
 	  print *
 	  print *,'ERROR: readDistortions - version',idfVersion,
      &	      ' of idf file not recognized'
+	  call exit(1)
+	endif
+	close(14)
+	return
+	end
+
+
+c	  readMagGradients will read from a file with mag gradients and tilt
+c	  angles for each view, as prepared by extractmagrad
+c	  magFile  specifies name of file with gradient information
+c	  maxVals is the maximum number of values that are allowed 
+c	  pixelSize is the pixel size in Angstroms
+c	  axisRot is the rotation of the tilt axis from vertical
+c	  tilt is an array to receive tilt angles
+c	  dmagPerUm is an array to receive percent mag per micron
+c	  rotPerUm is an array to receive degrees of rotation per micron
+c	  numVals is returned with the number of values
+c
+	subroutine readMagGradients(magFile, maxVals, pixelSize,
+     &	    axisRot, tilt, dmagPerUm, rotPerUm, numVals)
+	implicit none
+	character*(*) magFile
+	integer*4 magVersion, numVals, maxVals, i
+	real*4 pixelSize, axisRot, tilt(*), dmagPerUm(*), rotPerUm(*)
+c	  
+	call dopen(14, magFile, 'ro', 'f')
+	read(14, *) magVersion
+	if (magVersion .eq. 1) then
+	  read(14, *)numVals, pixelSize, axisRot
+	  if (numVals .gt. maxVals) then
+	    print *
+	    print *,'ERROR: readMagGradients - too many values for arrays'
+	    call exit(1)
+	  endif
+	  do i = 1, numVals
+	    read(14, *) tilt(i), dmagPerUm(i), rotPerUm(i)
+	  enddo
+
+	else
+	  print *
+	  print *,'ERROR: readMagGradients - version',magVersion,
+     &	      ' of gradient file not recognized'
 	  call exit(1)
 	endif
 	close(14)
