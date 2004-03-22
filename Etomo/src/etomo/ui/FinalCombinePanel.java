fe59a702a4f1061d7877e0b6424b5a3dc6aad3fe
SueHeld <sueh@colorado.edu>
2004-03-22 23:20:56 +0000

    bug# 250 Use Patch Region Model checkbox should be set from Patchcorr, not Matchorwarp

--- Etomo/src/etomo/ui/FinalCombinePanel.java
+++ Etomo/src/etomo/ui/FinalCombinePanel.java
@@ -44,6 +44,9 @@ import etomo.comscript.CombineParams;
  * 
  * <p>
  * $Log$
+ * Revision 3.5  2004/03/09 01:57:23  sueh
+ * bug# 381 added Restart at Volcombine button
+ *
  * Revision 3.4  2004/03/06 03:48:05  sueh
  * bug# 380 added Use linear interpolation checkbox (advanced)
  *
@@ -458,8 +461,6 @@ public class FinalCombinePanel implements ContextMenu {
    * @param matchorwarpParam
    */
   public void setMatchorwarpParams(ConstMatchorwarpParam matchorwarpParam) {
-    cbUsePatchRegionModel.setSelected(
-      !matchorwarpParam.getModelFile().equals(""));
     ltfWarpLimit.setText(matchorwarpParam.getWarpLimit());
     ltfRefineLimit.setText(matchorwarpParam.getRefineLimit());
 
