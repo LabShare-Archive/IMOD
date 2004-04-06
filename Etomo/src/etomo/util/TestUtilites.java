bda5e06e12d362acb26eb069b1e05b6d5c7362c1
rickg <devnull@localhost>
2004-04-06 02:48:13 +0000

    Added makeDirectories method

--- Etomo/src/etomo/util/TestUtilites.java
+++ Etomo/src/etomo/util/TestUtilites.java
@@ -12,23 +12,68 @@
  * 
  * @version $Revision$
  * 
- * <p> $Log$ </p>
+ * <p> $Log$
+ * <p> Revision 1.1  2004/04/02 18:44:26  rickg
+ * <p> Initial revision
+ * <p> </p>
  */
 
 package etomo.util;
 
+import java.io.File;
+import java.io.IOException;
+
+import etomo.process.SystemProcessException;
 import etomo.process.SystemProgram;
 
 public class TestUtilites {
   public static final String rcsid = "$Id$";
 
   /**
-   * Check out the specified test vector into the specified directory
+   * Make all of the directories on on the specified path if necessary.  If the
+   * path begins with the systems separator then it is an absolute path, if not
+   * it is relative to the current directory specified by the System property
+   * "user.dir". 
+   * @param newDirectory
+   */
+  public static void makeDirectories(String newDirectory)
+      throws IOException {
+    //  Create the test directories
+    File directory;
+    if (newDirectory.startsWith(File.separator)) {
+      directory = new File(newDirectory);
+    }
+    else {
+      directory = new File(System.getProperty("user.dir"), newDirectory);
+    }
+    if (!directory.exists()) {
+      if (!directory.mkdirs()) {
+        throw new IOException("Creating directory: "
+            + directory.getAbsolutePath());
+      }
+    }
+  }
+
+  /**
+   * Check out the specified test vector into the specified directory. Note the
+   * cvs export cannot handle a full path as an argument to -d.  The directory
+   * must reside in the current directory
    * @param directory
    * @param vector
    */
-  public static void checkoutVector(String directory, String vector) {
+  public static void checkoutVector(String directory, String vector)
+      throws SystemProcessException, InvalidParameterException {
 
+    if(vector.matches(File.separator)){
+      throw new InvalidParameterException("vector can not contain path separators");
+    }
+    File fileVector = new File(System.getProperty("user.dir") + directory, vector);
+    
+    if (fileVector.exists()) {
+      if (!fileVector.delete()) {
+        throw new SystemProcessException("Cannot delete vector: " + vector);
+      }
+    }
     String[] cvsCommand = new String[7];
     cvsCommand[0] = "cvs";
     cvsCommand[1] = "export";
@@ -39,6 +84,10 @@ public class TestUtilites {
     cvsCommand[6] = "ImodTests/EtomoTests/vectors/" + vector;
     SystemProgram cvs = new SystemProgram(cvsCommand);
     cvs.run();
+
+    if (cvs.getExitValue() > 0) {
+      throw new SystemProcessException(cvs.getStdErrorString());
+    }
   }
 
 }
