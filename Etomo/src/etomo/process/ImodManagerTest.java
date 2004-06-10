8a091a8ac81468b2d060275f3dbb35def1305ea9
SueHeld <sueh@colorado.edu>
2004-06-10 17:25:28 +0000

    bug# 462 replace the Tester class with ImodState, remove functions that are not being used

--- Etomo/src/etomo/process/ImodManagerTest.java
+++ Etomo/src/etomo/process/ImodManagerTest.java
@@ -72,7 +72,7 @@ public class ImodManagerTest extends TestCase {
 
   final public void testImodManager()
     throws AxisTypeException, SystemProcessException {
-    Tester tester;
+    ImodState tester;
     //Test single axis
     setUpSingle();
     imodManager = new ImodManager(applicationManager);
@@ -222,23 +222,6 @@ public class ImodManagerTest extends TestCase {
     tester.equals(imodManager.get(preview, b));
   }
 
-  private void setupNewSample(Tester tester) {
-    //sample.modelMode();
-    tester.modelMode();
-  }
-
-  private void setupNewPatchVectorModel(Tester tester) {
-    //patchVectorModel.modelMode();
-    tester.modelMode();
-  }
-
-  private void setupNewFiducialModel(Tester tester) {
-    //fiducialModel.setUseModv(true);
-    //fiducialModel.setOutputWindowID(false);
-    tester.setUseModv(true);
-    tester.setOutputWindowID(false);
-  }
-
   private void setUpSingle() {
     datasetName = datasetNameSingle;
     metaData.setDatasetName(datasetName);
@@ -250,23 +233,23 @@ public class ImodManagerTest extends TestCase {
     metaData.setAxisType(AxisType.DUAL_AXIS);
   }
 
-  private Tester newTester(String name) {
+  private ImodState newTester(String name) {
     return newTester(name, AxisType.SINGLE_AXIS, AxisID.ONLY);
   }
 
-  private Tester newTester(String name, AxisID axisID) {
+  private ImodState newTester(String name, AxisID axisID) {
     if (axisID == AxisID.ONLY) {
       return newTester(name, AxisType.SINGLE_AXIS, axisID);
     }
     return newTester(name, AxisType.DUAL_AXIS, axisID);
   }
 
-  private Tester newTester(String name, AxisType axisType) {
+  private ImodState newTester(String name, AxisType axisType) {
     return newTester(name, axisType, null);
   }
 
-  private Tester newTester(String name, AxisType axisType, AxisID axisID) {
-    Tester tester;
+  private ImodState newTester(String name, AxisType axisType, AxisID axisID) {
+    ImodState tester;
     if (axisType == AxisType.SINGLE_AXIS) {
       /* Tester should mimic original code
           if (axisType == AxisType.SINGLE_AXIS) {
@@ -284,45 +267,44 @@ public class ImodManagerTest extends TestCase {
           trimmedVolume = new ImodProcess(datasetName + ".rec");
       */
       if (name.equals(rawStack)) {
-        return new Tester(datasetName + ".st");
+        return new ImodState(datasetName + ".st");
       }
       if (name.equals(erasedStack)) {
-        return new Tester(datasetName + "_fixed.st");
+        return new ImodState(datasetName + "_fixed.st");
       }
       if (name.equals(coarseAligned)) {
-        return new Tester(datasetName + ".preali");
+        return new ImodState(datasetName + ".preali");
       }
       if (name.equals(fineAligned)) {
-        return new Tester(datasetName + ".ali");
+        return new ImodState(datasetName + ".ali");
       }
       if (name.equals(sample)) {
-        tester = new Tester("top.rec mid.rec bot.rec", "tomopitch.mod");
-        setupNewSample(tester);
+        tester = new ImodState("top.rec mid.rec bot.rec", "tomopitch.mod");
+        tester.setInitialMode(ImodState.MODEL_MODE);
         return tester;
       }
       if (name.equals(fullVolume)) {
-        tester = new Tester(datasetName + "_full.rec");
-        tester.setSwapYZ(true);
+        tester = new ImodState(datasetName + "_full.rec");
+        tester.setInitialSwapYZ(true);
         return tester;
       }
       if (name.equals(fiducialModel)) {
-        tester = new Tester();
-        setupNewFiducialModel(tester);
+        tester = new ImodState(ImodState.MODV);
         return tester;
       }
       if (name.equals(trimmedVolume)) {
-        return new Tester(datasetName + ".rec");
+        return new ImodState(datasetName + ".rec");
       }
       if (name.equals(trialTomogram)) {
-        tester = new Tester(datasetName);
-        tester.setSwapYZ(true);
+        tester = new ImodState(datasetName);
+        tester.setInitialSwapYZ(true);
         return tester;
       }
       if (name.equals(mtfFilter)) {
-        return new Tester(datasetName + "_filt.ali");
+        return new ImodState(datasetName + "_filt.ali");
       }
       if (name.equals(preview)) {
-        return new Tester(datasetName + ".st");
+        return new ImodState(datasetName + ".st");
       }
       return null;
     }
@@ -357,336 +339,110 @@ public class ImodManagerTest extends TestCase {
       */
       if (name == rawStack) {
         if (axisID == AxisID.FIRST) {
-          return new Tester(datasetName + "a.st");
+          return new ImodState(datasetName + "a.st");
         }
         else {
-          return new Tester(datasetName + "b.st");
+          return new ImodState(datasetName + "b.st");
         }
       }
       if (name.equals(erasedStack)) {
         if (axisID == AxisID.FIRST) {
-          return new Tester(datasetName + "a_fixed.st");
+          return new ImodState(datasetName + "a_fixed.st");
         }
         else {
-          return new Tester(datasetName + "b_fixed.st");
+          return new ImodState(datasetName + "b_fixed.st");
         }
       }
       if (name.equals(coarseAligned)) {
         if (axisID == AxisID.FIRST) {
-          return new Tester(datasetName + "a.preali");
+          return new ImodState(datasetName + "a.preali");
         }
         else {
-          return new Tester(datasetName + "b.preali");
+          return new ImodState(datasetName + "b.preali");
         }
       }
       if (name.equals(fineAligned)) {
         if (axisID == AxisID.FIRST) {
-          return new Tester(datasetName + "a.ali");
+          return new ImodState(datasetName + "a.ali");
         }
         else {
-          return new Tester(datasetName + "b.ali");
+          return new ImodState(datasetName + "b.ali");
         }
       }
       if (name.equals(sample)) {
         if (axisID == AxisID.FIRST) {
-          tester = new Tester("topa.rec mida.rec bota.rec", "tomopitcha.mod");
-          setupNewSample(tester);
+          tester = new ImodState("topa.rec mida.rec bota.rec", "tomopitcha.mod");
+          tester.setInitialMode(ImodState.MODEL_MODE);
           return tester;
         }
         else {
-          tester = new Tester("topb.rec midb.rec botb.rec", "tomopitchb.mod");
-          setupNewSample(tester);
+          tester = new ImodState("topb.rec midb.rec botb.rec", "tomopitchb.mod");
+          tester.setInitialMode(ImodState.MODEL_MODE);
           return tester;
         }
       }
       if (name.equals(fullVolume)) {
         if (axisID == AxisID.FIRST) {
-          tester = new Tester(datasetName + "a.rec");
-          tester.setSwapYZ(true);
+          tester = new ImodState(datasetName + "a.rec");
+          tester.setInitialSwapYZ(true);
           return tester;
         }
         else {
-          tester = new Tester(datasetName + "b.rec");
-          tester.setSwapYZ(true);
+          tester = new ImodState(datasetName + "b.rec");
+          tester.setInitialSwapYZ(true);
           return tester;
         }
       }
       if (name.equals(combinedTomogram)) {
-        tester = new Tester("sum.rec");
+        tester = new ImodState("sum.rec");
         tester.setSwapYZ(true);
         return tester;
       }
       if (name.equals(patchVectorModel)) {
-        tester = new Tester("patch_vector.mod");
-        tester.setModelView(true);
-        setupNewPatchVectorModel(tester);
+        tester = new ImodState("patch_vector.mod", ImodState.MODEL_VIEW);
+        tester.setInitialMode(ImodState.MODEL_MODE);
         return tester;
       }
       if (name.equals(matchCheck)) {
-        tester = new Tester("matchcheck.mat matchcheck.rec");
-        tester.setSwapYZ(true);
-        tester.setFillCache(true);
+        tester = new ImodState("matchcheck.mat matchcheck.rec");
+        tester.setInitialSwapYZ(true);
         return tester;
       }
       if (name.equals(fiducialModel)) {
         if (axisID == AxisID.FIRST) {
-          tester = new Tester();
-          setupNewFiducialModel(tester);
+          tester = new ImodState(ImodState.MODV);
           return tester;
         }
         else {
-          tester = new Tester();
-          setupNewFiducialModel(tester);
+          tester = new ImodState(ImodState.MODV);
           return tester;
         }
       }
       if (name.equals(trimmedVolume)) {
-        return new Tester(datasetName + ".rec");
+        return new ImodState(datasetName + ".rec");
       }
       if (name.equals(trialTomogram)) {
-        tester = new Tester(datasetName);
-        tester.setSwapYZ(true);
+        tester = new ImodState(datasetName);
+        tester.setInitialSwapYZ(true);
         return tester;
       }
       if (name.equals(mtfFilter)) {
         if (axisID == AxisID.FIRST) {
-          return new Tester(datasetName + "a_filt.ali");
+          return new ImodState(datasetName + "a_filt.ali");
         }
         else {
-          return new Tester(datasetName + "b_filt.ali");
+          return new ImodState(datasetName + "b_filt.ali");
         }
       }
       if (name == preview) {
         if (axisID == AxisID.FIRST) {
-          return new Tester(datasetName + "a.st");
+          return new ImodState(datasetName + "a.st");
         }
         else {
-          return new Tester(datasetName + "b.st");
+          return new ImodState(datasetName + "b.st");
         }
       }
       return null;
     }
   }
-
-  //testOpen and testModel functions mirror ImodManager's open and model functions
-
-  private void testOpen(Tester tester, String key, AxisID axisID)
-    throws AxisTypeException, SystemProcessException {
-    ImodManager imodManager = new ImodManager(applicationManager);
-    imodManager.setMetaData(metaData);
-    imodManager.open(key, axisID);
-    assertTrue(imodManager.isOpen(key, axisID));
-    tester.equals(imodManager.get(key, axisID));
-    imodManager.openBeadFixer(key, axisID);
-    imodManager.quit(key, axisID);
-  }
-
-  private void testOpen(Tester tester, String key)
-    throws AxisTypeException, SystemProcessException {
-    ImodManager imodManager = new ImodManager(applicationManager);
-    imodManager.setMetaData(metaData);
-    imodManager.open(key);
-    assertTrue(imodManager.isOpen(key));
-    tester.equals(imodManager.get(key));
-    imodManager.quit(key);
-  }
-
-  private void testOpen(Tester tester, String key, AxisID axisID, String model)
-    throws AxisTypeException, SystemProcessException {
-    ImodManager imodManager = new ImodManager(applicationManager);
-    imodManager.setMetaData(metaData);
-    imodManager.open(key, axisID, model);
-    assertTrue(imodManager.isOpen(key, axisID));
-    tester.equals(imodManager.get(key, axisID));
-    imodManager.openBeadFixer(key, axisID);
-    imodManager.quit(key, axisID);
-  }
-
-  private void testModel(
-    Tester tester,
-    String key,
-    AxisID axisID,
-    String modelName,
-    boolean modelMode,
-    boolean preserveContrast)
-    throws AxisTypeException, SystemProcessException {
-    ImodManager imodManager = new ImodManager(applicationManager);
-    imodManager.setMetaData(metaData);
-    imodManager.model(key, axisID, modelName, modelMode, preserveContrast);
-    assertTrue(imodManager.isOpen(key, axisID));
-    tester.equals(imodManager.get(key, axisID));
-    imodManager.openBeadFixer(key, axisID);
-    imodManager.quit(key, axisID);
-  }
-
-  private void testModel(
-    Tester tester,
-    String key,
-    AxisID axisID,
-    String modelName,
-    boolean modelMode)
-    throws AxisTypeException, SystemProcessException {
-    ImodManager imodManager = new ImodManager(applicationManager);
-    imodManager.setMetaData(metaData);
-    imodManager.model(key, axisID, modelName, modelMode);
-    assertTrue(imodManager.isOpen(key, axisID));
-    tester.equals(imodManager.get(key, axisID));
-    imodManager.openBeadFixer(key, axisID);
-    imodManager.quit(key, axisID);
-  }
-
-  private void testModel(
-    Tester tester,
-    String key,
-    AxisID axisID,
-    String modelName)
-    throws AxisTypeException, SystemProcessException {
-    ImodManager imodManager = new ImodManager(applicationManager);
-    imodManager.setMetaData(metaData);
-    imodManager.model(key, axisID, modelName);
-    assertTrue(imodManager.isOpen(key, axisID));
-    tester.equals(imodManager.get(key, axisID));
-    imodManager.openBeadFixer(key, axisID);
-    imodManager.quit(key, axisID);
-  }
-
-  /**
-      * Tester has an interface similar to ImodProcess.  It sets variables and checks
-      * them against ImodAssistant.  It is use to check the current functionalty
-      * against the functionality that existed before ImodAssistant was introduced.
-      * 
-      * @author sueh
-      */
-  private class Tester {
-
-    //Member variables mirror ImodAssistant
-
-    private String datasetName = "";
-    private boolean swapYZ = false;
-    private boolean modelView = false;
-    private boolean fillCache = false;
-    private boolean useModv = false;
-    private boolean preserveContrast = false;
-    private String modelName = "";
-    private String mode = ImodState.MOVIE;
-
-    public String toString() {
-      return getClass().getName() + "[" + paramString() + "]";
-    }
-
-    protected String paramString() {
-      return ",datasetName="
-        + datasetName
-        + ",modelName="
-        + modelName
-        + ",swapYZ"
-        + swapYZ
-        + ",modelView"
-        + modelView
-        + ",fillCache="
-        + fillCache
-        + ",useModv="
-        + useModv
-        + ",mode="
-        + mode
-        + ",preserveContrast="
-        + preserveContrast;
-    }
-
-    Tester() {
-    }
-    Tester(String datasetName) {
-      this.datasetName = new String(datasetName);
-    }
-    Tester(String datasetName, String modelName) {
-      this.datasetName = new String(datasetName);
-      setModelName(modelName);
-    }
-
-    public void equals(ImodState imodState) {
-      assertTrue(imodState.getDatasetName().equals(datasetName));
-      assertTrue(imodState.getModelName().equals(modelName));
-      assertTrue(imodState.getMode().equals(mode));
-      assertEquals(imodState.isSwapYZ(), swapYZ);
-      assertEquals(imodState.isModelView(), modelView);
-      assertEquals(imodState.isFillCache(), fillCache);
-      assertEquals(imodState.isUseModv(), useModv);
-      assertEquals(imodState.isPreserveContrast(), preserveContrast);
-    }
-
-    private void print(String key, ImodState imodState) {
-      if (key == "datasetName") {
-        System.out.println("imodState:");
-        System.out.println(key + "=" + imodState.getDatasetName());
-        System.out.println("this: " + key + "=" + datasetName);
-      }
-      if (key == "modelName") {
-        System.out.println("imodState:");
-        System.out.println(key + "=" + imodState.getModelName());
-        System.out.println("this: " + key + "=" + modelName);
-      }
-      else if (key == "mode") {
-        System.out.println("imodState:");
-        System.out.println(key + "=" + imodState.getMode());
-        System.out.println("this: " + key + "=" + mode);
-      }
-      else if (key == "useModv") {
-        System.out.println(
-          key + ": imodState=" + imodState.isUseModv() + ", this=" + useModv);
-      }
-    }
-
-    //Interface mirrors ImodProcess
-
-    public void setSwapYZ(boolean swapYZ) {
-      this.swapYZ = swapYZ;
-    }
-
-    public void setModelView(boolean modelView) {
-      this.modelView = modelView;
-    }
-
-    public void setFillCache(boolean fillCache) {
-      this.fillCache = fillCache;
-    }
-
-    public void setUseModv(boolean useModv) {
-      this.useModv = useModv;
-    }
-
-    public void setOutputWindowID(boolean outputWindowID) {
-    }
-
-    public void openModelPreserveContrast(String modelName) {
-      setModelName(modelName);
-      preserveContrast = true;
-    }
-
-    public void openModel(String modelName) {
-      setModelName(modelName);
-    }
-
-    public void modelMode() {
-      setMode(true);
-    }
-
-    public void movieMode() {
-      setMode(false);
-    }
-
-    public void setMode(boolean modelMode) {
-      if (modelMode) {
-        mode = ImodState.MODEL;
-      }
-      else {
-        mode = ImodState.MOVIE;
-      }
-    }
-
-    public void setModelName(String modelName) {
-      this.modelName = new String(modelName);
-    }
-
-  }
-
 }
