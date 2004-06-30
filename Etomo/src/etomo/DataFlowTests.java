054134a28d4f4295736541e0b06e5fff8d716fd6
rickg <devnull@localhost>
2004-06-30 17:36:46 +0000

    Added fiducialless capability and partial single axis handling

--- Etomo/src/etomo/DataFlowTests.java
+++ Etomo/src/etomo/DataFlowTests.java
@@ -20,6 +20,12 @@
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 3.3.2.1  2004/06/30 17:32:39  rickg
+ * <p> Added fiducialless capability and partial single axis handling
+ * <p>
+ * <p> Revision 3.3  2004/04/26 23:41:37  rickg
+ * <p> Use copy from Utilities class
+ * <p>
  * <p> Revision 3.2  2004/04/26 22:45:17  rickg
  * <p> Copy a seed in from data source directory at beginning
  * <p>
@@ -44,74 +50,109 @@ import java.io.IOException;
 
 import etomo.process.SystemProcessException;
 import etomo.type.AxisID;
+import etomo.type.AxisType;
+import etomo.type.MetaData;
 import etomo.ui.MainFrame;
 import etomo.util.Utilities;
 
 public class DataFlowTests {
-  public static final String rcsid = "$Id$";
 
   static ApplicationManager applicationManager;
   static MainFrame mainFrame;
   static String datasetName;
+  static boolean fiducialSpecified = false;
+  static boolean fiducialless = false;
 
   public static void main(String[] args) {
 
-    if (args.length < 1) {
-      System.err.println("usage: dataFlowTests.jar datasetName");
-      System.exit(1);
-    }
-    datasetName = args[0];
+    // Parse the command line
+    parseCommandLine(args);
 
     System.out.println("Current working directory: "
-        + System.getProperty("user.dir"));
-
+      + System.getProperty("user.dir"));
     System.out.println("Dataset name: " + datasetName);
+
+    // Copy the EDF from the data source directory, also need a dataset for now
+    // - since we don't have the EDF yet we don't know if its single or dual
+    // axis.  When we relax the MetaData.isValid restriction on the data not
+    // existing in the directory the data set copy can be moved into the next
+    // copy block below.
     try {
-      copyFromDataSource(datasetName + "a.st");
-      copyFromDataSource(datasetName + "b.st");
-      copyFromDataSource(datasetName + "a.rawtlt");
-      copyFromDataSource(datasetName + "b.rawtlt");
-      copyFromDataSource(datasetName + "a.seed");
       copyFromDataSource(datasetName + ".edf");
-      copycoms("a");
-      copycoms("b");
+      copyFromDataSource(datasetName + "a.st");
     }
-    catch (SystemProcessException e) {
-      e.printStackTrace();
-      System.exit(1);
+    catch (SystemProcessException except) {
+      except.printStackTrace();
     }
 
     //  Simple test: create a new ApplicationManager and cause it to quit
     String[] argsIn = new String[2];
     argsIn[0] = "--debug";
     argsIn[1] = System.getProperty("user.dir") + File.separator + datasetName
-        + ".edf";
+      + ".edf";
     applicationManager = new ApplicationManager(argsIn);
     mainFrame = applicationManager.getMainFrame();
+    // A hack around the const object returned we really know is not const
+    MetaData metaData = (MetaData) applicationManager.getMetaData();
+
+    try {
+      if (metaData.getAxisType() == AxisType.DUAL_AXIS) {
+        copyFromDataSource(datasetName + "b.st");
+        copyFromDataSource(datasetName + "a.rawtlt");
+        copyFromDataSource(datasetName + "b.rawtlt");
+        copycoms("a");
+        copycoms("b");
+      }
+      else {
+        copyFromDataSource(datasetName + ".st");
+        copyFromDataSource(datasetName + ".rawtlt");
+        copycoms("");
+      }
+    }
+    catch (SystemProcessException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    // If the fiducialless state has been specfied override the EDF file 
+    if (fiducialSpecified) {
+      metaData.setFiducialessAlignment(fiducialless);
+    }
 
     preProcessing(AxisID.FIRST);
-    preProcessing(AxisID.SECOND);
+    if (metaData.getAxisType() == AxisType.DUAL_AXIS) {
+      preProcessing(AxisID.SECOND);
+    }
 
     coarseAlignment(AxisID.FIRST);
-    coarseAlignment(AxisID.SECOND);
+    if (metaData.getAxisType() == AxisType.DUAL_AXIS) {
+      coarseAlignment(AxisID.SECOND);
+    }
 
-    fiducialModelGen(AxisID.FIRST);
-    fineAlignment(AxisID.FIRST);
-    transferfid(AxisID.SECOND);
-    fiducialModelGen(AxisID.SECOND);
-    fineAlignment(AxisID.SECOND);
+    if (!applicationManager.getMetaData().isFiducialessAlignment()) {
+      fiducialModelGen(AxisID.FIRST);
+      fineAlignment(AxisID.FIRST);
+      if (metaData.getAxisType() == AxisType.DUAL_AXIS) {
+        transferfid(AxisID.SECOND);
+        fiducialModelGen(AxisID.SECOND);
+        fineAlignment(AxisID.SECOND);
+      }
+    }
 
     tomogramPositioning(AxisID.FIRST);
-    tomogramPositioning(AxisID.SECOND);
+    if (metaData.getAxisType() == AxisType.DUAL_AXIS) {
+      tomogramPositioning(AxisID.SECOND);
+    }
 
     tomogramGeneration(AxisID.FIRST);
-    tomogramGeneration(AxisID.SECOND);
-
-    tomogramCombination();
+    if (metaData.getAxisType() == AxisType.DUAL_AXIS) {
+      tomogramGeneration(AxisID.SECOND);
+      tomogramCombination();
+    }
   }
 
   private static void copycoms(String axisExtension)
-      throws SystemProcessException {
+    throws SystemProcessException {
     copyFromDataSource("align" + axisExtension + ".com");
     copyFromDataSource("eraser" + axisExtension + ".com");
     copyFromDataSource("findsec" + axisExtension + ".com");
@@ -136,10 +177,10 @@ public class DataFlowTests {
   }
 
   private static void copyFromDataSource(String filename)
-      throws SystemProcessException {
+    throws SystemProcessException {
     System.out.println("Copying from DataSource directory: " + filename);
     File source = new File(System.getProperty("user.dir") + File.separator
-        + "DataSource", filename);
+      + "DataSource", filename);
     File dest = new File(System.getProperty("user.dir"), filename);
     try {
       Utilities.copyFile(source, dest);
@@ -147,7 +188,7 @@ public class DataFlowTests {
     catch (IOException e) {
       e.printStackTrace();
       System.err.println("Unable to copy " + source.getAbsolutePath() + " to "
-          + dest.getAbsolutePath());
+        + dest.getAbsolutePath());
       System.exit(-1);
     }
 
@@ -169,8 +210,10 @@ public class DataFlowTests {
     mainFrame.pack();
     applicationManager.crossCorrelate(axisID);
     waitForThread(axisID);
-    applicationManager.coarseAlign(axisID);
-    waitForThread(axisID);
+    if(!applicationManager.getMetaData().isFiducialessAlignment()) {
+      applicationManager.coarseAlign(axisID);
+      waitForThread(axisID);
+    }
     applicationManager.doneCoarseAlignDialog(axisID);
   }
 
@@ -183,6 +226,14 @@ public class DataFlowTests {
   }
 
   private static void fiducialModelGen(AxisID axisID) {
+    if (axisID == AxisID.FIRST) {
+      try {
+        copyFromDataSource(datasetName + axisID.getExtension() + ".seed");
+      }
+      catch (SystemProcessException e) {
+        e.printStackTrace();
+      }
+    }
     applicationManager.openFiducialModelDialog(axisID);
     mainFrame.pack();
     applicationManager.fiducialModelTrack(axisID);
@@ -217,11 +268,18 @@ public class DataFlowTests {
       e.printStackTrace();
       return;
     }
+    if(applicationManager.getMetaData().isWholeTomogramSample()) {
+      applicationManager.wholeTomogram(axisID);
+    }
+    else {
+      applicationManager.tomopitch(axisID);
+    }
+    waitForThread(axisID);
 
-    applicationManager.tomopitch(axisID);
-    waitForThread(axisID);
-    applicationManager.finalAlign(axisID);
-    waitForThread(axisID);
+    if(!applicationManager.getMetaData().isFiducialessAlignment()) {
+      applicationManager.finalAlign(axisID);
+      waitForThread(axisID);
+    }
     applicationManager.doneTomogramPositioningDialog(axisID);
   }
 
@@ -244,5 +302,43 @@ public class DataFlowTests {
     waitForThread(AxisID.ONLY);
     applicationManager.combine();
     waitForThread(AxisID.ONLY);
+  }
+
+  /**
+   * Parse the command line string array
+   * @param args
+   */
+  private static void parseCommandLine(String[] args) {
+    if (args.length < 1) {
+      System.err.println("usage: dataFlowTests.jar [options] datasetName");
+      System.err.println("");
+      System.err.println("Options:");
+      System.err
+        .println("--fiducial      Force a fiducial alignment (overide .edf");
+      System.err.println("");
+      System.err
+        .println("--fiducialless  Force a fiducialless alignment (overide .edf");
+      System.err.println("");
+
+      System.exit(1);
+    }
+
+    // Reset the dataset name 
+    datasetName = "";
+    //  Parse the command line arguments
+    for (int i = 0; i < args.length; i++) {
+      if (!args[i].startsWith("-")) {
+        datasetName = args[i];
+      }
+      if (args[i].equals("--fiducial")) {
+        fiducialSpecified = true;
+        fiducialless = false;
+      }
+
+      if (args[i].equals("--fiducialless")) {
+        fiducialSpecified = true;
+        fiducialless = true;
+      }
+    }
   }
 }
