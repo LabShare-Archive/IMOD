30bca4e930743f5bd03bd161c93ccddd31d31312
SueHeld <sueh@colorado.edu>
2004-08-20 21:38:42 +0000

    bug# 508 Added CombineComscriptStateTest and log.

--- Etomo/src/etomo/comscript/ComScriptTests.java
+++ Etomo/src/etomo/comscript/ComScriptTests.java
@@ -1,21 +1,25 @@
-/*
- * Created on Sep 30, 2003
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
 package etomo.comscript;
 
+/**
+* <p>Description: </p>
+*
+* <p>Copyright: Copyright 2004 </p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* Univeristy of Colorado</p>
+*
+* @author $Author$
+*
+* @version $Revision$
+*
+* <p> $Log$ </p>
+*/
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-/**
- * @author sueh
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
 public class ComScriptTests {
+  public static final String rcsid = "$$Id$$";
 
   public static Test suite() {
     TestSuite suite = new TestSuite("Test for etomo.comscript");
@@ -23,6 +27,7 @@ public class ComScriptTests {
     suite.addTestSuite(StringListTest.class);
     suite.addTestSuite(NewstParamTest.class);
     suite.addTestSuite(FortranInputStringTest.class);
+    suite.addTestSuite(CombineComscriptStateTest.class);
     //suite.addTest(new NewstParamTest("testParseComScriptCommand"));
     return suite;
   }
