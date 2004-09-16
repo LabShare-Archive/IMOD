bd9e4fffa6445a2096b4e3594c9876bc5a0aef76
mastcu <mast@colorado.edu>
2004-09-16 16:12:57 +0000

    Changed do loop indentation, declared for implicit none

--- flib/tiltalign/metro.f
+++ flib/tiltalign/metro.f
@@ -1,11 +1,24 @@
 C	METRO -- VARIABLE METRIC MINIMIZATION W/ FLETCHER'S SWITCHING METHOD
 C
 C		REV 1.0		10/1/79 -- JLC
+c
+c	  $Author$
+c
+c	  $Date$
+c
+c	  $Revision$
+c
+c	  $Log$
 C
 	SUBROUTINE METRO (N,X,F,G,FACTOR,EST,EPS,LIMITin,IER,H,KOUNT)
-	DIMENSION X(*),G(*),H(*)
+	implicit none
+	real*4 X(*),G(*),H(*)
+	real*4 factor, est, eps, f
+	integer*4 n, limitin,limit,ier,kount
 	DOUBLE PRECISION DGAM,DD,S,AK,YA,YB,GA,GB,Z,W,NUJ,NUK
 	DOUBLE PRECISION HG,GHG,HGGH,DG,DOT,MU
+	integer*4 n2, n2pn, n2p2n, j, k
+	real*4 step, gg, p, delta, sig, posdef
 	MU = 1.D-04
 C
 C SCRATCH VECTOR STORAGE IN H:
@@ -26,11 +39,13 @@ C EVAL INITIAL ARG, GENERATE IDENTITY MATRIX SCALED BY NORM GRAD
 	limit=abs(limitin)
 	CALL FUNCT (N,X,F,G)
 1	GG=DOT(G,G,N)
-	DO 20 J=1,N
-		DO 10 K=1,N
-10			H((J-1)*N+K)=0.
-20		H((J-1)*N+J)=1./SQRT(GG/N)
-C
+	DO J=1,N
+	  DO  K=1,N
+	    H((J-1)*N+K)=0.
+	  enddo
+	  H((J-1)*N+J)=1./SQRT(GG/N)
+	enddo
+C	  
 C ......................................   MAIN ITERATION LOOP ..............
 C
 C SAVE OLD F,X,G
@@ -39,15 +54,17 @@ C
 	IF(limitin.gt.0.and.MOD(KOUNT,10).EQ.0)WRITE(6,777)KOUNT,F
 777	FORMAT(T48,'Cycle',I5,T65,E14.7)
 C
-	DO 30 J=1,N
-		H(n2+  J)=X(J)
-		H(n2pn+J)=G(J)
-C
+	DO J=1,N
+	  H(n2+  J)=X(J)
+	  H(n2pn+J)=G(J)
+C	    
 C COMPUTE DIRECTION VECTOR
-		P=0.
-		DO 25 K=1,N
-25			P=P-H((J-1)*N+K)*G(K)
-30		H(n2p2n+J)=P
+	  P=0.
+	  DO K=1,N
+	    P=P-H((J-1)*N+K)*G(K)
+	  enddo
+	  H(n2p2n+J)=P
+	enddo
 C
 C COMPUTE COMPONENT OF NEW GRADIENT ALONG SEARCH VECTOR
 	YB=F
@@ -60,14 +77,16 @@ C
 C ------------------------------   EXTRAPOLATE -------------------------------
 	YA=YB
 	GA=GB
-	DO 50 J=1,N
-50		X(J)=X(J)+STEP*H(n2p2n+J)
+	DO  J=1,N
+	  X(J)=X(J)+STEP*H(n2p2n+J)
+	enddo
 	CALL FUNCT (N,X,F,G)
 	YB=F
 	GB=DOT(H(n2p2n+1),G,N)
 	DG=0.D0
-	DO 60 J=1,N
-60		DG=DG+(X(J)-H(n2+J))*H(n2pn+J)
+	DO J=1,N
+	  DG=DG+(X(J)-H(n2+J))*H(n2pn+J)
+	enddo
 	IF (DG.GE.0.D0) GO TO 600		!SOMETHING'S FLAKY
 	IF ((YB-YA)/DG.GE.MU) STEP=FACTOR	!GOOD IMPROVEMENT -- 
 						!USE FULL SHIFT NEXT
@@ -81,8 +100,9 @@ C NOT ENOUGH IMPROVEMENT -- TRY CUBIC INTERPOLATION IF STEP WAS TOO LARGE
 	W=DSQRT(Z**2-GA*GB)
 	AK=S*(GB+W-Z)/(GB-GA+2.D0*W)
 	IF (DABS(AK).GT.S) AK=AK*0.90D0*S/DABS(AK)
-	DO 80 J=1,N
-80		X(J)=X(J)-AK*H(n2p2n+J)
+	DO J=1,N
+	  X(J)=X(J)-AK*H(n2p2n+J)
+	enddo
 	CALL FUNCT (N,X,F,G)
 	IF (F.GT.YA.OR.F.GT.YB) GO TO 95	!INTERPOLATION FAILED
 C
@@ -102,10 +122,11 @@ C CHECK CONVERGENCE
 100	DELTA=0.
 	DGAM=0.D0
 	DG=0.D0
-	DO 110 J=1,N
-		DG=DG+(X(J)-H(n2+J))*H(n2pn+J)
-		DGAM=DGAM+(X(J)-H(n2+J))*(G(J)-H(n2pn+J))
-110		DELTA=DELTA+(X(J)-H(n2+J))**2
+	DO J=1,N
+	  DG=DG+(X(J)-H(n2+J))*H(n2pn+J)
+	  DGAM=DGAM+(X(J)-H(n2+J))*(G(J)-H(n2pn+J))
+	  DELTA=DELTA+(X(J)-H(n2+J))**2
+	enddo
 	IF (SQRT(DELTA).LE.EPS) RETURN
 	IF (DG.GE.0.D0) GO TO 600		!ERROR
 	IF (KOUNT.GE.LIMIT) GO TO 750
@@ -113,34 +134,38 @@ C
 C UPDATE COVARIANCE MATRIX ACCORDING TO SWITCHING ALGORITHM OF FLETCHER
 	GHG=0.D0
 	POSDEF=0.
-	DO 130 J=1,N
-		SIG=0.
-		HG=0.D0
-		DO 120 K=1,N
-			SIG=SIG+H((J-1)*N+K)*G(K)
-120			HG=HG+H((J-1)*N+K)*(G(K)-H(n2pn+K))
-		H(n2p2n+J)=HG
-		GHG=GHG+(G(J)-H(n2pn+J))*HG
-130		POSDEF=POSDEF+G(J)*SIG
+	DO J=1,N
+	  SIG=0.
+	  HG=0.D0
+	  DO K=1,N
+	    SIG=SIG+H((J-1)*N+K)*G(K)
+	    HG=HG+H((J-1)*N+K)*(G(K)-H(n2pn+K))
+	  enddo
+	  H(n2p2n+J)=HG
+	  GHG=GHG+(G(J)-H(n2pn+J))*HG
+	  POSDEF=POSDEF+G(J)*SIG
+	enddo
 	IF (POSDEF.LT.0.) GO TO 800		!ERROR
 C
 C H0 ALGORITHM
-	DO 140 J=1,N
-		DO 140 K=1,N
-			DD=(X(J)-H(n2+J))*(X(K)-H(n2+K))
-			HGGH=H(n2p2n+J)*H(n2p2n+K)
-			H((J-1)*N+K)=H((J-1)*N+K)+DD/DGAM-HGGH/GHG
-140			CONTINUE
+	DO J=1,N
+	  DO K=1,N
+	    DD=(X(J)-H(n2+J))*(X(K)-H(n2+K))
+	    HGGH=H(n2p2n+J)*H(n2p2n+K)
+	    H((J-1)*N+K)=H((J-1)*N+K)+DD/DGAM-HGGH/GHG
+	  enddo
+	enddo
 C
 C H1 ALGORITHM
 	IF (DGAM.LT.GHG) GO TO 21
-	DO 150 J=1,N
-		NUJ=DSQRT(GHG)*((X(J)-H(n2+J))/DGAM-H(n2p2n+J)/GHG)
-		DO 150 K=1,N
-			NUK=DSQRT(GHG)*((X(K)-H(n2+K))/DGAM-
-     .			H(n2p2n+K)/GHG)
-			H((J-1)*N+K)=H((J-1)*N+K)+NUJ*NUK
-150			CONTINUE
+	DO J=1,N
+	  NUJ=DSQRT(GHG)*((X(J)-H(n2+J))/DGAM-H(n2p2n+J)/GHG)
+	  DO K=1,N
+	    NUK=DSQRT(GHG)*((X(K)-H(n2+K))/DGAM-
+     &		H(n2p2n+K)/GHG)
+	    H((J-1)*N+K)=H((J-1)*N+K)+NUJ*NUK
+	  enddo
+	enddo
 	GO TO 21			!CONTINUE W NEXT DIRECTION
 C ..................................... END MAIN LOOP .....................
 C
@@ -157,15 +182,17 @@ C DONE -- SET ERROR CODE AND RETURN
 	END
 C**DOT
 	DOUBLE PRECISION FUNCTION DOT(A,B,N)
-	DIMENSION A(*),B(*)
+	implicit none
+	real*4 A(*),B(*), C
+	integer*4 n, j
 	DOUBLE PRECISION AD,BD
 C
 	DOT = 0.0
-	DO 100 J = 1,N
+	DO J = 1,N
 	  AD = A(J)
 	  BD = B(J)
 	  DOT = DOT + AD*BD
-100	CONTINUE
+	enddo
 C
 	C= DOT
 	DOT = C
