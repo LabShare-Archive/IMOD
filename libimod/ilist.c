b31d65f9b89cbcd3df0a26ab5ee5ed0013df1835
mastcu <mast@colorado.edu>
2004-11-21 06:15:27 +0000

    Fixed a declaration for Windows

--- libimod/ilist.c
+++ libimod/ilist.c
@@ -1,30 +1,21 @@
-/*  IMOD VERSION 2.02
- *
+/* 
  *  ilist.c -- Model list structure library routines.
  *
- *  Author: James Kremer email: kremer@colorado.edu
+ *  Original author: James Kremer
+ *  Modified by David Mastronarde  email: mast@colorado.edu
+ *
+ *  Copyright (C) 1995-2004 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
+/*  $Author$
 
-/*****************************************************************************
- *   Copyright (C) 1995-1996 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
+$Date$
+
+$Revision$
+
+$Log$
+*/
 
 #include <stdlib.h>
 #include "ilist.h"
@@ -72,11 +63,12 @@ void   ilistQuantum(Ilist *list, int size)
 Ilist *ilistDup(Ilist *list)
 {
   void *dsave;
+  Ilist *newList;
   if (!list)
     return(NULL);
 
   /* First get a new list, then copy the structure, saving the data pointer */
-  Ilist *newList = ilistNew(list->dsize, list->store);
+  newList = ilistNew(list->dsize, list->store);
   if (!newList)
     return(NULL);
   dsave = newList->data;
