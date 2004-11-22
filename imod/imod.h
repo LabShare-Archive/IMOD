9c2d9a7d1610ecc633a957031cbc54d4ab62b5d1
mastcu <mast@colorado.edu>
2004-11-22 00:22:48 +0000

    Added definition for showing help page

--- imod/imod.h
+++ imod/imod.h
@@ -1,31 +1,14 @@
-/*  IMOD VERSION 2.20
- *
+/*
  *  $Id$
  *
  *  Original author: James Kremer
  *  Revised by: David Mastronarde   email: mast@colorado.edu
+ *
+ *  Copyright (C) 1995-2004 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
 
-/*****************************************************************************
- *   Copyright (C) 1995-1998 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 /*  $Author$
 
     $Date$
@@ -143,6 +126,10 @@ void DLL_EX_IM imodPuts(const char *message);
 /* Call with a keyevent to execute 3dmod hot keys */
 void DLL_EX_IM imodDefaultKeys(QKeyEvent *event, ImodView *vw);
 
+/* Show a help page in Qt Assistant; set absolute to true to provide a full
+   path instead of a path relative to IMOD_DIR/html/3dmodHelp */
+void DLL_EX_IM imodShowHelpPage(const char *page, bool absolute = false);
+
 
 /*****************************************************************************/
 }
@@ -150,6 +137,9 @@ void DLL_EX_IM imodDefaultKeys(QKeyEvent *event, ImodView *vw);
 
 /*
     $Log$
+    Revision 3.8  2004/09/24 18:10:04  mast
+    Added new definition for plugins that can execute messages
+
     Revision 3.7  2004/05/31 23:10:56  mast
     Added macros for exporting/importing under Windows
 
