d0faac57000a16032b9682c5e54b478aaa984064
mastcu <mast@colorado.edu>
2004-12-03 17:24:50 +0000

    Fixed bug in deleting point with no contour, and changed Delete behavior

--- imod/imod_input.cpp
+++ imod/imod_input.cpp
@@ -4,28 +4,11 @@
  *
  *  Original author: James Kremer
  *  Revised by: David Mastronarde   email: mast@colorado.edu
+ *
+ *  Copyright (C) 1995-2004 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
-
-/*****************************************************************************
- *   Copyright (C) 1995-2001 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 
 /*  $Author$
 
@@ -139,13 +122,17 @@ void inputInsertPoint(ImodView *vw)
   imodDraw(vw, IMOD_DRAW_XYZ | IMOD_DRAW_MOD);
 }
 
+/* 
+ * Delete a single point, or whole contour
+ */
 void inputDeletePoint(ImodView *vw)
 {
-  Icont *cont;
-  if (vw->imod->mousemode == IMOD_MMODEL){
+  Icont *cont = imodContourGet(vw->imod);
 
-    cont = imodContourGet(vw->imod);
-    if (cont->psize == 1)
+  // Must be in model mode, have contour and have a current point or be empty
+  if (vw->imod->mousemode == IMOD_MMODEL && cont && 
+      (!cont->psize || vw->imod->cindex.point >= 0)) {
+    if (!cont->psize)
       vw->undo->contourRemoval();
     else
       vw->undo->pointRemoval();
@@ -1307,6 +1294,9 @@ bool inputTestMetaKey(QKeyEvent *event)
 
 /*
 $Log$
+Revision 4.21  2004/11/20 05:05:27  mast
+Changes for undo/redo capability
+
 Revision 4.20  2004/11/01 23:38:06  mast
 Changes for multiple selection and deletion of multiple contours
 
