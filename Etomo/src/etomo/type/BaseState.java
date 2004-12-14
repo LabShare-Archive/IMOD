e2eba04a91e76f2b57ad18328d3a71170f4c5ba4
SueHeld <sueh@colorado.edu>
2004-12-14 21:40:52 +0000

    bug# 572 Interface for TomogramState and JoinState.

---
+++ Etomo/src/etomo/type/BaseState.java
@@ -0,0 +1,22 @@
+package etomo.type;
+
+import etomo.storage.Storable;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
+*
+*<p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface BaseState extends Storable {
+  public static  final String  rcsid =  "$Id$";
+}
