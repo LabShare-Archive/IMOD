51ddd364c31d3a6b936cdb6c01a17f154efbae7f
SueHeld <sueh@colorado.edu>
2004-12-14 21:40:21 +0000

    bug # 565 Removing JoinProcessTrack so there is no commom code. Use BaseProcessTrack as an interface because BaseManager is using it.

--- Etomo/src/etomo/type/BaseProcessTrack.java
+++ Etomo/src/etomo/type/BaseProcessTrack.java
@@ -1,7 +1,5 @@
 package etomo.type;
 
-import java.util.Properties;
-
 import etomo.storage.Storable;
 
 /**
@@ -18,37 +16,18 @@ import etomo.storage.Storable;
 * @version $Revision$
 * 
 * <p> $Log$
+* <p> Revision 1.2  2004/11/19 23:31:43  sueh
+* <p> bug# 520 merging Etomo_3-4-6_JOIN branch to head.
+* <p>
 * <p> Revision 1.1.2.1  2004/09/29 19:16:18  sueh
 * <p> bug# 520 Base class for ProcessTrack and JoinProcessTrack.  Implements
 * <p> Storable with generic functions and abstract functions.
 * <p> </p>
 */
-public abstract class BaseProcessTrack implements Storable {
+public interface BaseProcessTrack extends Storable {
   public static  final String  rcsid =  "$Id$";
   
-  protected String revisionNumber;
-  protected boolean isModified = false;
-  
-  public abstract void store(Properties props, String prepend);
-  public abstract void load(Properties props, String prepend);
-  
-  public void store(Properties props) {
-    store(props, "");
-  }
-  
-  public void load(Properties props) {
-    load(props, "");
-  }
-  
-  public String getRevisionNumber() {
-    return revisionNumber;
-  }
-
-  public boolean isModified() {
-    return isModified;
-  }
-
-  public void resetModified() {
-    isModified = false;
-  }
+  public String getRevisionNumber();
+  public boolean isModified();
+  public void resetModified();
 }
