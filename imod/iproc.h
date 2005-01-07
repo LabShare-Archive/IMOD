83cd161fe2d36b74ccfa088bacd9f164584c0d7c
mastcu <mast@colorado.edu>
2005-01-07 21:59:13 +0000

    Added median filter

--- imod/iproc.h
+++ imod/iproc.h
@@ -1,65 +1,20 @@
-/*  IMOD VERSION 2.50
- *
+/*
  *  $Id$
  *
  *  Original author: James Kremer
  *  Revised by: David Mastronarde   email: mast@colorado.edu
+ *
+ *  Copyright (C) 1995-2005 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
-
-/*****************************************************************************
- *   Copyright (C) 1995-2001 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 /*  $Author$
 
     $Date$
 
     $Revision$
 
-    $Log$
-    Revision 3.8  2004/11/07 23:04:47  mast
-    Changes for thread and FFT stuff
-
-    Revision 3.7  2004/11/04 23:30:55  mast
-    Changes for rounded button style
-
-    Revision 3.6  2004/01/22 19:10:12  mast
-    Added slot for real button press
-
-    Revision 3.5  2004/01/05 18:03:50  mast
-    renamed vw to vi
-
-    Revision 3.4  2003/09/16 02:10:55  mast
-    Added working array because displayed image data cannot be used directly
-
-    Revision 3.3  2003/02/10 20:41:56  mast
-    Merge Qt source
-
-    Revision 3.2.2.2  2003/01/27 00:30:07  mast
-    Pure Qt version and general cleanup
-
-    Revision 3.2.2.1  2003/01/23 19:57:06  mast
-    Qt version
-
-    Revision 3.2  2002/12/01 15:34:41  mast
-    Changes to get clean compilation with g++
-
+    Log at end of file
 */
 
 #ifndef BD_IPROC_H_
@@ -68,6 +23,7 @@
 #define PROC_BACKGROUND 0
 #define PROC_FOREGROUND 255
 
+#include "mrcslice.h"
 #include "dialog_frame.h"
 class QWidgetStack;
 class QListBox;
@@ -111,6 +67,8 @@ class IProcWindow : public DialogFrame
   void fourFiltChanged(int which, int value, bool dragging);
   void binningChanged(int val);
   void subsetChanged(bool state);
+  void medSizeChanged(int val);
+  void med3DChanged(bool state);
 
  protected:
   void closeEvent ( QCloseEvent * e );
@@ -157,6 +115,9 @@ typedef struct
   QSpinBox      *fftBinSpin;
   QLabel        *fftLabel1;
   QLabel        *fftLabel2;
+  bool          median3D;
+  int           medianSize;
+  struct MRCvolume medianVol;
 } ImodIProc;
 
 
@@ -174,3 +135,36 @@ int iprocRethink(ImodView *vw);
 bool iprocBusy(void);
 
 #endif /* BD_IPROC_H_ */
+/*
+    $Log$
+    Revision 3.9  2004/11/11 15:55:34  mast
+    Changes to do FFT in a subarea
+
+    Revision 3.8  2004/11/07 23:04:47  mast
+    Changes for thread and FFT stuff
+
+    Revision 3.7  2004/11/04 23:30:55  mast
+    Changes for rounded button style
+
+    Revision 3.6  2004/01/22 19:10:12  mast
+    Added slot for real button press
+
+    Revision 3.5  2004/01/05 18:03:50  mast
+    renamed vw to vi
+
+    Revision 3.4  2003/09/16 02:10:55  mast
+    Added working array because displayed image data cannot be used directly
+
+    Revision 3.3  2003/02/10 20:41:56  mast
+    Merge Qt source
+
+    Revision 3.2.2.2  2003/01/27 00:30:07  mast
+    Pure Qt version and general cleanup
+
+    Revision 3.2.2.1  2003/01/23 19:57:06  mast
+    Qt version
+
+    Revision 3.2  2002/12/01 15:34:41  mast
+    Changes to get clean compilation with g++
+
+*/
