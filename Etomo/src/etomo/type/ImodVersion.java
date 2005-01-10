c8cf28456d3abe322f041191234f055264cdc863
SueHeld <sueh@colorado.edu>
2005-01-10 23:50:23 +0000

    bug# 578 A class containing the current Imod version.

---
+++ Etomo/src/etomo/type/ImodVersion.java
@@ -0,0 +1,21 @@
+package etomo.type;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
+*
+*<p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public class ImodVersion {
+  public static  final String  rcsid =  "$Id$";
+  
+  public static final String CURRENT_VERSION = "3.4.17";
+}
