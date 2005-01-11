ac29b6761883f1274f635a3ecaba53f379cf5217
mastcu <mast@colorado.edu>
2005-01-11 03:01:53 +0000

    *** empty log message ***

--- Etomo/src/etomo/type/ImodVersion.java
+++ Etomo/src/etomo/type/ImodVersion.java
@@ -1,24 +1,21 @@
 package etomo.type;
 /**
-* <p>Description: </p>
+* <p>Description: A class containing the current Imod version.</p>
 * 
 * <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
 *
 *<p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
 * University of Colorado</p>
 * 
 * @author $Author$
 * 
 * @version $Revision$
 * 
-* <p> $Log$
-* <p> Revision 1.1  2005/01/10 23:50:23  sueh
-* <p> bug# 578 A class containing the current Imod version.
 * <p> </p>
 */
 public class ImodVersion {
   public static  final String  rcsid =  "$Id$";
   
-  public static final String CURRENT_VERSION = "3.4.16";
+  public static final String CURRENT_VERSION = "3.4.17";
 }
