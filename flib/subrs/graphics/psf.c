3a5e1c4669639ffb710e43fcb8c9181befec5629
mastcu <mast@colorado.edu>
2005-01-21 18:19:28 +0000

    Allow IMOD_PS_FONT to be set to set the font

--- flib/subrs/graphics/psf.c
+++ flib/subrs/graphics/psf.c
@@ -12,6 +12,9 @@ $Date$
 $Revision$
 
 $Log$
+Revision 3.2  2004/11/05 18:53:39  mast
+Include local files with quotes, not brackets
+
 Revision 3.1  2003/10/29 04:31:14  mast
 switch to calling f2cString
 
@@ -27,11 +30,11 @@ switch to calling f2cString
 #ifdef F77STRING
 typedef struct
 {
-          unsigned short length;
-	       char dum1;
-	       char dum2;
-	       char *string;
-     } FString;
+  unsigned short length;
+  char dum1;
+  char dum2;
+  char *string;
+} FString;
 #endif
 
 #ifdef F77FUNCAP
@@ -60,98 +63,101 @@ static int lastsize = 0;
 
 int psopen(
 #ifdef F77STRING
-	   FString *f77str,
+           FString *f77str,
 #else
-	   char *filename,
+           char *filename,
 #endif
 
-	   float *lm, float *bm, float *dpi
+           float *lm, float *bm, float *dpi
 
 #ifndef F77STRING
-	   , int filename_size
+           , int filename_size
 #endif
-)
+           )
 {
 #ifdef F77STRING
-     int  filename_size  = f77str->length;
-     char *filename      = f77str->string;
+  int  filename_size  = f77str->length;
+  char *filename      = f77str->string;
 #endif
-     double ddpi, dlm, dbm;
-     char *fname = f2cString(filename, filename_size);
-     if (!fname) {
-       fprintf(stderr, "libps: error getting memory\n");
-       return(-1);
-     }
-     ddpi = *dpi; dlm = *lm; dbm = *bm;
-     ps = PSopen(fname, ddpi, dlm, dbm);
+  double ddpi, dlm, dbm;
+  char *fname = f2cString(filename, filename_size);
+  if (!fname) {
+    fprintf(stderr, "libps: error getting memory\n");
+    return(-1);
+  }
+  ddpi = *dpi; dlm = *lm; dbm = *bm;
+  ps = PSopen(fname, ddpi, dlm, dbm);
 
-     /* DNM 3/23/01: set font size to zero so a font specification will be
-	output to every new file */
-     lastsize = 0;
-     if (!ps)
-	  return(-1);
-     free(fname);
-     return(0);
+  /* DNM 3/23/01: set font size to zero so a font specification will be
+     output to every new file */
+  lastsize = 0;
+  if (!ps)
+    return(-1);
+  free(fname);
+  return(0);
 }
 
 
 void point(float *ix, float *iy)
 {
-     double x = *ix;
-     double y = *iy;
-     PSdrawPoint(ps, x, y);
+  double x = *ix;
+  double y = *iy;
+  PSdrawPoint(ps, x, y);
 }
 
 void frstpt(float *ix, float *iy)
 {
-     double x = *ix;
-     double y = *iy;
-     PSsetPoint(ps, x, y);
+  double x = *ix;
+  double y = *iy;
+  PSsetPoint(ps, x, y);
 }
 
 void vector(float *ix, float *iy)
 {
-     double x = *ix;
-     double y = *iy;
-     PSdrawVector(ps, x, y);
+  double x = *ix;
+  double y = *iy;
+  PSdrawVector(ps, x, y);
 }
 
 void frame(){PSpage(ps);}
 
 void wtstr(float *ix, float *iy,
 #ifdef F77STRING
-      FString *f77str,
+           FString *f77str,
 #else
-      char *text,
+           char *text,
 #endif
-      int *jsize, int *jor, int *jctr
+           int *jsize, int *jor, int *jctr
 
 #ifndef F77STRING
-      , int text_size
+           , int text_size
 #endif
-)
+           )
 
 {
-     float x = *ix;
-     float y = *iy;
+  float x = *ix;
+  float y = *iy;
 #ifdef F77STRING
-     int  text_size  = f77str->length;
-     char *text      = f77str->string;
+  int  text_size  = f77str->length;
+  char *text      = f77str->string;
 #endif
 
-     char *ctext = (char *)malloc(text_size + 1);
-     memcpy(ctext, text, text_size);
-     ctext[text_size] = 0x00;
+  char *useFont = getenv("IMOD_PS_FONT");
+  char *ctext = (char *)malloc(text_size + 1);
+  if (!useFont) 
+    useFont = defaultFont;
+  memcpy(ctext, text, text_size);
+  ctext[text_size] = 0x00;
 
-     if(lastsize != *jsize){
-       lastsize = *jsize;
-       PSsetFont(ps, defaultFont, lastsize);
-     }
+  if(lastsize != *jsize){
+    lastsize = *jsize;
+    PSsetFont(ps, useFont, lastsize);
+  }
 
-     PSdrawText(ps, ctext, x, y, *jor, *jctr);
+  PSdrawText(ps, ctext, x, y, *jor, *jctr);
 
-     /* DNM 3/23/01: plug the leak? */
-     free(ctext);
+  /* DNM 3/23/01: plug the leak? */
+  free(ctext);
 }
 
 void psclose(){PSclose(ps);}
