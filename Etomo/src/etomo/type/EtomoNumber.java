8cf1bfc85b63399aa39005e113f09bef4b211102
SueHeld <sueh@colorado.edu>
2005-01-21 23:26:02 +0000

    bug# 509 bug# 591  Added isUpdateCommand() in place of isSetAndNotDefault() as a standard why to decide if a parameter should be placed in a comscript.  Changed the name of value to currentValue. Added an empty value.  The currentValue is set to empty value when set(String) receives an empty or whitespace filled string.  The distinguishes it from a currentValue that was never set or set incorrectly.  THe only function which doesn't treate empty the same as null is getValue().  This allows EtomoNumber to display a blank field, even when the resetValue is in use.  However, since store() and updateCommand(), and isUpdateCommand() treate empty just like a null, the blank field isn't remembered.  This prevents the comscript, .edf, .ejf files from having lots of empty fields in them.  This means that the reset value will show when they are reloaded.  If a blank numeric field must be remembered and reload blank, write an alternate way to save it, treating empty like a regular value. Removed initialValue from constructor.  Added functionality to optionally prevent the setting of null values (changed initialize() and newNumber().

--- Etomo/src/etomo/type/EtomoNumber.java
+++ Etomo/src/etomo/type/EtomoNumber.java
@@ -19,6 +19,10 @@ import etomo.comscript.InvalidParameterException;
 * @version $Revision$
 * 
 * <p> $Log$
+* <p> Revision 1.5  2005/01/14 23:04:35  sueh
+* <p> Changing the name of set(ComScriptCommand) to parse.  Handle missing
+* <p> keyword by setting value to null.
+* <p>
 * <p> Revision 1.4  2005/01/10 23:31:00  sueh
 * <p> bug# 578 Standardized class so that every use of value goes through
 * <p> getValue().  GetValue() tries to find a non-null value by looking first at
@@ -62,101 +66,125 @@ public class EtomoNumber extends ConstEtomoNumber {
     super(type, name);
   }
   
-  public EtomoNumber(int type, int initialValue) {
-    super(type, initialValue);
-  }
-  
-  public EtomoNumber(int type, String name, int initialValue) {
-    super(type, name, initialValue);
-  }
-  
   public EtomoNumber(ConstEtomoNumber that) {
     super(that);
   }
   
-  public void load(Properties props) {
-    set(props.getProperty(name, toString(resetValue)));
-  }
-  public void load(Properties props, String prepend) {
-    set(props.getProperty(prepend + "." + name, toString(resetValue)));
+  public EtomoNumber(int type, String name, boolean preventNullValue, Number resetValue) {
+    super(type, name, preventNullValue, resetValue);
   }
   
+  public void load(Properties props) {
+    String stringValue = props.getProperty(name);
+    if (stringValue == null) {
+      reset();
+    }
+    else {
+      set(stringValue);
+    }
+  }
+  
+  public void load(Properties props, String prepend) {
+    String stringValue = props.getProperty(prepend + "." + name);
+    if (stringValue == null) {
+      reset();
+    }
+    else {
+      set(stringValue);
+    }
+  }
+  
+  /**
+   * Converts a string to a Number of the correct type.  If the string is
+   * empty, currentValue will be set to newNumber().  If the string is not a 
+   * valid number of the correct type, currentValue will be set to newNumber().
+   * If ceilingValue is not null and the new value exceeds ceilingValue,
+   * currentValue will be set to ceilingValue.
+   * @param value
+   * @return
+   */
   public EtomoNumber set(String value) {
     invalidReason = null;
-    if (value == null || value.matches("\\s*")) {
-      this.value = newNumber();
+    if (value == null) {
+      currentValue = newNumber();
+    }
+    else if (value.matches("\\s*")){
+      currentValue = newEmptyNumber();
     }
     else {
       StringBuffer invalidBuffer = new StringBuffer();
-      this.value = newNumber(value, invalidBuffer);
+      currentValue = newNumber(value, invalidBuffer);
       if (invalidBuffer.length() > 0) {
         invalidReason = invalidBuffer.toString();
-        this.value = newNumber();
+        currentValue = newNumber();
       }
     }
-    if (!isNull(ceilingValue) && isSet() && gt(this.value, ceilingValue)) {
-      this.value = newNumber(ceilingValue);
-    }
+    currentValue = applyCeilingValue(currentValue);
     validate();
     return this;
   }
   
   public ConstEtomoNumber parse(ComScriptCommand scriptCommand)
       throws InvalidParameterException {
-    if (!scriptCommand.hasKeyword(name)) {
-      value = newNumber();
-      return this;
+    return parse(scriptCommand, name);
+  }
+  
+  /**
+   * Parse scriptCommand for keyword.  If keyword is not found, call reset().
+   * If keyword is found, call set with the string value found in scriptCommand.
+   * @param scriptCommand
+   * @param keyword
+   * @return
+   * @throws InvalidParameterException
+   */
+  public ConstEtomoNumber parse(ComScriptCommand scriptCommand, String keyword)
+      throws InvalidParameterException {
+    if (!scriptCommand.hasKeyword(keyword)) {
+      reset();
     }
-    return set(scriptCommand.getValue(name));
+    return set(scriptCommand.getValue(keyword));
   }
   
   public EtomoNumber set(Number value) {
     invalidReason = null;
-    if (value == null) {
-      this.value = newNumber();
-    }
-    else if (!isNull(ceilingValue) && !isNull(value) && gt(value, ceilingValue)) {
-      this.value = newNumber(ceilingValue);
-    }
-    else {
-      this.value = newNumber(value);
-    }
+    currentValue = applyCeilingValue(value);
     validate();
+    return this;
+  }
+  
+  /**
+   * set currentValue from number.getValue().  Take the currentValueSet value
+   * from number.
+   * @param number
+   * @return
+   */
+  public ConstEtomoNumber set(ConstEtomoNumber number) {
+    set(number.getValue());
     return this;
   }
   
   public EtomoNumber set(int value) {
-    invalidReason = null;
-    this.value = newNumber(value);
-    if (!isNull(ceilingValue) && isSet() && gt(this.value, ceilingValue)) {
-      this.value = newNumber(ceilingValue);
-    }
-    validate();
-    return this;
+    return set(newNumber(value));
+  }
+  
+  public ConstEtomoNumber set(boolean value) {
+    return set(newNumber(value));
   }
   
   public EtomoNumber set(long value) {
-    invalidReason = null;
-    this.value = newNumber(value);
-    if (!isNull(ceilingValue) && isSet() && gt(this.value, ceilingValue)) {
-      this.value = newNumber(ceilingValue);
-    }
-    validate();
-    return this;
+    return set(newNumber(value));
   }
   
   public EtomoNumber set(double value) {
-    invalidReason = null;
-    this.value = newNumber(value);
-    if (!isNull(ceilingValue) && isSet() && gt(this.value, ceilingValue)) {
-      this.value = newNumber(ceilingValue);
-    }
-    validate();
-    return this;
+    return set(newNumber(value));
   }
   
+  /**
+   * Set currentValue to resetValue.  Sets currentValueSet to false.
+   * @return
+   */
   public EtomoNumber reset() {
-    value = newNumber(resetValue);
+    currentValue = applyCeilingValue(newNumber(resetValue));
     validate();
     return this;
   }
