5d5372e9517b9e42ee5f2ab754a60bbb8e6f0f60
mastcu <mast@colorado.edu>
2005-02-09 01:19:48 +0000

    Added flag for keeping track of changes started when shifted contours

--- imod/xzap.h
+++ imod/xzap.h
@@ -1,31 +1,13 @@
-/*  IMOD VERSION 2.50
- *
+/*
  *  xzap.h -- Header file for ZaP Window.
  *
  *  Original author: James Kremer
  *  Revised by: David Mastronarde   email: mast@colorado.edu
+ *
+ *  Copyright (C) 1995-2005 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
-
-/*****************************************************************************
- *   Copyright (C) 1995-2001 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 /*  $Author$
 
 $Date$
@@ -69,6 +51,7 @@ typedef struct zapwin
   int    rbMouseY1;
   int    startingBand;
   int    shiftingCont; /* Flag for shifting contour */
+  int    shiftRegistered;  /* Flag that contour changes have been registered */
   int    dragAddCount; /* Number of points added and not registered for undo */
   Iindex dragAddIndex; /* Starting obj, cont, point for first such point*/
   int    dragAddEnd;   /* Ending or last point not registered */
@@ -137,6 +120,9 @@ void zapReportRubberband();
 
 /*
 $Log$
+Revision 3.13  2004/11/20 05:05:27  mast
+Changes for undo/redo capability
+
 Revision 3.12  2004/11/04 17:02:41  mast
 Changes for switching to shifting contour as a mode that is turned on
 
