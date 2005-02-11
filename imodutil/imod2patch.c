c45a9419c40b10b451d34c4ac78e3009739aff21
mastcu <mast@colorado.edu>
2005-02-11 00:41:40 +0000

    Removed unneeded declaration

--- imodutil/imod2patch.c
+++ imodutil/imod2patch.c
@@ -1,30 +1,13 @@
-/*  IMOD VERSION 2.50
- *
+/*
  *  $Id$
  *
  *  Author: David Mastronarde  email: mast@colorado.edu
+ *
+ *  Copyright (C) 1995-2005 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
 
-/*****************************************************************************
- *   Copyright (C) 1995-2001 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 /*  $Author$
 
 $Date$
@@ -32,6 +15,9 @@ $Date$
 $Revision$
 
 $Log$
+Revision 3.5  2004/11/05 19:05:29  mast
+Include local files with quotes, not brackets
+
 Revision 3.4  2004/09/21 22:30:28  mast
 Fixed stray ~ in error string
 
@@ -52,7 +38,6 @@ fixed exit status
 #include "imodel.h"
 
 
-static int fgetline(FILE *fp, char s[],int limit);
 struct Mod_Model *imod_from_patches(FILE *fin, float scale);
 
 int main( int argc, char *argv[])
