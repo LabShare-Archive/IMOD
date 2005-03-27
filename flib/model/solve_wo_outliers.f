ecb36ba4cd1c7e68fc8719e2b12890e8c1aba78f
mastcu <mast@colorado.edu>
2005-03-27 18:57:00 +0000

    Moved erfcc to library

--- flib/model/solve_wo_outliers.f
+++ flib/model/solve_wo_outliers.f
@@ -5,6 +5,9 @@ c
 c	  $Revision$
 c
 c	  $Log$
+c	  Revision 3.4  2004/06/10 00:36:15  mast
+c	  Restarted amoeba for second round as recommended
+c	
 c	  Revision 3.3  2003/12/24 18:05:18  mast
 c	  Implemented constrained solution with in-plane linear
 c	  transformation and two rotation angles, using a simplex search,
@@ -514,23 +517,6 @@ c
 72	  format(1x,a1,i4,f15.5, 2f9.3,2f9.5,2f9.3)
 	endif
 	  
-	return
-	end
-
-
-c	  returns the complementary error function erfc(x) with fractional
-c	  error everywhere less than 1.2e-7
-c
-	real*4 function erfcc(x)
-	implicit none
-	real*4 x,z,t
-c
-	z=abs(x)
-	t=1./(1.+0.5*Z)
-	erfcc=t*exp(-z*z-1.26551223+T*(1.00002368+T*(.37409196+
-     &	    t*(.09678418+t*(-.18628806+t*(.27886807+t*(-1.13520398+
-     &	    t*(1.48851587+t*(-.82215223+t*.17087277)))))))))
-	if(x.lt.0) erfcc=2.-erfcc
 	return
 	end
 
