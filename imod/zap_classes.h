7efe0bd2ad42b0a9974cafab4f7466aca7a02858
mastcu <mast@colorado.edu>
2005-03-29 00:59:57 +0000

    Added 2nd toolbar

--- imod/zap_classes.h
+++ imod/zap_classes.h
@@ -1,30 +1,13 @@
-/*  IMOD VERSION 2.7.9
- *
+/*
  *  zap_classes.h -- Header file for ZaP mainwindow and GLwidget classes.
  *
  *  Author: David Mastronarde   email: mast@colorado.edu
+ *
+ *  Copyright (C) 1995-2005 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
 
-/*****************************************************************************
- *   Copyright (C) 1995-2002 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 /*  $Author$
 
 $Date$
@@ -78,6 +61,7 @@ class ZapWindow : public QMainWindow
 
   ZapGL *mGLw;
   QToolBar *mToolBar;
+  QToolBar *mToolBar2;
   struct zapwin *mZap;
 
   public slots:
@@ -144,6 +128,9 @@ protected:
 
 /*
 $Log$
+Revision 4.5  2004/05/07 22:16:07  mast
+Fixed array dimension problems caused by new toolbutton
+
 Revision 4.4  2004/05/05 17:32:00  mast
 Added rubberband definition and made mZap public
 
