68ce368ef8d2ebdfe2c2debdd64334791382d0d7
mastcu <mast@colorado.edu>
2005-04-13 23:50:06 +0000

    Set up to read in chunks, so it only needs one large array

--- flib/image/binvol.f
+++ flib/image/binvol.f
@@ -7,6 +7,9 @@ c
 c	  $Revision$
 c
 c	  $Log$
+c	  Revision 3.2  2005/04/13 05:31:02  mast
+c	  Made binning anisotropic
+c	
 c	  Revision 3.1  2004/10/28 22:22:49  mast
 c	  Added to package
 c	
@@ -17,20 +20,21 @@ c
 	integer*4 NX,NY,NZ,nxs,nys,nzs
 C
 	integer*4 NXYZ(3),MXYZ(3),nxyzs(3),nxyzst(3),mode
-	real*4 array(idim),brray(idim/4),dmin,dmax,dmean,dmsum,cell(6)
+	real*4 array(idim),dmin,dmax,dmean,dmsum,cell(6)
 	real*4 dmin2, dmax2, dmean2
-	integer*4 izout,iredfac(3),iredall,i,inz, ierr
+	integer*4 izout,iredfac(3),iredall,i,inz, ierr, isec, maxLines, inbase
+	integer*4 numChunks,iy,numLines, nReduced,nBinX,nBinY,nBinZ,iChunk
 	data nxyzst/0,0,0/
 C
-	EQUIVALENCE (NX,NXYZ),(nxs,nxyzs)
-	COMMON //NX,NY,NZ,nxs,nys,nzs
+	EQUIVALENCE (NX,NXYZ),(nxs,nxyzs),(nBinX,iredfac)
+	COMMON //NX,NY,NZ,nxs,nys,nzs,nBinX,nBinY,nBinZ
 c
 	character*120 filbig,filout
 	character*9 dat
 	character*8 tim
 	character*70 titstr
 	character*80 titlech
-	common /bigarr/array,brray
+	common /bigarr/array
 c
 	integer*4 numOptArg, numNonOptArg
 	integer*4 PipGetInteger
@@ -57,26 +61,23 @@ c
 	if (PipGetInOutFile('InputFile', 2, ' ', filout)
      &	    .ne. 0) call errorexit('NO OUTPUT FILE SPECIFIED')
 
+c	  
+	ierr = PipGetInteger('BinningFactor', iredall)
+	nBinX = iredall
+	nBinY = iredall
+	nBinZ = iredall
+	ierr = PipGetInteger('XBinningFactor', nBinX)
+	ierr = PipGetInteger('YBinningFactor', nBinY)
+	ierr = PipGetInteger('ZBinningFactor', nBinZ)
 C
 C	Open image files.
 C
 	CALL IMOPEN(1,filbig,'RO')
 	CALL IRDHDR(1,NXYZ,MXYZ,MODE,DMIN,DMAX,DMEAN)
 C
-	IF (NX*NY.GT.idim) call errorexit('INPUT IMAGES TOO LARGE FOR ARRAYS')
-C
 	CALL IMOPEN(3,filout,'NEW')
 c
 	CALL ITRHDR(3,1)
-c	  
-	ierr = PipGetInteger('BinningFactor', iredall)
-	iredfac(1) = iredall
-	iredfac(2) = iredall
-	iredfac(3) = iredall
-	ierr = PipGetInteger('XBinningFactor', iredfac(1))
-	ierr = PipGetInteger('YBinningFactor', iredfac(2))
-	ierr = PipGetInteger('ZBinningFactor', iredfac(3))
-
 c	  
 	call irtcel(1, cell)
 	do i = 1,3
@@ -86,6 +87,10 @@ c
 	  if (nxyzs(i) .lt. 1) call errorexit('Binning factor too large')
 	  cell(i) = iredfac(i)*nxyzs(i)*(cell(i)/mxyz(i))
 	enddo
+C
+	inbase = nxs * nys
+	IF (inbase + NX * nBinY.GT.idim) call errorexit('INPUT'
+     &	    //' IMAGES TOO LARGE FOR ARRAYS WITH GIVEN BINNING FACTORS')
 	call ialsiz(3,nxyzs,nxyzst)
 	call ialsam(3,nxyzs)
 	call ialcel(3,cell)
@@ -93,18 +98,32 @@ c
 	dmsum=0.
 	dmax=-1.e10
 	dmin=1.e10
-	call imposn(1,0,0)
+
+	maxLines = nBinY * (((idim - inbase) / nx) / nBinY)
+	numChunks = (ny + maxLines - 1) / maxLines
+	isec = 0
+
 	do izout=1,nzs
-	  do i=1,nxs*nys
-	    brray(i)=0.
+	  do i=1,inbase
+	    array(i)=0.
 	  enddo
-	  do inz=1,iredfac(3)
-	    call irdsec(1,array,*99)
-	    call add_into_array(array,nx,ny,brray,nxs,nys,iredfac)
+	  do inz=1,nBinZ
+	    call imposn(1,isec,0)
+	    isec = isec + 1
+	    iy = 0
+	    do iChunk = 1, numChunks
+	      numLines = min(maxLines, ny - (iChunk - 1) * maxLines)
+	      nReduced = numLines / nBinY
+	      call irdsecl(1,array(inbase + 1), numLines, *99) 
+	      
+	      call add_into_array(array(inbase + 1),nx,numLines,
+     &		  array(iy * nxs + 1),nxs,nReduced,iredfac)
+	      iy = iy + nReduced
+	    enddo
 	  enddo
 
-	  CALL IclDeN(brray,NXs,NYs,1,NXs,1,NYs,DMIN2,DMAX2,DMEAN2)
-	  CALL IWRSEC(3,brray)
+	  CALL IclDeN(array,NXs,NYs,1,NXs,1,NYs,DMIN2,DMAX2,DMEAN2)
+	  CALL IWRSEC(3,array)
 C
 	  DMAX = max(dmax,dmax2)
 	  DMIN = min(dmin,dmin2)
