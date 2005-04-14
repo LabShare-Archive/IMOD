271ac091a32c762ff250e247d52c7769445ec45a
mastcu <mast@colorado.edu>
2005-04-14 00:35:45 +0000

    Rewrote to read in chunks, put big array in common, redimensioned

--- flib/image/avgstack.f
+++ flib/image/avgstack.f
@@ -1,30 +1,36 @@
-	Program AVGSTACK
-
 c	-------------------------------------------------------------------
 c	--- This program reads in an image file containing multiple	---
-c	--- sections and averages the sections into a singles output	---
+c	--- sections and averages the sections into a single output	---
 c	--- image.							---
 c	--- Written: 27-Jan-1989 - sjm					---
 c	--- Updates: 19-Dec-1989 - dnm: fixed cell setting in header	---
 c	-------------------------------------------------------------------
+c
+c	  $Author$
+c
+c	  $Date$
+c
+c	  $Revision$
+c
+c	  $Log$
+c
+	implicit none
+	integer*4 maxarr, limarr
+	parameter (maxarr=5000, limarr=100*maxarr)
 
-
-	parameter (maxarr=2100)
-
-	real	array(maxarr*maxarr), brray(maxarr*maxarr)
-	real	dmax, dmin, dmean, title(20), cell(6)
-	integer	nxyz(3), mxyz(3), nxyzst(3)
+	real*4 array(maxarr*maxarr), brray(limarr)
+	real*4 dmax, dmin, dmean, title(20), cell(6)
+	integer*4 nxyz(3), mxyz(3), nxyzst(3),nx,ny,nz,mode
 	equivalence (nxyz,nx)
-	common//nx,ny,nz
-	character*80 input, output, table
+	common //nx,ny,nz
+	character*120 input, output
 	character dat*9, tim*8
 	data nxyzst / 0.,0.,0. /
-
-
-c
-c 7/7/00 CER: remove the encode's; titlech is the temp space
-c
         character*80 titlech
+	integer*4 ifst,ilst,ix,iy,maxLines, numChunks,isec,i,kti
+	integer*4 numsec,numLines,iChunk
+	common /bigarr/ array,brray
+	
 c
 c	---------------------------------
 c	--- initialize and input info ---
@@ -39,7 +45,7 @@ c	--- initialize and input info ---
 
 	call irdhdr(1,nxyz,mxyz,mode,dmin,dmax,dmean) !--- Get Image Header ---!
 
-	if (nx*ny .gt. maxarr**2)stop '- IMAGE TOO LARGE FOR ARRAYS'
+	if (nx*ny .gt. maxarr**2)call errorexit('IMAGE TOO LARGE FOR ARRAYS')
 	nz = nz - 1	!--- first section is 0, so nz = total # sections - 1
 	ifst = 0
 	ilst = nz
@@ -85,27 +91,36 @@ C       encode ( 80, 3000, title ) numsec, dat, tim
         write(titlech,3000) numsec,dat,tim
         read(titlech,'(20a4)')(title(kti),kti=1,20)
 
+	maxLines = limarr / nx
+	if (maxLines .lt. 1) call errorexit('IMAGE TOO LARGE FOR ARRAYS')
+
+	numChunks = (ny + maxLines - 1) / maxLines
 
 c	------------------------------------------------------------
 c	--- Add all requested sections from the input image file ---
 
-	call imposn(1,ifst,0)	!--- Move to first section ---!
 	do isec = ifst, ilst
 	   write ( *, * ) ' Adding section ', isec
-	   call irdsec(1,brray,*100) 
-	   do iy = 1 , nx*ny
-		 array(iy) = array(iy) + brray(iy)
-	   end do
-	end do
+	   iy = 1
+	   call imposn(1,isec,0)
+	   do iChunk = 1, numChunks
+	     numLines = min(maxLines, ny - (iChunk - 1) * maxLines)
+	     call irdsecl(1,brray, numLines, *100) 
+	     do ix = 1, nx*numLines
+	       array(iy) = array(iy) + brray(ix)
+	       iy = iy + 1
+	     enddo
+	   enddo
+	 enddo
 
-100	call imclose(1)
+	call imclose(1)
 
 
 c	--------------------------------
 c	--- Average the output image ---
 	write ( *, * ) ' Averaging stack...'
 	do iy = 1 , nx*ny
-	      array(iy) = array(iy) / float(numsec)
+	  array(iy) = array(iy) / float(numsec)
 	end do
 
 
@@ -125,7 +140,13 @@ c	--- Write out the average ---
 
 1000	format ( 1x, a6, ' file : ', $ )
 2000	format ( a50 )
-3000	format ( 'AVGSTACK: ', i4, ' sections averaged.',
-     &	    t57, a9, 2x, a8 )
+3000	format ( 'AVGSTACK: ', i4, ' sections averaged.', t57, a9, 2x, a8 )
+100	call errorexit('READING FILE')
+	end
 
+	subroutine errorexit(message)
+	character*(*) message
+	print *
+	print *,'ERROR: AVGSTACK - ',message
+	call exit(1)
 	end
