f22fd779fec7a7e51b56135d4d47867820e56cad
SueHeld <sueh@colorado.edu>
2005-04-16 01:54:54 +0000

    bug# 615 Class to hold static colors.

---
+++ Etomo/src/etomo/ui/Colors.java
@@ -0,0 +1,28 @@
+package etomo.ui;
+
+import java.awt.Color;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2005</p>
+*
+*<p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public class Colors {
+  public static  final String  rcsid =  "$Id$";
+  
+  static final Color backgroundA = new Color(153, 179, 204);
+  static final Color backgroundB = new Color(153, 204, 179);
+}
+/**
+* <p> $Log$ </p>
+*/
