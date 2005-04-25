1e9e0dd1268daf403e17587e7ad8f746e06674c7
SueHeld <sueh@colorado.edu>
2005-04-25 21:41:09 +0000

    bug# 615 Moving message dialog functions, menu appearance functions, and fitting and repainting functions from mainPanel and the child frame classes to EtomoFrame.  Added register() to initialize a static instance variable in EtomoFrame.

--- Etomo/src/etomo/ui/SubFrame.java
+++ Etomo/src/etomo/ui/SubFrame.java
@@ -3,8 +3,8 @@ package etomo.ui;
 import java.awt.BorderLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowEvent;
 
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -24,15 +24,24 @@ import etomo.BaseManager;
 * 
 * @version $Revision$
 */
-public class SubFrame extends EtomoFrame {
+public final class SubFrame extends EtomoFrame {
   public static  final String  rcsid =  "$Id$";
   
   private MainFrame mainFrame;
   private JPanel rootPanel;
   private JLabel statusBar;
   
-  SubFrame(MainFrame mainFrame) {
+  protected SubFrame(MainFrame mainFrame) {
+    register();
     this.mainFrame = mainFrame;
+  }
+  
+  protected final synchronized void register() {
+    if (subFrame != null) {
+      throw new IllegalStateException("Only one instance of SubFrame is allowed.");
+    }
+    subFrame = this;
+    main = false;
   }
   
   /**
@@ -42,28 +51,32 @@ public class SubFrame extends EtomoFrame {
    * @param currentManager
    * @param mRUList
    */
-  void initialize(String title, BaseManager currentManager, String[] mRUList) {
+  protected void initialize(String title, BaseManager currentManager, String[] mRUList) {
+    super.initialize();
     this.currentManager = currentManager;
-    ImageIcon iconEtomo = new ImageIcon(ClassLoader
-        .getSystemResource("images/etomo.png"));
     setTitle(title);
-    setIconImage(iconEtomo.getImage());
     mainPanel = mainFrame.getMainPanel();
     rootPanel = (JPanel) getContentPane();
     rootPanel.setLayout(new BorderLayout());
     statusBar = new JLabel(mainPanel.getStatusBarText());
     setAxis();
-    menu = EtomoMenu.newEtomoMenu();
-    createMenus();
-    menu.setEnabled(currentManager, true);
-    menu.setMRUFileLabels(mRUList, true);
+    menu.setEnabled(currentManager);
+    menu.setMRUFileLabels(mRUList);
+  }
+  
+  /**Overridden so we can exit when window is closed*/
+  protected void processWindowEvent(WindowEvent event) {
+    super.processWindowEvent(event);
+    if (event.getID() == WindowEvent.WINDOW_CLOSING) {
+      mainFrame.showAxisA();
+    }
   }
   
   /**
    * Set the main panel when switching managers.
    *
    */
-  void setMainPanel(String title, BaseManager currentManager) {
+  protected void setMainPanel(String title, BaseManager currentManager) {
     this.currentManager = currentManager;
     mainPanel = mainFrame.getMainPanel();
     setTitle(title);
@@ -74,7 +87,7 @@ public class SubFrame extends EtomoFrame {
    * Override superclass to call mainFrame for command which require switching
    * axis.
    */
-  void menuOptionsAction(ActionEvent event) {
+  protected void menuOptionsAction(ActionEvent event) {
     String command = event.getActionCommand();
     if (command.equals(menu.getActionCommandAxisA())
      || command.equals(menu.getActionCommandAxisB())
@@ -90,7 +103,7 @@ public class SubFrame extends EtomoFrame {
    * Update the axis when switch from single axis display to dual axis display.
    *
    */
-  void updateAxis() {
+  protected void updateAxis() {
     rootPanel.removeAll();
     setAxis();
   }
@@ -99,7 +112,7 @@ public class SubFrame extends EtomoFrame {
    * Add the axis scroll panel and the status bar, and set the location.
    *
    */
-  private void setAxis() {
+  protected void setAxis() {
     rootPanel.removeAll();
     JScrollPane axis = mainPanel.showBothAxis();
     if (axis != null)
@@ -116,6 +129,10 @@ public class SubFrame extends EtomoFrame {
 }
 /**
 * <p> $Log$
+* <p> Revision 1.3  2005/04/21 20:54:49  sueh
+* <p> bug# 615 Moved menu handling to EtomoFrame so that SubFrame can
+* <p> respond directly to menu commands.  Added title bar.
+* <p>
 * <p> Revision 1.2  2005/04/20 01:53:04  sueh
 * <p> bug# 615 Overrode pack() to prevent the scrollbar from disappearing.
 * <p> Added a menu (EtomoMenu).  Added action functions.
