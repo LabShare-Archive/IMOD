fc291f9c46f57fba2782de935b61df91a05d17c3
SueHeld <sueh@colorado.edu>
2005-05-10 03:00:01 +0000

    bug# 658 An exception thrown by ConstEtomoNumber.validate().  This exception is not thrown on set(); validate() would need to be called separately.

---
+++ Etomo/src/etomo/type/InvalidEtomoNumberException.java
@@ -0,0 +1,24 @@
+package etomo.type;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2005</p>
+*
+*<p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+public class InvalidEtomoNumberException extends Exception {
+  public static  final String  rcsid =  "$Id$";
+  
+  public InvalidEtomoNumberException(String message) {
+    super(message);
+  }
+}
+/**
+* <p> $Log$ </p>
+*/
