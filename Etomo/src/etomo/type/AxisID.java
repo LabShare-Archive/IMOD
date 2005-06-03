7d92a7b7935d0e7fcf04e52e8b1c1d03eb483a5b
SueHeld <sueh@colorado.edu>
2005-06-03 20:56:29 +0000

    bug# 671 Added selfTest() to make sure that AxisID is always set to ONLY when the AxisType is single.  This is an issue for file names, so this test should only be performed when getExtension() is called.  Added getStorageExtension() which does the same thing but doesn't call selfTest().  This function is used to to get the extension for storage, when the "a" is used for the first axis regardless of the axis type.

--- Etomo/src/etomo/type/AxisID.java
+++ Etomo/src/etomo/type/AxisID.java
@@ -1,5 +1,7 @@
 package etomo.type;
 
+import etomo.EtomoDirector;
+
 /**
  * <p>Description: </p>
  *
@@ -13,6 +15,9 @@ package etomo.type;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 3.0  2003/11/07 23:19:01  rickg
+ * <p> Version 1.0.0
+ * <p>
  * <p> Revision 2.0  2003/01/24 20:30:31  rickg
  * <p> Single window merge to main branch
  * <p>
@@ -27,13 +32,16 @@ public class AxisID {
   public static final String rcsid =
     "$Id$";
 
+  private static final String ONLY_AXIS_NAME = "Only";
+  
   private final String name;
+  private final boolean selfTest = EtomoDirector.getInstance().isSelfTest();
 
   private AxisID(String name) {
     this.name = name;
   }
 
-  public static final AxisID ONLY = new AxisID("Only");
+  public static final AxisID ONLY = new AxisID(ONLY_AXIS_NAME);
   public static final AxisID FIRST = new AxisID("First");
   public static final AxisID SECOND = new AxisID("Second");
 
@@ -45,9 +53,19 @@ public class AxisID {
   }
 
   /**
-   * Returns the extension associated with the specific AxisID.
+   * Returns the extension associated with the specific AxisID.  Used for
+   * creating file names.
    */
   public String getExtension() {
+    runSelfTest();
+    return getStorageExtension();
+  }
+  
+  /**
+   * Returns the extension associated with the specific AxisID.  Should not be
+   * used for creating file names.
+   */
+  public String getStorageExtension() {
     if (this == ONLY) {
       return "";
     }
@@ -76,5 +94,30 @@ public class AxisID {
     }
 
     return null;
+  }
+  
+  private void runSelfTest() {
+    if (!selfTest) {
+      return;
+    }
+    selfTest();
+  }
+  
+  public void selfTest() {
+    //state is always getExtension
+    //make sure that the extension is correct, or the file name will be wrong
+    AxisType axisType = null;
+    try {
+      axisType = EtomoDirector.getInstance().getCurrentManager()
+          .getBaseMetaData().getAxisType();
+    }
+    catch (IllegalStateException e) {
+      return;
+    }
+    if (axisType == AxisType.SINGLE_AXIS && !name.equals(ONLY_AXIS_NAME)) {
+      throw new IllegalStateException(
+          "AxisID should be 'Only' when AxisType is single.  AxisID = "
+              + name);
+    }
   }
 }
