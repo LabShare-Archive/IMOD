45ff3a012bbadcf86e0a69d285e1a65444adfc29
SueHeld <sueh@colorado.edu>
2005-06-21 00:44:42 +0000

    bug# 522 findNSections():  simplifying Montagesize.getInstance(). Directory and dataset information can be retrieved when the stack is constructed.

--- Etomo/src/etomo/process/BlendmontProcessMonitor.java
+++ Etomo/src/etomo/process/BlendmontProcessMonitor.java
@@ -86,9 +86,7 @@ public class BlendmontProcessMonitor extends LogFileProcessMonitor {
   protected void findNSections() throws InterruptedException,
       NumberFormatException, IOException {
     Montagesize montagesize = null;
-    montagesize = Montagesize.getInstance(applicationManager
-        .getPropertyUserDir(), applicationManager.getMetaData()
-        .getDatasetName(), axisID);
+    montagesize = Montagesize.getInstance(axisID);
     try {
       montagesize.read();
     }
@@ -101,6 +99,10 @@ public class BlendmontProcessMonitor extends LogFileProcessMonitor {
 }
 /**
 * <p> $Log$
+* <p> Revision 1.7  2005/06/20 16:46:32  sueh
+* <p> bug# 522 Changed  Montagesize so that read() is not called
+* <p> automatically.
+* <p>
 * <p> Revision 1.6  2005/04/12 19:30:59  sueh
 * <p> bug# 630 Handling mrctaper in full alignment.  End when mrctaper ends
 * <p> and put "mrctaper" into the title when it begins.
