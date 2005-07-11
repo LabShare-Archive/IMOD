5ad483b4eaa9b32153a20faee0abc4e696355cc5
SueHeld <sueh@colorado.edu>
2005-07-11 23:08:20 +0000

    bug# 619 Deleted the ParallelDialog class which extends JDialog and was used for showing the parallel panel in a demo because it isn't needed. Added interface ParallelDialog to provide a generic parent for ParallelPanel.  Gives a generic way to call a resume function.

--- Etomo/src/etomo/ui/ParallelDialog.java
+++ Etomo/src/etomo/ui/ParallelDialog.java
@@ -1,10 +1,4 @@
 package etomo.ui;
-
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-
-import etomo.type.AxisID;
-import etomo.type.DialogType;
 /**
 * <p>Description: </p>
 * 
@@ -18,33 +12,11 @@ import etomo.type.DialogType;
 * 
 * @version $Revision$
 */
-final public class ParallelDialog extends JDialog {
+public interface ParallelDialog {
   public static  final String  rcsid =  "$Id$";
   
-  private ParallelPanel parallelPanel = null;
-  private AxisID axisID = null;
-  
-  public ParallelDialog(JFrame frame, AxisID axisID) {
-    super(frame);
-    this.axisID = axisID;
-    parallelPanel = new ParallelPanel(axisID);
-    getRootPane().getContentPane().add(parallelPanel.getRootPanel());
-    setLocation(400, 400);
-  }
-  
-  public void setDialogType(DialogType dialogType) {
-    if (dialogType == DialogType.TOMOGRAM_GENERATION) {
-      setTitle("Parallel Tilt Processing");
-    }
-    else if (dialogType == DialogType.TOMOGRAM_COMBINATION) {
-      setTitle("Parallel Volcombine Processing");
-    }
-    parallelPanel.setDialogType(dialogType);
-  }
+  public void resume();
 }
 /**
-* <p> $Log$
-* <p> Revision 1.1  2005/07/01 21:20:53  sueh
-* <p> bug# 619 JDialog to demo parallel processing
-* <p> </p>
+* <p> $Log$ </p>
 */
