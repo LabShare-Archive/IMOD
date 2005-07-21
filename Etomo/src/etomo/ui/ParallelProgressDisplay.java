d8d86668922b7ff7c7791bbce3226227db67c6a0
SueHeld <sueh@colorado.edu>
2005-07-21 22:21:29 +0000

    bug# 532 Added setup and teardownParallelProgressDisplay() for ParallelProgressDisplay so that the pause button can function like a kill button.

--- Etomo/src/etomo/ui/ParallelProgressDisplay.java
+++ Etomo/src/etomo/ui/ParallelProgressDisplay.java
@@ -12,13 +12,22 @@ package etomo.ui;
 * 
 * @version $Revision$
 * 
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1  2005/07/11 23:13:46  sueh
+* <p> bug# 619 Added an interface that provides a way for a parallel process
+* <p> monitor to communicate with the parallel process panel without knowing
+* <p> anything about the gui.
+* <p> </p>
 */
 public interface ParallelProgressDisplay {
   public static  final String  rcsid =  "$Id$";
   
   public int getCpusSelected();
+  public void setupParallelProgressDisplay();
+  public void teardownParallelProgressDisplay();
+  
+  //TEMP demo functions
+  public void signalStartProgress();
   public void signalRandomRestart();
   public void signalRandomSuccess();
-  public void signalStartProgress();
 }
