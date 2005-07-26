f07a3480978801ae3fa7891804e9e05aa90076fc
SueHeld <sueh@colorado.edu>
2005-07-26 21:46:49 +0000

    bug# 701 Changed notifyKill() to notifyKilled().  Added setProcessendState().

--- Etomo/src/etomo/process/SystemProcessInterface.java
+++ Etomo/src/etomo/process/SystemProcessInterface.java
@@ -1,22 +1,32 @@
-/*
- * Created on May 22, 2003
- *
- * To change this generated comment go to 
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package etomo.process;
 
+import etomo.type.ProcessEndState;
+
 /**
- * @author rickg
- *
- * To change this generated comment go to 
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2002 - 2005</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+
 public interface SystemProcessInterface {
+  public static  final String  rcsid =  "$Id$";
+  
   public String[] getStdOutput();
   public String[] getStdError();
   public boolean isStarted();
   public boolean isDone();
   public String getShellProcessID();
-  public void notifyKill();
+  public void notifyKilled();
+  public void setProcessEndState(ProcessEndState endState);
 }
+/**
+* <p> $Log$ </p>
+*/
