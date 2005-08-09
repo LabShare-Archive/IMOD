6bf836d982e15f5ce6b1b29e17b9d87faece655c
SueHeld <sueh@colorado.edu>
2005-08-09 20:34:10 +0000

    bug# 711  Interface for panel or dialog containing instance(s) of Run3dmodButton.

---
+++ Etomo/src/etomo/ui/Run3dmodButtonContainer.java
@@ -0,0 +1,25 @@
+package etomo.ui;
+
+import etomo.type.Run3dmodMenuOption;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2005</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+interface Run3dmodButtonContainer {
+  public static  final String  rcsid =  "$Id$";
+  
+  public void run3dmod(Run3dmodButton button, Run3dmodMenuOption menuOption);
+}
+/**
+* <p> $Log$ </p>
+*/
