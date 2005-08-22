f681023067d3f9591d5da29a98da12cf1b5dff31
SueHeld <sueh@colorado.edu>
2005-08-22 18:09:55 +0000

    bug# 532 Added setPausedEnabled, to control pause button.  Added msgInterruptingProcess().

--- Etomo/src/etomo/ui/ParallelProgressDisplay.java
+++ Etomo/src/etomo/ui/ParallelProgressDisplay.java
@@ -13,6 +13,10 @@ package etomo.ui;
 * @version $Revision$
 * 
 * <p> $Log$
+* <p> Revision 1.4  2005/08/04 20:14:44  sueh
+* <p> bug# 532  Removed demo functions.  Added functions:  addSuccess,
+* <p> addRestart, and drop.
+* <p>
 * <p> Revision 1.3  2005/08/01 18:11:50  sueh
 * <p> bug# 532 Changed ProcessorTableRow.signalRestart() to addRestart.
 * <p>
@@ -30,10 +34,11 @@ package etomo.ui;
 public interface ParallelProgressDisplay {
   public static  final String  rcsid =  "$Id$";
   
-  public int getCpusSelected();
-  public void setupParallelProgressDisplay();
-  public void teardownParallelProgressDisplay();
+  public int getCPUsSelected();
   public void addSuccess(String computer);
   public void addRestart(String computer);
   public void drop(String computer);
+  public void setPauseEnabled(boolean pauseEnabled);
+  public void msgInterruptingProcess();
+  
 }
