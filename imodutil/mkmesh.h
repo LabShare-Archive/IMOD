ea6446b69f4180361ae41454aa991cd2bd1fbf3c
mastcu <mast@colorado.edu>
2005-09-11 19:21:07 +0000

    Added mina dn max arguments

--- imodutil/mkmesh.h
+++ imodutil/mkmesh.h
@@ -1,31 +1,10 @@
-/*  IMOD VERSION 2.50
- *
+/*
  *  $Id$
  *
  *  Original author: James Kremer
  *  Revised by: David Mastronarde   email: mast@colorado.edu
  */
 
-/*****************************************************************************
- *   Copyright (C) 1995-2001 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 /*  $Author$
 
 $Date$
@@ -33,6 +12,9 @@ $Date$
 $Revision$
 
 $Log$
+Revision 3.2  2004/11/05 19:05:29  mast
+Include local files with quotes, not brackets
+
 Revision 3.1  2003/08/26 03:49:00  mast
 Added flag definition for capping ends of tubes
 
@@ -70,6 +52,8 @@ int SkinObject(Iobj *obj,            /* The object to skin. */
 	       int incz,             /* Increment in Z values */
 	       unsigned int  flags,  /* flags.                    */
 	       int skipPasses,      /* Number of passes for skipped sections */
+           Ipoint triMin,       /* Min and max for triangle output */
+           Ipoint triMax,  
 	       int (*inCB)(int));     /* Callback status function. */
 
 Imesh *imeshReMeshNormal(Imesh *meshes, int *size, Ipoint *scale, int resol);
