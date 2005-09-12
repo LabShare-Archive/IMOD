7af29fe1f407e92f25cfde9ae30145bde29e7d40
SueHeld <sueh@colorado.edu>
2005-09-12 23:58:38 +0000

    bug# 532 Added save() to save a Storable class without overwriting preference entries from other Storable classes.  Added load(Storable) to load a single storable class.

--- Etomo/src/etomo/storage/ParameterStore.java
+++ Etomo/src/etomo/storage/ParameterStore.java
@@ -16,6 +16,9 @@ import java.util.*;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 3.0  2003/11/07 23:19:01  rickg
+ * <p> Version 1.0.0
+ * <p>
  * <p> Revision 2.0  2003/01/24 20:30:31  rickg
  * <p> Single window merge to main branch
  * <p>
@@ -73,7 +76,76 @@ public class ParameterStore {
     //
     outFile.close();
   }
+  
+  /**
+   * Save opens the given parameter file, collects the property key value
+   * pairs from the array of storable objects and stores them to the file.
+   * @param storableArray an array of storable objects that are iterated over
+   * to collect the
+   */
+  public void save(Storable[] storableArray, int numberStorablesExpected) throws IOException {
+    Properties props = null;
+    if (storableArray.length < numberStorablesExpected) {
+      //if not all the storables are being saved, don't overwrite the ones that
+      //are not being saved.
+      //
+      //  Open the parameter file
+      //
+      FileInputStream inFile = new FileInputStream(paramFile);
+      //
+      //  Load the key/value pairs into the properties object
+      //
+      props = new Properties();
+      props.load(inFile);
+      inFile.close();
+    }
+    //
+    //  Open the parameter file
+    //
+    FileOutputStream outFile = new FileOutputStream(paramFile);
 
+    //
+    //  Collect the key/value pairs from the array of storable objects
+    //
+    if (props == null) {
+      props = new Properties();
+    }
+    for (int i = 0; i < storableArray.length; i++) {
+      storableArray[i].store(props);
+    }
+
+    //
+    //  Write out the key/value to the file
+    //
+    props.store(outFile, null);
+
+    //
+    //  Close the output stream
+    //
+    outFile.close();
+  }
+  
+  /**
+   * Load in the stored property key value pairs and send them to the
+   * storable object.
+   */
+  public void load(Storable storable) throws IOException {
+    //
+    //  Open the parameter file
+    //
+    FileInputStream inFile = new FileInputStream(paramFile);
+    //
+    //  Load the key/value pairs into the properties object
+    //
+    Properties props = new Properties();
+    props.load(inFile);
+    inFile.close();
+    //
+    //  Send the key/value pairs to the storable object
+    //
+    storable.load(props);
+  }
+  
   /**
    * Load in the stored property key value pairs and send them to the array of
    * storable objects.
