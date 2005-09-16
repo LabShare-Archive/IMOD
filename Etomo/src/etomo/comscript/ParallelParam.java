26d279372715adb3e98907eae86d59c09b796265
SueHeld <sueh@colorado.edu>
2005-09-16 17:19:06 +0000

    bug# 532 A generic interface for ProcesschunksParam.  Allows ApplicationManager to have only one function which sets parameters in processchunks.  This is done by using ParallelDialog to represent the dialog.

---
+++ Etomo/src/etomo/comscript/ParallelParam.java
@@ -0,0 +1,24 @@
+package etomo.comscript;
+/**
+* <p>Description: Generic interface for params used by classes which implement
+* ParallelDialog.
+* 
+* Implemented by:
+* ProcesschunksParam</p>
+* 
+* <p>Copyright: Copyright (c) 2005</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+public interface ParallelParam {
+  public static  final String  rcsid =  "$Id$";
+}
+/**
+* <p> $Log$ </p>
+*/
