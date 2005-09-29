52810a30f7d9be00f0b5ddda921bf724fc8e1a63
SueHeld <sueh@colorado.edu>
2005-09-29 18:44:44 +0000

    bug# 532 Improved the handling of the different axis'.  Setting the axisID according to the axisType; so a single axis tomogram uses axis.ONLY and a dual axis used axis.FIRST.  Added member variables AxisID and AxisType.  Since axis.only and axis.first have different meanings, axisID can be used to decide if combine fields should be saved.  AxisType is used in selfTestInvarients() to make sure that the AxisID was set correctly.

--- Etomo/src/etomo/type/BaseScreenState.java
+++ Etomo/src/etomo/type/BaseScreenState.java
@@ -2,7 +2,9 @@ package etomo.type;
 
 import java.util.Properties;
 
-import etomo.storage.Storable;;
+import etomo.storage.Storable;
+import etomo.util.Utilities;
+;
 /**
 * <p>Description: AxisID level storable object for the .edf and .ejf files.
 * Should be used to store non-metadata.  Anything that is needed to run a
@@ -23,15 +25,47 @@ public class BaseScreenState implements Storable {
   
   public static final String PARALLEL_HEADER_GROUP = "Parallel.Header";
   
-  
-  private final String group;
   private final PanelHeaderState parallelHeaderState = new PanelHeaderState(PARALLEL_HEADER_GROUP);
   
-  public BaseScreenState(AxisID axisID) {
-    if (axisID == AxisID.ONLY) {
+  protected final AxisID axisID;
+  private final AxisType axisType;
+  private final String group;
+
+  public BaseScreenState(AxisID axisID, AxisType axisType) {
+    this.axisID = axisID;
+    this.axisType = axisType;
+    if (axisID == AxisID.ONLY && axisType == AxisType.DUAL_AXIS) {
       axisID = AxisID.FIRST;
     }
+    else if (axisID == AxisID.FIRST && axisType == AxisType.SINGLE_AXIS) {
+      axisID = AxisID.ONLY;
+    }
     group =  "ScreenState" + axisID.getExtension().toUpperCase();
+    selfTestInvariants();
+  }
+  
+  void selfTestInvariants() {
+    if (!Utilities.isSelfTest()) {
+      return;
+    }
+    if (axisType == AxisType.NOT_SET) {
+      throw new IllegalStateException("AxisType must be set.");
+    }
+    if (axisType == AxisType.SINGLE_AXIS && axisID == AxisID.SECOND) {
+      throw new IllegalStateException("AxisID cannot be B in a single axis dataset.");
+    }
+    if (axisType == AxisType.SINGLE_AXIS && axisID != AxisID.ONLY) {
+      throw new IllegalStateException("AxisID must be Only in a single axis dataset.");
+    }
+    if (axisType == AxisType.DUAL_AXIS && axisID == AxisID.ONLY) {
+      throw new IllegalStateException("AxisID must be A or B in a dual axis dataset.");
+    }
+    if (axisID == AxisID.FIRST && !group.endsWith("A")) {
+      throw new IllegalStateException("Group must end with A for the first axis.");
+    }
+    if (axisID == AxisID.SECOND && !group.endsWith("B")) {
+      throw new IllegalStateException("Group must end with B for the second axis.");
+    }
   }
   
   protected final String getPrepend(String prepend) {
@@ -67,6 +101,10 @@ public class BaseScreenState implements Storable {
 }
 /**
 * <p> $Log$
+* <p> Revision 1.2  2005/09/27 23:12:40  sueh
+* <p> bug# 532 Separating the panel name (Parallel) from the element name
+* <p> (Header) in the .edf file.
+* <p>
 * <p> Revision 1.1  2005/09/27 21:19:02  sueh
 * <p> bug# 532 This is a top level Storable object that should be written to the
 * <p> .edf and .ejf files.  One instance per axis is created.
