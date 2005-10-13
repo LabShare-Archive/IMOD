321d6390b9cde8bbaedde900c6c782fa7653811a
SueHeld <sueh@colorado.edu>
2005-10-13 22:36:24 +0000

    Bug# 532 In synchronized(), always copying all fields

--- Etomo/src/etomo/ui/InitialCombinePanel.java
+++ Etomo/src/etomo/ui/InitialCombinePanel.java
@@ -30,6 +30,14 @@ import etomo.type.Run3dmodMenuOptions;
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 3.20  2005/09/29 19:09:56  sueh
+ * <p> bug# 532 Add panel headers to all of the sections in Combine.  Hide the
+ * <p> sections in the tabs that are not visible so that the visible tab can become
+ * <p> small.  Added an expand() function to each tab to handle the
+ * <p> expand/contract requests of the panel header buttons.  Added set and get
+ * <p> parameters for ReconScreenState to set and get the state of the panel
+ * <p> headers.
+ * <p>
  * <p> Revision 3.19  2005/08/11 23:52:30  sueh
  * <p> bug# 711  Change enum Run3dmodMenuOption to
  * <p> Run3dmodMenuOptions, which can turn on multiple options at once.
@@ -317,7 +325,7 @@ public class InitialCombinePanel implements ContextMenu, InitialCombineFields, R
   private void buttonAction(ActionEvent event) {
     //  Synchronize this panel with the others
     tomogramCombinationDialog.synchronize(TomogramCombinationDialog.lblInitial,
-      true, TomogramCombinationDialog.ALL_FIELDS);
+      true);
 
     String command = event.getActionCommand();
 
--- Etomo/src/etomo/ui/SolvematchPanel.java
+++ Etomo/src/etomo/ui/SolvematchPanel.java
@@ -39,6 +39,14 @@ import etomo.type.Run3dmodMenuOptions;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 3.13  2005/09/29 19:11:02  sueh
+ * <p> bug# 532 Add panel headers to all of the sections in Combine.  Hide the
+ * <p> sections in the tabs that are not visible so that the visible tab can become
+ * <p> small.  Added an expand() function to each tab to handle the
+ * <p> expand/contract requests of the panel header buttons.  Added set and get
+ * <p> parameters for ReconScreenState to set and get the state of the panel
+ * <p> headers.
+ * <p>
  * <p> Revision 3.12  2005/08/27 22:42:22  sueh
  * <p> bug# 532 Changed Autodoc.get() to getInstance().
  * <p>
@@ -384,8 +392,7 @@ public class SolvematchPanel implements InitialCombineFields, Run3dmodButtonCont
   //  Action functions for setup panel buttons
   private void buttonAction(ActionEvent event) {
     //  Synchronize this panel with the others
-    tomogramCombinationDialog.synchronize(parentTitle, true,
-      TomogramCombinationDialog.ALL_FIELDS);
+    tomogramCombinationDialog.synchronize(parentTitle, true);
 
     String command = event.getActionCommand();
     if (command.equals(cbBinBy2.getActionCommand())) {
