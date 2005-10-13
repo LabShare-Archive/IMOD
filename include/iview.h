f792b4efa4106f79d32c1dc7dfef6396f0e459a2
mastcu <mast@colorado.edu>
2005-10-13 20:02:01 +0000

    Added an arg to a declaration

--- include/iview.h
+++ include/iview.h
@@ -1,31 +1,14 @@
-/*  IMOD VERSION 2.10
- *
+/*
  *  iview.h -- Image model view header.
  *
  *  Original Author: James Kremer
  *  Revised by: David Mastronarde   email: mast@colorado.edu
+ *
+ *  Copyright (C) 1995-2005 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
 
-/*****************************************************************************
- *   Copyright (C) 1995-1998 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 /*  $Author$
 
     $Date$
@@ -33,6 +16,9 @@
     $Revision$
 
     $Log$
+    Revision 3.5  2004/11/05 18:52:53  mast
+    Include local files with quotes, not brackets
+
     Revision 3.4  2004/09/28 15:12:03  mast
     Move clipping plane functions to iplane
 
@@ -59,11 +45,11 @@ extern "C" {
   Iview *imodViewNew(int size);
   void   imodViewDelete(Iview *vw);
   void   imodViewDefault(Iview *vw);
-  void imodViewDefaultScale(Imod *imod, Iview *vw, Ipoint *imageMax);
+  void imodViewDefaultScale(Imod *imod, Iview *vw, Ipoint *imageMax, 
+                            float binScale);
   void imodViewModelDefault(Imod *imod, Iview *vw, Ipoint *imageMax);
-  int imodViewWrite(Iview *vw, FILE *fout);
-  int imodViewWrite(Iview *vw, FILE *fout);
-  int imodViewModelWrite(Imod *imod);
+  int imodViewWrite(Iview *vw, FILE *fout, Ipoint *scale);
+  int imodViewModelWrite(Imod *imod, Ipoint *scale);
   int imodViewModelRead(Imod *imod);
   int imodViewClipRead(Imod *imod);
   int imodViewModelNew(Imod *imod);
