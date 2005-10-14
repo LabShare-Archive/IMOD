b65c50d05ec508f7a3a98226adcfe4e1b01c6f7e
mastcu <mast@colorado.edu>
2005-10-14 22:00:13 +0000

    Changes for reload and some cleanup

--- imod/imod_io.h
+++ imod/imod_io.h
@@ -1,31 +1,9 @@
-/*  IMOD VERSION 2.10
- *
+/*
  *  $Id$
  *
  *  Original Author: James Kremer
  *  Revised by: David Mastronarde   email: mast@colorado.edu
  */
-
-/*****************************************************************************
- *   Copyright (C) 1995-1998 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 /*  $Author$
 
     $Date$
@@ -33,6 +11,9 @@
     $Revision$
 
     $Log$
+    Revision 4.6  2004/06/04 03:15:43  mast
+    Added argument to loadModel to keep black/white level
+
     Revision 4.5  2004/01/05 17:57:30  mast
     Changed imod_io_image_load to just take vi as argument
 
@@ -83,17 +64,15 @@
 /* Functions */
 int imodIOGetError(void);
 char *imodIOGetErrorString(void);
+void setImod_filename(const char *name);
 int createNewModel(char *modelFilename);
-int openModel(char *modelFilename, bool keepBW);
+int openModel(char *modelFilename, bool keepBW, bool saveAs);
 Imod *LoadModel(FILE *mfin);
 int SaveModel(struct Mod_Model *mod);
 int SaveasModel(struct Mod_Model *mod);
-int SaveModelQuit(Imod *mod);
 int imod_model_changed(Imod *imodel);
 void imod_cleanup_autosave(void);
 int imod_autosave(struct Mod_Model *mod);
-int SaveImage(struct ViewInfo *vi);
-int WriteImage(FILE *fout, struct ViewInfo *vi, struct LoadInfo *li);
 unsigned char **imod_io_image_load(struct ViewInfo *vi);
 
 #endif /* IMOD_IO_H */
