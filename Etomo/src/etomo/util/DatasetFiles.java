db74d6fe6982e2e0dc6786c82d1977fb730e61bd
SueHeld <sueh@colorado.edu>
2005-10-27 00:37:37 +0000

    bug# 725 Added functions:  private correctAxisID, getMagGradientName, getPieceListFile, getPieceListFileName, getRawTilt, getRawTiltName, getStack, getStackName.

--- Etomo/src/etomo/util/DatasetFiles.java
+++ Etomo/src/etomo/util/DatasetFiles.java
@@ -3,7 +3,6 @@ package etomo.util;
 import java.io.File;
 
 import etomo.BaseManager;
-import etomo.EtomoDirector;
 import etomo.type.AxisID;
 import etomo.type.AxisType;
 import etomo.type.BaseMetaData;
@@ -24,32 +23,61 @@ import etomo.type.BaseMetaData;
 public final class DatasetFiles {
   public static final String rcsid = "$Id$";
 
+  private static final String TOMO_EXT = ".rec";
+  private static final String STACK_EXT = ".st";
+  
+  //Stacks
+  
+  public final static File getOriginalStack(BaseManager manager, AxisID axisID) {
+    BaseMetaData metaData = manager.getBaseMetaData();
+    axisID = correctAxisID(metaData, axisID);
+    return new File(manager.getPropertyUserDir(), metaData.getName()
+        + axisID.getExtension() + "_orig" + STACK_EXT);
+  }
+  
+  public final static File getStack(BaseManager manager, AxisID axisID) {
+    return getStack(manager.getPropertyUserDir(), manager.getBaseMetaData(), axisID);
+  }
+  
+  public final static File getStack(String propertyUserDir,
+      BaseMetaData metaData, AxisID axisID) {
+    return new File(propertyUserDir, getStackName(metaData, axisID));
+  }
+  
+  public final static String getStackName(BaseManager manager, AxisID axisID) {
+    return getStackName(manager.getBaseMetaData(), axisID);
+  }
+  
+  private final static String getStackName(BaseMetaData metaData, AxisID axisID) {
+    axisID = correctAxisID(metaData, axisID);
+    return metaData.getName() + axisID.getExtension() + STACK_EXT;
+  }
+
+  //Tomograms
+  
   public final static File getTomogram(BaseManager manager, AxisID axisID) {
+    BaseMetaData metaData = manager.getBaseMetaData();
+    axisID = correctAxisID(metaData, axisID);
     if (axisID == AxisID.ONLY) {
-      return Utilities.getFile(manager, axisID, "_full.rec");
+      return new File(manager.getPropertyUserDir(), metaData.getName() + "_full" + TOMO_EXT);
     }
-    return Utilities.getFile(manager, axisID, ".rec");
+    return new File(manager.getPropertyUserDir(), metaData.getName() + axisID.getExtension() + TOMO_EXT);
   }
 
   public final static File getCombinedTomogram(BaseManager manager) {
     if (manager.getBaseMetaData().getAxisType() != AxisType.DUAL_AXIS) {
       return null;
     }
-    return Utilities.getFile(manager.getPropertyUserDir(), "sum.rec");
-  }
-
-  public final static File getTrimmedTomogram(BaseManager manager) {
-    EtomoDirector director = EtomoDirector.getInstance();
-    File file = new File(manager.getPropertyUserDir(), manager.getBaseMetaData().getName() + ".rec");
-    return file;
+    return new File(manager.getPropertyUserDir(), "sum" + TOMO_EXT);
   }
   
   public final static File getTrimmedTomogram(BaseManager manager, AxisID axisID) {
-    if (manager.getBaseMetaData().getAxisType() != AxisType.DUAL_AXIS) {
-      return null;
-    }
-    return Utilities.getFile(manager.getPropertyUserDir(), ".rec");
+    BaseMetaData metaData = manager.getBaseMetaData();
+    axisID = correctAxisID(metaData, axisID);
+    return new File(manager.getPropertyUserDir(), metaData.getName() + axisID.getExtension() + TOMO_EXT);
   }
+  
+  //Other files
   
   public final static File getCommandsFile(BaseManager manager, String rootName) {
     return new File(manager.getPropertyUserDir(), getCommandsFileName(rootName));
@@ -58,25 +86,60 @@ public final class DatasetFiles {
   public final static String getCommandsFileName(String rootName) {
     return rootName + ".cmds";
   }
+    
+  public final static File getRawTilt(BaseManager manager, AxisID axisID) {
+    return new File(manager.getPropertyUserDir(), getRawTiltName(manager, axisID));
+  }
   
-  public final static File getOriginalStack(BaseManager manager, AxisID axisID) {
+  public final static String getRawTiltName(BaseManager manager, AxisID axisID) {
     BaseMetaData metaData = manager.getBaseMetaData();
+    axisID = correctAxisID(metaData, axisID);
+    return metaData.getName() + axisID.getExtension() + ".rawtlt";
+  }
+  
+  public final static File getPieceListFile(BaseManager manager, AxisID axisID) {
+    return new File(manager.getPropertyUserDir(), getPieceListFileName(manager, axisID));
+  }
+  
+  public final static String getPieceListFileName(BaseManager manager, AxisID axisID) {
+    BaseMetaData metaData = manager.getBaseMetaData();
+    axisID = correctAxisID(metaData, axisID);
+    return metaData.getName() + axisID.getExtension() + ".pl";
+  }
+  
+  public final static File getMagGradient(BaseManager manager, AxisID axisID) {
+    return new File(manager.getPropertyUserDir(), getMagGradientName(manager, axisID));
+  }
+  
+  public final static String getMagGradientName(BaseManager manager, AxisID axisID) {
+    BaseMetaData metaData = manager.getBaseMetaData();
+    axisID = correctAxisID(metaData, axisID);
+    return metaData.getName() + axisID.getExtension() + ".maggrad";
+  }
+  
+  //private
+  
+  private final static AxisID correctAxisID(BaseMetaData metaData, AxisID axisID) {
     AxisType axisType = metaData.getAxisType();
     if (axisType == AxisType.DUAL_AXIS && axisID == AxisID.ONLY) {
-      axisID = AxisID.FIRST;
+      return AxisID.FIRST;
     }
     else if (axisType == AxisType.SINGLE_AXIS && axisID == AxisID.FIRST) {
-      axisID = AxisID.ONLY;
+      return AxisID.ONLY;
     }
-    else if (axisType == AxisType.NOT_SET) {
-      return null;
+    else if (axisType == null || axisType == AxisType.NOT_SET) {
+      throw new IllegalStateException(
+          "AxisType is not set.  AxisType must be set before getting a dataset file name containing the axisID extension.");
     }
-    return new File(manager.getPropertyUserDir(), metaData.getName()
-        + axisID.getExtension() + "_orig.st");
+    return axisID;
   }
 }
 /**
  * <p> $Log$
+ * <p> Revision 1.4  2005/10/19 00:21:18  sueh
+ * <p> bug# 673 Added getOriginalStack() to return the File object for the
+ * <p> original stack (_orig.st).
+ * <p>
  * <p> Revision 1.3  2005/09/07 20:53:26  sueh
  * <p> bug# 532 Added functions to get a commands file (.cmds).
  * <p>
