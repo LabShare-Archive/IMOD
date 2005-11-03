b4888c47227214f9f0a7f91614b1a152f180b1b7
SueHeld <sueh@colorado.edu>
2005-11-03 00:51:23 +0000

    bug# 740 In calcFileSize():  Getting file to open from Blendmont when montage is true.  Taking starting and ending Z into account.

--- Etomo/src/etomo/process/MtffilterProcessMonitor.java
+++ Etomo/src/etomo/process/MtffilterProcessMonitor.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.IOException;
 
 import etomo.ApplicationManager;
+import etomo.comscript.BlendmontParam;
 import etomo.comscript.ComScriptManager;
 import etomo.comscript.MTFFilterParam;
 import etomo.comscript.NewstParam;
 import etomo.type.AxisID;
+import etomo.type.ViewType;
 import etomo.util.InvalidParameterException;
 import etomo.util.MRCHeader;
 /**
@@ -24,6 +26,11 @@ import etomo.util.MRCHeader;
 * @version $Revision$
 * 
 * <p> $Log$
+* <p> Revision 1.4  2005/07/29 00:52:06  sueh
+* <p> bug# 709 Going to EtomoDirector to get the current manager is unreliable
+* <p> because the current manager changes when the user changes the tab.
+* <p> Passing the manager where its needed.
+* <p>
 * <p> Revision 1.3  2005/06/20 16:46:56  sueh
 * <p> bug# 522 Made MRCHeader an n'ton.  Getting instance instead of
 * <p> constructing in calcFileSize().
@@ -56,19 +63,30 @@ public class MtffilterProcessMonitor extends FileSizeProcessMonitor {
     // Get the depth, mode, any mods to the X and Y size from the tilt 
     // command script and the input and output filenames. 
     ComScriptManager comScriptManager = applicationManager.getComScriptManager();
-    comScriptManager.loadNewst(axisID);
-    NewstParam newstParam = comScriptManager.getNewstComNewstParam(axisID);
+    String outputFilename;
+    if (applicationManager.getMetaData().getViewType() == ViewType.MONTAGE) {
+      comScriptManager.loadBlend(axisID);
+      BlendmontParam blendmontParam = comScriptManager.getBlendParam(axisID);
 
-    // Get the header from the raw stack to calculate the aligned stack stize
-    String newstOutputFilename = applicationManager.getPropertyUserDir() + "/"
-      + newstParam.getOutputFile();
-    MRCHeader newstOutputHeader = MRCHeader.getInstance(applicationManager
-        .getPropertyUserDir(), newstOutputFilename, axisID);
-    newstOutputHeader.read();
-    nX = newstOutputHeader.getNRows();
-    nY = newstOutputHeader.getNColumns();
-    nZ = newstOutputHeader.getNSections();
-    switch (newstOutputHeader.getMode()) {
+      // Get the header from the raw stack to calculate the aligned stack stize
+      outputFilename = applicationManager.getPropertyUserDir() + "/"
+          + blendmontParam.getImageOutputFile();
+    }
+    else {
+      comScriptManager.loadNewst(axisID);
+      NewstParam newstParam = comScriptManager.getNewstComNewstParam(axisID);
+
+      // Get the header from the raw stack to calculate the aligned stack stize
+      outputFilename = applicationManager.getPropertyUserDir() + "/"
+          + newstParam.getOutputFile();
+    }
+    MRCHeader outputHeader = MRCHeader.getInstance(applicationManager
+        .getPropertyUserDir(), outputFilename, axisID);
+    outputHeader.read();
+    nX = outputHeader.getNRows();
+    nY = outputHeader.getNColumns();
+    nZ = outputHeader.getNSections();
+    switch (outputHeader.getMode()) {
       case 0 :
         modeBytes = 1;
         break;
@@ -89,6 +107,20 @@ public class MtffilterProcessMonitor extends FileSizeProcessMonitor {
         break;
       default :
         throw new InvalidParameterException("Unknown mode parameter");
+    }
+    //take starting and ending Z into account
+    comScriptManager.loadMTFFilter(axisID);
+    MTFFilterParam mtfFilterParam = comScriptManager.getMTFFilterParam(axisID);
+    if (mtfFilterParam.isStartingZSet()) {
+      if (mtfFilterParam.isEndingZSet()) {
+        nZ = mtfFilterParam.getEndingZ() - mtfFilterParam.getStartingZ() + 1;
+      }
+      else {
+        nZ = nZ - mtfFilterParam.getStartingZ() + 1;
+      }
+    }
+    else if (mtfFilterParam.isEndingZSet()) {
+      nZ = mtfFilterParam.getEndingZ();
     }
 
     // Assumption: newst will write the output file with the same mode as the
