6c8a8d08cae81417f84599166bbc729e57c7fec5
SueHeld <sueh@colorado.edu>
2005-11-14 21:23:33 +0000

    bug# 744 An interface for BackgroundSystemProcess.

---
+++ Etomo/src/etomo/process/BackgroundProcessInterface.java
@@ -0,0 +1,20 @@
+package etomo.process;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2005</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+interface BackgroundProcessInterface extends SystemProcessInterface {
+  public static  final String  rcsid =  "$Id$";
+}
+/**
+* <p> $Log$ </p>
+*/
