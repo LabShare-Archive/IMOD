0907f0e5211ec87df84b177be4a06578de0de3a0
SueHeld <sueh@colorado.edu>
2005-11-14 21:25:43 +0000

    bug# 744 An interface for monitors used with DetachedProcess.

---
+++ Etomo/src/etomo/process/DetachedProcessMonitor.java
@@ -0,0 +1,21 @@
+package etomo.process;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2005</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+
+interface DetachedProcessMonitor extends ProcessMonitor {
+  public static  final String  rcsid =  "$Id$";
+}
+/**
+* <p> $Log$ </p>
+*/
