7567485b639a87b0bd655e657a11502cb4aa5712
SueHeld <sueh@colorado.edu>
2005-11-15 01:29:48 +0000

    bug# 733 fixing IMODBuild

--- Etomo/src/etomo/util/RemotePathTest.java
+++ Etomo/src/etomo/util/RemotePathTest.java
@@ -678,14 +678,14 @@ public class RemotePathTest extends TestCase {
    * getRemotePath can search for section with stripped hostname
    * getRemotePath returns remote path when local path is found
    * getRemotePath returns section name as mount name
-   *//*
+   */
   public final void test_getRemotePath_sectionRules() throws IOException {
     System.out.println();
     writeNewFile("test_getRemotePath_sectionRules", false, true, true, true,
         false, false);
     assertPathsFound();
     assertMountNameFound(strippedHostName);
-  }*/
+  }
 
   /**
    * Tests getRemotePath().
@@ -827,6 +827,9 @@ public class RemotePathTest extends TestCase {
 }
 /**
  * <p> $Log$
+ * <p> Revision 1.11  2005/11/15 01:26:12  sueh
+ * <p> bug# 733 fixing IMODBuild
+ * <p>
  * <p> Revision 1.10  2005/11/15 01:20:36  sueh
  * <p> bug# 733 fixing IMODBuild
  * <p>
