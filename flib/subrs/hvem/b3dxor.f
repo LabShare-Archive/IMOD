f6c33b44098318b49e95d5a812894f4b3f70f705
mastcu <mast@colorado.edu>
2005-12-08 15:56:46 +0000

    Addition for use of gfortran

---
+++ flib/subrs/hvem/b3dxor.f
@@ -0,0 +1,15 @@
+c	  B3DXOR returns the exclusive or of its two arguments
+c
+c	  $Author$
+c
+c	  $Date$
+c
+c	  $Revision$
+c
+c	  $Log$
+c
+	logical function b3dxor(a, b)
+	logical a, b
+	b3dxor = (a .and. .not.b) .or. (b .and. .not.a)
+	return
+	end
