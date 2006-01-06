36e539f316c35f1d766609ec1096209927d0e9ec
SueHeld <sueh@colorado.edu>
2006-01-06 22:18:06 +0000

    bug# 794 Fixed findNSections().  It was sleeping each time it read a line. Now it sleeps when there are no lines to read.

--- Etomo/src/etomo/process/MatchorwarpProcessMonitor.java
+++ Etomo/src/etomo/process/MatchorwarpProcessMonitor.java
@@ -18,7 +18,10 @@ import etomo.type.AxisID;
 * 
 * @version $Revision$
 * 
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1  2005/01/26 23:41:40  sueh
+* <p> bug# 83 Matchorwarp process monitor.
+* <p> </p>
 */
 public class MatchorwarpProcessMonitor extends LogFileProcessMonitor {
   public static  final String  rcsid =  "$Id$";
@@ -76,9 +79,12 @@ public class MatchorwarpProcessMonitor extends LogFileProcessMonitor {
     //  Search for the number of sections, we should see a header ouput first
     boolean foundNSections = false;
     nSections = -1;
+    Thread.sleep(updatePeriod);
     while (!foundNSections) {
-      Thread.sleep(updatePeriod);
       String line = logFileReader.readLine();
+      if (line == null) {
+        Thread.sleep(updatePeriod);
+      }
       if (line != null && line.trim().startsWith("Finished")) {
         line = line.trim();
         String[] strings = line.split("\\s+");
@@ -86,6 +92,7 @@ public class MatchorwarpProcessMonitor extends LogFileProcessMonitor {
         foundNSections = true;
       }
     }
+    Thread.sleep(updatePeriod);
   }
 
 
