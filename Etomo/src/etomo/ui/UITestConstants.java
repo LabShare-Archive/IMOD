5b1ae093c5a313f07f17dde2286637546eeac3ce
SueHeld <sueh@colorado.edu>
2006-01-11 23:19:08 +0000

    bug# 675 Added TOGGLE_BUTTON_ATTRIB

--- Etomo/src/etomo/ui/UITestConstants.java
+++ Etomo/src/etomo/ui/UITestConstants.java
@@ -15,14 +15,20 @@ package etomo.ui;
 final class UITestConstants {
   public static  final String  rcsid =  "$Id$";
   
-  static final String TEST_SECTION_TYPE = "Test";
+  static final String DIALOG_SECTION_TYPE = "Dialog";
   static final String RADIO_BUTTON_ATTRIB = "rb";
   static final String CHECK_BOX_ATTRIB = "cb";
   static final String TEXT_FIELD_ATTRIB = "tf";
   static final String SPINNER_ATTRIB = "sp";
   static final String BUTTON_ATTRIB = "bn";
+  static final String TOGGLE_BUTTON_ATTRIB = "tb";
   static final String POPUP_ATTRIB = "popup";
 }
 /**
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1  2006/01/04 20:28:21  sueh
+* <p> bug# 675 Moved constants that must be shared by non-test objects to an
+* <p> object which doesn't know about junit.  Overwise junit would have to be in
+* <p> the path for compiling and running EtomoDirector.
+* <p> </p>
 */
