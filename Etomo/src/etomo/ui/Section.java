a35a331590935cfe3417645cabbde2c21251aef1
SueHeld <sueh@colorado.edu>
2006-01-11 22:38:40 +0000

    bug# 675 Implementing WriteOnlyAttributeMap and WriteOnlyNameValuePairList

--- Etomo/src/etomo/ui/Section.java
+++ Etomo/src/etomo/ui/Section.java
@@ -1,57 +1,70 @@
 package etomo.ui;
 
-/**
-* <p>Description:</p>
-*
-* <p>Copyright: Copyright © 2002, 2003</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
-* University of Colorado</p>
-*
-* @author $$Author$$
-*
-* @version $$Revision$$
-*
-* <p> $$Log$
-* <p> $Revision 1.5  2005/12/23 02:20:08  sueh
-* <p> $bug# 675 Renamed getFirstSectionLocation to getSectionLocation.
-* <p> $Removed getSection(sectionLocation).  Changed nextSection so it gets the
-* <p> $current section and increments.  Encapsulated the attribute list into
-* <p> $AttributeList.  Added getAttributeLocation and nextAttribute.
-* <p> $
-* <p> $Revision 1.4  2005/12/01 00:25:16  sueh
-* <p> $bug# 775 Made getName() public.
-* <p> $
-* <p> $Revision 1.3  2005/05/17 19:40:17  sueh
-* <p> $bug# 372 Reducing the visibility of functions.
-* <p> $
-* <p> $Revision 1.2  2004/01/01 00:46:28  sueh
-* <p> $bug# 372 returning null instead of empty section when
-* <p> $section is not found
-* <p> $
-* <p> $Revision 1.1  2003/12/31 01:30:21  sueh
-* <p> $bug# storage for autodoc sections
-* <p> $$ </p>
-*/
+import java.util.Vector;
 
-public class Section implements AttributeCollection {
+/**
+ * <p>Description:</p>
+ *
+ * <p>Copyright: Copyright © 2002, 2003</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+ * University of Colorado</p>
+ *
+ * @author $$Author$$
+ *
+ * @version $$Revision$$
+ *
+ * <p> $$Log$
+ * <p> $Revision 1.6  2006/01/03 23:45:32  sueh
+ * <p> $bug# 675 Added getAttributeLocation(String).
+ * <p> $
+ * <p> $Revision 1.5  2005/12/23 02:20:08  sueh
+ * <p> $bug# 675 Renamed getFirstSectionLocation to getSectionLocation.
+ * <p> $Removed getSection(sectionLocation).  Changed nextSection so it gets the
+ * <p> $current section and increments.  Encapsulated the attribute list into
+ * <p> $AttributeList.  Added getAttributeLocation and nextAttribute.
+ * <p> $
+ * <p> $Revision 1.4  2005/12/01 00:25:16  sueh
+ * <p> $bug# 775 Made getName() public.
+ * <p> $
+ * <p> $Revision 1.3  2005/05/17 19:40:17  sueh
+ * <p> $bug# 372 Reducing the visibility of functions.
+ * <p> $
+ * <p> $Revision 1.2  2004/01/01 00:46:28  sueh
+ * <p> $bug# 372 returning null instead of empty section when
+ * <p> $section is not found
+ * <p> $
+ * <p> $Revision 1.1  2003/12/31 01:30:21  sueh
+ * <p> $bug# storage for autodoc sections
+ * <p> $$ </p>
+ */
+
+public class Section implements WriteOnlyAttributeMap,
+    WriteOnlyNameValuePairList {
   public static final String rcsid = "$$Id$$";
-  
+
   private String key = null; //required
   private Token type = null; //required
   private Token name = null; //required
-  private AttributeList attributeList = null;//optional
-  
+  private AttributeMap attributeMap = null;//optional
+  private Vector nameValuePairList = null;
+
   public String toString() {
     return getClass().getName() + "[" + paramString() + "]";
   }
 
   protected String paramString() {
-    return "key=" + key + ",type=" + type + ",name="
-        + name + ",\nattributeList=" + attributeList + ",\n" + super.toString();
+    return "key=" + key + ",type=" + type + ",name=" + name
+        + ",\nattributeMap=" + attributeMap + ",\n" + super.toString();
   }
-  
+
+  public String getString() {
+    return AutodocTokenizer.OPEN_CHAR + type.getValues() + ' '
+        + AutodocTokenizer.DEFAULT_DELIMITER + ' ' + name.getValues()
+        + AutodocTokenizer.CLOSE_CHAR;
+  }
+
   final static String getKey(Token type, Token name) {
     if (type == null && name == null) {
       return null;
@@ -64,7 +77,7 @@ public class Section implements AttributeCollection {
     }
     return type.getKey() + name.getKey();
   }
-  
+
   final static String getKey(String type, String name) {
     if (type == null && name == null) {
       return null;
@@ -77,20 +90,20 @@ public class Section implements AttributeCollection {
     }
     return Token.convertToKey(type) + Token.convertToKey(name);
   }
-  
+
   Section(Token type, Token name) {
     key = Section.getKey(type, name);
     this.type = type;
     this.name = name;
   }
-  
-  public AttributeCollection addAttribute(Token name) {
-    if (attributeList == null) {
-      attributeList = new AttributeList();
+
+  public WriteOnlyAttributeMap addAttribute(Token name) {
+    if (attributeMap == null) {
+      attributeMap = new AttributeMap(this, this);
     }
-    return attributeList.addAttribute(name);
+    return attributeMap.addAttribute(name);
   }
-  
+
   boolean equalsType(String type) {
     if (type == null) {
       return false;
@@ -101,56 +114,65 @@ public class Section implements AttributeCollection {
   String getKey() {
     return key;
   }
-  
+
   public final String getName() {
     if (name == null) {
       throw new IllegalStateException("name is required");
     }
-    return name.getValue(true);
+    return name.getValues();
   }
-  
+
   public int hashCode() {
     return key.hashCode();
   }
-  
+
   public Attribute getAttribute(String name) {
-    if (attributeList == null) {
+    if (attributeMap == null) {
       return null;
     }
-    return attributeList.getAttribute(name);
+    return attributeMap.getAttribute(name);
   }
-  
-  public final AttributeLocation getAttributeLocation() {
-    if (attributeList == null) {
-      return null;
+
+  public final void addNameValuePair(Attribute attrib, int valueIndex) {
+    if (attrib == null) {
+      return;
     }
-    return attributeList.getAttributeLocation();
+    NameValuePair pair = new NameValuePair(attrib, attrib
+        .getValueToken(valueIndex));
+    if (nameValuePairList == null) {
+      nameValuePairList = new Vector();
+    }
+    nameValuePairList.add(pair);
   }
-  
-  public final AttributeLocation getAttributeLocation(String name) {
-    if (attributeList == null) {
+
+  final NameValuePairLocation getNameValuePairLocation() {
+    if (nameValuePairList == null) {
       return null;
     }
-    return attributeList.getAttributeLocation(name);
+    return new NameValuePairLocation();
   }
-  
-  public final Attribute nextAttribute(AttributeLocation location) {
-    if (attributeList == null) {
+
+  final NameValuePair nextNameValuePair(NameValuePairLocation location) {
+    if (nameValuePairList == null || location == null
+        || location.isOutOfRange(nameValuePairList)) {
       return null;
     }
-    return attributeList.nextAttribute(location);
+    NameValuePair pair = (NameValuePair) nameValuePairList.get(location
+        .getIndex());
+    location.increment();
+    return pair;
   }
 
   void print() {
     Token token = null;
     System.out.print("Section: " + key + ":(");
-    System.out.print(type.getValue(true));
+    System.out.print(type.getValues());
     System.out.print(",");
-    System.out.print(name.getValue(true));
+    System.out.print(name.getValues());
     System.out.println(")");
     Attribute element = null;
-    if (attributeList != null) {
-      attributeList.print();
+    if (attributeMap != null) {
+      attributeMap.print();
     }
   }
 }
