b6cac5681156c98811314bc095e0484fc7543397
mastcu <mast@colorado.edu>
2006-02-13 05:14:50 +0000

    Added autocentering and seed mode

--- imod/beadfix.h
+++ imod/beadfix.h
@@ -11,41 +11,9 @@ $Date$
 
 $Revision$
 
-$Log$
-Revision 1.11  2005/02/19 01:29:50  mast
-Removed function to clear extra object
-
-Revision 1.10  2004/11/04 23:30:55  mast
-Changes for rounded button style
-
-Revision 1.9  2004/09/24 18:00:36  mast
-Added message capability
-
-Revision 1.8  2004/06/25 20:06:09  mast
-Made plug variables be class members instead to eliminate plug->
-
-Revision 1.7  2004/06/24 15:34:37  mast
-Changes for read and analyze at once
-
-Revision 1.6  2004/06/12 00:58:11  mast
-Switched to reading in whole file at once
-
-Revision 1.5  2004/05/28 18:57:42  mast
-Enable fixer to run align if thread support exists
-
-Revision 1.4  2004/05/07 22:14:26  mast
-Defined a variable instead of QT_THREAD_SUPPORT for the the Run Align button
-
-Revision 1.3  2004/05/03 19:17:20  mast
-Added thread class to run tiltalign
-
-Revision 1.2  2004/04/29 00:28:51  mast
-Added button to keep window on top
-
-Revision 1.1  2003/10/01 05:10:58  mast
-Incorporation as internal module in 3dmod
-
+Log at end
 */
+
 #ifndef BEADFIX_H
 #define BEADFIX_H
 
@@ -82,6 +50,7 @@ class AlignThread : public QThread
 
 class QPushButton;
 class QCheckBox;
+class QSpinBox;
 class QHBox;
 
 // A structure to hold all of the residual data that is read in, plus what
@@ -121,6 +90,10 @@ class BeadFixer : public DialogFrame
   ~BeadFixer() {};
   int openFileByName(const char *filename);
   int reread();
+  int insertPoint(float imx, float imy);
+  int modifyPoint(float imx, float imy);
+  int findCenter(float &imx, float &imy, int curz);
+  int executeMessage(QStringList *strings, int *arg);
 
   public slots:
   void buttonPressed(int which);
@@ -135,6 +108,10 @@ class BeadFixer : public DialogFrame
   void undoMove();
   void clearList();
   void onceToggled(bool state);
+  void seedToggled(bool state);
+  void autoCenToggled(bool state);
+  void lightToggled(bool state);
+  void diameterChanged(int value);
   void keepOnTop(bool state);
   void runAlign();
   void setFontDependentWidths();
@@ -172,6 +149,7 @@ class BeadFixer : public DialogFrame
   AreaData *mAreaList;                  /* Data about areas */
   int    mAreaMax;                      /* Size allocated */
   int    mBell;                         /* 1 to ring bell, -1 to suppress */ 
+  bool   mSeedMode;                     /* Seeding mode for inserting points */
   QPushButton *rereadBut;
   QPushButton *nextLocalBut;
   QPushButton *nextResBut;
@@ -182,6 +160,9 @@ class BeadFixer : public DialogFrame
   QPushButton *clearListBut;
   QPushButton *runAlignBut;
   QPushButton *openFileBut;
+  QCheckBox *autoCenBox;
+  QCheckBox *seedModeBox;
+  QSpinBox *diameterSpin;
   QHBox *topBox;
   bool mStayOnTop;
   bool mRunningAlign;
@@ -192,3 +173,42 @@ class BeadFixer : public DialogFrame
 };
 
 #endif
+/*
+$Log$
+Revision 1.12  2005/04/12 18:58:00  mast
+Added move all in local area
+
+Revision 1.11  2005/02/19 01:29:50  mast
+Removed function to clear extra object
+
+Revision 1.10  2004/11/04 23:30:55  mast
+Changes for rounded button style
+
+Revision 1.9  2004/09/24 18:00:36  mast
+Added message capability
+
+Revision 1.8  2004/06/25 20:06:09  mast
+Made plug variables be class members instead to eliminate plug->
+
+Revision 1.7  2004/06/24 15:34:37  mast
+Changes for read and analyze at once
+
+Revision 1.6  2004/06/12 00:58:11  mast
+Switched to reading in whole file at once
+
+Revision 1.5  2004/05/28 18:57:42  mast
+Enable fixer to run align if thread support exists
+
+Revision 1.4  2004/05/07 22:14:26  mast
+Defined a variable instead of QT_THREAD_SUPPORT for the the Run Align button
+
+Revision 1.3  2004/05/03 19:17:20  mast
+Added thread class to run tiltalign
+
+Revision 1.2  2004/04/29 00:28:51  mast
+Added button to keep window on top
+
+Revision 1.1  2003/10/01 05:10:58  mast
+Incorporation as internal module in 3dmod
+
+*/
