f55128617ae0db50cafa001f1a1d271ed442d97b
mastcu <mast@colorado.edu>
2006-02-25 22:08:49 +0000

    Cleaned up duplicate functions

--- include/imesh.h
+++ include/imesh.h
@@ -1,30 +1,19 @@
-/*  IMOD VERSION 2.02
- *
+/*
  *  imesh.h -- Image model mesh header.
  *
- *  Author: James Kremer email: kremer@colorado.edu
+ *  Author: James Kremer
+ *  Copyright (C) 1995-2005 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
+/*  $Author$
 
-/*****************************************************************************
- *   Copyright (C) 1995-1996 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
+$Date$
+
+$Revision$
+
+$Log$
+*/
 
 #ifndef IMESH_H
 #define IMESH_H
@@ -113,10 +102,6 @@ extern "C" {
   /* get info from meshes. */
   float imeshSurfaceArea(Imesh *mesh, Ipoint *mscale);
   float imeshVolume(Imesh *mesh, Ipoint *mscale);
-
-  /** internal functions **/
-  int imodel_mesh_addlist(Imesh *mesh, int val);
-  int imodel_mesh_addvert(Imesh *mesh, struct Mod_Point *pt);
 
 
 #ifdef __cplusplus
