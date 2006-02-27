5c829db5dfd0d2cb5801015eb4e4aac4785174bf
mastcu <mast@colorado.edu>
2006-02-27 19:46:40 +0000

    Added go to surface function

--- imod/imod_input.h
+++ imod/imod_input.h
@@ -1,77 +1,17 @@
-/*  IMOD VERSION 2.30
+/* imod_input.h - declarations for imod_input.cpp  
  *
- *  $Id$
- *
- *  Original author: James Kremer
- *  Revised by: David Mastronarde   email: mast@colorado.edu
+ *   Copyright (C) 1995-2006 by Boulder Laboratory for 3-Dimensional Electron
+ *   Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *   Colorado.  See dist/COPYRIGHT for full copyright notice.
  */
-
-/*****************************************************************************
- *   Copyright (C) 1995-2000 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
-/*  $Author$
-
-    $Date$
-
-    $Revision$
-
-    $Log$
-    Revision 3.9  2003/04/18 20:10:15  mast
-    changed name
-
-    Revision 3.8  2003/04/18 20:05:52  mast
-    Add function for rejecting meta key on Mac
-
-    Revision 3.7  2003/04/17 19:27:13  mast
-    keypad workaround for Mac
-
-    Revision 3.6  2003/03/29 00:22:43  mast
-    add forward declaration of ImodView
-
-    Revision 3.5  2003/03/13 07:15:16  mast
-    Add raise window function
-
-    Revision 3.4  2003/03/13 01:17:46  mast
-    Add function to convert keypad keys
-
-    Revision 3.3  2003/02/27 23:45:24  mast
-    Add function to truncate contour
-
-    Revision 3.2  2003/02/10 20:41:55  mast
-    Merge Qt source
-
-    Revision 3.1.2.4  2003/01/27 00:30:07  mast
-    Pure Qt version and general cleanup
-
-    Revision 3.1.2.3  2003/01/23 20:06:07  mast
-    remove declarations for imod_cont_edit
-
-    Revision 3.1.2.2  2003/01/13 01:15:42  mast
-    changes for Qt version of info window
-
-    Revision 3.1.2.1  2002/12/09 17:51:38  mast
-    add declartion of defaultKeyInput
-
-    Revision 3.1  2002/12/01 15:34:41  mast
-    Changes to get clean compilation with g++
-
+/*
+  $Author$
+    
+  $Date$
+  
+  $Revision$
+  
+  Log at end
 */
 
 #ifndef IMOD_INPUT_H
@@ -94,6 +34,7 @@ void inputNewSurface(ImodView *vw);
 void inputNextObject(ImodView *vw);
 void inputPrevObject(ImodView *vw);
 void inputAdjacentSurface(ImodView *vw, int direction);
+void inputGotoSurface(ImodView *vw, int target);
 void inputAdjacentContInSurf(ImodView *vw, int direction);
 void inputNextContour(ImodView *vw);
 void inputPrevContour(ImodView *vw);
@@ -126,3 +67,47 @@ void inputSetModelTime(ImodView *vw, int time);
 int mouse_in_box(int llx, int lly, int urx, int  ury, int mousex, int mousey);
 
 #endif /* imod_input.h */
+/*
+  $Log$
+  Revision 3.10  2004/11/20 05:05:27  mast
+  Changes for undo/redo capability
+
+  Revision 3.9  2003/04/18 20:10:15  mast
+  changed name
+
+  Revision 3.8  2003/04/18 20:05:52  mast
+  Add function for rejecting meta key on Mac
+
+  Revision 3.7  2003/04/17 19:27:13  mast
+  keypad workaround for Mac
+
+  Revision 3.6  2003/03/29 00:22:43  mast
+  add forward declaration of ImodView
+
+  Revision 3.5  2003/03/13 07:15:16  mast
+  Add raise window function
+
+  Revision 3.4  2003/03/13 01:17:46  mast
+  Add function to convert keypad keys
+
+  Revision 3.3  2003/02/27 23:45:24  mast
+  Add function to truncate contour
+
+  Revision 3.2  2003/02/10 20:41:55  mast
+  Merge Qt source
+
+  Revision 3.1.2.4  2003/01/27 00:30:07  mast
+  Pure Qt version and general cleanup
+
+  Revision 3.1.2.3  2003/01/23 20:06:07  mast
+  remove declarations for imod_cont_edit
+
+  Revision 3.1.2.2  2003/01/13 01:15:42  mast
+  changes for Qt version of info window
+
+  Revision 3.1.2.1  2002/12/09 17:51:38  mast
+  add declartion of defaultKeyInput
+
+  Revision 3.1  2002/12/01 15:34:41  mast
+  Changes to get clean compilation with g++
+*/
