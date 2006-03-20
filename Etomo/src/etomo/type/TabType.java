d4fe66a90a321176dc3957bc34f92a75816b5a0e
SueHeld <sueh@colorado.edu>
2006-03-20 18:00:26 +0000

    bug# 835 Type of manager (ApplicationManager, JoinManager, or ParallelManager).

---
+++ Etomo/src/etomo/type/TabType.java
@@ -0,0 +1,27 @@
+package etomo.type;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+public final class TabType {
+  public static  final String  rcsid =  "$Id$";
+  
+  public static final TabType RECON = new TabType();
+  public static final TabType JOIN = new TabType();
+  public static final TabType PARALLEL = new TabType();
+  
+  private TabType() {
+  }
+}
+/**
+* <p> $Log$ </p>
+*/
