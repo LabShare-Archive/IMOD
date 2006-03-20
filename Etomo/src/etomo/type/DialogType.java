ec9e82a919ce68cf93f203cfec6dc99c8f91cfe0
SueHeld <sueh@colorado.edu>
2006-03-20 17:57:34 +0000

    bug# 835 Added the PARALLEL dialog, made DialogType work with different managers instead of just ApplicationManager.

--- Etomo/src/etomo/type/DialogType.java
+++ Etomo/src/etomo/type/DialogType.java
@@ -1,38 +1,42 @@
 package etomo.type;
+
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
-*
-*<p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-* 
-* <p> $Log$
-* <p> Revision 1.3  2005/09/27 23:18:39  sueh
-* <p> bug# 532 Added getStorableName() to return standard names to use in the
-* <p> .edf file.
-* <p>
-* <p> Revision 1.2  2005/03/24 17:48:38  sueh
-* <p> bug# 621 Added Clean Up dialog.
-* <p>
-* <p> Revision 1.1  2005/01/14 03:06:31  sueh
-* <p> bug# 511 An enumerator class the identifies the dialogs in the
-* <p> tomogram reconstruction side of Etomo.  Contains a string to be used as
-* <p> button labels.  Also contains an index to be used to find dialog related
-* <p> data in arrays.  Stored in ProcessControlPanel and ProcessDialog as way to pass the
-* <p> identifier of the dialog to generic functions in ApplicationManager (see
-* <p> saveDialog, getDialog, getAdvanced).  Used to create and maintain the
-* <p> advanced arrays, which preserve the advanced settings for each dialog.
-* <p> </p>
-*/
-public class DialogType {
-  public static  final String  rcsid =  "$Id$";
-  
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
+ *
+ *<p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$
+ * <p> Revision 1.4  2005/09/29 18:46:03  sueh
+ * <p> bug# 532 Simplified the storable names.
+ * <p>
+ * <p> Revision 1.3  2005/09/27 23:18:39  sueh
+ * <p> bug# 532 Added getStorableName() to return standard names to use in the
+ * <p> .edf file.
+ * <p>
+ * <p> Revision 1.2  2005/03/24 17:48:38  sueh
+ * <p> bug# 621 Added Clean Up dialog.
+ * <p>
+ * <p> Revision 1.1  2005/01/14 03:06:31  sueh
+ * <p> bug# 511 An enumerator class the identifies the dialogs in the
+ * <p> tomogram reconstruction side of Etomo.  Contains a string to be used as
+ * <p> button labels.  Also contains an index to be used to find dialog related
+ * <p> data in arrays.  Stored in ProcessControlPanel and ProcessDialog as way to pass the
+ * <p> identifier of the dialog to generic functions in ApplicationManager (see
+ * <p> saveDialog, getDialog, getAdvanced).  Used to create and maintain the
+ * <p> advanced arrays, which preserve the advanced settings for each dialog.
+ * <p> </p>
+ */
+public final class DialogType {
+  public static final String rcsid = "$Id$";
+
   private static final int setupIndex = 0;
   private static final int preProcessingIndex = 1;
   private static final int coarseAlignmentIndex = 2;
@@ -43,15 +47,21 @@ public class DialogType {
   private static final int tomogramCombinationIndex = 7;
   private static final int postProcessingIndex = 8;
   private static final int cleanUpIndex = 9;
-  
-  public static final int TOTAL = cleanUpIndex + 1;
-  
+
+  public static final int TOTAL_RECON = cleanUpIndex + 1;
+
+  private static final int parallelIndex = 0;
+
+  public static final int TOTAL_PARALLEL = parallelIndex + 1;
+
   private final String name;
   private final int index;
+  private final TabType tabType;
 
-  private DialogType(int index) {
+  private DialogType(TabType tabType, int index) {
     this.index = index;
-    name = toString(index);
+    name = toString(tabType, index);
+    this.tabType = tabType;
   }
 
   /**
@@ -60,75 +70,101 @@ public class DialogType {
   public String toString() {
     return name;
   }
-  
+
   public String getStorableName() {
-    return getStorableName(index);
+    return getStorableName(tabType, index);
   }
-  
+
   public int toIndex() {
     return index;
   }
+
+  public static final DialogType SETUP = new DialogType(TabType.RECON, setupIndex);
+  public static final DialogType PRE_PROCESSING = new DialogType(TabType.RECON, 
+      preProcessingIndex);
+  public static final DialogType COARSE_ALIGNMENT = new DialogType(TabType.RECON, 
+      coarseAlignmentIndex);
+  public static final DialogType FIDUCIAL_MODEL = new DialogType(TabType.RECON, 
+      fiducialModelIndex);
+  public static final DialogType FINE_ALIGNMENT = new DialogType(TabType.RECON, 
+      fineAlignmentIndex);
+
+  public static final DialogType TOMOGRAM_POSITIONING = new DialogType(TabType.RECON, 
+      tomogramPositioningIndex);
+  public static final DialogType TOMOGRAM_GENERATION = new DialogType(TabType.RECON, 
+      tomogramGenerationIndex);
+  public static final DialogType TOMOGRAM_COMBINATION = new DialogType(TabType.RECON, 
+      tomogramCombinationIndex);
+  public static final DialogType POST_PROCESSING = new DialogType(TabType.RECON, 
+      postProcessingIndex);
+  public static final DialogType CLEAN_UP = new DialogType(TabType.RECON, cleanUpIndex);
   
-  public static final DialogType SETUP = new DialogType(setupIndex);
-  public static final DialogType PRE_PROCESSING = new DialogType(preProcessingIndex);
-  public static final DialogType COARSE_ALIGNMENT = new DialogType(coarseAlignmentIndex);
-  public static final DialogType FIDUCIAL_MODEL = new DialogType(fiducialModelIndex);
-  public static final DialogType FINE_ALIGNMENT = new DialogType(fineAlignmentIndex);
-  
-  public static final DialogType TOMOGRAM_POSITIONING = new DialogType(tomogramPositioningIndex);
-  public static final DialogType TOMOGRAM_GENERATION = new DialogType(tomogramGenerationIndex);
-  public static final DialogType TOMOGRAM_COMBINATION = new DialogType(tomogramCombinationIndex);
-  public static final DialogType POST_PROCESSING = new DialogType(postProcessingIndex);
-  public static final DialogType CLEAN_UP = new DialogType(cleanUpIndex);
-  
-  private String toString(int index) {
-    switch (index) {
-    case setupIndex:
-      return "Setup";
-    case preProcessingIndex:
-      return "Pre-processing";
-    case coarseAlignmentIndex:
-      return "Coarse Alignment";
-    case fiducialModelIndex:
-      return "Fiducial Model Gen.";
-    case fineAlignmentIndex:
-      return "Fine Alignment";
-    case tomogramPositioningIndex:
-      return "Tomogram Positioning";
-    case tomogramGenerationIndex:
-      return "Tomogram Generation";
-    case tomogramCombinationIndex:
-      return "Tomogram Combination";
-    case postProcessingIndex:
-      return "Post-processing";
-    case cleanUpIndex:
-      return "Clean Up";
+  public static final DialogType PARALLEL = new DialogType(TabType.PARALLEL, parallelIndex);
+
+  private String toString(TabType tabType, int index) {
+    if (tabType == TabType.RECON) {
+      switch (index) {
+      case setupIndex:
+        return "Setup";
+      case preProcessingIndex:
+        return "Pre-processing";
+      case coarseAlignmentIndex:
+        return "Coarse Alignment";
+      case fiducialModelIndex:
+        return "Fiducial Model Gen.";
+      case fineAlignmentIndex:
+        return "Fine Alignment";
+      case tomogramPositioningIndex:
+        return "Tomogram Positioning";
+      case tomogramGenerationIndex:
+        return "Tomogram Generation";
+      case tomogramCombinationIndex:
+        return "Tomogram Combination";
+      case postProcessingIndex:
+        return "Post-processing";
+      case cleanUpIndex:
+        return "Clean Up";
+      }
+    }
+    else if (tabType == TabType.PARALLEL) {
+      switch (index) {
+      case parallelIndex:
+        return "Parallel";
+      }
     }
     return "";
   }
-  
-  private String getStorableName(int index) {
-    switch (index) {
-    case setupIndex:
-      return "Setup";
-    case preProcessingIndex:
-      return "PreProc";
-    case coarseAlignmentIndex:
-      return "CoarseAlign";
-    case fiducialModelIndex:
-      return "FidModel";
-    case fineAlignmentIndex:
-      return "FineAlign";
-    case tomogramPositioningIndex:
-      return "TomoPos";
-    case tomogramGenerationIndex:
-      return "TomoGen";
-    case tomogramCombinationIndex:
-      return "Combine";
-    case postProcessingIndex:
-      return "PostProc";
-    case cleanUpIndex:
-      return "CleanUp";
+
+  private String getStorableName(TabType tabType, int index) {
+    if (tabType == TabType.RECON) {
+      switch (index) {
+      case setupIndex:
+        return "Setup";
+      case preProcessingIndex:
+        return "PreProc";
+      case coarseAlignmentIndex:
+        return "CoarseAlign";
+      case fiducialModelIndex:
+        return "FidModel";
+      case fineAlignmentIndex:
+        return "FineAlign";
+      case tomogramPositioningIndex:
+        return "TomoPos";
+      case tomogramGenerationIndex:
+        return "TomoGen";
+      case tomogramCombinationIndex:
+        return "Combine";
+      case postProcessingIndex:
+        return "PostProc";
+      case cleanUpIndex:
+        return "CleanUp";
+      }
+    }
+    else if (tabType == TabType.PARALLEL) {
+      switch (index) {
+      case parallelIndex:
+        return "Parallel";
+      }
     }
     return "";
   }
