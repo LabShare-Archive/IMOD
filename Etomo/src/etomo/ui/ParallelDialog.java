003b948d93393d648d3c98f0d93b129a068349d2
SueHeld <sueh@colorado.edu>
2006-03-20 18:05:32 +0000

    bug# 835 Dialog handling generic parallel processes.

--- Etomo/src/etomo/ui/ParallelDialog.java
+++ Etomo/src/etomo/ui/ParallelDialog.java
@@ -1,51 +1,191 @@
 package etomo.ui;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import etomo.EtomoDirector;
+import etomo.ParallelManager;
 import etomo.comscript.ParallelParam;
+import etomo.comscript.ProcesschunksParam;
+import etomo.storage.ChunkComscriptFileFilter;
+import etomo.type.AxisID;
+import etomo.type.BaseScreenState;
 import etomo.type.DialogType;
+import etomo.type.ParallelMetaData;
 
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright (c) 2005</p>
-*
-*<p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-*/
-public interface ParallelDialog {
-  public static  final String  rcsid =  "$Id$";
-  
-  public void getParameters(ParallelParam param);
-  public DialogType getDialogType();
-  public boolean isParallel();
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright (c) 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ */
+public final class ParallelDialog implements AbstractParallelDialog {
+  public static final String rcsid = "$Id$";
+
+  private static final DialogType DIALOG_TYPE = DialogType.PARALLEL;
+
+  private final ImageIcon iconFolder = new ImageIcon(ClassLoader
+      .getSystemResource("images/openFile.gif"));
+  private final SpacedPanel pnlRoot = new SpacedPanel();
+  private final SpacedPanel pnlProcessName = new SpacedPanel();
+  private final JButton btnChunkComscript = new JButton(iconFolder);
+  private final LabeledTextField ltfProcessName = new LabeledTextField(
+      "Process name: ");
+  private final MultiLineButton btnRunProcess = MultiLineButton
+      .getToggleButtonInstance("Run Parallel Process");
+
+  private final ParallelActionListener actionListener;
+  private final ChunkComscriptActionListener chunkComscriptActionListener;
+  private final ParallelManager manager;
+  private final AxisID axisID;
+
+  private File workingDir = null;
+
+  public ParallelDialog(ParallelManager manager, AxisID axisID) {
+    actionListener = new ParallelActionListener(this);
+    chunkComscriptActionListener = new ChunkComscriptActionListener(this);
+    this.manager = manager;
+    this.axisID = axisID;
+    //process name panel
+    pnlProcessName.setBoxLayout(BoxLayout.X_AXIS);
+    ltfProcessName.setTextPreferredWidth(125);
+    pnlProcessName.add(ltfProcessName);
+    btnChunkComscript.setPreferredSize(FixedDim.folderButton);
+    pnlProcessName.add(btnChunkComscript);
+    //root panel
+    pnlRoot.setBorder(new BeveledBorder("Parallel Process").getBorder());
+    pnlRoot.setBoxLayout(BoxLayout.Y_AXIS);
+    pnlRoot.add(pnlProcessName);
+    btnRunProcess.setSize();
+    pnlRoot.add(btnRunProcess);
+    pnlRoot.alignComponentsX(Component.CENTER_ALIGNMENT);
+    setActionListener();
+  }
+
+  public Container getContainer() {
+    return pnlRoot.getContainer();
+  }
+
+  public DialogType getDialogType() {
+    return DIALOG_TYPE;
+  }
+
+  public File getWorkingDir() {
+    return workingDir;
+  }
+
+  private void setActionListener() {
+    btnChunkComscript.addActionListener(chunkComscriptActionListener);
+    btnRunProcess.addActionListener(actionListener);
+  }
+
+  public void done() {
+    btnRunProcess.removeActionListener(actionListener);
+  }
+
+  public void setParameters(BaseScreenState screenState) {
+    btnRunProcess.setButtonState(screenState.getButtonState(btnRunProcess
+        .getButtonStateKey(DIALOG_TYPE)));
+  }
+
+  public void setParameters(ParallelMetaData metaData) {
+    ltfProcessName.setText(metaData.getRootName());
+  }
+
+  public void getParameters(BaseScreenState screenState) {
+    screenState.setButtonState(btnRunProcess.getButtonStateKey(), btnRunProcess
+        .getButtonState());
+  }
+
+  public void getParameters(ParallelParam param) {
+    ProcesschunksParam processchunksParam = (ProcesschunksParam) param;
+    processchunksParam.setRootName(ltfProcessName.getText());
+  }
+
+  public void getParameters(ParallelMetaData metaData) {
+    metaData.setRootName(ltfProcessName.getText());
+  }
+
+  public void updateDisplay(boolean setupMode) {
+    ltfProcessName.setEditable(setupMode);
+    btnChunkComscript.setEnabled(setupMode);
+  }
+
+  public final boolean isParallel() {
+    return true;
+  }
+
+  void action(ActionEvent event) {
+    String command = event.getActionCommand();
+    System.out.println(btnChunkComscript.getText());
+    if (command.equals(btnRunProcess.getText())) {
+      manager.processchunks(axisID, this, btnRunProcess);
+    }
+  }
+
+  void chunkComscriptAction() {
+    //  Open up the file chooser in the working directory
+    JFileChooser chooser = new JFileChooser(new File(EtomoDirector
+        .getInstance().getOriginalUserDir()));
+    ChunkComscriptFileFilter filter = new ChunkComscriptFileFilter();
+    chooser.setFileFilter(filter);
+    chooser.setPreferredSize(new Dimension(400, 400));
+    chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    int returnVal = chooser.showOpenDialog(pnlRoot.getContainer());
+    if (returnVal == JFileChooser.APPROVE_OPTION) {
+      File chunkComscript = chooser.getSelectedFile();
+      try {
+        String comFileName = chunkComscript.getName();
+        ltfProcessName.setText(comFileName.substring(0, comFileName
+            .indexOf('-')));
+        workingDir = chunkComscript.getParentFile();
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private class ParallelActionListener implements ActionListener {
+    ParallelDialog adaptee;
+
+    ParallelActionListener(ParallelDialog adaptee) {
+      this.adaptee = adaptee;
+    }
+
+    public void actionPerformed(ActionEvent event) {
+      adaptee.action(event);
+    }
+  }
+
+  private class ChunkComscriptActionListener implements ActionListener {
+    ParallelDialog adaptee;
+
+    ChunkComscriptActionListener(ParallelDialog adaptee) {
+      this.adaptee = adaptee;
+    }
+
+    public void actionPerformed(ActionEvent event) {
+      adaptee.chunkComscriptAction();
+    }
+  }
 }
 /**
-* <p> $Log$
-* <p> Revision 1.7  2005/09/21 16:46:16  sueh
-* <p> bug# 532 Removed all resume functionality from the dialogs.  Removed
-* <p> resume().  Removed getParallelProgressDisplay() because the parallel
-* <p> panel can be gotten from the manager.
-* <p>
-* <p> Revision 1.6  2005/09/16 21:20:41  sueh
-* <p> bug# 532 Changed ParallelDialog.resetParallelPanel() to
-* <p> resetParallelProgressDisplay() because ParallelDialog is generic.
-* <p>
-* <p> Revision 1.5  2005/09/16 20:56:43  sueh
-* <p> bug# 532 Moved call to resetParallelPanel() to
-* <p> ApplicationManager.processchunks().  Added resetParallelPanel() to
-* <p> ParallelDialog.
-* <p>
-* <p> Revision 1.4  2005/09/16 18:10:37  sueh
-* <p> bug# 532 Added getParallelProgressDisplay() and getParameters().
-* <p>
-* <p> Revision 1.3  2005/07/11 23:08:20  sueh
-* <p> bug# 619 Deleted the ParallelDialog class which extends JDialog and was
-* <p> used for showing the parallel panel in a demo because it isn't needed.
-* <p> Added interface ParallelDialog to provide a generic parent for
-* <p> ParallelPanel.  Gives a generic way to call a resume function.
-* <p> </p>
-*/
+ * <p> $Log$ </p>
+ */
