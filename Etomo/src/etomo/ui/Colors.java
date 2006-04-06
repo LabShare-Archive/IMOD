7e1ce48042aeb62357faead966cd9943c432170b
SueHeld <sueh@colorado.edu>
2006-04-06 23:33:58 +0000

    bug# 844 Added colors for the join and the generic parallel processing windows.

--- Etomo/src/etomo/ui/Colors.java
+++ Etomo/src/etomo/ui/Colors.java
@@ -15,14 +15,22 @@ import java.awt.Color;
 * 
 * @version $Revision$
 * 
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1  2005/04/16 01:54:54  sueh
+* <p> bug# 615 Class to hold static colors.
+* <p> </p>
 */
 public class Colors {
   public static  final String  rcsid =  "$Id$";
   
   static final Color backgroundA = new Color(153, 179, 204);
   static final Color backgroundB = new Color(153, 204, 179);
+  static final Color backgroundJoin = new Color(179, 153, 204);
+  static final Color backgroundParallel = new Color(204, 153, 179);
 }
 /**
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1  2005/04/16 01:54:54  sueh
+* <p> bug# 615 Class to hold static colors.
+* <p> </p>
 */
