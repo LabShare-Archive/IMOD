3429e5b0e9d4ab2f78f604d9c38362ef9a63b090
SueHeld <sueh@colorado.edu>
2006-04-06 20:12:55 +0000

    bug# 808 Added rotationAnglesList, a sparsely populated array.  Added functionality to delete and move a row.  Added revert functionality.

--- Etomo/src/etomo/type/JoinState.java
+++ Etomo/src/etomo/type/JoinState.java
@@ -1,6 +1,9 @@
 package etomo.type;
 
+import java.util.Hashtable;
 import java.util.Properties;
+
+import etomo.BaseManager;
 
 /**
  * <p>Description: </p>
@@ -16,6 +19,9 @@ import java.util.Properties;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 1.4  2005/12/16 18:26:06  sueh
+ * <p> bug# 785 Added doneMode.
+ * <p>
  * <p> Revision 1.3  2005/12/14 01:29:10  sueh
  * <p> bug# 782 Added toString().
  * <p>
@@ -28,8 +34,12 @@ import java.util.Properties;
  * <p> processes are run.
  * <p> </p>
  */
-public class JoinState implements BaseState {
+public final class JoinState implements BaseState {
   public static final String rcsid = "$Id$";
+
+  public static final String ROTATION_ANGLE_X = "RotationAngleX";
+  public static final String ROTATION_ANGLE_Y = "RotationAngleY";
+  public static final String ROTATION_ANGLE_Z = "RotationAngleZ";
 
   private static final String groupString = "JoinState";
   protected static final String sampleProducedString = "SampleProduced";
@@ -38,21 +48,26 @@ public class JoinState implements BaseState {
   private final EtomoNumber doneMode = new EtomoNumber("DoneMode");
 
   //set on the successful completion of finishjoin
-  protected EtomoNumber trialBinning = new EtomoNumber(
-      EtomoNumber.INTEGER_TYPE, "TrialBinning");
-  protected EtomoNumber trialSizeInX = new EtomoNumber(
-      EtomoNumber.INTEGER_TYPE, "TrialSizeInX");
-  protected EtomoNumber trialSizeInY = new EtomoNumber(
-      EtomoNumber.INTEGER_TYPE, "TrialSizeInY");
-  protected EtomoNumber trialShiftInX = new EtomoNumber(
-      EtomoNumber.INTEGER_TYPE, "TrialShiftInX");
-  protected EtomoNumber trialShiftInY = new EtomoNumber(
-      EtomoNumber.INTEGER_TYPE, "TrialShiftInY");
+  private final EtomoNumber trialBinning = new EtomoNumber("TrialBinning");
+  private final EtomoNumber trialSizeInX = new EtomoNumber("TrialSizeInX");
+  private final EtomoNumber trialSizeInY = new EtomoNumber("TrialSizeInY");
+  private final EtomoNumber trialShiftInX = new EtomoNumber("TrialShiftInX");
+  private final EtomoNumber trialShiftInY = new EtomoNumber("TrialShiftInY");
+
+  private final BaseManager manager;
+
+  private Hashtable rotationAnglesList = null;
+  private Hashtable revertRotationAnglesList = null;
+
+  private final EtomoNumber totalRows = new EtomoNumber("TotalRows");
+  private final EtomoNumber revertTotalRows = new EtomoNumber();
+
   //state variable for join setup tab
   protected boolean sampleProduced;
 
-  public JoinState() {
+  public JoinState(BaseManager manager) {
     reset();
+    this.manager = manager;
   }
 
   public String toString() {
@@ -91,6 +106,19 @@ public class JoinState implements BaseState {
     trialShiftInY.store(props, prepend);
     props.setProperty(group + sampleProducedString, Boolean
         .toString(sampleProduced));
+    totalRows.store(props, prepend);
+    //store the rotation angles under their current row number
+    if (rotationAnglesList != null) {
+      for (int i = 0; i < totalRows.getInt(); i++) {
+        SlicerAngles rotationAngles = (SlicerAngles) rotationAnglesList
+            .get(new Integer(i));
+        if (rotationAngles != null) {
+          rotationAngles.store(props, SectionTableRowData.createPrepend(
+              prepend, new EtomoNumber().set(i + 1)));
+        }
+      }
+    }
+
   }
 
   public boolean equals(JoinState that) {
@@ -121,6 +149,22 @@ public class JoinState implements BaseState {
     sampleProduced = Boolean.valueOf(
         props.getProperty(group + sampleProducedString, Boolean
             .toString(defaultSampleProduced))).booleanValue();
+    totalRows.load(props, prepend);
+    //retrieve the rotation angles by row number
+    rotationAnglesList = null;
+    if (!totalRows.isNull()) {
+      for (int i = 0; i < totalRows.getInt(); i++) {
+        SlicerAngles rotationAngles = new SlicerAngles();
+        rotationAngles.load(props, SectionTableRowData.createPrepend(prepend,
+            new EtomoNumber().set(i + 1)));
+        if (!rotationAngles.isEmpty()) {
+          if (rotationAnglesList == null) {
+            rotationAnglesList = new Hashtable();
+          }
+          rotationAnglesList.put(new Integer(i), rotationAngles);
+        }
+      }
+    }
   }
 
   public int getNewShiftInX(int min, int max) {
@@ -128,6 +172,19 @@ public class JoinState implements BaseState {
         - (max + min) / 2;
   }
 
+  public SlicerAngles getRotationAngles(Integer index) {
+    if (rotationAnglesList == null) {
+      return null;
+    }
+    return (SlicerAngles) rotationAnglesList.get(index);
+  }
+
+  /**
+   * calculate shift in y
+   * @param min
+   * @param max
+   * @return
+   */
   public int getNewShiftInY(int min, int max) {
     return trialShiftInY.getInt() + (trialSizeInY.getInt() + 1) / 2
         - (max + min) / 2;
@@ -139,6 +196,109 @@ public class JoinState implements BaseState {
 
   public boolean isSampleProduced() {
     return sampleProduced;
+  }
+  
+  public void setRevertState(boolean enableRevert) {
+    if (enableRevert && rotationAnglesList != null) {
+      revertRotationAnglesList = (Hashtable) rotationAnglesList.clone();
+      revertTotalRows.set(totalRows);
+    }
+    else {
+      revertRotationAnglesList = null;
+      revertTotalRows.set("");
+    }
+  }
+  
+  public void revert() {
+    rotationAnglesList = revertRotationAnglesList;
+    totalRows.set(revertTotalRows);
+  }
+
+  public void deleteRow(int rowIndex) {
+    if (rotationAnglesList == null) {
+      return;
+    }
+    if (rowIndex >= totalRows.getInt()) {
+      throw new IllegalStateException("rowIndex=" + rowIndex + ",totalRows="
+          + totalRows);
+    }
+    //delete row
+    Integer prevIndex = new Integer(rowIndex);
+    rotationAnglesList.remove(prevIndex);
+    //move the other rows up one row
+    for (int i = rowIndex + 1; i < totalRows.getInt(); i++) {
+      Integer curIndex = new Integer(i);
+      SlicerAngles rotationAngles = (SlicerAngles) rotationAnglesList
+          .remove(curIndex);
+      if (rotationAngles != null) {
+        rotationAnglesList.put(prevIndex, rotationAngles);
+      }
+      prevIndex = curIndex;
+    }
+  }
+  
+  public void printRotationAnglesList() {
+    System.out.println("rotationAnglesList=" + rotationAnglesList);
+  }
+
+  public void moveRowUp(int rowIndex) {
+    if (rotationAnglesList == null) {
+      return;
+    }
+    if (rowIndex == 0 || rowIndex >= totalRows.getInt()) {
+      throw new IllegalStateException("rowIndex=" + rowIndex + ",totalRows="
+          + totalRows);
+    }
+    Integer curIndex = new Integer(rowIndex);
+    Integer prevIndex = new Integer(rowIndex - 1);
+    //swap the current row with the one above it
+    SlicerAngles rotationAngles = (SlicerAngles) rotationAnglesList
+        .remove(curIndex);
+    SlicerAngles prevRotationAngles;
+    if (rotationAngles != null) {
+      prevRotationAngles = (SlicerAngles) rotationAnglesList.put(prevIndex,
+          rotationAngles);
+    }
+    else {
+      prevRotationAngles = (SlicerAngles) rotationAnglesList.remove(prevIndex);
+    }
+    if (prevRotationAngles != null) {
+      rotationAnglesList.put(curIndex, prevRotationAngles);
+    }
+  }
+
+  public void moveRowDown(int rowIndex) {
+    if (rotationAnglesList == null) {
+      return;
+    }
+    if (rowIndex >= totalRows.getInt() - 1) {
+      throw new IllegalStateException("rowIndex=" + rowIndex + ",totalRows="
+          + totalRows);
+    }
+    Integer curIndex = new Integer(rowIndex);
+    Integer nextIndex = new Integer(rowIndex + 1);
+    //swap the current row with the one below it
+    SlicerAngles rotationAngles = (SlicerAngles) rotationAnglesList
+        .remove(curIndex);
+    SlicerAngles nextRotationAngles;
+    if (rotationAngles != null) {
+      nextRotationAngles = (SlicerAngles) rotationAnglesList.put(nextIndex,
+          rotationAngles);
+    }
+    else {
+      nextRotationAngles = (SlicerAngles) rotationAnglesList.remove(nextIndex);
+    }
+    if (nextRotationAngles != null) {
+      rotationAnglesList.put(curIndex, nextRotationAngles);
+    }
+  }
+
+  public void setRotationAnglesList(Hashtable rotationAnglesList) {
+    this.rotationAnglesList = rotationAnglesList;
+  }
+
+  public void setTotalRows(int totalRows) {
+    this.totalRows.set(totalRows);
   }
 
   public void setTrialBinning(int trialBinning) {
