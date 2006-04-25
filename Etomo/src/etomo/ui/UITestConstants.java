d8e45907f41aa1aff4c9ae5906cb99912d7007d9
SueHeld <sueh@colorado.edu>
2006-04-25 19:39:37 +0000

    bug# 787 Placed most of the attribute string in UITestField

--- Etomo/src/etomo/ui/UITestConstants.java
+++ Etomo/src/etomo/ui/UITestConstants.java
@@ -16,16 +16,12 @@ final class UITestConstants {
   public static  final String  rcsid =  "$Id$";
   
   static final String DIALOG_SECTION_TYPE = "Dialog";
-  static final String RADIO_BUTTON_ATTRIB = "rb";
-  static final String CHECK_BOX_ATTRIB = "cb";
-  static final String TEXT_FIELD_ATTRIB = "tf";
-  static final String SPINNER_ATTRIB = "sp";
-  static final String BUTTON_ATTRIB = "bn";
-  static final String TOGGLE_BUTTON_ATTRIB = "tb";
-  static final String POPUP_ATTRIB = "popup";
 }
 /**
 * <p> $Log$
+* <p> Revision 1.2  2006/01/11 23:19:08  sueh
+* <p> bug# 675 Added TOGGLE_BUTTON_ATTRIB
+* <p>
 * <p> Revision 1.1  2006/01/04 20:28:21  sueh
 * <p> bug# 675 Moved constants that must be shared by non-test objects to an
 * <p> object which doesn't know about junit.  Overwise junit would have to be in
