1b382f3b069011ecbc364faf961073e3b8718ff3
SueHeld <sueh@colorado.edu>
2006-04-28 20:52:21 +0000

    bug# 787 Interface for factories which create AdocCommands.

---
+++ Etomo/src/etomo/storage/autodoc/AdocCommandFactory.java
@@ -0,0 +1,22 @@
+package etomo.storage.autodoc;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+public interface AdocCommandFactory {
+  public static final String rcsid = "$Id$";
+  
+  public AdocCommand newAdocCommand();
+}
+/**
+* <p> $Log$ </p>
+*/
