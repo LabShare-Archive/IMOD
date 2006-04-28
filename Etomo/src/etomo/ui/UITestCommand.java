9539d2f1a9184b1bb644a42cc582f5cd43139fab
SueHeld <sueh@colorado.edu>
2006-04-28 21:10:09 +0000

    bug# 787 Used to be UITestAxisSectionCommand.  Parses the uitest autodoc global name/value pairs.

--- Etomo/src/etomo/ui/UITestCommand.java
+++ Etomo/src/etomo/ui/UITestCommand.java
@@ -1,46 +1,31 @@
 package etomo.ui;
 
-import java.io.File;
-
+import etomo.storage.autodoc.AdocCommand;
 import etomo.storage.autodoc.NameValuePair;
-import etomo.type.DialogType;
-import etomo.type.EtomoAutodoc;
-import etomo.type.ProcessEndState;
 import etomo.type.UITestAction;
-import etomo.type.UITestField;
-
 /**
- * <p>Description: </p>
- * 
- * <p>Copyright: Copyright (c) 2006</p>
- *
- * <p>Organization:
- * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
- * University of Colorado</p>
- * 
- * @author $Author$
- * 
- * @version $Revision$
- */
-public class UITestCommand {
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright (c) 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+final class UITestCommand implements AdocCommand {
   public static final String rcsid = "$Id$";
-
-  private static final String ENABLED_ATTRIB = "enabled";
-
+  
   private UITestAction action = null;
-  private UITestField field = null;
-  private String fieldName = null;
-  private int fieldIndex = 0;
   private String value = null;
-  private String formattedValue = null;
   private String string = "";
-  private DialogType dialogType = null;
-  private ProcessEndState processEndState = null;
-  private boolean enabled = false;
   private boolean empty = true;
   private boolean known = false;
-  
-  void set(NameValuePair pair) {
+
+  public void set(NameValuePair pair) {
     reset();
     if (pair == null) {
       return;
@@ -50,122 +35,33 @@ public class UITestCommand {
     if (pair.levels() == 0) {
       return;
     }
-    int index = 0;
     //set the action
     action = UITestAction.getInstance(pair.getName(0));
-    if (action != UITestAction.SLEEP && action != UITestAction.COPY) {
-      //set the field
-      if (action == UITestAction.WAIT_FOR) {
-        index++;
-      }
-      else if (action == UITestAction.ASSERT) {
-        index++;
-      }
-      setField(pair, index);
-    }
     //get the value
     value = pair.getValue();
-    if (action == UITestAction.WAIT_FOR) {
-      //handle waitfor = 
-      if (field == null) {
-        dialogType = DialogType.getInstance(value);
-      }
-      //handle waitfor.process = 
-      else if (field == UITestField.PROCESS) {
-        processEndState = ProcessEndState.getInstance(value);
-      }
-    }
-    //this ignores the Version name/value pair
-    if (action != null || field != null) {
+    //ignore unknown commands
+    if (action == UITestAction.SLEEP || action == UITestAction.SOURCE
+        || action == UITestAction.TEST_DIR) {
       known = true;
     }
-  }
-
-  private void setField(NameValuePair pair, int index) {
-    int levels = pair.levels();
-    if (index >= levels) {
-      return;
-    }
-    field = UITestField.getInstance(pair.getName(index++));
-    if (index >= levels) {
-      return;
-    }
-    fieldName = pair.getName(index++);
-    if (index >= levels) {
-      return;
-    }
-    String name = pair.getName(index++);
-    if (name.equals(ENABLED_ATTRIB)) {
-      enabled = true;
-      return;
-    }
-    try {
-      fieldIndex = Integer.parseInt(name);
-    }
-    catch (NumberFormatException e) {
-    }
-    if (index >= levels) {
-      return;
-    }
-    name = pair.getName(index++);
-    if (name.equals(ENABLED_ATTRIB)) {
-      enabled = true;
-      return;
+    else {
+      action = null;
     }
   }
 
-  void setVariable(String variable, String assignment) {
-    if (formattedValue == null) {
-      formattedValue = value;
-    }
-    if (formattedValue == null) {
-      return;
-    }
-    formattedValue = formattedValue.replaceAll(EtomoAutodoc.VAR_TAG + "\\"
-        + "{" + variable + "}", assignment);
-    formattedValue = formattedValue.replaceAll(EtomoAutodoc.VAR_TAG + variable
-        + File.separatorChar, assignment + File.separatorChar);
-  }
-
-  void unsetVariables() {
-    formattedValue = null;
-  }
-
-  void reset() {
+  public void reset() {
     empty = true;
     known = false;
     action = null;
-    field = null;
-    fieldName = null;
-    fieldIndex = 0;
     value = null;
-    formattedValue = null;
     string = "";
-    dialogType = null;
-    processEndState = null;
-    enabled = false;
   }
 
-  UITestAction getAction() {
+  public UITestAction getAction() {
     return action;
   }
 
-  UITestField getField() {
-    return field;
-  }
-
-  String getFieldName() {
-    return fieldName;
-  }
-
-  int getFieldIndex() {
-    return fieldIndex;
-  }
-
-  String getValue() {
-    if (formattedValue != null) {
-      return formattedValue;
-    }
+  public String getValue() {
     return value;
   }
 
@@ -173,26 +69,18 @@ public class UITestCommand {
     return string;
   }
 
-  DialogType getDialogType() {
-    return dialogType;
-  }
-
-  ProcessEndState getProcessEndState() {
-    return processEndState;
-  }
-
-  boolean isEnabled() {
-    return enabled;
-  }
-  
   boolean isEmpty() {
     return empty;
   }
-  
+
   boolean isKnown() {
     return known;
   }
+
+  public boolean isSecondaryAutodoc() {
+    return false;
+  }
 }
 /**
- * <p> $Log$ </p>
- */
+* <p> $Log$ </p>
+*/
