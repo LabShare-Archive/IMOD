ee5f40803d5ac94701864762faf9f7b4d510d992
SueHeld <sueh@colorado.edu>
2006-05-01 21:15:49 +0000

    bug# 787

--- Etomo/src/etomo/storage/autodoc/AdocCommand.java
+++ Etomo/src/etomo/storage/autodoc/AdocCommand.java
@@ -5,7 +5,7 @@ import etomo.type.UITestAction;
 /**
 * <p>Description: </p>
 * 
-* <p>Copyright: Copyright (c) 2006</p>
+* <p>Copyright: Copyright 2006</p>
 *
 * <p>Organization:
 * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
@@ -25,5 +25,8 @@ public interface AdocCommand {
   public String getValue();
 }
 /**
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1  2006/04/28 20:51:51  sueh
+* <p> bug# 787 Interface for commands read by the AdocCommandReader.
+* <p> </p>
 */
