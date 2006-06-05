0ffeb831e7b204a4abab5de8dbb46beaf0a7cd75
SueHeld <sueh@colorado.edu>
2006-06-05 16:27:00 +0000

    bug# 766 Interface for monitors which are responsible for getting the pid.

---
+++ Etomo/src/etomo/process/OutfileProcessMonitor.java
@@ -0,0 +1,22 @@
+package etomo.process;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+public interface OutfileProcessMonitor extends DetachedProcessMonitor {
+  public static final String rcsid = "$Id$";
+  
+  public String getPid();
+}
+/**
+* <p> $Log$ </p>
+*/
