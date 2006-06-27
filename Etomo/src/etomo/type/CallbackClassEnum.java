8122f2f61a634997b150a1e0a57921826f90e3a7
SueHeld <sueh@colorado.edu>
2006-06-27 22:34:21 +0000

    bug# 852 An enumerator of classes that implement CallbackClass.  Can get instances of callback classes.

---
+++ Etomo/src/etomo/type/CallbackClassEnum.java
@@ -0,0 +1,31 @@
+package etomo.type;
+
+import etomo.util.CallbackClass;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+*/
+public final class CallbackClassEnum {
+  public static final String rcsid = "$Id$";
+  
+  public static CallbackClassEnum getInstance(String callbackClassName) {
+    return null;
+  }
+  
+  public CallbackClass getCallbackClass() {
+    return null;
+  }
+}
+/**
+* <p> $Log$ </p>
+*/
