e8931e5bc85d54675709dcd2c5ba8e7af531fd23
SueHeld <sueh@colorado.edu>
2006-07-28 17:41:40 +0000

    bug# 909 Added getInstance(boolean)

--- Etomo/src/etomo/type/MatchMode.java
+++ Etomo/src/etomo/type/MatchMode.java
@@ -1,29 +1,30 @@
 package etomo.type;
+
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright (c) 2006</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-*/
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright (c) 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ */
 public final class MatchMode {
-  public static  final String  rcsid =  "$Id$";
-  
+  public static final String rcsid = "$Id$";
+
   public static final MatchMode B_TO_A = new MatchMode();
   public static final MatchMode A_TO_B = new MatchMode();
-  
+
   private static final String B_TO_A_STRING = "B_TO_A";
   private static final String A_TO_B_STRING = "A_TO_B";
-  
+
   private MatchMode() {
   }
-  
+
   public String toString() {
     if (this == B_TO_A) {
       return B_TO_A_STRING;
@@ -33,7 +34,7 @@ public final class MatchMode {
     }
     return "";
   }
-  
+
   public static MatchMode getInstance(String string) {
     if (string == null) {
       return null;
@@ -46,7 +47,17 @@ public final class MatchMode {
     }
     return null;
   }
+
+  public static MatchMode getInstance(boolean matchBToA) {
+    if (matchBToA) {
+      return B_TO_A;
+    }
+    return A_TO_B;
+  }
 }
 /**
-* <p> $Log$ </p>
-*/
+ * <p> $Log$
+ * <p> Revision 1.1  2006/03/16 01:54:18  sueh
+ * <p> bug# 828 Enum to handle the match direction in Combine.
+ * <p> </p>
+ */
