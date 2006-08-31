0cd4108353de525739afcbc58c015737331be881
mastcu <mast@colorado.edu>
2006-08-31 20:58:26 +0000

    Extract values from model and put back in patch file

--- imodutil/imod2patch.c
+++ imodutil/imod2patch.c
@@ -13,8 +13,137 @@
 $Date$
 
 $Revision$
+Log at end
+*/
 
+#include <stdio.h>
+#include <string.h>
+
+#include "imodel.h"
+#include "istore.h"
+
+static int istoreFindValue(Ilist *list, int index, int type, float *value,
+                           int *listInd);
+
+int main( int argc, char *argv[])
+{
+  int i;
+  FILE *fin, *fout;
+  struct Mod_Model *mod;
+  int npatch = 0;
+  int nvalue = 0;
+  int ix, iy, iz;
+  float dx, dy, dz;
+  float value, maxval = -1.e30;
+  int ob, co, listInd;
+  Ipoint *pts;
+  char format[10] = "%10.4f\n";
+
+  setExitPrefix("ERROR: imod2patch - ");
+
+
+  if (argc != 3){
+    if (argc != 1)
+      printf("ERROR: imod2patch - wrong # of arguments\n");
+    printf("imod2patch usage:\n");
+    printf("imod2patch imod_model patch_file\n");
+    exit(1);
+  }
+
+
+  i = 1;
+
+  mod = imodRead(argv[i]);
+  if (!mod)
+    exitError("Reading model %s\n", argv[i]);
+  
+  if (imodBackupFile(argv[++i]))
+    exitError("Renaming existing output file to %s\n", argv[i]);
+  
+  fout = fopen(argv[i], "w");
+  if (!fout)
+    exitError("Could not open %s\n", argv[i]);
+
+  /* Count up patches and values and find max of values */
+  for (ob = 0; ob < mod->objsize; ob++) {
+    listInd = 0;
+    for (co = 0; co < mod->obj[ob].contsize; co++) {
+      if (mod->obj[ob].cont[co].psize >= 2) {
+        npatch++;
+        if (istoreFindValue(mod->obj[ob].store, co, GEN_STORE_VALUE1, &value,
+                            &listInd)) {
+          maxval = B3DMAX(maxval, value);
+          nvalue++;
+        }
+      }
+    }
+  }
+
+  /* If values are greater than one they are probably residuals and only need
+     2 decimal places */
+  if (nvalue && maxval > 1.01)
+    strcpy(format, "%10.2f\n");
+
+  fprintf(fout, "%d   edited positions\n", npatch);
+  for (ob = 0; ob < mod->objsize; ob++) {
+    listInd = 0;
+    for (co = 0; co < mod->obj[ob].contsize; co++) {
+      if (mod->obj[ob].cont[co].psize >= 2) {
+        pts = mod->obj[ob].cont[co].pts;
+        ix = pts[0].x + 0.5;
+        iy = pts[0].y + 0.5;
+        iz = pts[0].z + 0.5;
+        dx = (pts[1].x - pts[0].x) / mod->pixsize;
+        dy = (pts[1].y - pts[0].y) / mod->pixsize;
+        dz = (pts[1].z - pts[0].z) / mod->pixsize;
+        if (mod->flags & IMODF_FLIPYZ)
+          fprintf(fout, "%6d %5d %5d %8.2f %8.2f %8.2f", 
+                  ix, iz, iy, dx, dz, dy);
+        else
+          fprintf(fout, "%6d %5d %5d %8.2f %8.2f %8.2f", 
+                  ix, iy, iz, dx, dy, dz);
+        if (nvalue) {
+          value = 0.;
+          istoreFindValue(mod->obj[ob].store, co, GEN_STORE_VALUE1, &value,
+                          &listInd);
+          fprintf(fout, format, value);
+          
+        } else
+          fprintf(fout, "\n");
+
+      }
+    }
+  }
+  fclose(fout);
+  exit(0);
+}
+
+
+/* This is meant to be called sequentially for all indexes in the entity,
+   not for random access */
+static int istoreFindValue(Ilist *list, int index, int type, float *value,
+                     int *listInd)
+{
+  Istore *store;
+  while (*listInd < ilistSize(list)) {
+    store = istoreItem(list, *listInd);
+    if ((store->flags & GEN_STORE_NOINDEX) || store->index.i > index)
+      break;
+    (*listInd)++;
+
+    if (store->index.i == index && store->type == type) {
+      *value = store->value.f;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+/*
 $Log$
+Revision 3.6  2005/02/11 00:41:40  mast
+Removed unneeded declaration
+
 Revision 3.5  2004/11/05 19:05:29  mast
 Include local files with quotes, not brackets
 
@@ -31,81 +160,3 @@ Revision 3.1  2002/12/23 21:34:22  mast
 fixed exit status
 
 */
-
-#include <stdio.h>
-#include <string.h>
-
-#include "imodel.h"
-
-
-struct Mod_Model *imod_from_patches(FILE *fin, float scale);
-
-int main( int argc, char *argv[])
-{
-  int i;
-  FILE *fin, *fout;
-  struct Mod_Model *mod;
-  float scale = 10.0;
-  int npatch = 0;
-  int ix, iy, iz;
-  float dx, dy, dz;
-  int ob, co;
-  Ipoint *pts;
-
-  if (argc != 3){
-    if (argc != 1)
-      printf("ERROR: imod2patch - wrong # of arguments\n");
-    printf("imod2patch version 1.0 usage:\n");
-    printf("imod2patch imod_model patch_file\n");
-    exit(1);
-  }
-
-
-  i = 1;
-
-  mod = imodRead(argv[i]);
-  if (!mod){
-    fprintf(stdout, "ERROR: imod2patch - reading model %s\n",
-            argv[i]);
-    exit(3);
-  }
-  
-  if (imodBackupFile(argv[++i])) {
-    fprintf(stdout, "ERROR: imod2patch - renaming existing output file "
-            "to %s\n", argv[i]);
-    exit(1);
-  }
-  
-  fout = fopen(argv[i], "w");
-  if (!fout){
-    fprintf(stdout, "ERROR: imod2patch - Couldn't open %s\n", argv[i]);
-    exit(3);
-  }
-
-  for (ob = 0; ob < mod->objsize; ob++)
-    for (co = 0; co < mod->obj[ob].contsize; co++)
-      if (mod->obj[ob].cont[co].psize >= 2)
-        npatch++;
-
-  fprintf(fout, "%d   edited positions\n", npatch);
-  for (ob = 0; ob < mod->objsize; ob++)
-    for (co = 0; co < mod->obj[ob].contsize; co++)
-      if (mod->obj[ob].cont[co].psize >= 2) {
-        pts = mod->obj[ob].cont[co].pts;
-        ix = pts[0].x + 0.5;
-        iy = pts[0].y + 0.5;
-        iz = pts[0].z + 0.5;
-        dx = (pts[1].x - pts[0].x) / mod->pixsize;
-        dy = (pts[1].y - pts[0].y) / mod->pixsize;
-        dz = (pts[1].z - pts[0].z) / mod->pixsize;
-        if (mod->flags & IMODF_FLIPYZ)
-          fprintf(fout, "%d %d %d %.2f, %.2f, %.2f\n", 
-                  ix, iz, iy, dx, dz, dy);
-        else
-          fprintf(fout, "%d %d %d %.2f, %.2f, %.2f\n", 
-                  ix, iy, iz, dx, dy, dz);
-      }
-  fclose(fout);
-  exit(0);
-}
-
