209c2983b198fbfb97f11a9f6d3c333613b9adbf
mastcu <mast@colorado.edu>
2006-09-28 21:13:23 +0000

    Added huge seek routine

--- include/b3dutil.h
+++ include/b3dutil.h
@@ -11,39 +11,9 @@
 $Date$
 
 $Revision$
-
-$Log$
-Revision 1.10  2006/09/19 16:33:32  mast
-fix prototypes
-
-Revision 1.9  2006/08/27 23:47:00  mast
-Moved fgetline from mrcfiles to b3dutil
-
-Revision 1.8  2006/06/26 14:49:09  mast
-Moved miscellaneous functions to b3dutil
-
-Revision 1.7  2005/02/11 01:41:04  mast
-Swicthed to b3dIMin/Max
-
-Revision 1.6  2004/11/12 15:22:13  mast
-Added min and max functions and macros
-
-Revision 1.5  2004/03/18 17:56:57  mast
-Added header byte count routine
-
-Revision 1.4  2004/01/17 20:34:03  mast
-Add b3d file routines
-
-Revision 1.3  2003/11/04 17:14:21  mast
-Add include of stdio so that FILE is defined
-
-Revision 1.2  2003/11/01 16:41:13  mast
-Add error routines
-
-Revision 1.1  2003/10/24 02:44:36  mast
-Initial creation
-
+Log at end
 */
+
 #ifndef B3DUTIL_H
 #define B3DUTIL_H
 
@@ -74,6 +44,8 @@ extern "C" {
   size_t b3dFwrite(void *buf, size_t size, size_t count, FILE *fp);
   void b3dRewind(FILE *fp);
   int mrc_big_seek(FILE *fp, int base, int size1, int size2, int flag);
+  int mrcHugeSeek(FILE *fp, int base, int x, int y, int z, int nx, int ny, 
+                  int dsize, int flag);
   int fgetline(FILE *fp, char s[],int limit);
 
   void b3dHeaderItemBytes(int *nflags, int *nbytes);
@@ -109,3 +81,40 @@ extern "C" {
 
 
 #endif
+
+/*
+$Log$
+Revision 1.11  2006/09/20 23:02:15  mast
+Added header usage function
+
+Revision 1.10  2006/09/19 16:33:32  mast
+fix prototypes
+
+Revision 1.9  2006/08/27 23:47:00  mast
+Moved fgetline from mrcfiles to b3dutil
+
+Revision 1.8  2006/06/26 14:49:09  mast
+Moved miscellaneous functions to b3dutil
+
+Revision 1.7  2005/02/11 01:41:04  mast
+Swicthed to b3dIMin/Max
+
+Revision 1.6  2004/11/12 15:22:13  mast
+Added min and max functions and macros
+
+Revision 1.5  2004/03/18 17:56:57  mast
+Added header byte count routine
+
+Revision 1.4  2004/01/17 20:34:03  mast
+Add b3d file routines
+
+Revision 1.3  2003/11/04 17:14:21  mast
+Add include of stdio so that FILE is defined
+
+Revision 1.2  2003/11/01 16:41:13  mast
+Add error routines
+
+Revision 1.1  2003/10/24 02:44:36  mast
+Initial creation
+
+*/
