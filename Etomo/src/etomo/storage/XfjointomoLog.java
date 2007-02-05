ababe8c0fc600ee7694df548ae44d41c3bf5b50b
SueHeld <sueh@colorado.edu>
2007-02-05 23:08:48 +0000

    bug# 962 Class for reading xfjointomo log file.

---
+++ Etomo/src/etomo/storage/XfjointomoLog.java
@@ -0,0 +1,205 @@
+package etomo.storage;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import etomo.BaseManager;
+import etomo.type.EtomoNumber;
+import etomo.util.DatasetFiles;
+
+/**
+ * <p>Description: Class to read the xfjointomo log.  This class should try to
+ * avoid throwing runtime errors because this will keep the process tracking
+ * functionality from completing.</p>
+ * 
+ * <p>Copyright: Copyright 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+public final class XfjointomoLog {
+  public static final String rcsid = "$Id$";
+
+  private static final Hashtable INSTANCE_LIST = new Hashtable();
+  private static final int MAX_ERROR_INDEX = 14;
+
+  private final Hashtable rowList = new Hashtable();
+  private final Vector rowArray = new Vector();
+  private final String dir;
+  private LogFile logFile = null;
+
+  public static XfjointomoLog getInstance(BaseManager manager) {
+    XfjointomoLog instance = (XfjointomoLog) INSTANCE_LIST
+        .get(getUniqueKey(manager));
+    if (instance != null) {
+      return instance;
+    }
+    return createInstance(manager);
+  }
+
+  public void reset() {
+    logFile = null;
+  }
+
+  public boolean rowExists(String boundary) throws LogFile.ReadException {
+    load();
+    return rowList.containsKey(boundary);
+  }
+
+  public String getBestGap(String boundary) throws LogFile.ReadException {
+    load();
+    Row row = (Row) rowList.get(boundary);
+    if (row == null) {
+      return null;
+    }
+    return row.getBestGap();
+  }
+
+  public String getMeanError(String boundary) throws LogFile.ReadException {
+    load();
+    Row row = (Row) rowList.get(boundary);
+    if (row == null) {
+      return null;
+    }
+    return row.getMeanError();
+  }
+
+  public String getMaxError(String boundary) throws LogFile.ReadException {
+    load();
+    Row row = (Row) rowList.get(boundary);
+    if (row == null) {
+      return null;
+    }
+    return row.getMaxError();
+  }
+  
+  public synchronized boolean gapsExist() throws LogFile.ReadException {
+    load();
+    boolean gapsExist =false;
+    for (int i=0;i<rowArray.size();i++) {
+      gapsExist = gapsExist||((Row)rowArray.get(i)).gapExists();
+    }
+    return gapsExist;
+  }
+
+  private XfjointomoLog(BaseManager manager) {
+    dir = manager.getPropertyUserDir();
+  }
+
+  private synchronized static XfjointomoLog createInstance(BaseManager manager) {
+    //make sure another thread didn't already run createInstance for this manager
+    XfjointomoLog instance = (XfjointomoLog) INSTANCE_LIST
+        .get(getUniqueKey(manager));
+    if (instance != null) {
+      return instance;
+    }
+    //create instance
+    instance = new XfjointomoLog(manager);
+    INSTANCE_LIST.put(getUniqueKey(manager), instance);
+    return instance;
+  }
+
+  /**
+   * Unlike .edj files, multiple .ejf files can be placed in one directory.
+   * @param manager
+   * @return
+   */
+  private static String getUniqueKey(BaseManager manager) {
+    return manager.getPropertyUserDir() + manager.getName();
+  }
+
+  /**
+   * Load data from the log file.  Or reload data, if reset() has been called.
+   * @throws LogFile.ReadException
+   */
+  private synchronized void load() throws LogFile.ReadException {
+    if (logFile != null) {
+      return;
+    }
+    rowList.clear();
+    logFile = LogFile.getInstance(dir, DatasetFiles.XFJOINTOMO_LOG);
+    long readId = logFile.openReader();
+    String boundary = null;
+    String line = null;
+    while ((line = logFile.readLine(readId)) != null) {
+      if (line.indexOf("At boundary") != -1) {
+        //load the data from the line
+        String[] stringArray = line.trim().split("\\s+");
+        if (stringArray.length < MAX_ERROR_INDEX + 1) {
+          continue;
+        }
+        boundary = parseBoundary(stringArray);
+        if (boundary == null || boundary.equals("")) {
+          continue;
+        }
+        Row row = new Row(parseBestGap(stringArray),
+            parseMeanError(stringArray), parseMaxError(stringArray));
+        rowList.put(boundary, row);
+        rowArray.add(row);
+      }
+    }
+    logFile.closeReader(readId);
+  }
+
+  private String parseBoundary(String[] stringArray) {
+    return trimTrailingComma(stringArray[2].trim());
+  }
+
+  private String parseBestGap(String[] stringArray) {
+    return stringArray[6].trim();
+  }
+
+  private String parseMeanError(String[] stringArray) {
+    return trimTrailingComma(stringArray[11].trim());
+  }
+
+  private String parseMaxError(String[] stringArray) {
+    return stringArray[MAX_ERROR_INDEX].trim();
+  }
+
+  private String trimTrailingComma(String string) {
+    int lastCharIndex = string.length() - 1;
+    if (string.charAt(lastCharIndex) == ',') {
+      if (string.length() == 1) {
+        return "";
+      }
+      return string.substring(0, lastCharIndex);
+    }
+    return string;
+  }
+
+  private final class Row {
+    private final EtomoNumber bestGap=new EtomoNumber(EtomoNumber.Type.FLOAT);
+    private final String meanError;
+    private final String maxError;
+
+    Row(String bestGap, String meanError, String maxError) {
+      this.bestGap.set(bestGap);
+      this.meanError = meanError;
+      this.maxError = maxError;
+    }
+
+    String getBestGap() {
+      return bestGap.toString();
+    }
+
+    String getMeanError() {
+      return meanError;
+    }
+
+    String getMaxError() {
+      return maxError;
+    }
+    
+    boolean gapExists() {
+      return  !bestGap.equals(0);
+    }
+  }
+}
