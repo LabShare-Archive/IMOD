67c181e85eb9645abc85cc81df9c6de814144dd7
SueHeld <sueh@colorado.edu>
2007-02-05 23:33:17 +0000

    bug# 962 Composite display class containing a spinner and a button.

---
+++ Etomo/src/etomo/ui/BinnedXY3dmodButton.java
@@ -0,0 +1,96 @@
+package etomo.ui;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SpinnerNumberModel;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+final class BinnedXY3dmodButton {
+  public static final String rcsid = "$Id$";
+  private final Run3dmodButton button;
+  private final LabeledSpinner spinner;
+  private final JLabel label;
+  private JPanel panel = null;
+
+  BinnedXY3dmodButton(String label, Run3dmodButtonContainer container) {
+    spinner = new LabeledSpinner("Open binned by ", new SpinnerNumberModel(1,
+        1, 50, 1));
+    this.label = new JLabel(" in X and Y");
+    button = new Run3dmodButton(label, container);
+  }
+
+  Container getContainer() {
+    if (panel == null) {
+      panel = new JPanel();
+      panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+      panel.add(Box.createHorizontalGlue());
+      panel.add(Box.createHorizontalGlue());
+      panel.add(Box.createHorizontalGlue());
+      SpacedPanel borderPanel = new SpacedPanel();
+      borderPanel.setBoxLayout(BoxLayout.Y_AXIS);
+      borderPanel.setBorder(BorderFactory.createEtchedBorder());
+      borderPanel.setComponentAlignmentX(Component.CENTER_ALIGNMENT);
+      JPanel spinnerPanel = new JPanel();
+      spinnerPanel.setLayout(new BoxLayout(spinnerPanel, BoxLayout.X_AXIS));
+      spinnerPanel.add(spinner.getContainer());
+      spinnerPanel.add(label);
+      borderPanel.add(spinnerPanel);
+      button.setSize();
+      borderPanel.add(button);
+      panel.add(borderPanel.getContainer());
+      panel.add(Box.createHorizontalGlue());
+      panel.add(Box.createHorizontalGlue());
+      panel.add(Box.createHorizontalGlue());
+    }
+    return panel;
+  }
+
+  void setSpinnerTooltip(String tooltip) {
+    tooltip = new TooltipFormatter().setText(tooltip).format();
+    spinner.setToolTipText(tooltip);
+    label.setToolTipText(tooltip);
+  }
+
+  void setButtonTooltip(String tooltip) {
+    button.setToolTipText(new TooltipFormatter().setText(tooltip).format());
+  }
+
+  void addActionListener(ActionListener actionListener) {
+    button.addActionListener(actionListener);
+  }
+
+  String getActionCommand() {
+    return button.getActionCommand();
+  }
+
+  int getInt() {
+    return ((Integer) spinner.getValue()).intValue();
+  }
+
+  void setEnabled(boolean enabled) {
+    spinner.setEnabled(enabled);
+    label.setEnabled(enabled);
+    button.setEnabled(enabled);
+  }
+}
