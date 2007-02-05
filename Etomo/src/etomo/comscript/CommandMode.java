fdf370647a6414899402b4815743376e656d9bcb
SueHeld <sueh@colorado.edu>
2007-02-05 21:35:41 +0000

    bug# 962 An interface for inner Mode classes.

---
+++ Etomo/src/etomo/comscript/CommandMode.java
@@ -0,0 +1,20 @@
+package etomo.comscript;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+interface CommandMode {
+  public static final String rcsid = "$Id$";
+}
