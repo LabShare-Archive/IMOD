8a76bbaa182a2874734dcee17a32197b802928fd
SueHeld <sueh@colorado.edu>
2007-02-05 23:05:20 +0000

    bug# 962 Filter for model files.

---
+++ Etomo/src/etomo/storage/ModelFileFilter.java
@@ -0,0 +1,39 @@
+package etomo.storage;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+import etomo.util.DatasetFiles;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public final class ModelFileFilter  extends FileFilter implements
+java.io.FileFilter {
+  public static  final String  rcsid =  "$Id$";
+  
+  public boolean accept(File f) {
+    String filePath = f.getAbsolutePath();
+    //  If this is a file test its extension, all others should return true
+    if (f.isFile() && !filePath.endsWith(DatasetFiles.MODEL_EXT)) {
+      return false;
+    }
+    return true;
+  }
+
+  public String getDescription() {
+    return "Model file";
+  }
+}
