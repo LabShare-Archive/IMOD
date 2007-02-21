2f95340718e160b8b961c59e937e2b18d7d530b5
SueHeld <sueh@colorado.edu>
2007-02-21 04:18:57 +0000

    bug# 964 Const interface for PanelHeaderState.

---
+++ Etomo/src/etomo/type/ConstPanelHeaderState.java
@@ -0,0 +1,23 @@
+package etomo.type;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface ConstPanelHeaderState {
+  public static  final String  rcsid =  "$Id$";
+  
+  public String getOpenCloseState();
+  public  String getAdvancedBasicState();
+  public  String getMoreLessState();
+}
