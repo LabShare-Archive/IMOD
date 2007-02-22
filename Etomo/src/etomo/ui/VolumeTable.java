1d67e5c648221645b7c09044a360e736ab942a0d
SueHeld <sueh@colorado.edu>
2007-02-22 20:40:00 +0000

    bug# 964 Added addRow().

--- Etomo/src/etomo/ui/VolumeTable.java
+++ Etomo/src/etomo/ui/VolumeTable.java
@@ -1,6 +1,8 @@
 package etomo.ui;
 
-import java.awt.Component;
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -10,8 +12,10 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 import etomo.PeetManager;
+import etomo.storage.TomogramFileFilter;
 
 /**
  * <p>Description: </p>
@@ -27,25 +31,39 @@ import etomo.PeetManager;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 1.2  2007/02/21 04:25:18  sueh
+ * <p> bug# 964 Changed PeetManager.setName() to PeetManager.setParamFile().
+ * <p>
  * <p> Revision 1.1  2007/02/20 20:37:32  sueh
  * <p> bug# 964 A list of tomograms for the PEET interface.
  * <p> </p>
  */
-final class VolumeTable {
+final class VolumeTable implements Expandable {
   public static final String rcsid = "$Id$";
 
   private final RowList rowList = new RowList();
   private final MultiLineButton btnAddTomogram = new MultiLineButton(
       "Add Tomogram");
   private final JPanel pnlButtons = new JPanel();
+  private final HeaderCell header1Tomogram = new HeaderCell("Tomogram");
+  private final ExpandButton tomogramExpandButton;
+  private final JPanel pnlTable = new JPanel();
+  private final GridBagLayout layout = new GridBagLayout();
+  private final GridBagConstraints constraints = new GridBagConstraints();
   private final PeetManager manager;
   private JPanel rootPanel = null;
 
-  VolumeTable(PeetManager manager) {
-    this.manager = manager;
+  public void expand(ExpandButton button) {
+
   }
 
-  Component getComponent() {
+  VolumeTable(PeetManager manager) {
+    this.manager = manager;
+    tomogramExpandButton = ExpandButton.getInstance(this,
+        ExpandButton.Type.MORE);
+  }
+
+  Container getContainer() {
     create();
     return rootPanel;
   }
@@ -58,13 +76,35 @@ final class VolumeTable {
     if (rootPanel != null) {
       return;
     }
+    //table
+    pnlTable.setLayout(layout);
+    pnlTable.setBorder(LineBorder.createBlackLineBorder());
+    constraints.fill = GridBagConstraints.BOTH;
+    //headers
+    //First row
+    constraints.anchor = GridBagConstraints.CENTER;
+    constraints.weightx = 0.1;
+    constraints.weighty = 0.0;
+    constraints.gridheight = 1;
+    constraints.gridwidth = 1;
+    header1Tomogram.add(pnlTable, layout, constraints);
+    constraints.gridwidth = GridBagConstraints.REMAINDER;
+    constraints.weightx = 0.0;
+    tomogramExpandButton.add(pnlTable, layout, constraints);
+    //border
+    SpacedPanel pnlBorder = new SpacedPanel();
+    pnlBorder.setBoxLayout(BoxLayout.Y_AXIS);
+    pnlBorder.setBorder(new EtchedBorder("Boundary Table").getBorder());
+    pnlBorder.add(pnlTable);
     //buttons
     pnlButtons.setLayout(new BoxLayout(pnlButtons, BoxLayout.X_AXIS));
     btnAddTomogram.setSize();
     pnlButtons.add(btnAddTomogram.getComponent());
     //root
     rootPanel = new JPanel();
+    rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.Y_AXIS));
     rootPanel.setBorder(BorderFactory.createEtchedBorder());
+    rootPanel.add(pnlBorder.getContainer());
     rootPanel.add(pnlButtons);
     //actions
     btnAddTomogram.addActionListener(new VTActionListener(this));
@@ -85,6 +125,19 @@ final class VolumeTable {
     }
     JFileChooser chooser = new JFileChooser(new File(manager
         .getPropertyUserDir()));
+    chooser.setFileFilter(new TomogramFileFilter());
+    chooser.setPreferredSize(UIParameters.INSTANCE.getFileChooserDimension());
+    chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    int returnVal = chooser.showOpenDialog(rootPanel);
+    if (returnVal == JFileChooser.APPROVE_OPTION) {
+      addRow(chooser.getSelectedFile());
+    }
+  }
+
+  private void addRow(File tomogram) {
+    rowList.add(tomogram, pnlTable, layout, constraints, tomogramExpandButton
+        .isExpanded());
+    UIHarness.INSTANCE.pack(manager);
   }
 
   private static final class RowList {
@@ -92,6 +145,14 @@ final class VolumeTable {
 
     int size() {
       return list.size();
+    }
+
+    synchronized void add(File tomogram, JPanel panel, GridBagLayout layout,
+        GridBagConstraints constraints, boolean expanded) {
+      VolumeRow row = new VolumeRow(tomogram, list.size(), panel, layout,
+          constraints, expanded);
+      list.add(row);
+      row.display();
     }
   }
 
