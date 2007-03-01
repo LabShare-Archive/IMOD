0d08486c1510ec65d5c887db94c3b097f5ed8364
SueHeld <sueh@colorado.edu>
2007-03-01 01:35:47 +0000

    bug# 964 Interface to use with HighlighterButton.

---
+++ Etomo/src/etomo/ui/Highlightable.java
@@ -0,0 +1,21 @@
+package etomo.ui;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+interface Highlightable {
+  public static  final String  rcsid =  "$Id$";
+  
+  void highlight(boolean highlight);
+}
