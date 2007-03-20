86e7421de488ce0e426b329f44a537ac46e44fcb
SueHeld <sueh@colorado.edu>
2007-03-20 23:03:17 +0000

    bug# 964 Added getInitMotlFile, getTiltRangeStart, and getTiltRangeEnd.

--- Etomo/src/etomo/type/ConstPeetMetaData.java
+++ Etomo/src/etomo/type/ConstPeetMetaData.java
@@ -12,10 +12,16 @@ package etomo.type;
 * 
 * @version $Revision$
 * 
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1  2007/02/21 04:19:17  sueh
+* <p> bug# 964 Const interface for PeetMetaData.
+* <p> </p>
 */
 public interface ConstPeetMetaData {
   public static  final String  rcsid =  "$Id$";
   
   public String getName();
+  public String getInitMotlFile(int key);
+  public String getTiltRangeStart(int key);
+  public String getTiltRangeEnd(int key);
 }
