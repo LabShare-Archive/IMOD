e7e26e11af00b4ba9930766cac3f3c27f6001df5
mastcu <mast@colorado.edu>
2007-04-07 21:35:49 +0000

    Switched to exitError

--- flib/blend/shuffler.f
+++ flib/blend/shuffler.f
@@ -4,24 +4,8 @@ c       The routine looks to see if it is already in memory; if not, it loads
 c       the section, replacing the frame that has not been used in the
 c       longest time, and returns the index of the frame's start in INDEX
 c       
-c       $Author$
-c       
-c       $Date$
-c       
-c       $Revision$
-c       
-c       $Log$
-c       Revision 3.3  2005/02/28 21:17:09  mast
-c       Implemented a piece to memory index lookup table, made it compose
-c       distortion/gradient field whenever it loads a piece, and made it
-c       undistort the piece when edge functions are being built
-c	
-c       Revision 3.2  2003/06/20 20:19:22  mast
-c       Used standard error exit
-c	
-c       Revision 3.1  2002/08/19 04:28:29  mast
-c       Changed to use include file and to not take ARRAY as an argument.
-c	
+c       $Id$
+c       Log at end of file
 c       
       subroutine shuffler(izwant,index)
 c       
@@ -129,7 +113,7 @@ c
         endif
       endif
       return
-99    call errorexit('reading file')
+99    call exitError('READING FILE')
       end
 
 
@@ -151,3 +135,19 @@ c
       return
       end
 
+c       
+c       $Log$
+c       Revision 3.4  2006/02/06 21:52:55  mast
+c       Chnaged to use tilt angles separate from mag gradient file
+c
+c       Revision 3.3  2005/02/28 21:17:09  mast
+c       Implemented a piece to memory index lookup table, made it compose
+c       distortion/gradient field whenever it loads a piece, and made it
+c       undistort the piece when edge functions are being built
+c	
+c       Revision 3.2  2003/06/20 20:19:22  mast
+c       Used standard error exit
+c	
+c       Revision 3.1  2002/08/19 04:28:29  mast
+c       Changed to use include file and to not take ARRAY as an argument.
+c	
