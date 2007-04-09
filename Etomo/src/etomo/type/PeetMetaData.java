19828c5e8a023f5aa0b255b501a1d1e118bf213b
SueHeld <sueh@colorado.edu>
2007-04-09 21:11:33 +0000

    bug# 964 Added support for reference.

--- Etomo/src/etomo/type/PeetMetaData.java
+++ Etomo/src/etomo/type/PeetMetaData.java
@@ -18,7 +18,12 @@ import etomo.util.DatasetFiles;
  * 
  * @version $Revision$
  * 
+ * @notthreadsafe
+ * 
  * <p> $Log$
+ * <p> Revision 1.7  2007/03/26 23:35:39  sueh
+ * <p> bug# 964 Setting axisType.
+ * <p>
  * <p> Revision 1.6  2007/03/20 23:04:01  sueh
  * <p> bug# 964 Added initMotlFile, and tiltRangeStart, and tiltRangeEnd.
  * <p>
@@ -43,35 +48,44 @@ public class PeetMetaData extends BaseMetaData implements ConstPeetMetaData {
 
   public static final String NEW_TITLE = "PEET";
 
-  private static final String ROOT_NAME_KEY = "RootName";
   private static final String TILT_RANGE_KEY = "TiltRange";
   private static final String GROUP_KEY = "Peet";
-  private String rootName = "";
-  private IntKeyList initMotlFile = IntKeyList.getStringInstance("InitMotlFile");
-  private IntKeyList tiltRangeStart = IntKeyList.getStringInstance("TILT_RANGE_KEY"+".Start");
-  private IntKeyList tiltRangeEnd = IntKeyList.getStringInstance("TILT_RANGE_KEY"+".End");
+  private static final String REFERENCE_KEY = "Reference";
+  private final StringProperty rootName = new StringProperty("RootName");
+  private final IntKeyList initMotlFile = IntKeyList
+      .getStringInstance("InitMotlFile");
+  private final IntKeyList tiltRangeStart = IntKeyList
+      .getStringInstance("TILT_RANGE_KEY" + ".Start");
+  private final IntKeyList tiltRangeEnd = IntKeyList
+      .getStringInstance("TILT_RANGE_KEY" + ".End");
+  private final EtomoNumber referenceVolume = new EtomoNumber(REFERENCE_KEY
+      + ".Volume");
+  private final EtomoNumber referenceParticle = new EtomoNumber(REFERENCE_KEY
+      + ".Particle");
+  private final StringProperty referenceFile = new StringProperty(REFERENCE_KEY
+      + ".File");
 
   public PeetMetaData() {
     fileExtension = DatasetFiles.PEET_DATA_FILE_EXT;
-    axisType  = AxisType.SINGLE_AXIS;
+    axisType = AxisType.SINGLE_AXIS;
   }
 
   public String getMetaDataFileName() {
-    if (rootName == null) {
+    if (rootName.isEmpty()) {
       return null;
     }
-    return DatasetFiles.getParallelDataFileName(rootName);
+    return DatasetFiles.getPeetDataFileName(rootName.toString());
   }
 
   public String getName() {
     if (rootName.equals("")) {
       return NEW_TITLE;
     }
-    return rootName;
+    return rootName.toString();
   }
 
   public void setName(String name) {
-    rootName = name;
+    rootName.set(name);
   }
 
   public boolean isValid() {
@@ -83,11 +97,11 @@ public class PeetMetaData extends BaseMetaData implements ConstPeetMetaData {
    * @return error message if invalid
    */
   public String validate() {
-    if (rootName.equals("") || rootName.matches("\\s*")) {
+    if (rootName.isEmpty()) {
       return "Missing root name.";
     }
-    if (rootName.indexOf(File.pathSeparatorChar) != -1
-        || rootName.indexOf(File.separatorChar) != -1) {
+    if (rootName.toString().indexOf(File.pathSeparatorChar) != -1
+        || rootName.toString().indexOf(File.separatorChar) != -1) {
       return "Invalid root name, " + rootName + ".";
     }
     return null;
@@ -101,43 +115,73 @@ public class PeetMetaData extends BaseMetaData implements ConstPeetMetaData {
     reset();
     prepend = createPrepend(prepend);
     String group = prepend + ".";
-    rootName = props.getProperty(group + ROOT_NAME_KEY,"");
-    initMotlFile.load(props,prepend);
-    tiltRangeStart.load(props,prepend);
-    tiltRangeEnd.load(props,prepend);
+    rootName.load(props,prepend);
+    initMotlFile.load(props, prepend);
+    tiltRangeStart.load(props, prepend);
+    tiltRangeEnd.load(props, prepend);
+    referenceVolume.load(props, prepend);
+    referenceParticle.load(props, prepend);
+    referenceFile.load(props,prepend);
   }
 
   public void store(Properties props, String prepend) {
     prepend = createPrepend(prepend);
     String group = prepend + ".";
-    props.setProperty(group + ROOT_NAME_KEY, rootName);
-    initMotlFile.store(props,prepend);
-    tiltRangeStart.store(props,prepend);
-    tiltRangeEnd.store(props,prepend);
+    rootName.store(props,prepend);
+    initMotlFile.store(props, prepend);
+    tiltRangeStart.store(props, prepend);
+    tiltRangeEnd.store(props, prepend);
+    referenceVolume.store(props, prepend);
+    referenceParticle.store(props, prepend);
+    referenceFile.store(props,  prepend);
   }
-  
+
   public String getInitMotlFile(int key) {
     return initMotlFile.getString(key);
   }
-  
-  public void setInitMotlFile(String initMotlFile,int key) {
-    this.initMotlFile.put(key,initMotlFile);
+
+  public void setInitMotlFile(String initMotlFile, int key) {
+    this.initMotlFile.put(key, initMotlFile);
   }
-  
-  public void setTiltRangeStart(String tiltRangeStart,int key) {
-    this.tiltRangeStart.put(key,tiltRangeStart);
+
+  public void setTiltRangeStart(String tiltRangeStart, int key) {
+    this.tiltRangeStart.put(key, tiltRangeStart);
   }
-  
-  public void setTiltRangeEnd(String tiltRangeEnd,int key) {
-    this.tiltRangeEnd.put(key,tiltRangeEnd);
+
+  public void setTiltRangeEnd(String tiltRangeEnd, int key) {
+    this.tiltRangeEnd.put(key, tiltRangeEnd);
   }
-  
+
   public String getTiltRangeStart(int key) {
     return tiltRangeStart.getString(key);
   }
-  
+
   public String getTiltRangeEnd(int key) {
     return tiltRangeEnd.getString(key);
+  }
+
+  public String getReferenceFile() {
+    return referenceFile.toString();
+  }
+
+  public ConstEtomoNumber getReferenceParticle() {
+    return referenceParticle;
+  }
+
+  public ConstEtomoNumber getReferenceVolume() {
+    return referenceVolume;
+  }
+
+  public void setReferenceFile(String referenceFile) {
+    this.referenceFile.set(referenceFile);
+  }
+
+  public void setReferenceParticle(String referenceParticle) {
+    this.referenceParticle.set(referenceParticle);
+  }
+
+  public void setReferenceVolume(Number referenceVolume) {
+    this.referenceVolume.set(referenceVolume);
   }
 
   private static String createPrepend(String prepend) {
@@ -151,5 +195,8 @@ public class PeetMetaData extends BaseMetaData implements ConstPeetMetaData {
     initMotlFile.reset();
     tiltRangeStart.reset();
     tiltRangeEnd.reset();
+    referenceVolume.reset();
+    referenceParticle.reset();
+    referenceFile.reset();
   }
 }
