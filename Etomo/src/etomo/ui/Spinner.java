3f90e6448e4565e7daf2f579f0075f098a218272
SueHeld <sueh@colorado.edu>
2007-04-13 21:52:51 +0000

    bug# 964 Added setValue(ParsedElement).

--- Etomo/src/etomo/ui/Spinner.java
+++ Etomo/src/etomo/ui/Spinner.java
@@ -8,6 +8,7 @@ import javax.swing.SpinnerNumberModel;
 import etomo.EtomoDirector;
 import etomo.storage.autodoc.AutodocTokenizer;
 import etomo.type.ConstEtomoNumber;
+import etomo.type.ParsedElement;
 import etomo.type.UITestField;
 import etomo.util.Utilities;
 
@@ -25,6 +26,9 @@ import etomo.util.Utilities;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 1.2  2007/04/09 21:22:34  sueh
+ * <p> bug# 964 Added Number getValue().
+ * <p>
  * <p> Revision 1.1  2007/03/30 23:53:15  sueh
  * <p> bug# 964 A self-naming unlabeled spinner.
  * <p> </p>
@@ -61,6 +65,15 @@ final class Spinner {
     model.setMaximum(new Integer(max));
   }
 
+  void setValue(ParsedElement value) {
+    if (value.isEmpty()) {
+      spinner.setValue((Integer) model.getMinimum());
+    }
+    else {
+      spinner.setValue(value.getRawNumber());
+    }
+  }
+  
   void setValue(ConstEtomoNumber value) {
     if (value.isNull()) {
       spinner.setValue((Integer) model.getMinimum());
