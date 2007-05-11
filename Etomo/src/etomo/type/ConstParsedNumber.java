43e04244259f08a0048aa0a0906611e80fd6c68e
SueHeld <sueh@colorado.edu>
2007-05-11 15:57:32 +0000

    bug# 964 A constant interface for ParsedNumber.

---
+++ Etomo/src/etomo/type/ConstParsedNumber.java
@@ -0,0 +1,19 @@
+package etomo.type;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface ConstParsedNumber {
+  public static  final String  rcsid =  "$Id$";
+}
