53fb8c6ae2f8966ad8b408df6d9ec2e36b953f9e
SueHeld <sueh@colorado.edu>
2007-05-18 23:54:07 +0000

    bug# 987 Added getLabeledInstance() for a non-default model.

--- Etomo/src/etomo/ui/Spinner.java
+++ Etomo/src/etomo/ui/Spinner.java
@@ -1,8 +1,10 @@
 package etomo.ui;
 
-import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 
 import javax.swing.BoxLayout;
+import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
@@ -29,6 +31,9 @@ import etomo.util.Utilities;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 1.4  2007/05/01 22:30:14  sueh
+ * <p> bug# 964 Added getLabeledInstance.
+ * <p>
  * <p> Revision 1.3  2007/04/13 21:52:51  sueh
  * <p> bug# 964 Added setValue(ParsedElement).
  * <p>
@@ -42,12 +47,15 @@ import etomo.util.Utilities;
 final class Spinner {
   public static final String rcsid = "$Id$";
 
-  private SpinnerNumberModel model = new SpinnerNumberModel(1, 1, 1, 1);
-  private JSpinner spinner = new JSpinner(model);
+  private final SpinnerNumberModel model;
+  private final JSpinner spinner;
   private JPanel panel = null;
   private JLabel label = null;
 
-  private Spinner(String label, boolean labeled) {
+  private Spinner(final String label, final boolean labeled, final int value,
+      final int minimum, final int maximum) {
+    model = new SpinnerNumberModel(value, minimum, maximum, 1);
+    spinner = new JSpinner(model);
     String name = Utilities.convertLabelToName(label);
     spinner.setName(name);
     if (EtomoDirector.getInstance().isPrintNames()) {
@@ -62,43 +70,65 @@ final class Spinner {
       panel.add(this.label);
       panel.add(spinner);
     }
+    // Set the maximum height of the text field box to twice the
+    // font size since it is not set by default
+    Dimension maxSize = spinner.getMaximumSize();
+    if (this.label != null
+        && this.label.getFont().getSize() > spinner.getFont().getSize()) {
+      maxSize.setSize(maxSize.getWidth(), 2 * this.label
+          .getFont().getSize());
+    }
+    else {
+      maxSize.setSize(maxSize.getWidth(), 2 * spinner
+          .getFont().getSize());
+    }
+    spinner.setMaximumSize(maxSize);
+  }
+  
+  private final JFormattedTextField getTextField() {
+    return ((JSpinner.DefaultEditor) spinner.getEditor()).getTextField();
   }
 
-  static Spinner getInstance(String name) {
-    return new Spinner(name, false);
+  static Spinner getInstance(final String name) {
+    return new Spinner(name, false, 1, 1, 1);
   }
 
-  static Spinner getLabeledInstance(String label) {
-    return new Spinner(label, true);
+  static Spinner getLabeledInstance(final String label) {
+    return new Spinner(label, true, 1, 1, 1);
   }
 
-  void setToolTipText(String text) {
+  static Spinner getLabeledInstance(final String label, final int value,
+      final int minimum, final int maximum) {
+    return new Spinner(label, true, value, minimum, maximum);
+  }
+
+  void setToolTipText(final String text) {
     String tooltip = TooltipFormatter.INSTANCE.format(text);
     spinner.setToolTipText(tooltip);
-    if (label!=null) {
+    if (label != null) {
       label.setToolTipText(tooltip);
     }
   }
 
-  Component getComponent() {
+  Container getContainer() {
     if (panel == null) {
       return spinner;
     }
     return panel;
   }
 
-  void setEnabled(boolean enabled) {
+  void setEnabled(final boolean enabled) {
     spinner.setEnabled(enabled);
-    if (label!=null) {
+    if (label != null) {
       label.setEnabled(enabled);
     }
   }
 
-  void setMax(int max) {
+  void setMax(final int max) {
     model.setMaximum(new Integer(max));
   }
 
-  void setValue(ParsedElement value) {
+  void setValue(final ParsedElement value) {
     if (value.isEmpty()) {
       spinner.setValue((Integer) model.getMinimum());
     }
@@ -107,7 +137,7 @@ final class Spinner {
     }
   }
 
-  void setValue(ConstEtomoNumber value) {
+  void setValue(final ConstEtomoNumber value) {
     if (value.isNull()) {
       spinner.setValue((Integer) model.getMinimum());
     }
