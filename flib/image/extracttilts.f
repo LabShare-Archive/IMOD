dc0a838d22a371697e816ffe9e07da06ba85b592
mastcu <mast@colorado.edu>
2007-05-19 00:04:12 +0000

    Added exposure dose

--- flib/image/extracttilts.f
+++ flib/image/extracttilts.f
@@ -1,4 +1,4 @@
-*************EXTRACTTILTS.FOR**********************************************
+*************EXTRACTTILTS.F**********************************************
 c       
 c       EXTRACTTILTS will extract tilt angles or other per-section 
 c       information from the header of an image file, if they are present,
@@ -8,28 +8,8 @@ c       See man page for more details
 c       
 c       David Mastronarde, 1/2/00
 c       
-c       $Author$
-c       
-c       $Date$
-c       
-c       $Revision$
-c       
-c       $Log$
-c       Revision 3.5  2006/02/24 22:11:15  mast
-c       Exit with ERROR if no items found in header
-c
-c       Revision 3.4  2004/04/02 00:50:25  mast
-c       Had to initialize iftilt
-c	
-c       Revision 3.3  2004/03/19 04:34:19  mast
-c       had to declare lnblnk
-c	
-c       Revision 3.2  2004/03/18 16:37:44  mast
-c       Converted to PIP and added options for extracting other info
-c	
-c       Revision 3.1  2003/06/05 00:11:14  mast
-c       Change STOP to standardized ERROR exit
-c	
+c       $Id$
+c       Log at end of file
 c       
       implicit none
       integer maxextra, maxtilts, maxpiece
@@ -39,13 +19,13 @@ c
       real*4 array(maxextra/4)
       integer*4 ixpiece(maxpiece),iypiece(maxpiece),izpiece(maxpiece)
 C       
-      CHARACTER*120 FILIN,filout
-      character*16 typeText(5) /'tilt angle', ' ', 'stage position',
-     &    'magnification', 'intensity value'/
+      CHARACTER*160 FILIN,filout
+      character*16 typeText(6) /'tilt angle', ' ', 'stage position',
+     &    'magnification', 'intensity value', 'exposure dose'/
 C       
       integer*4 nz, ierr, iftilt, ifmag, ifstage, npiece, i, nbyte, iflags
       integer*4 maxz, iunout, lenText, mode, itype, ifc2, ntilt, nbsym
-      integer*4 ntiltout, lnblnk
+      integer*4 ntiltout, lnblnk, ifdose
       real*4 dmin, dmax, dmean
       EQUIVALENCE (Nz,NXYZ(3))
 c       
@@ -56,19 +36,19 @@ c
 c       fallbacks from ../../manpages/autodoc2man -2 2  extracttilts
 c       
       integer numOptions
-      parameter (numOptions = 7)
+      parameter (numOptions = 8)
       character*(40 * numOptions) options(1)
       options(1) =
-     &    'input:InputFile:FN:@output:OutputFile:FN:@'//
-     &    'tilts:TiltAngles:B:@stage:StagePositions:B:@'//
-     &    'mag:Magnifications:B:@intensities:Intensities:B:@'//
-     &    'help:usage:B:'
+     &    'input:InputFile:FN:@output:OutputFile:FN:@tilts:TiltAngles:B:@'//
+     &    'stage:StagePositions:B:@mag:Magnifications:B:@'//
+     &    'intensities:Intensities:B:@exp:ExposureDose:B:@help:usage:B:'
 C       
       filout = ' '
       ifmag = 0
       ifstage = 0
       ifC2 = 0
       iftilt = 0
+      ifdose = 0
 c       
 c       Pip startup: set error, parse options, check help, set flag if used
 c       
@@ -87,13 +67,15 @@ c
         ierr = PipGetBoolean('Magnifications', ifmag)
         ierr = PipGetBoolean('StagePositions', ifstage)
         ierr = PipGetBoolean('Intensities', ifC2)
-        if (iftilt + ifmag + ifstage + ifC2 .eq. 0) iftilt = 1
-        if (iftilt + ifmag + ifstage + ifC2 .ne. 1) call exitError(
+        ierr = PipGetBoolean('ExposureDose', ifdose)
+        if (iftilt + ifmag + ifstage + ifC2 + ifdose .eq. 0) iftilt = 1
+        if (iftilt + ifmag + ifstage + ifC2 + ifdose .ne. 1) call exitError(
      &      'YOU MUST ENTER ONLY ONE OPTION FOR DATA TO EXTRACT')
         if (iftilt .ne. 0) itype = 1
         if (ifstage .ne. 0) itype = 3
         if (ifmag .ne. 0) itype = 4
         if (ifC2 .ne. 0) itype = 5
+        if (ifdose .ne. 0) itype = 6
       else
         iftilt = 1
         itype = 1
@@ -162,6 +144,7 @@ c
       if (ifC2 .ne. 0) write(iunout,'(f8.5)')(tilt(i),i=1,ntiltout)
       if (ifstage .ne. 0)  write(iunout,'(2f9.2)')
      &    (tilt(i), val2(i), i=1,ntiltout)
+      if (ifdose .ne. 0) write(iunout,'(f13.5)')(tilt(i),i=1,ntiltout)
 
       if (iunout.eq. 1) then
         close(1)
@@ -172,3 +155,23 @@ c
 c       
       call exit(0)
       END
+
+c       $Log$
+c       Revision 3.6  2006/07/08 13:44:57  mast
+c       Raised limites, switchet to exitError
+c
+c       Revision 3.5  2006/02/24 22:11:15  mast
+c       Exit with ERROR if no items found in header
+c
+c       Revision 3.4  2004/04/02 00:50:25  mast
+c       Had to initialize iftilt
+c	
+c       Revision 3.3  2004/03/19 04:34:19  mast
+c       had to declare lnblnk
+c	
+c       Revision 3.2  2004/03/18 16:37:44  mast
+c       Converted to PIP and added options for extracting other info
+c	
+c       Revision 3.1  2003/06/05 00:11:14  mast
+c       Change STOP to standardized ERROR exit
+c	
