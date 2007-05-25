b42d1cbe5fe1a7e0dc09afa6eea8ad6eb3535d7f
SueHeld <sueh@colorado.edu>
2007-05-25 00:25:41 +0000

    bug# 994 Generic interface for the FailureReason class.

---
+++ Etomo/src/etomo/type/FailureReasonInterface.java
@@ -0,0 +1,23 @@
+package etomo.type;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface FailureReasonInterface {
+  public static  final String  rcsid =  "$Id$";
+  
+  public String getReason();
+
+  public String getTooltip();
+}
