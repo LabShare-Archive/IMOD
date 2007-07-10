aac2ea67eb184aff13b40668de42a51a40e4c16e
SueHeld <sueh@colorado.edu>
2007-07-10 00:31:09 +0000

    bug# 1022 Added .tlt to the description.

--- Etomo/src/etomo/storage/TiltFileFilter.java
+++ Etomo/src/etomo/storage/TiltFileFilter.java
@@ -3,33 +3,39 @@ package etomo.storage;
 import java.io.File;
 
 import javax.swing.filechooser.FileFilter;
+
+import etomo.util.DatasetFiles;
+
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright 2006</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-* 
-* <p> $Log$ </p>
-*/
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$
+ * <p> Revision 1.1  2007/03/26 23:33:03  sueh
+ * <p> bug# 964 Tilt file filter (.tlt).
+ * <p> </p>
+ */
 public class TiltFileFilter extends FileFilter {
-  public static  final String  rcsid =  "$Id$";
-  
+  public static final String rcsid = "$Id$";
+
   public boolean accept(File f) {
     //  If this is a file test its extension, all others should return true
-    if (f.isFile() && !f.getAbsolutePath().endsWith(".tlt")) {
+    if (f.isFile() && !f.getAbsolutePath().endsWith(DatasetFiles.TILT_FILE_EXT)) {
       return false;
     }
     return true;
   }
 
   public String getDescription() {
-    return "Tilt Angles File";
+    return "Tilt Angles File (" + DatasetFiles.TILT_FILE_EXT + ")";
   }
 }
