0f2c55c64343ef9275184f54f62789ea6897c0f6
SueHeld <sueh@colorado.edu>
2007-08-10 17:34:29 +0000

    bug# 847 Interface for a class that responds to options in MenuButton's right click menu.

---
+++ Etomo/src/etomo/ui/MenuButtonContainer.java
@@ -0,0 +1,19 @@
+package etomo.ui;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+interface MenuButtonContainer {
+  public static  final String  rcsid =  "$Id$";
+}
