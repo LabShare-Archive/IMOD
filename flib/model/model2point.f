fcbcf66ce392ae31f1d20d07182fb65bd721f072
mastcu <mast@colorado.edu>
2007-09-12 16:29:25 +0000

    PIP conversion and output options

--- flib/model/model2point.f
+++ flib/model/model2point.f
@@ -1,54 +1,87 @@
-* * * * * * MODEL2POINT * * * * *
-c	  
-c	  MODEL2POINT will convert an imod or WIMP ascii model file to a
-c	  list of points with integer coordinates.  Each point will be written
-c	  on a separate line, and any information about which object or contour
-c	  the point came from will be omitted.  The input file may be either
-c	  an imod model or a WIMP ascii model file; the output file will
-c	  contain the list of points.
-c	  
-C	  The program will accept file names either from the command line or as
-C	  entries to the program after it is started.  If there are two names
-C	  on the command line, they will be taken as the input and output file
-C	  names; if there is one name, it will be taken as the input file name
-C	  and the program will ask for the output file name; if there are no
-C	  command line arguments, the program will ask for both input and
-C	  output file names.
-C
-	character*80 modelfile,pointfile
-	logical exist,readw_or_imod
+*       * * * * * MODEL2POINT * * * * *
+c       
+c       MODEL2POINT will convert an IMOD or WIMP model file to a list of
+c       points with integer or floating point coordinates.  Each point will be
+c       written on a separate line, and the coordinates may be written either
+c       with no information about which object or contour the point came from,
+c       or with contour numbers, or with object numbers.
+c       
+c       See man page for details
 c
-	include 'model.inc'
+c       $Id$
+c       $Log$
+c       
+      implicit none
+      character*160 modelfile,pointfile
+      logical*4 printObj, printCont, floating
+      integer*4 ierr,iobject,ninobj,ipt,ipnt,modObj, modCont, npnts
+      logical exist,readw_or_imod
+c       
+      include 'model.inc'
+
+      integer*4 numOptArg, numNonOptArg
+      integer*4 PipGetLogical
+      integer*4 PipGetInOutFile
+c       
+c       fallbacks from ../../manpages/autodoc2man -2 2  model2point
+c       
+      integer numOptions
+      parameter (numOptions = 6)
+      character*(40 * numOptions) options(1)
+      options(1) =
+     &    'input:InputFile:FN:@output:OutputFile:FN:@float:FloatingPoint:B:@'//
+     &    'object:ObjectAndContour:B:@contour:Contour:B:@help:usage:B:'
 c
-c	  read model in; leave it closed
-c
-	call getinout(2,modelfile,pointfile)
-75	exist=readw_or_imod(modelfile)
-	close(20)
-	if(.not.exist)then
-	  print *,'bad model file'
-	  call exit(0)
-	endif
-c	  
-	call dopen(1,pointfile,'new','f')
-c
-c	  scan through all objects to get points
-c	  
-	npnts=0
-	do iobject=1,max_mod_obj
-	  ninobj=npt_in_obj(iobject)
-	  if(ninobj.gt.0)then
-	    do ipt=1,ninobj
-	      ipnt=object(ipt+ibase_obj(iobject))
-	      if(ipnt.gt.0)then
-		write(1,102) nint(p_coord(1,ipnt)),
-     &		    nint(p_coord(2,ipnt)), nint(p_coord(3,ipnt))
-		npnts=npnts+1
-	      endif
-	    enddo
-	  endif
-	enddo
-102	format(3i6)
-	print *,npnts,' points output to file'
-	call exit(0)
-	end
+      printObj = .false.
+      printCont = .false.
+      floating = .false.
+c       
+c       Pip startup: set error, parse options, check help, set flag if used
+c       
+      call PipReadOrParseOptions(options, numOptions, 'model2point',
+     &    'ERROR: MODEL2POINT - ', .false., 2, 1, 1, numOptArg,
+     &    numNonOptArg)
+      if (PipGetInOutFile('InputFile', 1, ' ', modelfile)
+     &    .ne. 0) call exitError('NO INPUT FILE SPECIFIED')
+      if (PipGetInOutFile('OutputFile', 2, ' ', pointfile)
+     &    .ne. 0) call exitError('NO OUTPUT FILE SPECIFIED')
+c       
+c       read model in
+c       
+      exist=readw_or_imod(modelfile)
+      if(.not.exist)call exitError('READING MODEL FILE')
+c       
+      call dopen(1,pointfile,'new','f')
+      ierr = PipGetLogical('ObjectAndContour', printObj)
+      ierr = PipGetLogical('Contour', printCont)
+      ierr = PipGetLogical('FloatingPoint', floating)
+c       
+c       scan through all objects to get points
+c       
+      npnts=0
+      do iobject=1,max_mod_obj
+        ninobj=npt_in_obj(iobject)
+        if(ninobj.gt.0)then
+          call objtocont(iobject, obj_color, modObj, modCont)
+          do ipt=1,ninobj
+            ipnt=object(ipt+ibase_obj(iobject))
+            if(ipnt.gt.0)then
+              if (printObj) write(1,103)modObj
+              if (printCont .or. printObj) write(1,103)modCont
+              if (floating) then
+                write(1,104) p_coord(1,ipnt), p_coord(2,ipnt), p_coord(3,ipnt)
+              else
+                write(1,102) nint(p_coord(1,ipnt)),
+     &              nint(p_coord(2,ipnt)), nint(p_coord(3,ipnt))
+              endif
+              npnts=npnts+1
+            endif
+          enddo
+        endif
+      enddo
+102   format(3i7)
+103   format(i6,$)
+104   format(3f10.2)
+      print *,npnts,' points output to file'
+      call exit(0)
+      end
