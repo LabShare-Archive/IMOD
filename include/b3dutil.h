025af84d04c98c8cb1465ca348c059d554f03aef
mastcu <mast@colorado.edu>
2007-10-01 15:27:41 +0000

    Split out everything not in b3dutil.c

--- include/b3dutil.h
+++ include/b3dutil.h
@@ -14,6 +14,9 @@
 
 #include <stdio.h>
 
+/* Include this since this include file was split off from here */
+#include "cfsemshare.h"
+
 #define MAX_IMOD_ERROR_STRING  512
 
 #define B3DMIN(a,b) ((a) < (b) ? (a) : (b))
@@ -49,37 +52,6 @@ extern "C" {
   int b3dIMin(int narg, ...);
   int b3dIMax(int narg, ...);
 
-  /* parselist.c  - for parsing a list of integers */
-  int *parselist (char *line, int *nlist);
-
-  /* amoeba.c - simplex minimization routine */
-  void amoeba(float *p, float *y, int mp, int ndim, float ftol, 
-              void (*funk)(float *, float *), int *iterP, float *ptol,
-              int *iloP);
-  void amoebaInit(float *p, float *y, int mp, int ndim, float delfac, 
-                  float ptolFac, float *a, float *da, 
-                  void (*funk)(float *, float *), float *ptol);
-
-  /* samplemeansd.c - for computing mean and SD quickly by sampling */
-  int sampleMeanSD(unsigned char **image, int type, int nx, int ny,
-                   float sample, int nxMatt, int myMatt, int nxUse, int nyUse,
-                   float *mean, float *sd);
-
-  /* colormap.c */
-  int *cmapStandardRamp(void);
-  int *cmapInvertedRamp(void);
-  int cmapConvertRamp(int *rampData, unsigned char table[3][256]);
-  int cmapReadConvert(char *filename, unsigned char table[3][256]);
-
-  /* cubinterp.c */
-  void cubinterp(float *array, float *bray, int nxa, int nya, int nxb, int nyb,
-                 float amat[2][2], float xc, float yc, float xt, float yt,
-                 float scale, float dmean, int linear);
-
-  /* reduce_by_binning.c */
-  int reduceByBinning(void *datain, int mode, int nx, int ny, int nbin,
-                      void *dataout, int *nxr, int *nyr);
-
 #ifdef __cplusplus
 }
 #endif
@@ -89,6 +61,9 @@ extern "C" {
 
 /*
 $Log$
+Revision 1.14  2007/09/20 15:42:12  mast
+Added declarations for new routines
+
 Revision 1.13  2007/04/26 19:07:44  mast
 Add B3DNINT
 
