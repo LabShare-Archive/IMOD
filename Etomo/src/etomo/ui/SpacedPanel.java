053cb2920e03ada4238726509aa9d443db6cce8a
SueHeld <sueh@colorado.edu>
2007-11-06 20:32:03 +0000

    bug# 1047 Added addRigidArea(Dimension).

--- Etomo/src/etomo/ui/SpacedPanel.java
+++ Etomo/src/etomo/ui/SpacedPanel.java
@@ -14,135 +14,141 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.Border;
+
 /**
-* <p>Description: A JPanel-like object that places rigid areas between 
-* components.  It can optionally create rigid areas outside the components on
-* the layout axis.  If rigid areas are needed outside the components on both
-* axes, use DoubleSpacedPanel.  It is most useful for creating panels which
-* would normally need rigid areas.  It should work like a JPanel.</p>
-* 
-* <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
-*
-*<p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-* 
-* <p> $Log$
-* <p> Revision 1.18  2007/05/08 01:21:10  sueh
-* <p> bug# 964 Added setToolTipText().
-* <p>
-* <p> Revision 1.17  2007/05/01 00:45:48  sueh
-* <p> bug# 964 Added remove(SpacedPanel).
-* <p>
-* <p> Revision 1.16  2007/03/30 23:52:55  sueh
-* <p> bug# 964 Added add(etomo.ui.TextField)
-* <p>
-* <p> Revision 1.15  2007/03/07 21:14:36  sueh
-* <p> bug# 981 Turned RadioButton into a wrapper rather then a child of JRadioButton,
-* <p> because it is getting more complicated.
-* <p>
-* <p> Revision 1.14  2007/02/05 23:44:33  sueh
-* <p> bug# 962 Fixing alignment setting.
-* <p>
-* <p> Revision 1.13  2006/07/20 17:22:20  sueh
-* <p> bug# 848 Made UIParameters a singleton.
-* <p>
-* <p> Revision 1.12  2006/03/27 21:07:33  sueh
-* <p> bug# 836 Added addMouseListener().
-* <p>
-* <p> Revision 1.11  2006/01/03 23:54:45  sueh
-* <p> bug# 675 Converted JCheckBox's to CheckBox.  Converted JRadioButton's
-* <p> toRadioButton.
-* <p>
-* <p> Revision 1.10  2005/09/29 19:11:14  sueh
-* <p> add comment
-* <p>
-* <p> Revision 1.9  2005/08/10 20:47:49  sueh
-* <p> bug# 711 Removed MultiLineToggleButton.  Making toggling an attribute
-* <p> of MultiLineButton.
-* <p>
-* <p> Revision 1.8  2005/08/09 21:09:55  sueh
-* <p> bug# 711 Moving button sizing from UIUtilities to the multi line button
-* <p> classes.
-* <p>
-* <p> Revision 1.7  2005/07/11 23:23:46  sueh
-* <p> bug# 619 Added add(container).
-* <p>
-* <p> Revision 1.6  2005/07/06 23:49:19  sueh
-* <p> bug# 619 Removed DoubleSpacedPanel and FormattedPanel.  Placed
-* <p> their functionality in SpacedPanel.  Simplified the construction of
-* <p> SpacedPanel.
-* <p>
-* <p> Revision 1.5  2005/07/01 21:23:27  sueh
-* <p> bug# 619 Added getRootPanel() to return the JPanel in panel.
-* <p>
-* <p> Revision 1.4  2004/12/30 19:32:42  sueh
-* <p> bug# 567 Added setAlignmentX() to set alignment for the panel.
-* <p>
-* <p> Revision 1.3  2004/12/01 03:48:46  sueh
-* <p> bug# 557 Added add option to space with glue instead of rigid areas.
-* <p> Added an option to automatically use 5 pixels when spacing with rigid
-* <p> areas.
-* <p>
-* <p> Revision 1.2  2004/11/20 00:04:29  sueh
-* <p> bug# 520 merging Etomo_3-4-6_JOIN branch to head.
-* <p>
-* <p> Revision 1.1.2.5  2004/11/11 01:43:14  sueh
-* <p> bug# 520 Removed unused functions add(Component).
-* <p>
-* <p> Revision 1.1.2.4  2004/11/08 22:31:33  sueh
-* <p> bug# 520 Added an add() function that can handle a Component.  Not
-* <p> spacing this component since it be a spacer.
-* <p>
-* <p> Revision 1.1.2.3  2004/10/13 23:16:04  sueh
-* <p> bug# 520 Added a way to set and reset component alignments.  Added
-* <p> specialized add() functions for generic etomo ui objects.  Added a way to
-* <p> prevent spacing being added at the top of a panel and after a component.
-* <p>
-* <p> Revision 1.1.2.2  2004/09/23 23:52:23  sueh
-* <p> bug# 520 Added a class description.
-* <p>
-* <p> Revision 1.1.2.1  2004/09/23 23:41:40  sueh
-* <p> bug# 520 Panel which automatically places rigid areas between
-* <p> components.  Can choose whether to place rigid areas before and after
-* <p> placing all components.
-* <p> </p>
-*/
+ * <p>Description: A JPanel-like object that places rigid areas between 
+ * components.  It can optionally create rigid areas outside the components on
+ * the layout axis.  If rigid areas are needed outside the components on both
+ * axes, use DoubleSpacedPanel.  It is most useful for creating panels which
+ * would normally need rigid areas.  It should work like a JPanel.</p>
+ * 
+ * <p>Copyright: Copyright (c) 2002, 2003, 2004</p>
+ *
+ *<p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$
+ * <p> Revision 1.19  2007/05/26 00:33:23  sueh
+ * <p> bug# 994 Not automatically setting button size in SpacedPanel anymore.
+ * <p> Setting button size in UI.
+ * <p>
+ * <p> Revision 1.18  2007/05/08 01:21:10  sueh
+ * <p> bug# 964 Added setToolTipText().
+ * <p>
+ * <p> Revision 1.17  2007/05/01 00:45:48  sueh
+ * <p> bug# 964 Added remove(SpacedPanel).
+ * <p>
+ * <p> Revision 1.16  2007/03/30 23:52:55  sueh
+ * <p> bug# 964 Added add(etomo.ui.TextField)
+ * <p>
+ * <p> Revision 1.15  2007/03/07 21:14:36  sueh
+ * <p> bug# 981 Turned RadioButton into a wrapper rather then a child of JRadioButton,
+ * <p> because it is getting more complicated.
+ * <p>
+ * <p> Revision 1.14  2007/02/05 23:44:33  sueh
+ * <p> bug# 962 Fixing alignment setting.
+ * <p>
+ * <p> Revision 1.13  2006/07/20 17:22:20  sueh
+ * <p> bug# 848 Made UIParameters a singleton.
+ * <p>
+ * <p> Revision 1.12  2006/03/27 21:07:33  sueh
+ * <p> bug# 836 Added addMouseListener().
+ * <p>
+ * <p> Revision 1.11  2006/01/03 23:54:45  sueh
+ * <p> bug# 675 Converted JCheckBox's to CheckBox.  Converted JRadioButton's
+ * <p> toRadioButton.
+ * <p>
+ * <p> Revision 1.10  2005/09/29 19:11:14  sueh
+ * <p> add comment
+ * <p>
+ * <p> Revision 1.9  2005/08/10 20:47:49  sueh
+ * <p> bug# 711 Removed MultiLineToggleButton.  Making toggling an attribute
+ * <p> of MultiLineButton.
+ * <p>
+ * <p> Revision 1.8  2005/08/09 21:09:55  sueh
+ * <p> bug# 711 Moving button sizing from UIUtilities to the multi line button
+ * <p> classes.
+ * <p>
+ * <p> Revision 1.7  2005/07/11 23:23:46  sueh
+ * <p> bug# 619 Added add(container).
+ * <p>
+ * <p> Revision 1.6  2005/07/06 23:49:19  sueh
+ * <p> bug# 619 Removed DoubleSpacedPanel and FormattedPanel.  Placed
+ * <p> their functionality in SpacedPanel.  Simplified the construction of
+ * <p> SpacedPanel.
+ * <p>
+ * <p> Revision 1.5  2005/07/01 21:23:27  sueh
+ * <p> bug# 619 Added getRootPanel() to return the JPanel in panel.
+ * <p>
+ * <p> Revision 1.4  2004/12/30 19:32:42  sueh
+ * <p> bug# 567 Added setAlignmentX() to set alignment for the panel.
+ * <p>
+ * <p> Revision 1.3  2004/12/01 03:48:46  sueh
+ * <p> bug# 557 Added add option to space with glue instead of rigid areas.
+ * <p> Added an option to automatically use 5 pixels when spacing with rigid
+ * <p> areas.
+ * <p>
+ * <p> Revision 1.2  2004/11/20 00:04:29  sueh
+ * <p> bug# 520 merging Etomo_3-4-6_JOIN branch to head.
+ * <p>
+ * <p> Revision 1.1.2.5  2004/11/11 01:43:14  sueh
+ * <p> bug# 520 Removed unused functions add(Component).
+ * <p>
+ * <p> Revision 1.1.2.4  2004/11/08 22:31:33  sueh
+ * <p> bug# 520 Added an add() function that can handle a Component.  Not
+ * <p> spacing this component since it be a spacer.
+ * <p>
+ * <p> Revision 1.1.2.3  2004/10/13 23:16:04  sueh
+ * <p> bug# 520 Added a way to set and reset component alignments.  Added
+ * <p> specialized add() functions for generic etomo ui objects.  Added a way to
+ * <p> prevent spacing being added at the top of a panel and after a component.
+ * <p>
+ * <p> Revision 1.1.2.2  2004/09/23 23:52:23  sueh
+ * <p> bug# 520 Added a class description.
+ * <p>
+ * <p> Revision 1.1.2.1  2004/09/23 23:41:40  sueh
+ * <p> bug# 520 Panel which automatically places rigid areas between
+ * <p> components.  Can choose whether to place rigid areas before and after
+ * <p> placing all components.
+ * <p> </p>
+ */
 final class SpacedPanel {
-  public static  final String  rcsid =  "$Id$";
-  
-  private static final Dimension MULTI_LINE_BUTTON_DIM = UIParameters.INSTANCE.getButtonDimension();
-  
-  private JPanel panel;
-  private JPanel innerPanel = null;
-  private JPanel outerPanel = null;
+  public static final String rcsid = "$Id$";
+
+  private static final Dimension MULTI_LINE_BUTTON_DIM = UIParameters.INSTANCE
+      .getButtonDimension();
+
+  private final JPanel panel = new JPanel();
+  private final JPanel innerPanel = new JPanel();
+  private final JPanel outerPanel;
+  private final StringBuffer xDescription = new StringBuffer();
+  private final StringBuffer yDescription = new StringBuffer();
+
   private boolean componentAdded = false;
   private boolean layoutSet = false;
   private boolean previousComponentWasSpaced = false;
-  private int axis;
   private Float componentAlignmentX = null;
-  private StringBuffer xDescription = new StringBuffer();
-  private StringBuffer yDescription = new StringBuffer();
-  
+
+  private int axis;
+
   SpacedPanel() {
     this(false);
   }
-  
+
   /**
    * 
    * @param yAxisPadding - add a rigid area at the top of the panel
    */
-  SpacedPanel(boolean yAxisPadding) {
+  SpacedPanel(final boolean yAxisPadding) {
     //panels
     outerPanel = new JPanel();
     outerPanel.setLayout(new BoxLayout(outerPanel, BoxLayout.Y_AXIS));
-    innerPanel = new JPanel();
     innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.X_AXIS));
-    panel = new JPanel();
     //innerPanel
     innerPanel.add(Box.createRigidArea(FixedDim.x5_y0));
     xDescription.append("first FixedDim.x5_y0,");
@@ -157,17 +163,18 @@ final class SpacedPanel {
     outerPanel.add(Box.createRigidArea(FixedDim.x0_y5));
     yDescription.append("last FixedDim.x0_y5,");
   }
-  
+
   public String toString() {
-    return ",axis="+axis+"\n,xDescription="+xDescription+"\n,yDescription="+yDescription;
+    return ",axis=" + axis + "\n,xDescription=" + xDescription
+        + "\n,yDescription=" + yDescription;
   }
-  
-  final void setBoxLayout(int axis) {
+
+  final void setBoxLayout(final int axis) {
     this.axis = axis;
     panel.setLayout(new BoxLayout(panel, axis));
     layoutSet = true;
   }
-  
+
   private final void addSpacing() {
     if (!componentAdded) {
       componentAdded = true;
@@ -188,19 +195,19 @@ final class SpacedPanel {
       previousComponentWasSpaced = false;
     }
   }
-  
-  void remove(SpacedPanel panel) {
+
+  void remove(final SpacedPanel panel) {
     this.panel.remove(panel.getContainer());
   }
-  
-  final void add(Container container) {
+
+  final void add(final Container container) {
     addSpacing();
     panel.add(container);
     xDescription.append("Container,");
     yDescription.append("Container,");
   }
-  
-  final void add(SpacedPanel spacedPanel) {
+
+  final void add(final SpacedPanel spacedPanel) {
     addSpacing();
     panel.add(spacedPanel.getContainer());
     if (componentAlignmentX != null) {
@@ -209,10 +216,10 @@ final class SpacedPanel {
     xDescription.append("SpacedPanel,");
     yDescription.append("SpacedPanel,");
     previousComponentWasSpaced = true;
-    
+
   }
-  
-  final void add(JScrollPane jScrollPane) {
+
+  final void add(final JScrollPane jScrollPane) {
     addSpacing();
     panel.add(jScrollPane);
     if (componentAlignmentX != null) {
@@ -221,8 +228,8 @@ final class SpacedPanel {
     xDescription.append("JScrollPane,");
     yDescription.append("JScrollPane,");
   }
-  
-  final void add(JPanel jPanel) {
+
+  final void add(final JPanel jPanel) {
     addSpacing();
     panel.add(jPanel);
     if (componentAlignmentX != null) {
@@ -231,8 +238,8 @@ final class SpacedPanel {
     xDescription.append("JPanel,");
     yDescription.append("JPanel,");
   }
-  
-  final void add(JComboBox jComboBox) {
+
+  final void add(final JComboBox jComboBox) {
     addSpacing();
     panel.add(jComboBox);
     if (componentAlignmentX != null) {
@@ -242,7 +249,7 @@ final class SpacedPanel {
     yDescription.append("JComboBox,");
   }
 
-  final void add(LabeledTextField labeledTextField) {
+  void add(final LabeledTextField labeledTextField) {
     addSpacing();
     panel.add(labeledTextField.getContainer());
     if (componentAlignmentX != null) {
@@ -251,8 +258,18 @@ final class SpacedPanel {
     xDescription.append("LabeledTextField,");
     yDescription.append("LabeledTextField,");
   }
-  
-  final void add(TextField textField) {
+
+  void add(final Spinner spinner) {
+    addSpacing();
+    panel.add(spinner.getContainer());
+    if (componentAlignmentX != null) {
+      spinner.setAlignmentX(componentAlignmentX.floatValue());
+    }
+    xDescription.append("LabeledTextField,");
+    yDescription.append("LabeledTextField,");
+  }
+
+  void add(final TextField textField) {
     addSpacing();
     panel.add(textField.getComponent());
     if (componentAlignmentX != null) {
@@ -261,8 +278,8 @@ final class SpacedPanel {
     xDescription.append("LabeledTextField,");
     yDescription.append("LabeledTextField,");
   }
-  
-  final void add(JLabel jLabel) {
+
+  void add(final JLabel jLabel) {
     addSpacing();
     panel.add(jLabel);
     if (componentAlignmentX != null) {
@@ -271,8 +288,8 @@ final class SpacedPanel {
     xDescription.append("JLabel,");
     yDescription.append("JLabel,");
   }
-  
-  final void add(RadioButton radioButton) {
+
+  void add(final RadioButton radioButton) {
     addSpacing();
     panel.add(radioButton.getComponent());
     if (componentAlignmentX != null) {
@@ -281,8 +298,8 @@ final class SpacedPanel {
     xDescription.append("RadioButton,");
     yDescription.append("RadioButton,");
   }
-  
-  final void add(CheckBox checkBox) {
+
+  void add(final CheckBox checkBox) {
     addSpacing();
     panel.add(checkBox);
     if (componentAlignmentX != null) {
@@ -291,8 +308,8 @@ final class SpacedPanel {
     xDescription.append("CheckBox,");
     yDescription.append("CheckBox,");
   }
-  
-  final void add(SpacedTextField spacedTextField) {
+
+  void add(final SpacedTextField spacedTextField) {
     addSpacing();
     panel.add(spacedTextField.getContainer());
     if (componentAlignmentX != null) {
@@ -302,15 +319,15 @@ final class SpacedPanel {
     yDescription.append("SpacedTextField,");
     previousComponentWasSpaced = true;
   }
-  
-  final void add(PanelHeader panelHeader) {
+
+  void add(final PanelHeader panelHeader) {
     addSpacing();
     panel.add(panelHeader.getContainer());
     xDescription.append("PanelHeader,");
     yDescription.append("PanelHeader,");
   }
-  
-  final void add(LabeledSpinner labeledSpinner) {
+
+  void add(final LabeledSpinner labeledSpinner) {
     addSpacing();
     panel.add(labeledSpinner.getContainer());
     if (componentAlignmentX != null) {
@@ -319,8 +336,8 @@ final class SpacedPanel {
     xDescription.append("LabeledSpinner,");
     yDescription.append("LabeledSpinner,");
   }
-  
-  final void add(MultiLineButton multiLineButton) {
+
+  void add(final MultiLineButton multiLineButton) {
     addSpacing();
     //multiLineButton.setSize();
     panel.add(multiLineButton.getComponent());
@@ -330,8 +347,8 @@ final class SpacedPanel {
     xDescription.append("MultiLineButton,");
     yDescription.append("MultiLineButton,");
   }
-  
-  final void add(JButton button) {
+
+  void add(final JButton button) {
     addSpacing();
     panel.add(button);
     if (componentAlignmentX != null) {
@@ -340,17 +357,17 @@ final class SpacedPanel {
     xDescription.append("JButton,");
     yDescription.append("JButton,");
   }
-  
-  final void addMouseListener(MouseListener mouseListener) {
+
+  void addMouseListener(final MouseListener mouseListener) {
     panel.addMouseListener(mouseListener);
   }
-  
-  final void addHorizontalGlue() {
+
+  void addHorizontalGlue() {
     panel.add(Box.createHorizontalGlue());
     xDescription.append("HorizontalGlue,");
   }
-  
-  final void addRigidArea() {
+
+  void addRigidArea() {
     if (axis == BoxLayout.X_AXIS) {
       panel.add(Box.createRigidArea(FixedDim.x5_y0));
       xDescription.append("FixedDim.x5_y0,");
@@ -361,30 +378,38 @@ final class SpacedPanel {
     }
   }
   
-  final void removeAll() {
+  void addRigidArea(Dimension dim) {
+    if (axis == BoxLayout.X_AXIS) {
+      panel.add(Box.createRigidArea(dim));
+      xDescription.append("rigid area,");
+    }
+    else if (axis == BoxLayout.Y_AXIS) {
+      panel.add(Box.createRigidArea(dim));
+      yDescription.append("rigid area,");
+    }
+  }
+
+  void removeAll() {
     componentAdded = false;
     panel.removeAll();
   }
-  
-  final void setComponentAlignmentX(float componentAlignmentX) {
+
+  void setComponentAlignmentX(final float componentAlignmentX) {
     this.componentAlignmentX = new Float(componentAlignmentX);
   }
-  
-  final void alignComponentsX(float alignment) {
+
+  void alignComponentsX(final float alignment) {
     setComponentAlignmentX(alignment);
     alignComponentsX(outerPanel, alignment);
     alignComponentsX(innerPanel, alignment);
     alignComponentsX(panel, alignment);
   }
-  
-  void setToolTipText(String text) {
+
+  void setToolTipText(final String text) {
     panel.setToolTipText(TooltipFormatter.INSTANCE.format(text));
   }
-  
-  private final void alignComponentsX(JPanel panel, float alignment) {
-    if (panel == null) {
-      return;
-    }
+
+  private final void alignComponentsX(final JPanel panel, final float alignment) {
     Component[] children = panel.getComponents();
     for (int i = 0; i < children.length; i++) {
       if (children[i] instanceof JComponent) {
@@ -393,8 +418,8 @@ final class SpacedPanel {
       }
     }
   }
-  
-  final Container getContainer() {
+
+  Container getContainer() {
     if (outerPanel != null) {
       return outerPanel;
     }
@@ -403,12 +428,12 @@ final class SpacedPanel {
     }
     return panel;
   }
-  
-  final JPanel getJPanel() {
+
+  JPanel getJPanel() {
     return (JPanel) getContainer();
   }
-  
-  final void setAlignmentX(float alignmentX) {
+
+  void setAlignmentX(final float alignmentX) {
     if (outerPanel != null) {
       outerPanel.setAlignmentX(alignmentX);
     }
@@ -417,12 +442,12 @@ final class SpacedPanel {
     }
     panel.setAlignmentX(alignmentX);
   }
-  
-  final void setBorder(Border border) {
+
+  void setBorder(final Border border) {
     panel.setBorder(border);
   }
-  
-  final void setVisible(boolean visible) {
+
+  void setVisible(final boolean visible) {
     getContainer().setVisible(visible);
   }
 }
