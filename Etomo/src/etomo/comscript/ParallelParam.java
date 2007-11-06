37f3c9efb6ea7ff051a1981fbe521f75a2880e12
SueHeld <sueh@colorado.edu>
2007-11-06 19:14:35 +0000

    bug# 1047 Added getProcessName and getSubcommandMode.

--- Etomo/src/etomo/comscript/ParallelParam.java
+++ Etomo/src/etomo/comscript/ParallelParam.java
@@ -1,4 +1,7 @@
 package etomo.comscript;
+
+import etomo.type.ProcessName;
+
 /**
 * <p>Description: Generic interface for params used by classes which implement
 * ParallelDialog.
@@ -18,7 +21,19 @@ package etomo.comscript;
 */
 public interface ParallelParam {
   public static  final String  rcsid =  "$Id$";
+  
+  /**
+   * @return the process that the parallel process will run
+   */
+  public ProcessName getProcessName();
+  public CommandMode getSubcommandMode();
 }
 /**
-* <p> $Log$ </p>
+* <p> $Log$
+* <p> Revision 1.1  2005/09/16 17:19:06  sueh
+* <p> bug# 532 A generic interface for ProcesschunksParam.  Allows
+* <p> ApplicationManager to have only one function which sets parameters in
+* <p> processchunks.  This is done by using ParallelDialog to represent the
+* <p> dialog.
+* <p> </p>
 */
