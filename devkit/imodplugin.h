8f72bf8397cbddfc318ec4de22d3d3c2573cccd3
mastcu <mast@colorado.edu>
2007-11-27 17:53:06 +0000

    Add undo

--- devkit/imodplugin.h
+++ devkit/imodplugin.h
@@ -1,30 +1,7 @@
-/*  IMOD VERSION 2.02
- *
+/*
  *  $Id$
  *
- *  Author: James Kremer email: kremer@colorado.edu
  */
-
-/*****************************************************************************
- *   Copyright (C) 1995-1996 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
 #ifndef IMODPLUGIN_H
 #define IMODPLUGIN_H
 
@@ -66,9 +43,10 @@ typedef struct privateStruct ImodView;
 #define IMODVIEWP_H
 #define CONTROLP_H
 #define IMODPLUGP_H
+#define UNDOREDO_H
 #include "imod.h"
 #include "control.h"
 #include "imodplug.h"
-
+#include "undoredo.h"
 
 #endif
