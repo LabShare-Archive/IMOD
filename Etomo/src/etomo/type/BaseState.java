6b2b8609b488dc2ec039edcfadc57c2f18249c35
SueHeld <sueh@colorado.edu>
2007-12-10 22:34:10 +0000

    bug# 1041 Removed current processchunks process name.

--- Etomo/src/etomo/type/BaseState.java
+++ Etomo/src/etomo/type/BaseState.java
@@ -18,6 +18,10 @@ import etomo.storage.Storable;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 1.2  2007/08/29 21:44:28  sueh
+ * <p> bug# 1041 Made BaseState an abstract class, added
+ * <p> killedProcesschunksProcessName.
+ * <p>
  * <p> Revision 1.1  2004/12/14 21:40:52  sueh
  * <p> bug# 572 Interface for TomogramState and JoinState.
  * <p> </p>
@@ -25,71 +29,19 @@ import etomo.storage.Storable;
 public abstract class BaseState implements Storable {
   public static final String rcsid = "$Id$";
 
-  private static final String KILLED_PROCESSCHUNKS_PROCESS_NAME = "KilledProcesschunksProcessName";
-  private static final String KILLED_PROCESSCHUNKS_PROCESS_NAME_A = "A."
-      + KILLED_PROCESSCHUNKS_PROCESS_NAME;
-  private static final String KILLED_PROCESSCHUNKS_PROCESS_NAME_B = "B."
-      + KILLED_PROCESSCHUNKS_PROCESS_NAME;
-
-  private ProcessName killedProcesschunksProcessNameA = null;
-  private ProcessName killedProcesschunksProcessNameB = null;
-
   abstract String createPrepend(String prepend);
 
   public void store(Properties props, String prepend) {
     prepend = createPrepend(prepend);
-    if (killedProcesschunksProcessNameA == null) {
-      props.remove(prepend + "." + KILLED_PROCESSCHUNKS_PROCESS_NAME_A);
-    }
-    else {
-      props.setProperty(prepend + "." + KILLED_PROCESSCHUNKS_PROCESS_NAME_A,
-          killedProcesschunksProcessNameA.toString());
-    }
-    if (killedProcesschunksProcessNameB == null) {
-      props.remove(prepend + "." + KILLED_PROCESSCHUNKS_PROCESS_NAME_B);
-    }
-    else {
-      props.setProperty(prepend + "." + KILLED_PROCESSCHUNKS_PROCESS_NAME_B,
-          killedProcesschunksProcessNameB.toString());
-    }
   }
 
   public void load(Properties props, String prepend) {
     //reset
-    killedProcesschunksProcessNameA = null;
-    killedProcesschunksProcessNameB = null;
     //load
     prepend = createPrepend(prepend);
-    killedProcesschunksProcessNameA = ProcessName.getInstance(props
-        .getProperty(prepend + "." + KILLED_PROCESSCHUNKS_PROCESS_NAME_A));
-    killedProcesschunksProcessNameB = ProcessName.getInstance(props
-        .getProperty(prepend + "." + KILLED_PROCESSCHUNKS_PROCESS_NAME_B));
-  }
-
-  public ProcessName getKilledProcesschunksProcessName(AxisID axisID) {
-    if (axisID == AxisID.SECOND) {
-      return killedProcesschunksProcessNameB;
-    }
-    return killedProcesschunksProcessNameA;
-  }
-
-  public void setKilledProcesschunksProcessName(AxisID axisID,
-      String input) {
-    if (axisID == AxisID.SECOND) {
-      killedProcesschunksProcessNameB = ProcessName.getInstance(input,axisID);
-    }
-    else {
-      killedProcesschunksProcessNameA = ProcessName.getInstance(input,axisID);
-    }
   }
 
   public boolean equals(BaseState input) {
-    if (killedProcesschunksProcessNameA != input.killedProcesschunksProcessNameA) {
-      return false;
-    }
-    if (killedProcesschunksProcessNameB != input.killedProcesschunksProcessNameB) {
-      return false;
-    }
     return true;
   }
 }
