7c5f12256e5795c8410f9689a7b0b7a79eeb92e4
SueHeld <sueh@colorado.edu>
2007-12-17 18:34:48 +0000

    bug# 1061 Added isSecondCommandLine and getSecondCommandLine.

--- Etomo/src/etomo/comscript/DetachedCommand.java
+++ Etomo/src/etomo/comscript/DetachedCommand.java
@@ -22,9 +22,14 @@ public interface DetachedCommand extends Command {
    */
   public String getCommandString();
   public boolean isValid();
+  public boolean isSecondCommandLine();
+  public String getSecondCommandLine();
 }
 /**
 * <p> $Log$
+* <p> Revision 1.2  2006/07/20 23:11:03  sueh
+* <p> bug#k 885 Added isValid().
+* <p>
 * <p> Revision 1.1  2006/01/06 02:36:34  sueh
 * <p> bug# 792 Command interface for DetachedCommand.  Can create a safe
 * <p> command string that can go into a run file.
