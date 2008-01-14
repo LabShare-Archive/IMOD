92ada593c2a3d126771afce31048ce37207b41ce
SueHeld <sueh@colorado.edu>
2008-01-14 22:01:06 +0000

    bug# 1050 A const interface for StringProperty.

---
+++ Etomo/src/etomo/type/ConstStringProperty.java
@@ -0,0 +1,22 @@
+package etomo.type;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface ConstStringProperty {
+  public static  final String  rcsid =  "$Id$";
+  
+  public String toString();
+  public boolean isEmpty();
+}
