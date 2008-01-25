3bdeb471847eb977a27dee6fb28011ea4d8f956f
SueHeld <sueh@colorado.edu>
2008-01-25 18:24:13 +0000

    bug# 1069 In initializeComscriptMatchString made the word boundary optional to work with Java 1.6.

--- Etomo/src/etomo/comscript/CombineComscriptState.java
+++ Etomo/src/etomo/comscript/CombineComscriptState.java
@@ -21,6 +21,9 @@ import etomo.util.DatasetFiles;
 * @version $$Revision$$
 *
 * <p> $Log$
+* <p> Revision 1.10  2007/12/26 22:11:32  sueh
+* <p> bug# 1052 Moved argument handling from EtomoDirector to a separate class.
+* <p>
 * <p> Revision 1.9  2007/09/07 00:17:12  sueh
 * <p> bug# 989 Using a public INSTANCE to refer to the EtomoDirector singleton
 * <p> instead of getInstance and createInstance.
@@ -353,13 +356,13 @@ public class CombineComscriptState implements ComscriptState {
    */
   private static void initializeComscriptMatchString() {
     //match 0 or more characters and a word boundary
-    StringBuffer stringBuffer = new StringBuffer(".*\\b[");
+    StringBuffer stringBuffer = new StringBuffer(".*\\b?[");
     //match 1 or the strings in COMMANDS
     for (int i = 0; i < NUM_COMMANDS; i++) {
       stringBuffer.append("\\Q" + COMMANDS[i] + "\\E");
     }
     //match ".com", a word boundary, and 0 or more characters
-    stringBuffer.append("]\\.com\\b.*");
+    stringBuffer.append("]\\.com\\b?.*");
     COMSCRIPT_MATCH_STRING = stringBuffer.toString();
   }
   
