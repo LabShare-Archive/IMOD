4b1c96911b5796725b3372f8739217db10308db5
SueHeld <sueh@colorado.edu>
2008-02-18 23:35:30 +0000

    bug# 1079 Added .EM and .csv file extensions.

--- Etomo/src/etomo/storage/MotlFileFilter.java
+++ Etomo/src/etomo/storage/MotlFileFilter.java
@@ -3,33 +3,38 @@ package etomo.storage;
 import java.io.File;
 
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright 2006</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-* 
-* <p> $Log$
-* <p> Revision 1.2  2007/06/05 18:10:04  sueh
-* <p> bug# 1011 Accepting any file that ends in .em.
-* <p>
-* <p> Revision 1.1  2007/03/01 01:14:47  sueh
-* <p> bug# 964 File filter for MOTL files (PEET interface).
-* <p> </p>
-*/
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$
+ * <p> Revision 1.3  2008/02/18 21:36:36  sueh
+ * <p> bug# 1079 Accepting .csv files.
+ * <p>
+ * <p> Revision 1.2  2007/06/05 18:10:04  sueh
+ * <p> bug# 1011 Accepting any file that ends in .em.
+ * <p>
+ * <p> Revision 1.1  2007/03/01 01:14:47  sueh
+ * <p> bug# 964 File filter for MOTL files (PEET interface).
+ * <p> </p>
+ */
 public final class MotlFileFilter extends javax.swing.filechooser.FileFilter
-implements java.io.FileFilter {
-  public static  final String  rcsid =  "$Id$";
-  
+    implements java.io.FileFilter {
+  public static final String rcsid = "$Id$";
+
   public boolean accept(File f) {
     //  If this is a file test its extension, all others should return true
-    if (f.isFile() && !f.getAbsolutePath().endsWith(".em")&& !f.getAbsolutePath().endsWith(".csv")) {
+    if (f.isFile() && !f.getAbsolutePath().endsWith(".em")
+        && !f.getAbsolutePath().endsWith(".EM")
+        && !f.getAbsolutePath().endsWith(".csv")) {
       return false;
     }
     return true;
