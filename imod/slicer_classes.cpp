2f935d0fc4b20df6eba571b34446b6e66e00b1bb
mastcu <mast@colorado.edu>
2008-03-06 06:16:27 +0000

    Fixed artifact on right edge in zoomed HQ image

--- imod/slicer_classes.cpp
+++ imod/slicer_classes.cpp
@@ -918,6 +918,10 @@ void fillImageArray(SlicerStruct *ss, int panning, int meanOnly)
     izoom = 1;
   }
 
+  if (imodDebug('s'))
+    imodPrintStderr("winx %d winy %d isize %d jsize %d shortcut %d ilimshort"
+                    " %d jlimshort %d\n", ss->winx, ss->winy, isize, jsize, 
+                    shortcut, ilimshort, ilimshort);
   //  int timeStart = imodv_sys_time();
   /* DNM: don't need to clear array in advance */
 
@@ -1154,7 +1158,7 @@ static void findIndexLimits(int isize, int xsize, float xo, float xsx,
   float endCoord = xo + (isize - 1) * xsx + offset;
   float startCoord = xo + offset;
  
-  /*if (debug) 
+  /*if (imodDebug('s')) 
     imodPrintStderr("xo = %f, xsx = %f, start = %f, end = %f\n", xo,xsx,startCoord, endCoord); */
   /* If start and end is all to one side of data, set limits to middle to skip
      the line */
@@ -1166,14 +1170,14 @@ static void findIndexLimits(int isize, int xsize, float xo, float xsx,
     /* Otherwise evaluate place where line cuts volume for this coordinate */
   } else if (xsx > 1.e-6 || xsx < -1.e-6) {
     flower = -(startCoord + 1.) / xsx;
-    fupper = (xsize - 1. - startCoord) / xsx;
+    fupper = (xsize - startCoord) / xsx;
     if (xsx < 0) {
       ftmp = flower;
       flower = fupper;
       fupper = ftmp;
     }
-    /*if (debug) 
-      imodPrintStderr("lower = %f, upper = %f\n", flower, fupper); */
+    /* if (imodDebug('s')) 
+       imodPrintStderr("lower = %f, upper = %f\n", flower, fupper); */
     if (flower > *fstart)
       *fstart = flower;
     if (fupper < *fend)
@@ -1302,7 +1306,7 @@ static void fillArraySegment(int jstart, int jlimit)
           if (xi >= 0 && xi < xsize && yi >= 0 && yi < ysize &&
               z > -0.5 && zi < zsize) {
             val = (*ivwFastGetValue)(xi, yi, zi);
-                         
+
             /* do quadratic interpolation. */
             dx = x - xi - 0.5;
             dy = y - yi - 0.5;
@@ -1358,12 +1362,11 @@ static void fillArraySegment(int jstart, int jlimit)
           y += ysx;
           z += zsx;
 
-          if (shortcut != 0 && ((i >= izoom && j % izoom == 0) ||
-                                (i >= izoom - 1 && j % izoom != 0))
-              && i < ilimshort && j >= izoom && j < jlimshort) {
+          if (shortcut != 0 && i >= izoom && i < ilimshort && 
+              j >= izoom && j < jlimshort) {
             ishort = izoom - 1;
             if (j % izoom)
-              ishort = ilimshort - izoom;
+              ishort = ilimshort - izoom - 1;
             x += xsx * ishort;
             y += ysx * ishort;
             z += zsx * ishort;
@@ -1468,261 +1471,13 @@ static void fillArraySegment(int jstart, int jlimit)
     zzo += zsz;
   }
 }
-#define PLIST_CHUNK 1000
 
-static int taper_slice(Islice *sl, int ntaper, int inside)
-{
-  struct pixdist {
-    unsigned short int x, y;
-    unsigned short dist;
-    signed char dx, dy;
-  };
+/*
 
-  Ival val;
-  float fracs[128], fillpart[128];
-  float fillval, lastval;
-  int len, longest;
-  int i, ix, iy, xnext, ynext, xp, yp, lastint, interval, found;
-  int dir, tapdir, col, row, ncol, ind, xstart, ystart, ndiv;
-  int dist;
-  struct pixdist *plist;
-  int pllimit = PLIST_CHUNK;
-  int plsize = 0;
-  unsigned char *bitmap;
-  int xsize = sl->xsize;
-  int ysize = sl->ysize;
-  int bmsize = (xsize * ysize + 7) / 8;
-  int dxout[4] = {0, 1, 0, -1};
-  int dyout[4] = {-1, 0, 1, 0};
-  int dxnext[4] = {1, 0, -1, 0};
-  int dynext[4] = {0, 1, 0, -1};
-
-
-  /* find longest string of repeated values along the edge */
-  longest = 0;
-  for (iy = 0; iy < ysize; iy += ysize - 1) {
-    len = 0;
-    sliceGetVal(sl, 0, iy, val);
-    lastval = val[0];
-    for (ix = 1; ix < xsize; ix++) {
-      sliceGetVal(sl, ix, iy, val);
-      if (val[0] == lastval) {
-        /* same as last, add to count, see if this is a new
-           best count*/
-        len++;
-        if (len > longest) {
-          longest = len;
-          fillval = lastval;
-        }
-      } else {
-        /* different, reset count and set new lastval */
-        len = 0;
-        lastval = val[0];
-      }
-    }
-  }         
-  for (ix = 0; ix < xsize; ix += xsize - 1) {
-    len = 0;
-    sliceGetVal(sl, ix, 0, val);
-    lastval = val[0];
-    for (iy = 1; iy < ysize; iy++) {
-      sliceGetVal(sl, ix, iy, val);
-      if (val[0] == lastval) {
-        /* same as last, add to count, see if this is a new
-           best count */
-        len++;
-        if (len > longest) {
-          longest = len;
-          fillval = lastval;
-        }
-      } else {
-        /* different, reset count and set new lastval */
-        len = 0;
-        lastval = val[0];
-      }
-    }
-  }         
-
-  /* If length below a criterion (what?) , return without error */
-  if (longest < 10)
-    return 0;
-
-  /* set the slice mean so that sliceGetValue will return this outside the
-     edges */
-  sl->mean = fillval;
-
-  /* look from left edge along a series of lines to find the first point
-     that is not a fill point */
-  lastint = 0;
-  found = 0;
-  for (ndiv = 2; ndiv <= ysize; ndiv++) {
-    interval = (ysize + 2) / ndiv;
-    if (interval == lastint)
-      continue;
-    lastint = interval;
-    for (iy = interval; iy < ysize; iy += interval) {
-      for (ix = 0; ix < xsize; ix++) {
-        sliceGetVal(sl, ix, iy, val);
-        if (val[0] != fillval) {
-          found = 1;
-          break;
-        }
-      }
-      if (found)
-        break;
-    }
-    if (found)
-      break;
-  }
-
-  if (!found)
-    return 0;
-
-  /* Get initial chunk of pixel list and bitmap */
-  plist = (struct pixdist *)malloc(PLIST_CHUNK * sizeof(struct pixdist));
-  if (plist)
-    bitmap = (unsigned char *)malloc(bmsize);
-  if (!plist || !bitmap) {
-    if (plist)
-      free(plist);
-    return (-1);
-  }
-
-  /* clear bitmap */
-  for (i = 0; i < bmsize; i++)
-    bitmap[i] = 0;
-
-  dir = 3;
-  xstart = ix;
-  ystart = iy;
-  tapdir = 1 - 2 * inside;
-
-  do {
-    ncol = 1;
-    xnext = ix + dxout[dir] + dxnext[dir];
-    ynext = iy + dyout[dir] + dynext[dir];
-    sliceGetVal(sl, xnext, ynext, val);
-    if (val[0] != fillval) {
-      /* case 1: inside corner */
-      ix = xnext;
-      iy = ynext;
-      dir = (dir + 3) % 4;
-      if (inside)
-        ncol = ntaper + 1;
-    } else {
-      xnext = ix + dxnext[dir];
-      ynext = iy + dynext[dir];
-      sliceGetVal(sl, xnext, ynext, val);
-      if (val[0] != fillval) {
-        /* case 2: straight edge to next pixel */
-        ix = xnext;
-        iy = ynext;
-      } else {
-        /* case 3: outside corner, pixel stays the same */
-        dir = (dir + 1) % 4;
-        if (!inside)
-          ncol = ntaper + 1;
-      }
-    }
-      
-    /* If outside pixel is outside the data, nothing to add to lists */
-    xp = ix + dxout[dir];
-    yp = iy + dyout[dir];
-    if (xp < 0 || xp >= xsize || yp < 0 || yp >= ysize)
-      continue;
-
-    /* Loop on all the pixels to mark */
-    for (col = 0; col < ncol; col++) {
-      for (row = 1 - inside; row <= ntaper - inside; row++) {
-        xp = ix + tapdir * row * dxout[dir] - col * dxnext[dir];
-        yp = iy + tapdir * row * dyout[dir] - col * dynext[dir];
-
-        /* skip if pixel outside area */
-        if (xp < 0 || xp >= xsize || yp < 0 || yp >= ysize)
-          continue;
-
-        /* skip marking outside pixels for inside taper or
-           inside pixels for outside taper */
-        sliceGetVal(sl, xp, yp, val);
-        if ((inside && val[0] == fillval) || 
-            (!inside && val[0] != fillval))
-          continue;
-
-        dist = col * col + row * row;
-        ind = xsize * yp + xp;
-        if (bitmap[ind / 8] & (1 << (ind % 8))) {
-
-          /* If the pixel is already marked, find it on the
-             list and see if it's closer this time */
-          for (i = plsize - 1; i >= 0; i--) {
-            if (plist[i].x == xp && plist[i].y == yp) {
-              if (plist[i].dist > dist) {
-                plist[i].dist = dist;
-                plist[i].dx = (signed char)(ix - xp);
-                plist[i].dy = (signed char)(iy - yp);
-              }
-              break;
-            }
-          }
-        } else {
-
-          /* Otherwise, mark pixel in bitmap and make a new 
-             entry on the list */
-          bitmap[ind / 8] |= (1 << (ind % 8));
-          if (plsize >= pllimit) {
-            pllimit += PLIST_CHUNK;
-            plist = (struct pixdist *) realloc(plist,
-                                               pllimit * sizeof(struct pixdist));
-            if (!plist) {
-              free (bitmap);
-              return (-1);
-            }
-          }
-          plist[plsize].x = xp;
-          plist[plsize].y = yp;
-          plist[plsize].dist = dist;
-          plist[plsize].dx = (signed char)(ix - xp);
-          plist[plsize++].dy = (signed char)(iy - yp);
-        }
-      }
-    }
-
-  } while (ix != xstart || iy != ystart || dir != 3);
-
-  /* make tables of fractions and amounts to add of fillval */
-  for (i = 1; i <= ntaper; i++) {
-    dist = inside ? i : ntaper + 1 - i;
-    fracs[i] = (float)dist / (ntaper + 1);
-    fillpart[i] = (1. - fracs[i]) * fillval;
-  }
-
-  /* Process the pixels on the list */
-  for (i = 0; i < plsize; i++) {
-    ind = sqrt((double)plist[i].dist) + inside;
-    if (ind > ntaper)
-      continue;
-    ix = plist[i].x;
-    iy = plist[i].y;
-    if (inside) {
-      xp = ix;
-      yp = iy;
-    } else {
-      xp = ix + plist[i].dx;
-      yp = iy + plist[i].dy;
-    }
-    sliceGetVal(sl, xp, yp, val);
-    /*  val[0] = fracs[plist[i].dist] * val[0] + fillpart[plist[i].dist]; */
-    val[0] = fracs[ind] * val[0] + fillpart[ind];
-    slicePutVal(sl, ix, iy, val);
-  }
-
-  free(plist);
-  free(bitmap);
-  return 0;
-}
-
- /*
 $Log$
+Revision 4.26  2008/01/28 19:11:31  mast
+Fixed hot key in tooltip for show slice
+
 Revision 4.25  2007/11/13 19:11:44  mast
 Used the IMOD_PROCESSORS value to increase the max voxels to compute,
 made it reduce quality before number of slices if slices are below 75%
