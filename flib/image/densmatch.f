3d217e17528a2ea85fe9025e8caf6b99a8e8a8e1
mastcu <mast@colorado.edu>
2008-04-26 19:45:12 +0000

    Added options for setting match area and offset; minimum sampling

--- flib/image/densmatch.f
+++ flib/image/densmatch.f
@@ -12,61 +12,61 @@ c       See man page for details.
 c       
 c       David Mastronarde, November 1995
 c       
-c       $Author$
-c       
-c       $Date$
-c       
-c       $Revision$
-c       
-c       $Log$
-c       Revision 3.2  2006/02/26 20:22:08  mast
-c       Made it process image in chunks so there is no more size limit
-c
-c       Revision 3.1  2004/08/24 04:15:38  mast
-c       Converted to PIP input, made it 5K capable, added report-only option
-c	
+c       $Id$
+c       Log at end of file
 c       
       implicit none
       integer idim, maxsamp
       parameter (idim=1000000,maxsamp=100000)
-      integer*4 nx,ny,nz
-      COMMON //NX,NY,NZ
+      integer*4 nx,ny,nz, ifXmm, ifYmm, ifZmm, ifMM(3)
+      integer*4 ixs, iys, izs, nxuse, nyuse, nzuse, ixyzs(3), nxyzuse(3)
+      integer*4 nsx,nsy,nsz,nsxyz(3)
+      real*4 dxsamp,dysamp,dzsamp,dxyzsamp(3)
+      COMMON //NX,NY,NZ, ifXmm, ifYmm, ifZmm,ixs, iys, izs, nxuse, nyuse,nzuse,
+     &    nsx,nsy,nsz,dxsamp,dysamp,dzsamp
 C       
       integer*4 NXYZ(3),MXYZ(3)
       real*4 ARRAY(idim)
 C       
       CHARACTER*160 FILIN,FILOUT
 C       
-      EQUIVALENCE (NX,NXYZ)
+      EQUIVALENCE (NX,NXYZ),(ifXmm,ifMM),(ixs,ixyzs),(nxuse, nxyzuse)
+      equivalence (nsx,nsxyz),(dxsamp,dxyzsamp)
       real*4 avg(2),sd(2)
 C       
-      character dat*9,tim*8
-c       
-c       7/7/00 CER: remove the encode's; titlech is the temp space
-c       
+      character dat*9,tim*8,errstr*9
       character*80 titlech
       logical report
-      integer*4 nout,ierr,iun,mode,idsamp,nsx,ixs,nsy,iys,nsz,izs,ndat,jz,iz
+      integer*4 nout,ierr,iun,mode,idsamp,ndat,jz,iz
       integer*4 jy,iy,jx,ix,i, maxLines, numChunks, iChunk, numLines, iyl
+      integer*4 iShift(3)
+      integer*4 iStart(3), iEnd(3)
       real*4 dmin,dmax,dmean,sem,scl,fadd,tsum,tmin,tmax,tmean
       real*8 dsum8, dsumsq8, tsum8, tsumsq8
 c       
       logical pipinput
       integer*4 numOptArg, numNonOptArg
       integer*4 PipGetString,PipGetInteger,PipGetFloat, PipGetLogical
-      integer*4 PipGetInOutFile
+      integer*4 PipGetInOutFile,PipGetTwoIntegers, PipGetThreeIntegers
 c       
 c       fallbacks from ../../manpages/autodoc2man -2 2  densmatch
 c       
       integer numOptions
-      parameter (numOptions = 5)
+      parameter (numOptions = 9)
       character*(40 * numOptions) options(1)
       options(1) =
      &    'reference:ReferenceFile:FN:@scaled:ScaledFile:FN:@'//
-     &    'output:OutputFile:FN:@report:ReportOnly:B:@help:usage:B:'
-c       
+     &    'output:OutputFile:FN:@report:ReportOnly:B:@'//
+     &    'xminmax:XMinAndMax:IP:@yminmax:YMinAndMax:IP:@'//
+     &    'zminmax:ZMinAndMax:IP:@offset:OffsetRefToScaledXYZ:FT:@'//
+     &    'help:usage:B:'
       filout = ' '
       report = .false.
+      do i = 1, 3
+        ifMM(i) = 0
+        iShift(i) = 0
+        iStart(i) = 0
+      enddo
 c       
 c       Pip startup: set error, parse options, check help, set flag if used
 c       
@@ -78,12 +78,12 @@ c
 c       Get input and output files
 c       
       if (PipGetInOutFile('ReferenceFile', 1,
-     &    'Name of reference volume', filin) .ne. 0) call errorexit(
+     &    'Name of reference volume', filin) .ne. 0) call exitError(
      &    'NO REFERENCE FILE WAS SPECIFIED')
       call imopen(1,filin,'ro')
 c       
       if (PipGetInOutFile('ScaledFile', 2, 'Name of volume to be scaled',
-     &    filin) .ne. 0) call errorexit(
+     &    filin) .ne. 0) call exitError(
      &    'NO FILE WAS SPECIFIED TO BE SCALED')
       call imopen(2,filin,'old')
 c       
@@ -95,40 +95,70 @@ c
         call imopen(3,filout,'new')
         nout=3
       endif
-      if (pipinput) ierr = PipGetLogical('ReportOnly', report)
+      if (pipinput) then
+        ierr = PipGetLogical('ReportOnly', report)
+        ifXmm = 1 - PipGetTwoIntegers('XMinAndMax', iStart(1), iEnd(1))
+        ifYmm = 1 - PipGetTwoIntegers('YMinAndMax', iStart(2), iEnd(2))
+        ifZmm = 1 - PipGetTwoIntegers('ZMinAndMax', iStart(3), iEnd(3))
+        if (PipGetThreeIntegers('OffsetRefToScaledXYZ', iShift(1), iShift(2),
+     &      iShift(3)) .eq. 0 .and. ifXmm + ifYmm + ifZmm .eq. 0)
+     &      call exitError(
+     &      'YOU MUST ENTER MIN AND MAX X, Y, OR Z IF YOU ENTER OFFSETS')
+      endif
       call PipDone()
 c       
 c       sample each volume to find mean and SD
 c       
       do iun=1,2
         call irdhdr(iun,nxyz,mxyz,mode,dmin,dmax,dmean)
-        if(nx.ge.idim)call errorexit('IMAGE TOO LARGE IN X FOR ARRAY')
-        idsamp=((float(nx*ny)*nz)/(8.*maxsamp))**.3333 + 1.
-        nsx=(nx/2)/idsamp+1
-        ixs=nx/2-0.5*nsx*idsamp
-        ixs=max(0,ixs)
-        if(nsx.eq.1)ixs=nx/2
-        nsy=(ny/2)/idsamp+1
-        iys=ny/2-0.5*nsy*idsamp
-        iys=max(0,iys)
-        if(nsy.eq.1)iys=ny/2
-        nsz=(nz/2)/idsamp+1
-        izs=nz/2-0.5*nsz*idsamp
-        izs=max(0,izs)
-        if(nsz.eq.1)izs=nz/2
+        if(nx.ge.idim)call exitError('IMAGE TOO LARGE IN X FOR ARRAY')
+        do i = 1, 3
+          nxyzuse(i) = nxyz(i) / 2
+          ixyzs(i) = nxyz(i) / 4
+          if (ifMM(i) .ne. 0) then
+            ixyzs(i) = iStart(i)
+            nxyzuse(i) = iEnd(i) + 1 - iStart(i)
+            if (iun .eq. 2) ixyzs(i) = ixyzs(i) + iShift(i)
+            errstr = ' '
+            if (ixyzs(i) .lt. 0 .or. ixyzs(i) .ge.nxyz(i)) errstr = 'STARTING '
+            if (ixyzs(i) + nxyzuse(i) .le. 0 .or.
+     &          ixyzs(i) + nxyzuse(i) .gt. nxyz(i)) errstr = 'ENDING '
+            if (errstr .ne. ' ') then
+              write(*,'(/,a,a,a,a,i2)')'ERROR: DENSMATCH - ',errstr,
+     &            char(ichar('W') + i),' COORDINATE OUT OF RANGE IN VOLUME #',
+     &            iun
+              call exit(1)
+            endif
+          endif
+        enddo
+        idsamp=(((float(nxuse)*nyuse)*nzuse)/maxsamp)**.3333 + 1.
+c         
+c         Make sure there are at least 10 samples in each direction
+        do i = 1, 3
+          nsxyz(i)=max((nxyzuse(i) - 1)/idsamp + 1, min(nxyzuse(i), 10))
+          if (nsxyz(i) .eq. 1) then
+            dxyzsamp(i) = 1
+            ixyzs(i) = (iEnd(i) + 1 + iStart(i)) / 2
+          else
+            dxyzsamp(i) = (nxyzuse(i) - 1.) / (nsxyz(i) - 1.)
+          endif
+c          write(*,'(a,i2,a,i2,a,i5,a,i5,a,i4,a,f9.2)')'vol',iun,' axis',i,
+c     &        ' start', ixyzs(i),' use',nxyzuse(i),' #samp',nsxyz(i),' dsamp',
+c     &        dxyzsamp(i)
+        enddo
         ndat=0
         dsum8 = 0.
         dsumsq8 = 0.
         do jz=1,nsz
-          iz=izs+(jz-1)*idsamp
+          iz=izs+(jz-1)*dzsamp
           tsum8 = 0.
           tsumsq8 = 0.
           do jy=1,nsy
-            iy=iys+(jy-1)*idsamp
+            iy=iys+(jy-1)*dysamp
             call imposn(iun,iz,iy)
             call irdlin(iun,array,*99)
             do jx=1,nsx
-              ix=1+ixs+(jx-1)*idsamp
+              ix=1+ixs+(jx-1)*dxsamp
               ndat=ndat+1
               tsum8 = tsum8 + array(ix)
               tsumsq8 = tsumsq8 + array(ix)**2
@@ -199,12 +229,17 @@ c       read(titlech,'(20a4)')(title(kti),kti=1,20)
       call exit(0)
 3000  format ( 'DENSMATCH: Scaled volume to match another',t57,a9,2x,
      &    a8)
-99    call errorexit('READING FILE')
+99    call exitError('READING FILE')
       end
 
-      subroutine errorexit(message)
-      character*(*) message
-      print *
-      print *,'ERROR: DENSMATCH - ',message
-      call exit(1)
-      end
+c       
+c       $Log$
+c       Revision 3.3  2006/09/28 21:25:22  mast
+c       changed call to avgsd8
+c
+c       Revision 3.2  2006/02/26 20:22:08  mast
+c       Made it process image in chunks so there is no more size limit
+c
+c       Revision 3.1  2004/08/24 04:15:38  mast
+c       Converted to PIP input, made it 5K capable, added report-only option
+c	
