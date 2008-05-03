c7affc1448bcfc7aaa8a813db7d2b2802c1a9590
SueHeld <sueh@colorado.edu>
2008-05-03 00:44:39 +0000

    bug# 847 Interface for ProcessSeries to be used outside of manager andi expert classes and in process functions which to do not add to a process series.

---
+++ Etomo/src/etomo/type/ConstProcessSeries.java
@@ -0,0 +1,25 @@
+package etomo.type;
+
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2006</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface ConstProcessSeries {
+  public static  final String  rcsid =  "$Id$";
+  
+  public String peekNextProcess();
+  public boolean startNextProcess( AxisID axisID,
+       ProcessResultDisplay processResultDisplay);
+}
