0004064a747c2ea0ef25de1f5262254e3a147dc9
SueHeld <sueh@colorado.edu>
2008-05-03 00:55:56 +0000

    bug# 847 Changed run3dmod to action to make it more standard.

--- Etomo/src/etomo/ui/Run3dmodButtonContainer.java
+++ Etomo/src/etomo/ui/Run3dmodButtonContainer.java
@@ -3,27 +3,33 @@ package etomo.ui;
 import etomo.type.Run3dmodMenuOptions;
 
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright (c) 2005</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-*/
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright (c) 2005</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ */
 interface Run3dmodButtonContainer {
-  public static  final String  rcsid =  "$Id$";
-  
-  public void run3dmod(Run3dmodButton button, Run3dmodMenuOptions menuOptions);
+  public static final String rcsid = "$Id$";
+
+  public void action(Run3dmodButton button, Run3dmodMenuOptions menuOptions);
 }
 /**
-* <p> $Log$
-* <p> Revision 1.1  2005/08/09 20:34:10  sueh
-* <p> bug# 711  Interface for panel or dialog containing instance(s) of
-* <p> Run3dmodButton.
-* <p> </p>
-*/
+ * <p> $Log$
+ * <p> Revision 1.2  2005/08/11 23:58:17  sueh
+ * <p> bug# 711  Change enum Run3dmodMenuOption to
+ * <p> Run3dmodMenuOptions, which can turn on multiple options at once.
+ * <p> This allows ImodState to combine input from the context menu and the
+ * <p> pulldown menu.
+ * <p>
+ * <p> Revision 1.1  2005/08/09 20:34:10  sueh
+ * <p> bug# 711  Interface for panel or dialog containing instance(s) of
+ * <p> Run3dmodButton.
+ * <p> </p>
+ */
