e218a6ca488a6c10e2d2ca608aefd9c12494fc50
SueHeld <sueh@colorado.edu>
2008-05-03 00:46:02 +0000

    bug# 847 Renamed setOptions to orGlobalOptions, which is a better description of its functionality.

--- Etomo/src/etomo/type/Run3dmodMenuOptions.java
+++ Etomo/src/etomo/type/Run3dmodMenuOptions.java
@@ -3,32 +3,32 @@ package etomo.type;
 import etomo.ui.UIHarness;
 
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright (c) 2005</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-*/
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright (c) 2005</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEM),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ */
 public class Run3dmodMenuOptions {
-  public static  final String  rcsid =  "$Id$";
-  
+  public static final String rcsid = "$Id$";
+
   private boolean startupWindow = false;
   private boolean binBy2 = false;
   private boolean allowBinningInZ = false;
   private boolean noOptions = false;
-  
+
   public String toString() {
     return "[startupWindow=" + startupWindow + ",binBy2=" + binBy2
         + ",\nallowBinningInZ=" + allowBinningInZ + ",noOptions=" + noOptions
         + "]";
   }
-  
+
   public void setNoOptions(boolean noOptions) {
     this.noOptions = noOptions;
     if (noOptions) {
@@ -36,57 +36,60 @@ public class Run3dmodMenuOptions {
       binBy2 = false;
     }
   }
-  
-  public void getOptions() {
+
+  public void orGlobalOptions() {
     if (noOptions) {
       return;
     }
     boolean startupWindow = UIHarness.INSTANCE.is3dmodStartupWindow();
     if (startupWindow) {
-      this.startupWindow  = true;
+      this.startupWindow = true;
     }
-    boolean binBy2  = UIHarness.INSTANCE.is3dmodBinBy2();
+    boolean binBy2 = UIHarness.INSTANCE.is3dmodBinBy2();
     if (binBy2) {
-      this.binBy2  = true;
+      this.binBy2 = true;
     }
   }
-  
+
   public void setAllowBinningInZ(boolean allowBinningInZ) {
     this.allowBinningInZ = allowBinningInZ;
   }
-  
+
   public void setStartupWindow(boolean startupWindow) {
     if (noOptions) {
       return;
     }
     this.startupWindow = startupWindow;
   }
-  
+
   public void setBinBy2(boolean binBy2) {
     if (noOptions) {
       return;
     }
     this.binBy2 = binBy2;
   }
-  
+
   public boolean isBinBy2() {
     return binBy2;
   }
-  
+
   public boolean isAllowBinningInZ() {
     return allowBinningInZ;
   }
-  
+
   public boolean isStartupWindow() {
     return startupWindow;
   }
 }
 /**
-* <p> $Log$
-* <p> Revision 1.1  2005/08/11 23:41:31  sueh
-* <p> bug# 711  Class to store menu options for running 3dmod.  Change enum
-* <p> Run3dmodMenuOption to Run3dmodMenuOptions, which can turn on
-* <p> multiple options at once.  This allows ImodState to combine input from
-* <p> the context menu and the pulldown menu.
-* <p> </p>
-*/
+ * <p> $Log$
+ * <p> Revision 1.2  2005/08/12 00:21:16  sueh
+ * <p> bug# 711 changed StartUpWindow to StartupWindow.
+ * <p>
+ * <p> Revision 1.1  2005/08/11 23:41:31  sueh
+ * <p> bug# 711  Class to store menu options for running 3dmod.  Change enum
+ * <p> Run3dmodMenuOption to Run3dmodMenuOptions, which can turn on
+ * <p> multiple options at once.  This allows ImodState to combine input from
+ * <p> the context menu and the pulldown menu.
+ * <p> </p>
+ */
