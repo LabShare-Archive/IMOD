d3f91aceb6e42ff29a5560836d1cb3395d0d207a
SueHeld <sueh@colorado.edu>
2008-05-13 23:06:32 +0000

    bug# 847 Reformatted.

--- Etomo/src/etomo/ui/Run3dmodButtonContainer.java
+++ Etomo/src/etomo/ui/Run3dmodButtonContainer.java
@@ -18,10 +18,14 @@ import etomo.type.Run3dmodMenuOptions;
 interface Run3dmodButtonContainer {
   public static final String rcsid = "$Id$";
 
-  public void action(Run3dmodButton button, Run3dmodMenuOptions menuOptions);
+  public void action(Run3dmodButton button,
+      Run3dmodMenuOptions run3dmodMenuOptions);
 }
 /**
  * <p> $Log$
+ * <p> Revision 1.3  2008/05/03 00:55:56  sueh
+ * <p> bug# 847 Changed run3dmod to action to make it more standard.
+ * <p>
  * <p> Revision 1.2  2005/08/11 23:58:17  sueh
  * <p> bug# 711  Change enum Run3dmodMenuOption to
  * <p> Run3dmodMenuOptions, which can turn on multiple options at once.
