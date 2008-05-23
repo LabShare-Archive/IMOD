c16966005581ecc25aae863a1038350f262f80b7
mastcu <mast@colorado.edu>
2008-05-23 22:56:08 +0000

    Added float support, NTSC gray option, standardized error output

--- mrc/tif2mrc.c
+++ mrc/tif2mrc.c
@@ -28,9 +28,11 @@
 int read_cheeze_tiff(FILE *tif_fp, unsigned char *pixels);
 static float minmaxmean(unsigned char *tifdata, int mode, int unsign, 
                         int divide, int xsize, int ysize, int *min, int *max);
-static void convertrgb(unsigned char *tifdata, int xsize, int ysize);
+static void convertrgb(unsigned char *tifdata, int xsize, int ysize, int ntsc);
 static void expandIndexToRGB(unsigned char **datap, ImodImageFile *iifile, 
                               int section);
+static void manageMode(Tf_info *tiff, int keepUshort, int forceSigned, 
+                       int makegray, int *pixSize, int *mode);
 int main( int argc, char *argv[])
 {
 
@@ -55,6 +57,7 @@ int main( int argc, char *argv[])
   int keepUshort = 0;
   int forceSigned = 0;
   int readFirst = 0;
+  int useNTSC = 0;
   int xsize = XSIZE, ysize = YSIZE;
   int mrcxsize = 0, mrcysize = 0, mrcsizeset;
   int bpix;
@@ -78,25 +81,25 @@ int main( int argc, char *argv[])
   max = -100000;
 
   if (argc < 3){
-    fprintf(stderr, "Tif2mrc Version %s %s %s\n" , VERSION_NAME,
+    printf("Tif2mrc Version %s %s %s\n" , VERSION_NAME,
             __DATE__, __TIME__);
     imodCopyright();
-    fprintf(stderr,
-            "Usage: %s [options] <tiff files...> <mrcfile>\n"
-            , progname);
-    fprintf(stderr, "Options:\n");
-    fprintf(stderr, "\t-g      Convert 24-bit RGB to 8-bit grayscale\n");
-    fprintf(stderr, "\t-u      Convert unsigned 16-bit values by "
+    printf("Usage: %s [options] <tiff files...> <mrcfile>\n" , progname);
+    printf("Options:\n");
+    printf("\t-g      Convert 24-bit RGB to 8-bit grayscale\n");
+    printf("\t-G      Convert 24-bit RGB to 8-bit grayscale with NTSC"
+            " scaling\n");
+    printf("\t-u      Convert unsigned 16-bit values by "
             "subtracting 32768\n");
-    fprintf(stderr, "\t-d      Convert unsigned 16-bit values by "
+    printf("\t-d      Convert unsigned 16-bit values by "
             "dividing by 2\n");
-    fprintf(stderr, "\t-k      Keep unsigned 16-bit values; store in unsigned "
+    printf("\t-k      Keep unsigned 16-bit values; store in unsigned "
             "integer mode\n");
-    fprintf(stderr, "\t-s      Store as signed integers (mode 1) even if data "
+    printf("\t-s      Store as signed integers (mode 1) even if data "
             "are unsigned\n");
-    fprintf(stderr, "\t-f      Read only first image of multi-page file\n");
-    fprintf(stderr, "\t-o x,y  Set output file size in X and Y\n");
-    fprintf(stderr, "\t-b file Background subtract image in given file\n");
+    printf("\t-f      Read only first image of multi-page file\n");
+    printf("\t-o x,y  Set output file size in X and Y\n");
+    printf("\t-b file Background subtract image in given file\n");
 
     exit(3);
   }
@@ -110,6 +113,11 @@ int main( int argc, char *argv[])
 
       case 'g': /* convert rgb to gray scale */
         makegray = TRUE;
+        break;
+ 
+      case 'G': /* convert rgb to gray scale */
+        makegray = TRUE;
+        useNTSC = 1;
         break;
  
       case 'u': /* treat ints as unsigned */
@@ -151,13 +159,13 @@ int main( int argc, char *argv[])
        
   }
   if ( (argc - 1) < (i + 1)){
-    fprintf(stderr, "%s: argument error: no output file specified.\n",
+    printf("ERROR: %s - Argument error: no output file specified.\n",
             progname);
     exit(3);
   }
 
   if (divide + unsign + keepUshort + forceSigned > 1) {
-    fprintf(stderr, "%s: You must select only one of -u, -d, -k, or -s.\n",
+    printf("ERROR: %s - You must select only one of -u, -d, -k, or -s.\n",
             progname);
     exit(3);
   }
@@ -173,7 +181,7 @@ int main( int argc, char *argv[])
     int tiffPages;
 
     if (tiff_open_file(argv[i], openmode, &tiff)) {
-      fprintf(stderr, "tif2mrc: Couldn't open %s.\n", argv[i]);
+      printf("ERROR: tif2mrc - Couldn't open %s.\n", argv[i]);
       exit(3);
     }
        
@@ -191,13 +199,13 @@ int main( int argc, char *argv[])
       printf("Reading multi-paged TIFF file.\n");
 
       if (bg){
-        fprintf(stderr, "Background subtraction not supported for "
+        printf("ERROR: tif2mrc - Background subtraction not supported for "
                 "multi-paged images.\n");
         exit(1);
       }
 
       if (mrcxsize) 
-        fprintf(stderr, "Warning: output file size option ignored for "
+        printf("Warning: output file size option ignored for "
                 "multi-paged file\n");
 
       if (!tiff.iifile) {
@@ -211,13 +219,13 @@ int main( int argc, char *argv[])
       }
 
       if (imodBackupFile(argv[argc - 1])) {
-        fprintf(stderr, "%s: Error, couldn't create backup", progname);
+        printf("ERROR: %s - Couldn't create backup", progname);
         exit(3);
       }
       mrcfp = fopen(argv[argc - 1], "wb");
       if (!mrcfp){
         perror("tif2mrc");
-        fprintf(stderr, "Error opening %s\n", argv[argc - 1]);
+        printf("ERROR: tif2mrc - Opening %s\n", argv[argc - 1]);
         exit(2);
       }
 
@@ -226,23 +234,7 @@ int main( int argc, char *argv[])
       xysize = xsize * ysize;
       mrc_head_new(&hdata, xsize, ysize, tiffPages, mode);
       mrc_head_write(mrcfp, &hdata);
-      pixSize = 1;
-      if (tiff.BitsPerSample == 16){
-        mode = MRC_MODE_SHORT;
-        pixSize = 2;
-      }
-
-      /* Use unsigned mode either if user requested it or if the file 
-         specifies unsigned and the user did not request signed */
-      if (mode == MRC_MODE_SHORT && 
-          (keepUshort || (!forceSigned && tiff.iifile && 
-                          tiff.iifile->type == IITYPE_USHORT)))
-        mode = MRC_MODE_USHORT;
-
-      if (tiff.PhotometricInterpretation / 2 == 1 && !makegray){
-        mode = MRC_MODE_RGB;
-        pixSize = 3;
-      }
+      manageMode(&tiff, keepUshort, forceSigned, makegray, &pixSize, &mode);
 
       printf("Converting %d images size %d x %d\n", 
              tiffPages, xsize, ysize);
@@ -253,9 +245,9 @@ int main( int argc, char *argv[])
           (tiffp, &tiff, section);
 
         if (!tifdata){
-          printf("Warning: failed to get image data for "
+          printf("ERROR: tif2mrc - Failed to get image data for "
                  "section %d\n", section);
-          continue;
+          exit(1);
         }
 
         if (tiff.PhotometricInterpretation == 3)
@@ -263,7 +255,7 @@ int main( int argc, char *argv[])
 
         /* convert RGB to gray scale */
         if (tiff.PhotometricInterpretation / 2 == 1 && makegray)
-          convertrgb(tifdata, xsize, ysize);
+          convertrgb(tifdata, xsize, ysize, useNTSC);
 
          
         mean += minmaxmean(tifdata, mode, unsign, divide, xsize, 
@@ -311,20 +303,20 @@ int main( int argc, char *argv[])
   /* read in bg file */
   if (bg){
     if (tiff_open_file(bgfile, openmode, &tiff)) {
-      fprintf(stderr, "tif2mrc: Couldn't open %s.\n", bgfile);
+      printf("ERROR: tif2mrc - Couldn't open %s.\n", bgfile);
       exit(3);
     }
     bgfp = tiff.fp;
 
     bgdata = (unsigned char *)tiff_read_file(bgfp, &tiff);
     if (!bgdata){
-      fprintf(stderr, "tif2mrc: Error reading %s.\n", bgfile);
+      printf("ERROR: tif2mrc - Reading %s.\n", bgfile);
       exit(3);
     }
     bgBits = tiff.BitsPerSample;
     if ((bgBits != 8 && bgBits !=16) || tiff.PhotometricInterpretation >= 
         2) {
-      fprintf(stderr, "tif2mrc: Background file must be 8 or 16-bit "
+      printf("ERROR: tif2mrc - Background file must be 8 or 16-bit "
               "grayscale\n");
       exit(3);
     }
@@ -361,13 +353,13 @@ int main( int argc, char *argv[])
 
   /* Write out mrcheader */
   if (imodBackupFile(argv[argc - 1])) {
-    fprintf(stderr, "%s: Error, couldn't create backup", progname);
+    printf("ERROR: %s - Couldn't create backup", progname);
     exit(3);
   }
   mrcfp = fopen(argv[argc - 1], "wb");
   if (!mrcfp){
     perror("tif2mrc");
-    fprintf(stderr, "Error opening %s\n", argv[argc - 1]);
+    printf("ERROR: tif2mrc - Opening %s\n", argv[argc - 1]);
     exit(2);
   }
   mrc_head_new(&hdata, xsize, ysize, argc - i - 1, mode);
@@ -380,7 +372,7 @@ int main( int argc, char *argv[])
        
     /* Open the TIFF file. */
     if (tiff_open_file(argv[i], openmode, &tiff)) {
-      fprintf(stderr, "tif2mrc: Couldn't open %s.\n", argv[i]);
+      printf("ERROR: tif2mrc - Couldn't open %s.\n", argv[i]);
       exit(3);
     }
     printf("Opening %s for input\n", argv[i]);
@@ -390,7 +382,7 @@ int main( int argc, char *argv[])
     /* Read in tiff file */
     tifdata = (unsigned char *)tiff_read_file(tiffp, &tiff);
     if (!tifdata){
-      fprintf(stderr, "tif2mrc: Error reading %s.\n", argv[i]);
+      printf("ERROR: tif2mrc - Reading %s.\n", argv[i]);
       exit(3);
     }
 
@@ -402,30 +394,14 @@ int main( int argc, char *argv[])
         mrcxsize = xsize;
         mrcysize = ysize;
       }
-      pixSize = 1;
-      if (tiff.BitsPerSample == 16){
-        mode = MRC_MODE_SHORT;
-        pixSize = 2;
-      }
-
-      /* Use unsigned mode either if user requested it or if the file 
-         specifies unsigned and the user did not request signed */
-      if (mode == MRC_MODE_SHORT && 
-          (keepUshort || (!forceSigned && tiff.iifile && 
-                          tiff.iifile->type == IITYPE_USHORT)))
-        mode = MRC_MODE_USHORT;
-      
-      if (tiff.PhotometricInterpretation / 2 == 1 && !makegray){
-        mode = MRC_MODE_RGB;
-        pixSize = 3;
-      }
+      manageMode(&tiff, keepUshort, forceSigned, makegray, &pixSize, &mode);
     }
 
     if ((tiff.BitsPerSample == 16 && mode != MRC_MODE_SHORT && mode != 
          MRC_MODE_USHORT) ||
         (tiff.PhotometricInterpretation / 2 == 1 && !makegray 
         && mode != MRC_MODE_RGB)) {
-      fprintf(stderr, "tif2mrc Error: All files must have the same"
+      printf("ERROR: tif2mrc - All files must have the same"
               " data type.\n");
       exit(3);
     }
@@ -435,13 +411,13 @@ int main( int argc, char *argv[])
 
     /* convert RGB to gray scale */
     if (tiff.PhotometricInterpretation / 2 == 1 && makegray)
-      convertrgb(tifdata, xsize, ysize);
+      convertrgb(tifdata, xsize, ysize, useNTSC);
        
     /* Correct for bg */
     if (bg){
-      if ((mode == MRC_MODE_BYTE && bgBits == 16) || 
-          (mode == MRC_MODE_SHORT && bgBits == 8)) {
-        fprintf(stderr, "tif2mrc Error: Background data must have "
+      if ((mode != MRC_MODE_SHORT && mode != MRC_MODE_USHORT && bgBits == 16) 
+          || (mode != MRC_MODE_BYTE && bgBits == 8)) {
+        printf("ERROR: tif2mrc - Background data must have "
                 " the same data type as the image files.\n");
         exit(3);
       }
@@ -479,7 +455,7 @@ int main( int argc, char *argv[])
       b3dFwrite(tifdata , pixSize, xsize*ysize, mrcfp);
 
     }else{
-      fprintf(stderr, "tif2mrc: Warning, file %s not same size.\n", argv[i]);
+      printf("WARNING: tif2mrc - File %s not same size.\n", argv[i]);
 
       /* Unequal sizes: set the fill value and pointer */
       switch (mode) {
@@ -493,11 +469,14 @@ int main( int argc, char *argv[])
         break;
       case MRC_MODE_SHORT:
         shortFill = tmean;
-        fillPtr = &shortFill;
+        fillPtr = (unsigned char *)&shortFill;
         break;
       case MRC_MODE_USHORT:
         ushortFill = tmean;
-        fillPtr = &ushortFill;
+        fillPtr = (unsigned char *)&ushortFill;
+        break;
+      case MRC_MODE_FLOAT:
+        fillPtr = (unsigned char *)&tmean;
         break;
       }
 
@@ -562,21 +541,60 @@ int main( int argc, char *argv[])
 
 }
 
-static void convertrgb(unsigned char *tifdata, int xsize, int ysize)
+/* Figure out the mode and the bytes/pixel from various values and options */
+static void manageMode(Tf_info *tiff, int keepUshort, int forceSigned, 
+                       int makegray, int *pixSize, int *mode)
 {
-  unsigned char *out, *in;
-  int x, pixel;
-  in = tifdata;
-  out = tifdata;
-     
-  for (x = 0; x < xsize*ysize; x++) {
-    pixel = *in++;
-    pixel += *in++;
-    pixel += *in++;
-    *out++ = pixel / 3;
+  *pixSize = 1;
+  if (tiff->BitsPerSample == 16){
+    *mode = MRC_MODE_SHORT;
+    *pixSize = 2;
+  }
+  if (tiff->BitsPerSample == 32){
+    *mode = MRC_MODE_FLOAT;
+    *pixSize = 4;
+  }
+
+  /* Use unsigned mode either if user requested it or if the file 
+     specifies unsigned and the user did not request signed */
+  if (*mode == MRC_MODE_SHORT && 
+      (keepUshort || (!forceSigned && tiff->iifile && 
+                      tiff->iifile->type == IITYPE_USHORT)))
+    *mode = MRC_MODE_USHORT;
+
+  if (tiff->PhotometricInterpretation / 2 == 1 && !makegray){
+    *mode = MRC_MODE_RGB;
+    *pixSize = 3;
   }
 }
 
+/* Convert rgb to gray scale by old equal weighting or NTSC weighting */
+static void convertrgb(unsigned char *tifdata, int xsize, int ysize, int ntsc)
+{
+  unsigned char *out, *in;
+  int x, pixel;
+  float fpixel;
+  in = tifdata;
+  out = tifdata;
+ 
+  if (ntsc) {
+    for (x = 0; x < xsize*ysize; x++) {
+      fpixel = *in++ * 0.3;
+      fpixel += *in++ * 0.59;
+      fpixel += *in++ * 0.11;
+      *out++ = (int)(fpixel + 0.5f);
+    }
+  } else {
+    for (x = 0; x < xsize*ysize; x++) {
+      pixel = *in++;
+      pixel += *in++;
+      pixel += *in++;
+      *out++ = pixel / 3;
+    }
+  }
+}
+
+/* Look up each index value in colormap and output r,g,b values */
 static void expandIndexToRGB(unsigned char **datap, ImodImageFile *iifile, 
                               int section)
 {
@@ -584,13 +602,13 @@ static void expandIndexToRGB(unsigned char **datap, ImodImageFile *iifile,
   unsigned char *outdata, *map;
   int xysize, i, ind;
   if (!iifile || !iifile->colormap) {
-    fprintf(stderr, "tif2mrc Error: Colormap data not read in properly.\n");
+    printf("ERROR: tif2mrc - Colormap data not read in properly.\n");
     exit(3);
   }
   xysize = iifile->nx * iifile->ny;
   outdata = (unsigned char *)malloc(3 * xysize);
   if (!outdata) {
-    fprintf(stderr, "tif2mrc Error: Insufficient memory.\n");
+    printf("ERROR: tif2mrc - Insufficient memory.\n");
     exit(3);
   }
   *datap = outdata;
@@ -604,7 +622,8 @@ static void expandIndexToRGB(unsigned char **datap, ImodImageFile *iifile,
   }
 }
 
-
+/* Find the min/max/mean of the data for the given mode and manage subtraction/
+   division for unsigned to signed conversion */
 static float minmaxmean(unsigned char *tifdata, int mode, int unsign, 
                         int divide, int xsize, int ysize, int *min, int *max)
 {
@@ -612,6 +631,7 @@ static float minmaxmean(unsigned char *tifdata, int mode, int unsign,
   int x, y, pixel;
   b3dInt16 *sptr;
   b3dUInt16 *usptr;
+  b3dFloat *fptr;
 
   if (mode == MRC_MODE_BYTE)
     for (x = 0; x < xsize * ysize; x++) {
@@ -663,11 +683,25 @@ static float minmaxmean(unsigned char *tifdata, int mode, int unsign,
       tmean += pixel;
     }
   }
+  if (mode == MRC_MODE_FLOAT) {
+    fptr = (b3dFloat *)tifdata;
+    for (x = 0; x < xsize * ysize; x++) {
+      pixel = fptr[x];
+      if (pixel < *min)
+        *min = pixel;
+      if(pixel > *max)
+        *max = pixel;
+      tmean += pixel;
+    }
+  }
   return (tmean / (xsize * ysize));
 }
 
 /* 
    $Log$
+   Revision 3.16  2007/10/15 21:42:57  mast
+   Fixed log setup
+
    Revision 3.15  2007/10/15 21:36:24  mast
    Fixed output of unequal sized data to use b3dFwrite instead of fputc, made
    it put out centered data and work for all modes, added output size option
