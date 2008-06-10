a0a01b57361218cecbafe303d13f820c03370755
mastcu <mast@colorado.edu>
2008-06-10 02:02:27 +0000

    Pass view to moveby

--- imod/imodv_light.h
+++ imod/imodv_light.h
@@ -1,30 +1,9 @@
 /*   imodv_light.h  -  declarations for imodv_light.cpp
  *
- *   Copyright (C) 1995-2002 by Boulder Laboratory for 3-Dimensional Electron
- *   Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
- *   Colorado.  See implementation file for full copyright notice.
- */                                                                           
-
-/*  $Author$
-
-$Date$
-
-$Revision$
-
-$Log$
-Revision 4.3  2004/11/05 19:08:12  mast
-Include local files with quotes, not brackets
-
-Revision 4.2  2004/09/21 20:18:51  mast
-Moved clipping function to imodv_ogl
-
-Revision 4.1  2003/02/10 20:41:55  mast
-Merge Qt source
-
-Revision 1.1.2.1  2002/12/18 04:10:30  mast
-initial creation
-
-*/
+ *
+ *  $Id$
+ *  Log at end of file
+ */
 
 #ifndef IMODV_LIGHT_H
 #define IMODV_LIGHT_H
@@ -40,7 +19,27 @@ void light_move(int *x, int *y);
 void light_on(Iobj *obj);
 void light_off(void);
 void imodvSetLight(Iview *vw);
-void light_moveby(int x, int y);
+void light_moveby(Iview *vw, int x, int y);
 void light_adjust(Iobj *obj, float r, float g, float b, int trans);
 
 #endif
+
+/*
+
+$Log$
+Revision 4.4  2005/06/20 22:20:29  mast
+Pass transparency to light_adjust
+
+Revision 4.3  2004/11/05 19:08:12  mast
+Include local files with quotes, not brackets
+
+Revision 4.2  2004/09/21 20:18:51  mast
+Moved clipping function to imodv_ogl
+
+Revision 4.1  2003/02/10 20:41:55  mast
+Merge Qt source
+
+Revision 1.1.2.1  2002/12/18 04:10:30  mast
+initial creation
+
+*/
