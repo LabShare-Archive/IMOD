b37d1ab91e6dfc17e32762a7bb71966f06943737
SueHeld <sueh@colorado.edu>
2008-06-20 20:01:23 +0000

    bug# 1119 Added NON_MATLAB_ITERATOR_ARRAY, which is the only type to use with ParsedIteratorDescriptor.  For clarity _NUMBER to MATLAB and NON_MATLAB.

--- Etomo/src/etomo/type/ParsedElementType.java
+++ Etomo/src/etomo/type/ParsedElementType.java
@@ -14,27 +14,34 @@ package etomo.type;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 1.2  2008/04/15 21:26:53  sueh
+ * <p> bug# 1105 Added array types.
+ * <p>
  * <p> Revision 1.1  2008/04/02 02:19:24  sueh
  * <p> bug# 1097 Added a type because the type changes how an empty
  * <p> element displays itself.
  * <p> </p>
  */
 public final class ParsedElementType {
-  public static final ParsedElementType NON_MATLAB = new ParsedElementType();
-  public static final ParsedElementType MATLAB = new ParsedElementType();
+  public static final ParsedElementType NON_MATLAB_NUMBER = new ParsedElementType();
+  public static final ParsedElementType MATLAB_NUMBER = new ParsedElementType();
   static final ParsedElementType NON_MATLAB_ARRAY = new ParsedElementType();
-  static final ParsedElementType MATLAB_ARRAY = new ParsedElementType();
+  static final ParsedElementType NON_MATLAB_ITERATOR_ARRAY = new ParsedElementType();
+  public static final ParsedElementType MATLAB_ARRAY = new ParsedElementType();
   static final ParsedElementType STRING = new ParsedElementType();
 
   public String toString() {
-    if (this == NON_MATLAB) {
-      return "Non Matlab";
+    if (this == NON_MATLAB_NUMBER) {
+      return "Non Matlab Number";
     }
     if (this == NON_MATLAB_ARRAY) {
       return "Non Matlab Array";
     }
-    if (this == MATLAB) {
-      return "Matlab";
+    if (this == NON_MATLAB_ITERATOR_ARRAY) {
+      return "Non Matlab Iterator Array";
+    }
+    if (this == MATLAB_NUMBER) {
+      return "Matlab Number";
     }
     if (this == MATLAB_ARRAY) {
       return "Matlab Array";
@@ -46,27 +53,37 @@ public final class ParsedElementType {
   }
 
   boolean isArray() {
-    if (this == NON_MATLAB_ARRAY || this == MATLAB_ARRAY) {
+    if (this == NON_MATLAB_ARRAY || this == MATLAB_ARRAY
+        || this == NON_MATLAB_ITERATOR_ARRAY) {
+      return true;
+    }
+    return false;
+  }
+
+  boolean isIterator() {
+    if (this == NON_MATLAB_ITERATOR_ARRAY) {
       return true;
     }
     return false;
   }
 
   boolean isMatlab() {
-    if (this == MATLAB || this == MATLAB_ARRAY) {
+    if (this == MATLAB_NUMBER || this == MATLAB_ARRAY) {
       return true;
     }
     return false;
   }
 
   ParsedElementType toArrayInstance() {
-    if (this == NON_MATLAB_ARRAY || this == MATLAB_ARRAY || this == STRING) {
+    if (this == NON_MATLAB_ARRAY || this == MATLAB_ARRAY
+        || this == NON_MATLAB_ITERATOR_ARRAY || this == STRING) {
+      //Already an array or has no array equivalent
       return this;
     }
-    if (this == NON_MATLAB) {
+    if (this == NON_MATLAB_NUMBER) {
       return NON_MATLAB_ARRAY;
     }
-    if (this == MATLAB) {
+    if (this == MATLAB_NUMBER) {
       return MATLAB_ARRAY;
     }
     return NON_MATLAB_ARRAY;
