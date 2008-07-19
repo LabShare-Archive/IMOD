c3cce4170a04a772877bbcbd7d8f92b0c2999926
SueHeld <sueh@colorado.edu>
2008-07-19 01:11:49 +0000

    bug# 1125 Added cbParallelProcessing and ltfCpus.

--- Etomo/src/etomo/ui/SettingsDialog.java
+++ Etomo/src/etomo/ui/SettingsDialog.java
@@ -5,6 +5,8 @@ import java.awt.event.*;
 import javax.swing.*;
 
 import etomo.EtomoDirector;
+import etomo.storage.CpuAdoc;
+import etomo.type.AxisID;
 import etomo.type.UserConfiguration;
 
 /**
@@ -15,86 +17,143 @@ import etomo.type.UserConfiguration;
  * To enable and disable the creation of type comments go to
  * Window>Preferences>Java>Code Generation.
  */
-public class SettingsDialog extends JDialog {
+public final class SettingsDialog extends JDialog {
   public static final String rcsid = "$Id$";
 
   // Font selection panel
-  JPanel panelFontSelect = new JPanel();
-  String[] fontFamilies = GraphicsEnvironment.getLocalGraphicsEnvironment()
-      .getAvailableFontFamilyNames();
-  JList listFontFamily = new JList(fontFamilies);
-  JScrollPane scrollFontFamily = new JScrollPane(listFontFamily);
-  LabeledTextField ltfFontSize = new LabeledTextField("Size:");
-  JPanel panelSettings = new JPanel();
-  LabeledTextField ltfTooltipsInitialDelay = new LabeledTextField(
-      "Tooltips initial delay");
-  LabeledTextField ltfTooltipsDismissDelay = new LabeledTextField(
-      "Tooltips dismiss delay");
-  CheckBox cbNativeLAF = new CheckBox("Native look & feel");
-  CheckBox cbAdvancedDialogs = new CheckBox("Always use advanced dialogs");
-  CheckBox cbAutoFit = new CheckBox("Auto-fit");
-  CheckBox cbCompactDisplay = new CheckBox("Compact Display");
-
-  JPanel panelButtons = new JPanel();
-  JButton buttonCancel = new JButton("Cancel");
-  JButton buttonApply = new JButton("Apply");
-  JButton buttonDone = new JButton("Done");
-  CheckBox cbSingleAxis = new CheckBox(SetupDialog.AXIS_TYPE_LABEL + ":  "
-      + SetupDialog.SINGLE_AXIS_LABEL);
-  CheckBox cbNoParallelProcessing = new CheckBox("Start with "
+  private final String[] fontFamilies = GraphicsEnvironment
+      .getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+  private final JList listFontFamily = new JList(fontFamilies);
+  private final LabeledTextField ltfFontSize = new LabeledTextField("Size: ");
+  private final LabeledTextField ltfTooltipsInitialDelay = new LabeledTextField(
+      "Tooltips initial delay: ");
+  private final LabeledTextField ltfTooltipsDismissDelay = new LabeledTextField(
+      "Tooltips dismiss delay: ");
+  private final CheckBox cbNativeLAF = new CheckBox("Native look & feel");
+  private final CheckBox cbAdvancedDialogs = new CheckBox(
+      "Always use advanced dialogs");
+  private final CheckBox cbAutoFit = new CheckBox("Auto-fit");
+  private final CheckBox cbCompactDisplay = new CheckBox("Compact Display");
+  private final JButton buttonCancel = new JButton("Cancel");
+  private final JButton buttonApply = new JButton("Apply");
+  private final JButton buttonDone = new JButton("Done");
+  private final CheckBox cbParallelProcessing = new CheckBox("Enable "
+      + ParallelPanel.FIELD_LABEL);
+  private final LabeledTextField ltfCpus = new LabeledTextField(
+      ProcessorTable.NUMBER_CPUS_HEADER + ": ");
+  private final CheckBox cbSingleAxis = new CheckBox(
+      SetupDialog.AXIS_TYPE_LABEL + ":  " + SetupDialog.SINGLE_AXIS_LABEL);
+  private final CheckBox cbNoParallelProcessing = new CheckBox("Start with "
       + ParallelPanel.FIELD_LABEL + " off");
-  CheckBox cbTiltAnglesRawtltFile = new CheckBox("Angle Source:  "
-      + TiltAnglePanel.EXISTING_RAWTILT_FILE);
-  CheckBox cbSwapYAndZ = new CheckBox(TrimvolPanel.REORIENTATION_GROUP_LABEL
-      + "  " + TrimvolPanel.SWAP_YZ_LABEL);
+  private final CheckBox cbTiltAnglesRawtltFile = new CheckBox(
+      "Angle Source:  " + TiltAnglePanel.EXISTING_RAWTILT_FILE);
+  private final CheckBox cbSwapYAndZ = new CheckBox(
+      TrimvolPanel.REORIENTATION_GROUP_LABEL + "  "
+          + TrimvolPanel.SWAP_YZ_LABEL);
 
-  public SettingsDialog() {
-    panelSettings = (JPanel) getContentPane();
-    panelSettings.setLayout(new BoxLayout(panelSettings, BoxLayout.Y_AXIS));
+  private SettingsDialog() {
+  }
 
+  public static SettingsDialog getInstance(String propertyUserDir) {
+    SettingsDialog instance = new SettingsDialog();
+    instance.buildDialog();
+    instance.loadData(propertyUserDir);
+    instance.addListeners();
+    return instance;
+  }
+
+  private void buildDialog() {
     setTitle("eTomo Settings");
-
+    SpacedPanel pnlMain = new SpacedPanel();
+    pnlMain.setBoxLayout(BoxLayout.Y_AXIS);
+    pnlMain.setComponentAlignmentX(Box.LEFT_ALIGNMENT);
+    ((JPanel) getContentPane()).add(pnlMain.getComponent());
     //  Layout the font panel
+    SpacedPanel panelFontSelect = new SpacedPanel();
+    panelFontSelect.setBoxLayout(BoxLayout.X_AXIS);
     panelFontSelect.add(new JLabel("Font family:"));
     listFontFamily.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     listFontFamily.setVisibleRowCount(3);
     ltfFontSize.setColumns(3);
+    JScrollPane scrollFontFamily = new JScrollPane(listFontFamily);
     panelFontSelect.add(scrollFontFamily);
     panelFontSelect.add(ltfFontSize.getContainer());
-    panelSettings.add(panelFontSelect);
-    panelSettings.add(ltfTooltipsInitialDelay.getContainer());
-    panelSettings.add(ltfTooltipsDismissDelay.getContainer());
-    panelSettings.add(cbAutoFit);
+    pnlMain.add(panelFontSelect);
+    pnlMain.add(ltfTooltipsInitialDelay.getContainer());
+    pnlMain.add(ltfTooltipsDismissDelay.getContainer());
+    //Settings
+    SpacedPanel pnlSettings = new SpacedPanel();
+    pnlSettings.setBoxLayout(BoxLayout.X_AXIS);
+    pnlSettings.setComponentAlignmentX(Box.LEFT_ALIGNMENT);
+    pnlMain.add(pnlSettings);
+    JPanel pnlGeneralSettings = new JPanel();
+    pnlGeneralSettings.setLayout(new BoxLayout(pnlGeneralSettings,
+        BoxLayout.Y_AXIS));
+    pnlSettings.add(pnlGeneralSettings);
+    pnlGeneralSettings.add(cbAutoFit);
     //TEMP bug# 614
     cbAutoFit.setEnabled(false);
     //TEMP
-    panelSettings.add(cbNativeLAF);
-    panelSettings.add(cbAdvancedDialogs);
-    panelSettings.add(cbCompactDisplay);
-    panelSettings.add(Box.createRigidArea(FixedDim.x0_y10));
-    JPanel panelDefaults = new JPanel();
-    panelDefaults.setLayout(new BoxLayout(panelDefaults, BoxLayout.Y_AXIS));
+    pnlGeneralSettings.add(cbNativeLAF);
+    pnlGeneralSettings.add(cbAdvancedDialogs);
+    pnlGeneralSettings.add(cbCompactDisplay);
+    //parallel processing settings
+    JPanel pnlParallelProcessing = new JPanel();
+    pnlParallelProcessing.setLayout(new BoxLayout(pnlParallelProcessing,
+        BoxLayout.Y_AXIS));
+    pnlParallelProcessing.setBorder(new EtchedBorder(
+        "User Level Parallel Processing").getBorder());
+    pnlSettings.add(pnlParallelProcessing);
+    JPanel pnlCheckBoxParallelProcessing = new JPanel();
+    pnlCheckBoxParallelProcessing.setLayout(new BoxLayout(
+        pnlCheckBoxParallelProcessing, BoxLayout.X_AXIS));
+    pnlCheckBoxParallelProcessing.add(Box.createHorizontalGlue());
+    pnlCheckBoxParallelProcessing.add(cbParallelProcessing);
+    pnlParallelProcessing.add(pnlCheckBoxParallelProcessing);
+    pnlParallelProcessing.add(ltfCpus.getContainer());
+    //defaults settings
+    SpacedPanel panelDefaults = new SpacedPanel();
+    panelDefaults.setBoxLayout(BoxLayout.Y_AXIS);
+    panelDefaults.setComponentAlignmentX(Box.LEFT_ALIGNMENT);
     panelDefaults.setBorder(new EtchedBorder("Defaults").getBorder());
     panelDefaults.add(cbSingleAxis);
     panelDefaults.add(cbNoParallelProcessing);
     panelDefaults.add(cbTiltAnglesRawtltFile);
     panelDefaults.add(cbSwapYAndZ);
-    panelSettings.add(panelDefaults);
-    panelSettings.add(Box.createRigidArea(FixedDim.x0_y10));
-
-    //  Bind the buttons and lay them out
-    buttonCancel.addActionListener(new buttonSettingsCancelListener(this));
-    buttonApply.addActionListener(new buttonSettingsApplyListener(this));
-    buttonDone.addActionListener(new buttonSettingsDoneListener(this));
-    panelButtons.setLayout(new BoxLayout(panelButtons, BoxLayout.X_AXIS));
+    pnlMain.add(panelDefaults);
+    //buttons
+    SpacedPanel panelButtons = new SpacedPanel();
+    panelButtons.setBoxLayout(BoxLayout.X_AXIS);
+    panelButtons.setAlignmentX(Box.CENTER_ALIGNMENT);
     panelButtons.add(buttonCancel);
     panelButtons.add(buttonApply);
     panelButtons.add(buttonDone);
-
-    panelSettings.add(panelButtons);
-    panelSettings.add(Box.createRigidArea(FixedDim.x0_y10));
-
+    pnlMain.add(panelButtons);
     pack();
+  }
+
+  /**
+   * Get data from CpuAdoc.
+   * @param propertyUserDir
+   */
+  private void loadData(String propertyUserDir) {
+    CpuAdoc cpuAdoc = CpuAdoc.getInstance(AxisID.FIRST, propertyUserDir);
+    //Disable parallel processing if it was enabled by a way that takes
+    //precidence over this one (cpu.adoc or IMOD_PROCESSORS).
+    cbParallelProcessing.setEnabled(!cpuAdoc.isFile() && !cpuAdoc.isEnvVar());
+  }
+
+  private void updateDisplay() {
+    ltfCpus.setEnabled(cbParallelProcessing.isEnabled()
+        && cbParallelProcessing.isSelected());
+  }
+
+  private void addListeners() {
+    SettingsDialogListener listener = new SettingsDialogListener(this);
+    buttonCancel.addActionListener(listener);
+    buttonApply.addActionListener(listener);
+    buttonDone.addActionListener(listener);
+    cbParallelProcessing.addActionListener(listener);
   }
 
   public void setParameters(UserConfiguration userConfig) {
@@ -111,6 +170,8 @@ public class SettingsDialog extends JDialog {
     cbNoParallelProcessing.setSelected(userConfig.getNoParallelProcessing());
     cbTiltAnglesRawtltFile.setSelected(userConfig.getTiltAnglesRawtltFile());
     cbSwapYAndZ.setSelected(userConfig.getSwapYAndZ());
+    cbParallelProcessing.setSelected(userConfig.getParallelProcessing());
+    ltfCpus.setText(userConfig.getCpus());
 
     // Get the current font parameters to set the UI
     // Since they may not be all the same make the assumption that the first
@@ -135,6 +196,7 @@ public class SettingsDialog extends JDialog {
       }
     }
     ltfFontSize.setText(currentFontSize);
+    updateDisplay();
   }
 
   public void getParameters(UserConfiguration userConfig) {
@@ -154,6 +216,8 @@ public class SettingsDialog extends JDialog {
     userConfig.setNoParallelProcessing(cbNoParallelProcessing.isSelected());
     userConfig.setTiltAnglesRawtltFile(cbTiltAnglesRawtltFile.isSelected());
     userConfig.setSwapYAndZ(cbSwapYAndZ.isSelected());
+    userConfig.setParallelProcessing(cbParallelProcessing.isSelected());
+    userConfig.setCpus(ltfCpus.getText());
   }
 
   public boolean isAppearanceSettingChanged(UserConfiguration userConfig) {
@@ -167,66 +231,41 @@ public class SettingsDialog extends JDialog {
         || userConfig.getSwapYAndZ() != cbSwapYAndZ.isSelected()
         || userConfig.getFontSize() != Integer.parseInt(ltfFontSize.getText())
         || !userConfig.getFontFamily().equals(
-            fontFamilies[listFontFamily.getSelectedIndex()])) {
+            fontFamilies[listFontFamily.getSelectedIndex()])
+        || userConfig.getParallelProcessing() != cbParallelProcessing
+            .isSelected()
+        || !userConfig.getCpus().toString().equals(ltfCpus.getText())) {
       return true;
     }
     return false;
   }
 
-  void buttonCancelAction() {
-    EtomoDirector.INSTANCE.closeSettingsDialog();
+  void action(final String command) {
+    if (command.equals(buttonCancel.getActionCommand())) {
+      EtomoDirector.INSTANCE.closeSettingsDialog();
+    }
+    else if (command.equals(buttonApply.getActionCommand())) {
+      EtomoDirector.INSTANCE.getSettingsParameters();
+    }
+    else if (command.equals(buttonDone.getActionCommand())) {
+      EtomoDirector.INSTANCE.getSettingsParameters();
+      EtomoDirector.INSTANCE.saveSettingsDialog();
+      EtomoDirector.INSTANCE.closeSettingsDialog();
+    }
+    else if (command.equals(cbParallelProcessing.getActionCommand())) {
+      updateDisplay();
+    }
   }
 
-  void buttonApplyAction() {
-    EtomoDirector.INSTANCE.getSettingsParameters();
-  }
+  private final class SettingsDialogListener implements ActionListener {
+    private final SettingsDialog adaptee;
 
-  void buttonDoneAction() {
-    EtomoDirector.INSTANCE.getSettingsParameters();
-    EtomoDirector.INSTANCE.saveSettingsDialog();
-    EtomoDirector.INSTANCE.closeSettingsDialog();
-  }
+    private SettingsDialogListener(final SettingsDialog settingsDialog) {
+      adaptee = settingsDialog;
+    }
 
-}
-
-/**
- *  Listeners to bind the buttons their action functions
- */
-class buttonSettingsCancelListener implements ActionListener {
-
-  SettingsDialog adaptee;
-
-  buttonSettingsCancelListener(SettingsDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.buttonCancelAction();
-  }
-}
-
-class buttonSettingsApplyListener implements ActionListener {
-
-  SettingsDialog adaptee;
-
-  buttonSettingsApplyListener(SettingsDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.buttonApplyAction();
-  }
-}
-
-class buttonSettingsDoneListener implements ActionListener {
-
-  SettingsDialog adaptee;
-
-  buttonSettingsDoneListener(SettingsDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.buttonDoneAction();
+    public void actionPerformed(final ActionEvent actionEvent) {
+      adaptee.action(actionEvent.getActionCommand());
+    }
   }
 }
