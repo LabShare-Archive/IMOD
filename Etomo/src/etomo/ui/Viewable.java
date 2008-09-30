96891468ee91ca8d7473deb060af936e7b6446e0
SueHeld <sueh@colorado.edu>
2008-09-30 22:53:28 +0000

    bug# 1113 An interface for tables that can be used with a Viewport.

---
+++ Etomo/src/etomo/ui/Viewable.java
@@ -0,0 +1,24 @@
+package etomo.ui;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2008</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+interface Viewable {
+  public static final String rcsid = "$Id$";
+
+  public void repositionViewer();
+
+  public int size();
+}
