0fb9ac467387c6305334cd11243562eca66c0322
SueHeld <sueh@colorado.edu>
2008-10-06 22:48:50 +0000

    bug# 1113 Made the viewport size changeable.  Added a minimum size. Simplified the decisions to call resetViewport:  always call it.  This cleaned up bugs.

--- Etomo/src/etomo/ui/Viewport.java
+++ Etomo/src/etomo/ui/Viewport.java
@@ -7,7 +7,11 @@ import javax.swing.JComponent;
 /**
  * <p>Description: Calculates a viewport that displays a portion of a table.
  * Receives paging commands, can tell a table when to redisplay the viewport.
- * Can tell a row whether it is in the viewport.</p>
+ * Can tell a row whether it is in the viewport.
+ * 
+ * This class calls resetViewport constantly.  This is the simplest way to
+ * handle the great variety of potential situations that can come up when trying
+ * the handle all the different requirements of the four (so far) tables.</p>
  * 
  * <p>Copyright: Copyright 2008</p>
  *
@@ -20,6 +24,9 @@ import javax.swing.JComponent;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 1.2  2008/10/01 22:54:28  sueh
+ * <p> bug# 1113 Renamed Viewer.repositionViewer to msgViewportMoved.
+ * <p>
  * <p> Revision 1.1  2008/09/30 22:55:16  sueh
  * <p> bug# 1113.  Class that keeps track of which rows should be displayed in
  * <p> a table.  Does not contain a container, but does have a paging panel that
@@ -29,17 +36,24 @@ import javax.swing.JComponent;
 final class Viewport {
   public static final String rcsid = "$Id$";
 
-  private static final int VIEWPORT_SIZE = 10;
+  private static final int MINUMUM_SIZE = 5;
 
   private final PagingPanel pagingPanel;
   private final Viewable viewable;
+  private final int size;
 
   private int start = 0;
   private int end = -1;
 
-  Viewport(final Viewable viewable, final JComponent parent1,
+  Viewport(final Viewable viewable, final int size, final JComponent parent1,
       final JComponent parent2, final JComponent parent3, final String uniqueKey) {
     this.viewable = viewable;
+    if (size < MINUMUM_SIZE) {
+      this.size = MINUMUM_SIZE;
+    }
+    else {
+      this.size = size;
+    }
     pagingPanel = PagingPanel.getInstance(this, parent1, parent2, parent3,
         uniqueKey);
     pagingPanel.setVisible(false);
@@ -50,7 +64,7 @@ final class Viewport {
   }
 
   void pageUpButtonAction() {
-    moveViewport(start - VIEWPORT_SIZE);
+    moveViewport(start - size);
   }
 
   void upButtonAction() {
@@ -62,86 +76,89 @@ final class Viewport {
   }
 
   void pageDownButtonAction() {
-    moveViewport(start + VIEWPORT_SIZE);
+    moveViewport(start + size);
   }
 
   void endButtonAction() {
-    moveViewport(viewable.size() - VIEWPORT_SIZE);
+    moveViewport(viewable.size() - size);
   }
 
   /**
-   * Notifies viewable if there has been a change in the viewport.
-   * @param start
+   * Sets a new start value.  Notifies viewable if there has been a change in
+   * the viewport.  Used by button action functions.  Calls resetViewport with
+   * newStart.
+   * @param newStart - new start value
    */
   private void moveViewport(final int newStart) {
-    updateDisplay();
     //save old start and end values
     int origStart = start;
     int origEnd = end;
-    if (!resetViewport(newStart)) {
-      return;
-    }
-    //decide whether viewable must update its display.
-    if (start != origStart || end != origEnd) {
+    //Reset viewport and decide whether viewable must update its display.
+    if (resetViewport(newStart) && start != origStart || end != origEnd) {
       viewable.msgViewportMoved();
     }
   }
 
   /**
-   * Sets the visibility of the paging panel based on whether
-   * the viewable size is large enough to page.
-   */
-  private void updateDisplay() {
-    //No paging if the viewport is set to the entire viewable area and the
-    //viewable area is smaller or equals to the viewport.
-    if (viewable.size() <= VIEWPORT_SIZE) {
-      //Hide the paging panel when it is unnecesary.
-      pagingPanel.setVisible(false);
-    }
-    else {
-      pagingPanel.setVisible(true);
-    }
-  }
-
-  /**
-   * Takes a new start index, fixes it based on the size and viewport size,
+   * Takes newStart index, fixes it based on the size and viewport size,
    * and sets it to the start member variable.  The new start index may be the
-   * value of the start member variable or it may be a different number.  Makes
+   * value of the start member variable or it may be a new value.  Makes
    * sure that the viewport is always equal to the viewport size, unless the
    * viewable size is less then the viewport size.  Sets the end member variable
    * from the start member variable and from the viewport size.  Fixes the end
    * location.
-   * @param newStart
+   * Also hides and shows the paging panel and enables/disabled the paging
+   * buttons.  Combining all of this functionality in one function and always
+   * running resetViewport is most reliable.
+   * This function needs to handle every situation that the viewport can get
+   * into.
+   * @param newStart - start with be set to newStart and then adjusted
    * @return false if the viewable size is too small to page
    */
   private boolean resetViewport(final int newStart) {
     int viewableSize = viewable.size();
     //No paging if the viewport is set to the entire viewable area and the
     //viewable area is smaller or equals to the viewport.
-    if (viewableSize <= VIEWPORT_SIZE && start == 0 && end == viewableSize - 1) {
-      return false;
+    if (viewableSize <= size) {
+      //Hide the paging panel when it is unnecesary.
+      pagingPanel.setVisible(false);
     }
-    start = newStart;
-    //fix start
-    //Prevent the viewport from going out of the viewable area or displaying
-    //less then the viewport size.
-    if (start > viewableSize - VIEWPORT_SIZE) {
-      start = viewableSize - VIEWPORT_SIZE;
+    else {
+      pagingPanel.setVisible(true);
     }
-    //Handles the case where the viewable area is smaller then the viewport
-    //size.
-    if (start < 0) {
-      start = 0;
+    boolean changed;
+    //No paging if the viewport is set to the entire viewable area and the
+    //viewable area is smaller or equals to the viewport.
+    if (viewableSize <= size && start == 0 && end == viewableSize - 1) {
+      changed = false;
     }
-    //set end
-    end = start + VIEWPORT_SIZE - 1;
-    //fix end
-    //Handles the case where the viewable area is smaller then the viewport
-    //size.
-    if (end >= viewableSize) {
-      end = viewableSize - 1;
+    else {
+      changed = true;
+      //set start
+      start = newStart;
+      //fix start
+      //Prevent the viewport from going out of the viewable area or displaying
+      //less then the viewport size.
+      if (start > viewableSize - size) {
+        start = viewableSize - size;
+      }
+      //Handles the case where the viewable area is smaller then the viewport
+      //size.
+      if (start < 0) {
+        start = 0;
+      }
+      //set end
+      end = start + size - 1;
+      //fix end
+      //Handles the case where the viewable area is smaller then the viewport
+      //size.
+      if (end >= viewableSize) {
+        end = viewableSize - 1;
+      }
     }
-    return true;
+    pagingPanel.setUpEnabled(start > 0);
+    pagingPanel.setDownEnabled(end < viewable.size() - 1);
+    return changed;
   }
 
   /**
@@ -152,33 +169,38 @@ final class Viewport {
   }
 
   /**
-   * Force an index to appear in the viewport.  Move the viewport as little as
-   * possible.
-   * @param index
-   * @return true if the viewport may have changed
+   * Runs resetViewport with the existing start value.  Useful for 0 length
+   * tables, especially when multiple rows where removed at the same time - the
+   * Processor table with nothing checked for instance.
    */
-  boolean includeRowInViewport(final int index) {
-    updateDisplay();
-    //If index is below the viewport, find a start value which puts the index in
-    //the viewport.
-    if (index > end) {
-      return resetViewport(index - VIEWPORT_SIZE + 1);
-    }
-    else {
-      return resetViewport(index);
-    }
+  void msgViewableChanged() {
+    resetViewport(start);
   }
 
   /**
+   * Force newIndex to appear in the viewport.  Useful for deleting, adding, and
+   * moving up or down.  Calls resetViewport.  Moves the viewport as little as
+   * possible.
+   * @param newIndex
+   * @return true if the viewport may have changed
+   */
+  boolean adjustViewport(final int newIndex) {
+    //If newIndex is below the viewport, find a start value which puts the index
+    //in the viewport.
+    if (newIndex > end) {
+      return resetViewport(newIndex - size + 1);
+    }
+    return resetViewport(newIndex);
+  }
+
+  /**
+   * Returns true if an index is current in the viewport.  Useful for displaying
+   * individual rows.  Calls resetViewport.
    * @param index
-   * @return True if an index is current in the viewport.
+   * @return True if an index is currently in the viewport.
    */
   boolean inViewport(final int index) {
-    updateDisplay();
-    if (start < 0 || start > viewable.size() - VIEWPORT_SIZE
-        || end < VIEWPORT_SIZE - 1 || end >= viewable.size()) {
-      resetViewport(start);
-    }
+    resetViewport(start);
     return index >= start && index <= end;
   }
 }
