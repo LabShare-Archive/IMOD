9fa947f3b8ecdb0a6f83754f0b892a5bee850084
mastcu <mast@colorado.edu>
2008-10-21 14:52:07 +0000

    Use third highest bin to limit the scaling

--- imod/histwidget.cpp
+++ imod/histwidget.cpp
@@ -1,3 +1,16 @@
+/*
+ *  histwidget.cpp - To draw the histogram widget for isosurface dialog
+ *
+ *  Author: Quanren Xiong   email: xiongq@colorado.edu
+ *
+ *  Copyright (C) 1995-2008 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
+ *
+ *  $Id$
+ *  Log at end of file
+ */
+
 #include <qwidget.h>
 #include <qpainter.h>
 #include <qpen.h>
@@ -11,12 +24,31 @@ HistWidget::HistWidget(QWidget *parent): QWidget(parent)
 
 void HistWidget::setHistMinMax()
 {
+  int i, maxbin1 = 0, maxbin2 = -1;
+  float max2 = -1, max3 = -1;
   minHist=hist[0];
   maxHist=hist[0];
-  for(int i=1;i<256;i++){
-     if(hist[i]>maxHist) maxHist=hist[i];
-     if(hist[i]<minHist) minHist=hist[i];
-   }
+  for(i=1;i<256;i++){
+    if(hist[i]>maxHist) {
+      maxHist=hist[i];
+      maxbin1 = i;
+    }
+    if(hist[i]<minHist) minHist=hist[i];
+  }
+
+  // Find the third highest bin and limit the max to somewhat higher than it
+  for (i=0; i < 256; i++) {
+    if (i != maxbin1 && hist[i] > max2) {
+      max2 = hist[i];
+      maxbin2 = i;
+    }
+  }
+  for (i=0; i < 256; i++) {
+    if (i != maxbin1 && i != maxbin2 && hist[i] > max3)
+      max3 = hist[i];
+  }
+  if (max3 > 0 && maxHist > 1.1 * max3)
+    maxHist = 1.1 * max3;
 }
 
 float HistWidget::computePercentile(float percentile)
@@ -53,3 +85,8 @@ void HistWidget::paintEvent(QPaintEvent *)
    }
 }
 
+/*
+
+$Log$
+
+*/
