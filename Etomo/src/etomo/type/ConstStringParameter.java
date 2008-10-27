d7f223888ad9f66dbe70fadebd562af767aa368c
SueHeld <sueh@colorado.edu>
2008-10-27 18:37:16 +0000

    bug# 1141 Constant interface for StringParameter.

---
+++ Etomo/src/etomo/type/ConstStringParameter.java
@@ -0,0 +1,23 @@
+package etomo.type;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2008</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface ConstStringParameter {
+  public static  final String  rcsid =  "$Id$";
+  
+  public boolean equals(String input);
+
+  public boolean endsWith(String input);
+}
