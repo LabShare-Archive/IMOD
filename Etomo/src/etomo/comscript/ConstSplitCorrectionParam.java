28ee003497ed01533e2510444d26c21d5ca61d21
SueHeld <sueh@colorado.edu>
2008-10-27 17:45:22 +0000

    bug# 1141 Constant interface to SplitCorrectionParam.

---
+++ Etomo/src/etomo/comscript/ConstSplitCorrectionParam.java
@@ -0,0 +1,22 @@
+package etomo.comscript;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2008</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+public interface ConstSplitCorrectionParam {
+  public static final String rcsid = "$Id$";
+
+  public String[] getCommand();
+}
