64d246ba5bf5f8c318e07814ce7bdb5848de64d1
SueHeld <sueh@colorado.edu>
2008-10-27 17:44:58 +0000

    bug# 1141 Constant interface to CtfPlotterParam.

---
+++ Etomo/src/etomo/comscript/ConstCtfPlotterParam.java
@@ -0,0 +1,26 @@
+package etomo.comscript;
+
+import etomo.type.ConstEtomoNumber;
+import etomo.type.ConstStringParameter;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2008</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface ConstCtfPlotterParam {
+  public static  final String  rcsid =  "$Id$";
+  
+  public ConstStringParameter getConfigFile();
+  public ConstEtomoNumber getExpectedDefocus();
+}
