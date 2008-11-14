33a36098feaab913441d0d5eb5dc830c72532ec1
mastcu <mast@colorado.edu>
2008-11-14 20:41:06 +0000

    Fix it right!

--- libcfshr/simplestat.c
+++ libcfshr/simplestat.c
@@ -61,7 +61,7 @@ void avgSD(float *x, int n, float *avg, float *sd, float *sem)
     sx += d;
     sxsq += d * d;
   }
-  sumsToAvgsd(sx, sxsq, n, &avnew, sd);
+  sumsToAvgSD(sx, sxsq, n, &avnew, sd);
   *avg += avnew;
   *sem = 0.;
   if (n > 0)
@@ -372,6 +372,9 @@ void lsfit2pred(float *x1, float *x2, float *y, int *n, float *a, float *b,
 /*
 
 $Log$
+Revision 1.3  2008/11/14 20:40:15  mast
+Switched name of call from avgsd
+
 Revision 1.2  2008/11/14 20:38:03  mast
 Change to remove b3dutil include
 
