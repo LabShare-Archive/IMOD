3b5c598746f30b01a9c5609740eed46664a47651
mastcu <mast@colorado.edu>
2008-12-08 17:24:19 +0000

    More functions for montage snapshots

--- imod/imod_moviecon.h
+++ imod/imod_moviecon.h
@@ -3,31 +3,11 @@
  *   Copyright (C) 1995-2002 by Boulder Laboratory for 3-Dimensional Electron
  *   Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
  *   Colorado.  See implementation file for full copyright notice.
- */                                                                           
+ * 
+ * $Id$
+ * Log at end
+ */
 
-/*  $Author$
-
-$Date$
-
-$Revision$
-
-$Log$
-Revision 4.3  2004/11/29 19:25:21  mast
-Changes to do QImage instead of RGB snapshots
-
-Revision 4.2  2003/12/18 22:44:23  mast
-New functions for starting at current point and slicer movies
-
-Revision 4.1  2003/02/10 20:41:55  mast
-Merge Qt source
-
-Revision 1.1.2.2  2003/01/27 00:30:07  mast
-Pure Qt version and general cleanup
-
-Revision 1.1.2.1  2003/01/14 21:44:51  mast
-initial creation
-
-*/
 #ifndef IMOD_MOVIECON_H
 #define IMOD_MOVIECON_H
 
@@ -37,7 +17,16 @@ int imcGetIncrement(ImodView *vw, int xyzt);
 void imcGetStartEnd(ImodView *vw, int xyzt, int *stout, int *endout);
 void imodMovieConDialog(ImodView *vw);
 float imcGetInterval(void);
-int imcGetMontageFactor();
+int imcGetMontageFactor(void);
+void imcSetMontageFactor(int val);
+bool imcGetScaleSizes(void);
+void imcSetScaleSizes(bool state);
+int imcGetSizeScaling(void);
+void imcSetSizeScaling(int value);
+bool imcGetSnapMontage(bool forDoing);
+void imcSetSnapMontage(bool state);
+bool imcGetSnapWholeMont(void);
+void imcSetSnapWholeMont(bool state);
 void imcSetMovierate(ImodView *vw, int newrate);
 void imcResetAll(ImodView *vw);
 void imcUpdateDialog();
@@ -60,6 +49,27 @@ void imcStartHereSelected(int which);
 void imcSnapSelected(int which);;
 void imcRateEntered(float value);
 void imcIncrementRate(int dir);
-void imcSetMontageFactor(int val);
 
 #endif
+/*
+
+$Log$
+Revision 4.4  2005/09/15 14:25:25  mast
+Added zap montage factor functions
+
+Revision 4.3  2004/11/29 19:25:21  mast
+Changes to do QImage instead of RGB snapshots
+
+Revision 4.2  2003/12/18 22:44:23  mast
+New functions for starting at current point and slicer movies
+
+Revision 4.1  2003/02/10 20:41:55  mast
+Merge Qt source
+
+Revision 1.1.2.2  2003/01/27 00:30:07  mast
+Pure Qt version and general cleanup
+
+Revision 1.1.2.1  2003/01/14 21:44:51  mast
+initial creation
+
+*/
