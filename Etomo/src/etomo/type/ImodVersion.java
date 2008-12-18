f453043a071565461179d69affa0c7e0a3d11801
mastcu <mast@colorado.edu>
2008-12-18 06:30:36 +0000

    *** empty log message ***

--- Etomo/src/etomo/type/ImodVersion.java
+++ Etomo/src/etomo/type/ImodVersion.java
@@ -17,5 +17,5 @@ package etomo.type;
 public class ImodVersion {
   public static  final String  rcsid =  "$Id$";
   
-  public static final String CURRENT_VERSION = "3.13.0.";
+  public static final String CURRENT_VERSION = "4.0.0.";
 }
