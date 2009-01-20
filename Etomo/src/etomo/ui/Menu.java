89e170ac8a52643928aeb5c569f3cb5cb6abd02b
SueHeld <sueh@colorado.edu>
2009-01-20 20:14:16 +0000

    bug# 1102 A self-naming JMenu.

---
+++ Etomo/src/etomo/ui/Menu.java
@@ -0,0 +1,46 @@
+package etomo.ui;
+
+import javax.swing.JMenu;
+
+import etomo.EtomoDirector;
+import etomo.storage.autodoc.AutodocTokenizer;
+import etomo.type.UITestFieldType;
+import etomo.util.Utilities;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2008</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+final class Menu extends JMenu {
+  public static final String rcsid = "$Id$";
+
+  public Menu(String s) {
+    super(s);
+  }
+
+  public void setText(String text) {
+    super.setText(text);
+    setName(text);
+  }
+
+  public void setName(String text) {
+    String name = Utilities.convertLabelToName(text);
+    super.setName(name);
+    if (EtomoDirector.INSTANCE.getArguments().isPrintNames()) {
+      System.out.println(UITestFieldType.MENU_ITEM.toString()
+          + AutodocTokenizer.SEPARATOR_CHAR + name + ' '
+          + AutodocTokenizer.DEFAULT_DELIMITER + ' ');
+    }
+  }
+}
