32674c03ccb7e303a1a3a4fddbda929d31efb68c
SueHeld <sueh@colorado.edu>
2009-01-20 20:47:38 +0000

    bug# 1102 Class to store an interface section.

--- Etomo/src/etomo/uitest/InterfaceSection.java
+++ Etomo/src/etomo/uitest/InterfaceSection.java
@@ -1,10 +1,18 @@
 package etomo.uitest;
 
-import etomo.storage.autodoc.ReadOnlyAttribute;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import etomo.storage.LogFile;
+import etomo.storage.autodoc.AutodocFactory;
 import etomo.storage.autodoc.ReadOnlyAutodoc;
-import etomo.storage.autodoc.ReadOnlySection;
-import etomo.type.UITestAction;
-import etomo.type.UITestField;
+import etomo.type.AxisID;
+import etomo.type.UITestActionType;
+import etomo.type.UITestSubjectType;
 
 /**
  * <p>Description: </p>
@@ -20,81 +28,140 @@ import etomo.type.UITestField;
  * @version $Revision$
  * 
  * <p> $Log$
+ * <p> Revision 1.2  2008/05/30 22:37:18  sueh
+ * <p> bug# 1102 Isolating the etomo.uitest package so it is not needed for
+ * <p> running EtomoDirector.
+ * <p>
  * <p> Revision 1.1  2008/05/30 21:36:33  sueh
  * <p> bug# 1102 Class representing the Interface sections in uitest.adoc.
  * <p> </p>
  */
-final class InterfaceSection {
+final class InterfaceSection extends Assert {
   public static final String rcsid = "$Id$";
 
-  private static final String DEFAULT_STRING = "default";
+  private final String sectionName;
 
-  private final ReadOnlySection section;
-  private final ReadOnlyAttribute dialogAttribute;
+  Map commandMap = null;
 
-  private InterfaceSection(ReadOnlySection section,
-      ReadOnlyAttribute dialogAttribute) {
-    this.section = section;
-    this.dialogAttribute = dialogAttribute;
-  }
-
-  static InterfaceSection getInstance(ReadOnlyAutodoc autodoc,
-      UITestCommand command) {
-    ReadOnlySection section = null;
-    ReadOnlyAttribute attribute = null;
-    try {
-      section = autodoc.getSection("Interface", command.getValue());
-      attribute = section.getAttribute(UITestAction.DIALOG.toString());
-    }
-    catch (NullPointerException e) {
-    }
-    return new InterfaceSection(section, attribute);
+  InterfaceSection(String sectionName) {
+    this.sectionName = sectionName;
   }
 
   /**
-   * Get the navigation UITestField from the value side of a Dialog attribute.
-   * @return the UITestField of the value of an attribute of the form:  navigation = value
+   * Get the unique panel command from the section.  Used to get the main frame
+   * for the current axis.
+   * @return
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws LogFile.ReadException
    */
-  UITestField getNavigationField() {
-    try {
-      return UITestField.getInstance(section.getAttribute("navigation")
-          .getValue());
-    }
-    catch (NullPointerException e) {
-      //Default navigation is bn
-      return UITestField.BUTTON;
-    }
+  Command getGotoFrameCommand(AxisID axisID) throws FileNotFoundException,
+      IOException, LogFile.ReadException {
+    readSection();
+    return (Command) commandMap.get(UITestActionType.GOTO.toString()
+        + UITestSubjectType.FRAME.toString() + axisID.getExtension());
   }
 
   /**
-   * Get the default dialog from the section if it is not already set.  Compare
-   * it to dialog.
-   * @param dialog
-   * @return true if there is an attribute:  Dialog.dialogName = default
+   * Get the unique panel command from the section.  Used to get the main frame
+   * for the current axis.
+   * @return
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws LogFile.ReadException
    */
-  boolean isDefaultDialog(String dialogName) {
-    try {
-      return dialogAttribute.getAttribute(dialogName).getValue().equals(
-          DEFAULT_STRING);
-    }
-    catch (NullPointerException e) {
-      return false;
-    }
+  Command getOpenFrameCommand(AxisID axisID) throws FileNotFoundException,
+      IOException, LogFile.ReadException {
+    readSection();
+    return (Command) commandMap.get(UITestActionType.OPEN.toString()
+        + UITestSubjectType.FRAME.toString() + axisID.getExtension());
   }
 
   /**
-   * Get a index of a dialog tab.
+   * Get the open.interface command.
+   * @return
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws LogFile.ReadException
+   */
+  Command getOpenInterfaceCommand() throws FileNotFoundException, IOException,
+      LogFile.ReadException {
+    readSection();
+    Command command= (Command) commandMap.get(UITestActionType.OPEN.toString()
+        + UITestSubjectType.INTERFACE.toString());
+    return command;
+  }
+
+  /**
+   * Get the open.dialog command for dialogName.
    * @param dialogName
-   * @return the value of a name pair of the form:  Dialog.dialogName.index = value
+   * @return
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws LogFile.ReadException
    */
-  int getIndex(String dialogName) {
+  Command getOpenDialogCommand(String dialogName) throws FileNotFoundException,
+      IOException, LogFile.ReadException {
+    readSection();
+    return (Command) commandMap.get(UITestActionType.OPEN.toString()
+        + UITestSubjectType.DIALOG.toString() + dialogName);
+  }
 
-    try {
-      return Integer.parseInt(dialogAttribute.getAttribute(dialogName)
-          .getAttribute("index").getValue());
+  /**
+   * Reads the entire section and stores each command in a hash map, key by
+   * action, subject, axis, and modifier (when they appear in the command).
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws LogFile.ReadException
+   */
+  private void readSection() throws FileNotFoundException, IOException,
+      LogFile.ReadException {
+    if (commandMap != null) {
+      return;
     }
-    catch (NullPointerException e) {
-      return 0;
+    commandMap = new HashMap();
+    ReadOnlyAutodoc autodoc = AutodocFactory.getInstance(AutodocFactory.UITEST,
+        AxisID.ONLY);
+    CommandReader reader = CommandReader.getSectionReader(autodoc,
+        SectionType.INTERFACE.toString(), sectionName, null, null);
+    Command command = null;
+    while (!reader.isDone()) {
+      command = reader.nextCommand(command);
+      if (command != null && command.isKnown()) {
+        //Build key for hash map
+        StringBuffer key = new StringBuffer();
+        //actionType
+        UITestActionType actionType = command.getActionType();
+        Subject subject = command.getSubject();
+        assertNotNull(
+            "action type required for all interface section commands ("
+                + command + ")", actionType);
+        key.append(actionType.toString());
+        //subject type
+        UITestSubjectType subjectType = command.getSubjectType();
+        if (subjectType != null) {
+          key.append(subjectType.toString());
+        }
+        //subject name
+        if (subject != null) {
+          String name = subject.getName();
+          if (name != null) {
+            key.append(name);
+          }
+          //subject axis
+          AxisID axisID = subject.getAxisID();
+          if (axisID != AxisID.ONLY) {
+            key.append(axisID.getExtension());
+          }
+        }
+        if (command.isSubsection()) {
+          key.append(command.getValue());
+        }
+        commandMap.put(key.toString(), command);
+        //Each command must be saved in commandMap.  Set the command variable to
+        //null to prevent the command reader from reusing it.
+        command = null;
+      }
     }
   }
 }
