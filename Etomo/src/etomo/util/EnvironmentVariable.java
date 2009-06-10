6c031fbe873080a9d183cd03f9e982a157245fe2
SueHeld <sueh@colorado.edu>
2009-06-10 17:27:05 +0000

    bug# 1202 Added exists.

--- Etomo/src/etomo/util/EnvironmentVariable.java
+++ Etomo/src/etomo/util/EnvironmentVariable.java
@@ -124,10 +124,95 @@ public final class EnvironmentVariable {
     }
     return value;
   }
+  
+  
+  /**
+   * Return true if an environment variable value exists.  Doesn't use
+   * variableList because getValue doesn't distinguish between an empty env var
+   * and a non-existant one.  If unable to check, returns false.
+   * 
+   * @param varName
+   * @return boolean
+   */
+  public boolean exists(String propertyUserDir, String varName, AxisID axisID,
+      ManagerKey managerKey) {
+    //  There is not a real good way to access the system environment variables
+    //  since the primary method was deprecated
+    SystemProgram readEnvVar;
+    if (Utilities.isWindowsOS()) {
+      String var = "%" + varName + "%";
+      readEnvVar = new SystemProgram(propertyUserDir, new String[] { "cmd.exe",
+          "/C", "echo", var }, axisID, managerKey);
+      try {
+        readEnvVar.run();
+      }
+      catch (Exception excep) {
+        excep.printStackTrace();
+        System.err.println(excep.getMessage());
+        System.err.println("Unable to run cmd command to find " + varName
+            + " environment variable");
+
+        return false;
+      }
+      String[] stderr = readEnvVar.getStdError();
+      if (stderr != null && stderr.length > 0) {
+        System.err.println("Error running 'cmd.exe' command");
+        for (int i = 0; i < stderr.length; i++) {
+          System.err.println(stderr[i]);
+        }
+      }
+      // Return the first line from the command
+      String[] stdout = readEnvVar.getStdOutput();
+      if (stdout != null && stdout.length > 0) {
+        // if the variable isn't set, echo will return the string sent to it
+        if (!stdout[0].equals(var)) {
+          return true;
+        }
+      }
+    }
+    // Non windows environment
+    else {
+      readEnvVar = new SystemProgram(propertyUserDir, new String[] { "env" },
+          axisID, managerKey);
+      try {
+        readEnvVar.run();
+      }
+      catch (Exception excep) {
+        excep.printStackTrace();
+        System.err.println(excep.getMessage());
+        System.err.println("Unable to run env command to find " + varName
+            + " environment variable");
+
+        return false;
+      }
+      String[] stderr = readEnvVar.getStdError();
+      if (stderr.length > 0) {
+        System.err.println("Error running 'env' command");
+        for (int i = 0; i < stderr.length; i++) {
+          System.err.println(stderr[i]);
+        }
+      }
+
+      // Search through the evironment string array to find the request
+      // environment variable
+      String searchString = varName + "=";
+      int nChar = searchString.length();
+      String[] stdout = readEnvVar.getStdOutput();
+      for (int i = 0; i < stdout.length; i++) {
+        if (stdout[i].indexOf(searchString) == 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
 /**
  * <p>
  * $Log$
+ * Revision 1.4  2009/03/17 00:46:43  sueh
+ * bug# 1186 Pass managerKey to everything that pops up a dialog.
+ *
  * Revision 1.3  2006/07/21 22:24:47  sueh
  * bug# 901 Added IMOD_CALIB_DIR constant
  *
