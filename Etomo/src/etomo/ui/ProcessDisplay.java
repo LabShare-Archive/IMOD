cf5561086374d003ae1b590ca990806bad9d0575
SueHeld <sueh@colorado.edu>
2009-06-11 17:01:18 +0000

    bug# 1221 Generic interface for panels which contain process parameters.  Used with next process functionality.

---
+++ Etomo/src/etomo/ui/ProcessDisplay.java
@@ -0,0 +1,19 @@
+package etomo.ui;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2008</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface ProcessDisplay {
+  public static  final String  rcsid =  "$Id$";
+}
