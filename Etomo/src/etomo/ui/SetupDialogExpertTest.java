5678d51f8cd1cfdaffd462217c0014c846e39f45
SueHeld <sueh@colorado.edu>
2009-08-24 20:24:19 +0000

    bug# 1254 Unit tests for SetupDialogExpert.

---
+++ Etomo/src/etomo/ui/SetupDialogExpertTest.java
@@ -0,0 +1,104 @@
+package etomo.ui;
+
+import etomo.ApplicationManager;
+import etomo.EtomoDirector;
+import etomo.type.AxisType;
+import etomo.type.ViewType;
+import junit.framework.TestCase;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2008</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+public final class SetupDialogExpertTest extends TestCase {
+  public static final String rcsid = "$Id$";
+
+  public void testGetInstance() {
+    ApplicationManager manager = (ApplicationManager) EtomoDirector.INSTANCE
+        .getCurrentManagerForTest();
+    manager.openSetupDialog();
+    assertNotNull("Manager should be able to create expert.", manager
+        .getSetupDialogExpert());
+  }
+
+  public void testAction() {
+    ApplicationManager manager = (ApplicationManager) EtomoDirector.INSTANCE
+        .getCurrentManagerForTest();
+    manager.openSetupDialog();
+    SetupDialogExpert expert = manager.getSetupDialogExpert();
+    expert.action(SetupDialog.SINGLE_AXIS_LABEL);
+    expert.action(SetupDialog.SINGLE_FRAME_LABEL);
+    expert.action(SetupDialog.MONTAGE_LABEL);
+  }
+
+  public void testCheckForSharedDirectory() {
+    ApplicationManager manager = (ApplicationManager) EtomoDirector.INSTANCE
+        .getCurrentManagerForTest();
+    manager.openSetupDialog();
+    SetupDialogExpert expert = manager.getSetupDialogExpert();
+    expert.checkForSharedDirectory();
+  }
+
+  public void testDoAutomation() {
+    ApplicationManager manager = (ApplicationManager) EtomoDirector.INSTANCE
+        .getCurrentManagerForTest();
+    manager.openSetupDialog();
+    SetupDialogExpert expert = manager.getSetupDialogExpert();
+    expert.doAutomation();
+  }
+
+  public void testGetAxisType() {
+    ApplicationManager manager = (ApplicationManager) EtomoDirector.INSTANCE
+        .getCurrentManagerForTest();
+    manager.openSetupDialog();
+    SetupDialogExpert expert = manager.getSetupDialogExpert();
+    assertEquals("Should default to dual axis.", expert.getAxisType(),
+        AxisType.DUAL_AXIS);
+  }
+
+  public void testGetContainer() {
+    ApplicationManager manager = (ApplicationManager) EtomoDirector.INSTANCE
+        .getCurrentManagerForTest();
+    manager.openSetupDialog();
+    SetupDialogExpert expert = manager.getSetupDialogExpert();
+    assertNotNull("Container should exist", expert.getContainer());
+  }
+
+  public void testGetCurrentBackupDirectory() {
+    ApplicationManager manager = (ApplicationManager) EtomoDirector.INSTANCE
+        .getCurrentManagerForTest();
+    manager.openSetupDialog();
+    SetupDialogExpert expert = manager.getSetupDialogExpert();
+    assertTrue(
+        "Backup directory was not entered so it should be the current directory.",
+        expert.getCurrentBackupDirectory().equals(
+            System.getProperty("user.dir")));
+  }
+
+  public void testSetViewType() {
+    ApplicationManager manager = (ApplicationManager) EtomoDirector.INSTANCE
+        .getCurrentManagerForTest();
+    manager.openSetupDialog();
+    SetupDialogExpert expert = manager.getSetupDialogExpert();
+    expert.setViewType(ViewType.MONTAGE);
+  }
+
+  public void testDone() {
+    ApplicationManager manager = (ApplicationManager) EtomoDirector.INSTANCE
+        .getCurrentManagerForTest();
+    manager.openSetupDialog();
+    assertFalse("Fields are not filled so done should fail.", manager
+        .doneSetupDialog());
+  }
+}
