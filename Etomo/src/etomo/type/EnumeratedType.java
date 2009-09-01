c099d1251728a90605fd630262eb12b0c02186bf
SueHeld <sueh@colorado.edu>
2009-09-01 02:46:30 +0000

    bug# 1222 Added getValue and toString.

--- Etomo/src/etomo/type/EnumeratedType.java
+++ Etomo/src/etomo/type/EnumeratedType.java
@@ -1,21 +1,30 @@
 package etomo.type;
+
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright 2006</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-* 
-* <p> $Log$ </p>
-*/
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2007</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$
+ * <p> Revision 1.1  2007/04/13 19:56:28  sueh
+ * <p> bug# 964 Interface for enumeration types, so that they can be stored in radio
+ * <p> buttons.
+ * <p> </p>
+ */
 public interface EnumeratedType {
-  public static  final String  rcsid =  "$Id$";
-  
+  public static final String rcsid = "$Id$";
+
   public boolean isDefault();
+
+  public ConstEtomoNumber getValue();
+  
+  public String toString();
 }
