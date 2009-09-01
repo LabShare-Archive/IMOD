7a18592fee63bbab3761f5fc6ee382f637d01f7e
SueHeld <sueh@colorado.edu>
2009-09-01 03:10:21 +0000

    bug# 1222 Type of a panel.

---
+++ Etomo/src/etomo/type/PanelType.java
@@ -0,0 +1,25 @@
+package etomo.type;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2008</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+public final class PanelType {
+  public static final String rcsid = "$Id$";
+
+  public static final PanelType ERASE_GOLD = new PanelType();
+  public static final PanelType CCD_ERASER = new PanelType();
+  public static final PanelType BLENDMONT = new PanelType();
+  public static final PanelType NEW_STACK = new PanelType();
+}
