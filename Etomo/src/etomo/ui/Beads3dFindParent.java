22ecaac54e245384a1e9fd657290b04e7f76d17f
SueHeld <sueh@colorado.edu>
2009-09-22 23:54:26 +0000

    bug# 1269 Fixed misspelling.

---
+++ Etomo/src/etomo/ui/Beads3dFindParent.java
@@ -0,0 +1,24 @@
+package etomo.ui;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2008</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$
+* <p> Revision 3.1  2009/09/01 03:18:25  sueh
+* <p> bug# 1222
+* <p> </p>
+*/
+interface Beads3dFindParent {
+  public static  final String  rcsid =  "$Id$";
+  
+  public void updateParallelProcess();
+}
