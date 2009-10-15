5912c5839c35fbba0d4623c46eee90aa62adaf3a
SueHeld <sueh@colorado.edu>
2009-10-15 23:27:31 +0000

    bug# 1274 Added msgLabelChanged.

--- Etomo/src/etomo/ui/Cell.java
+++ Etomo/src/etomo/ui/Cell.java
@@ -1,21 +1,30 @@
 package etomo.ui;
+
 /**
-* <p>Description: </p>
-* 
-* <p>Copyright: Copyright 2006</p>
-*
-* <p>Organization:
-* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
-* University of Colorado</p>
-* 
-* @author $Author$
-* 
-* @version $Revision$
-* 
-* <p> $Log$ </p>
-*/
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2006</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$
+ * <p> Revision 1.1  2007/04/02 21:44:11  sueh
+ * <p> bug# 964 Interface for HeaderCell and InputCell.
+ * <p> </p>
+ */
 interface Cell {
-  public static  final String  rcsid =  "$Id$";
-  
+  public static final String rcsid = "$Id$";
+
   public void setEnabled(boolean enable);
+
+  /**
+   * Message from row header or column header that their label has changed.
+   */
+ public void msgLabelChanged();
 }
