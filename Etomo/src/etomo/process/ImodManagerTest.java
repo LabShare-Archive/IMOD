8e2b08d10632a8947b0b92d277450b0f37a4c72f
SueHeld <sueh@colorado.edu>
2009-10-28 14:27:00 +0000

    bug# 1275 fixed cast problem

--- Etomo/src/etomo/process/ImodManagerTest.java
+++ Etomo/src/etomo/process/ImodManagerTest.java
@@ -58,6 +58,7 @@ public class ImodManagerTest extends TestCase {
    */
   public ImodManagerTest(String arg0) {
     super(arg0);
+    EtomoDirector.INSTANCE.openTomogram(true, AxisID.ONLY);
     manager = EtomoDirector.INSTANCE.getCurrentManagerForTest();
     metaData = new MetaData((ApplicationManager) manager);
   }
