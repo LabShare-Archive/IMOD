513e81f3c3bcf9ce5f670c659e204bd1d8da988e
SueHeld <sueh@colorado.edu>
2009-12-08 02:51:18 +0000

    bug# 1286 Interface of PeetDialog.

---
+++ Etomo/src/etomo/ui/YAxisTypeParent.java
@@ -0,0 +1,24 @@
+package etomo.ui;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2008</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+interface YAxisTypeParent {
+  public static final String rcsid = "$Id$";
+
+  public void updateDisplay();
+
+  public boolean isVolumeTableEmpty();
+}
