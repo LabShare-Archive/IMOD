432ab2c442e3aae0e25348e6c1cd1779af48b5f8
SueHeld <sueh@colorado.edu>
2009-12-19 01:07:54 +0000

    bug# 1294 Added lambdaForSmoothing and middleContourFile.

--- Etomo/src/etomo/comscript/FlattenWarpParam.java
+++ Etomo/src/etomo/comscript/FlattenWarpParam.java
@@ -20,20 +20,26 @@ import etomo.util.DatasetFiles;
  * 
  * @version $Revision$
  * 
- * <p> $Log$ </p>
+ * <p> $Log$
+ * <p> Revision 1.1  2009/06/05 01:46:57  sueh
+ * <p> bug# 1219 Represents the flattenwarp process interface.
+ * <p> </p>
  */
 public final class FlattenWarpParam {
   public static final String rcsid = "$Id$";
 
   public static final String ONE_SURFACE_OPTION = "OneSurface";
   public static final String WARP_SPACING_X_AND_Y_OPTION = "WarpSpacingXandY";
+  public static final String LAMBDA_FOR_SMOOTHING_OPTION = "LambdaForSmoothing";
 
   private final List command = new ArrayList();
   private final FortranInputString warpSpacingXandY = new FortranInputString(2);//optional
 
   private final BaseManager manager;
 
+  private FortranInputString lambdaForSmoothing = null;//optional
   private boolean oneSurface = false;
+  private String middleContourFile = null;
 
   public FlattenWarpParam(BaseManager manager) {
     this.manager = manager;
@@ -51,8 +57,16 @@ public final class FlattenWarpParam {
       command.add("-" + ONE_SURFACE_OPTION);
     }
     if (!warpSpacingXandY.isNull()) {
-      command.add("-"+WARP_SPACING_X_AND_Y_OPTION);
+      command.add("-" + WARP_SPACING_X_AND_Y_OPTION);
       command.add(warpSpacingXandY.toString(true));
+    }
+    if (middleContourFile != null) {
+      command.add("-MiddleContourFile");
+      command.add(middleContourFile);
+    }
+    if (lambdaForSmoothing != null && !lambdaForSmoothing.isNull()) {
+      command.add("-" + LAMBDA_FOR_SMOOTHING_OPTION);
+      command.add(lambdaForSmoothing.toString(true));
     }
   }
 
@@ -87,6 +101,27 @@ public final class FlattenWarpParam {
       return e.getMessage();
     }
     return null;
+  }
+
+  /**
+   * @param input
+   * @return error message if number is not a number and not blank, otherwise null
+   */
+  public String setLambdaForSmoothing(String input) {
+    try {
+      lambdaForSmoothing = FortranInputString.getInstance(input);
+    }
+    catch (NumberFormatException e) {
+      return e.getMessage();
+    }
+    catch (FortranInputSyntaxException e) {
+      return e.getMessage();
+    }
+    return null;
+  }
+
+  public void setMiddleContourFile(String input) {
+    middleContourFile = input;
   }
 
   public ProcessName getProcessName() {
