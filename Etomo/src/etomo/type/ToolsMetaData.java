1c79a6ce3519cfee198627e5a0a12672537b31ed
SueHeld <sueh@colorado.edu>
2010-02-17 04:50:15 +0000

    bug# 1301 Meta data for tools.

---
+++ Etomo/src/etomo/type/ToolsMetaData.java
@@ -0,0 +1,72 @@
+package etomo.type;
+
+import java.io.File;
+
+/**
+ * <p>Description: </p>
+ * 
+ * <p>Copyright: Copyright 2010</p>
+ *
+ * <p>Organization:
+ * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+ * University of Colorado</p>
+ * 
+ * @author $Author$
+ * 
+ * @version $Revision$
+ * 
+ * <p> $Log$ </p>
+ */
+public final class ToolsMetaData extends BaseMetaData {
+  public static final String rcsid = "$Id$";
+
+  private final DialogType dialogType;
+  private final ToolType toolType;
+
+  private String rootName = null;
+
+  public ToolsMetaData(DialogType dialogType, ToolType toolType) {
+    this.dialogType = dialogType;
+    this.toolType = toolType;
+    axisType = AxisType.SINGLE_AXIS;
+  }
+
+  String createPrepend(String prepend) {
+    return null;
+  }
+
+  public String getDatasetName() {
+    return rootName;
+  }
+
+  public void setRootName(File file) {
+    rootName = file.getName();
+  }
+
+  public String getMetaDataFileName() {
+    //Tools projects are not saved
+    return null;
+  }
+
+  public String getName() {
+    if (rootName == null) {
+      return toolType.toString();
+    }
+    return rootName;
+  }
+
+  public boolean isValid() {
+    return validate() == null;
+  }
+
+  /**
+   * returns null if valid
+   * @return error message if invalid
+   */
+  public String validate() {
+    if (rootName == null) {
+      return "Missing root name.";
+    }
+    return null;
+  }
+}
