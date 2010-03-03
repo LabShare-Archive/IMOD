4b55a37fa8b291dd707938f9e948729d9ebd9f01
SueHeld <sueh@colorado.edu>
2010-03-03 04:58:31 +0000

    bug# 1311 Added panel IDs for the tiltxcorr panel.

--- Etomo/src/etomo/type/PanelId.java
+++ Etomo/src/etomo/type/PanelId.java
@@ -14,11 +14,17 @@ package etomo.type;
  * 
  * @version $Revision$
  * 
- * <p> $Log$ </p>
+ * <p> $Log$
+ * <p> Revision 3.1  2010/02/17 04:52:36  sueh
+ * <p> bug# 1301 Using the manager instead of the manager key do pop up
+ * <p> messages.
+ * <p> </p>
  */
 public final class PanelId {
   public static final String rcsid = "$Id$";
   
   public static final PanelId POST_FLATTEN_VOLUME = new PanelId();
   public static final PanelId TOOLS_FLATTEN_VOLUME = new PanelId();
+  public static final PanelId CROSS_CORRELATION=new PanelId();
+  public static final PanelId PATCH_TRACKING = new PanelId();
 }
