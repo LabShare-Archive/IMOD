f5d881c2f6489a0dd63ef2fc0c5bea9a2a167056
mastcu <mast@colorado.edu>
2010-03-24 02:19:50 +0000

    Changed type of assumed variable to use in call to function

--- imod/imod_assistant.h
+++ imod/imod_assistant.h
@@ -1,36 +1,10 @@
 /*
  *  imod_assistant.h - declarations for imod_assistant.cpp
+ *
+ *  $Id$
+ *  Log at end of file
  */
 
-/*  $Author$
-
-    $Date$
-
-    $Revision$
-
-    $Log$
-    Revision 1.6  2005/02/24 22:28:07  mast
-    Added variables for fallback IMOD_DIR
-
-    Revision 1.5  2004/12/24 02:11:40  mast
-    Removed absolute argument from show page
-
-    Revision 1.4  2004/12/06 04:39:19  mast
-    Made truly standalone, took out of library back into 3dmod
-
-    Revision 1.2  2004/12/04 19:21:21  mast
-    Added DLL macro definition and use
-
-    Revision 1.1  2004/12/04 02:05:39  mast
-    Added to libdiaqt
-
-    Revision 1.2  2004/11/24 18:30:16  mast
-    Add adp file on startup
-
-    Revision 1.1  2004/11/22 00:21:46  mast
-    Addition to program
-
-*/
 #ifndef IMOD_ASSISTANT_H
 #define IMOD_ASSISTANT_H
 
@@ -58,10 +32,39 @@ private:
   QString mPath;
   QString mAdp;
   QString mImodDir;
-  bool mAssumedIMOD;
+  int mAssumedIMOD;
   bool mKeepSideBar;
   QAssistantClient *mAssistant;
   QString mTitle;
 };
 
 #endif
+
+/*
+
+$Log$
+Revision 1.7  2006/06/18 23:42:39  mast
+Added sidebar argument
+
+Revision 1.6  2005/02/24 22:28:07  mast
+Added variables for fallback IMOD_DIR
+
+Revision 1.5  2004/12/24 02:11:40  mast
+Removed absolute argument from show page
+
+Revision 1.4  2004/12/06 04:39:19  mast
+Made truly standalone, took out of library back into 3dmod
+
+Revision 1.2  2004/12/04 19:21:21  mast
+Added DLL macro definition and use
+
+Revision 1.1  2004/12/04 02:05:39  mast
+Added to libdiaqt
+
+Revision 1.2  2004/11/24 18:30:16  mast
+Add adp file on startup
+
+Revision 1.1  2004/11/22 00:21:46  mast
+Addition to program
+
+*/
