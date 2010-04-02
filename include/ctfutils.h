6b96971f4b5849c7b333b9ecb3ce0bb3edac1101
mastcu <mast@colorado.edu>
2010-04-02 00:03:31 +0000

    Added

---
+++ include/ctfutils.h
@@ -0,0 +1,26 @@
+/*
+ * ctfutils.h - declarations for utility functions used by ctfplotter and
+ *                 ctfphaseflip
+ *
+ *  $Id$
+ */
+#ifndef CTFUTILS_H
+#define CTFUTILS_H
+
+typedef struct ilist_struct Ilist;
+
+typedef struct defocus_struct {
+  int startingSlice;
+  int endingSlice;
+  double lAngle;
+  double hAngle;
+  double defocus;
+} SavedDefocus;
+
+float *readTiltAngles(const char *angleFile, int mNzz, float mAngleSign,
+                      float &mMinAngle, float &mMaxAngle);
+void addItemToDefocusList(Ilist *mSaved, SavedDefocus toSave);
+Ilist *readDefocusFile(const char *mFnDefocus);
+int checkAndFixDefocusList(Ilist *list, float *angles, int nz);
+
+#endif
