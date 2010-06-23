72988c035791f60c8ed490f75f8d8615e6aa5b0a
mastcu <mast@colorado.edu>
2010-06-23 23:15:01 +0000

    Add safer variation

--- flib/subrs/xfsubs/xfrdall.f
+++ flib/subrs/xfsubs/xfrdall.f
@@ -1,11 +1,31 @@
-c routine to read all elements in file into list
-        subroutine xfrdall(nunit,flist,nlist,*)
-        real*4 flist(2,3,*)
-        nlist=0
-10      nt=nlist+1
-        call xfread(nunit,flist(1,1,nt),*20,*96)
-        nlist=nt
-        go to 10
-20      return
-96      return 1
-        end
+c       A bad routine to read all elements in file into list
+      subroutine xfrdall(nunit,flist,nlist,*)
+      implicit none
+      integer*4 nunit, nlist,nt
+      real*4 flist(2,3,*)
+      nlist=0
+10    nt=nlist+1
+      call xfread(nunit,flist(1,1,nt),*20,*96)
+      nlist=nt
+      go to 10
+20    return
+96    return 1
+      end
+
+c       A better routine
+      subroutine xfrdall2(nunit,flist,nlist, limlist, ierr)
+      implicit none
+      integer*4 nunit, nlist,nt, limlist, ierr
+      real*4 flist(2,3,*), ftmp(2,3)
+      ierr = 1
+      nlist = 0
+10    call xfread(nunit,ftmp,*20,*96)
+      nlist = nlist + 1
+      if (nlist .gt. limlist) return
+      call xfcopy(ftmp, flist(1,1,nlist))
+      go to 10
+20    ierr = 0
+      return
+96    ierr = 2
+      return
+      end
