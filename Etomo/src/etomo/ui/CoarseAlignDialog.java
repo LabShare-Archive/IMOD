19138fea1636d4d52e11fa632350859e93c5790f
SueHeld <sueh@colorado.edu>
2010-10-11 20:34:46 +0000

    bug# 1379 In constructor, passing this to TiltxcorrPanel so can call this class's pop up menu function.

--- Etomo/src/etomo/ui/CoarseAlignDialog.java
+++ Etomo/src/etomo/ui/CoarseAlignDialog.java
@@ -11,6 +11,10 @@
  * @version $Revision$
  *
  * <p> $Log$
+ * <p> Revision 3.55  2010/03/08 21:02:51  sueh
+ * <p> bug# 1311 Moved the open coarse aligned stack button so that it is next to
+ * <p> its associated run button.
+ * <p>
  * <p> Revision 3.54  2010/03/03 05:03:22  sueh
  * <p> bug# 1311 Added TiltxcorrPanel and radio button for it.
  * <p>
@@ -317,7 +321,7 @@ public final class CoarseAlignDialog extends ProcessDialog implements
     btnMidas = (MultiLineButton) displayFactory.getMidas();
     setToolTipText();
     tiltxcorrPanel = TiltxcorrPanel.getCrossCorrelationInstance(
-        applicationManager, axisID, dialogType, btnAdvanced);
+        applicationManager, axisID, dialogType, btnAdvanced, this);
     pnlPrenewst = new PrenewstPanel(applicationManager, axisID, dialogType,
         this, btnAdvanced);
     btnExecute.setText("Done");
