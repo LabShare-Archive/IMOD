ea2ea3215cbe2735e0db641522290b7f0fd9ed1c
mastcu <mast@colorado.edu>
2011-02-12 04:45:08 +0000

    start in HQ mode

--- imod/preferences.h
+++ imod/preferences.h
@@ -1,11 +1,11 @@
 /*   preferences.h  -  declarations for preferences.cpp
  *
- *   Copyright (C) 1995-2002 by Boulder Laboratory for 3-Dimensional Electron
+ *   Copyright (C) 1995-2011 by Boulder Laboratory for 3-Dimensional Electron
  *   Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
  *   Colorado.  See dist/COPYRIGHT for full copyright notice.
  *
  *  $Id$
- *  Log at end of file
+ *  No more Log
  */                                                                           
 
 #ifndef IMOD_PREFERENCES_H
@@ -41,7 +41,7 @@ int hotSliderKey();
 #endif
 
 #define EXCLUDE_STYLES
-#define MAXZOOMS 18
+#define MAXZOOMS 22
 #define MAX_GEOMETRIES 10
 #define MAX_NAMED_COLORS 8
 
@@ -64,6 +64,7 @@ typedef struct imod_pref_struct
   TRIPLET(bool, silentBeep);         // Silence the alarm in wprint
   //  TRIPLET(bool, tooltipsOn);         // Enable tool tips
   TRIPLET(bool, classicSlicer);       // Use classic slicer
+  TRIPLET(bool, startInHQ);          // Start windows in HQ mode
   TRIPLET(bool, startAtMidZ);         // Go to middle Z at start
   TRIPLET(int, autoConAtStart);      // Do autocontrast at start
   TRIPLET(bool, attachToOnObj);      // Attach to ON objects only
@@ -143,6 +144,7 @@ class ImodPreferences : public QObject
   int minCurrentModPtSize();
   bool silentBeep() {return mCurrentPrefs.silentBeep;};
   bool classicSlicer() {return mCurrentPrefs.classicSlicer;};
+  bool startInHQ() {return mCurrentPrefs.startInHQ;};
   bool classicWarned();
   bool attachToOnObj() {return mCurrentPrefs.attachToOnObj;};
   bool slicerNewSurf() {return mCurrentPrefs.slicerNewSurf;};
@@ -219,78 +221,3 @@ extern ImodPreferences *ImodPrefs;
 
 #endif // IMOD_PREFERENCES_H
 
-/*
-$Log$
-Revision 1.24  2009/11/21 23:06:43  mast
-Setting to control slicer new surfaces
-
-Revision 1.23  2009/03/22 19:44:46  mast
-Switched to taking all styles that exist
-
-Revision 1.22  2009/02/26 20:03:32  mast
-Add paging by big steps
-
-Revision 1.21  2009/01/15 16:33:18  mast
-Qt 4 port
-
-Revision 1.20  2008/12/10 01:04:22  mast
-Added function to set JPEG quality
-
-Revision 1.19  2008/09/24 02:39:28  mast
-Added option for attach function to look only at On objects
-
-Revision 1.18  2008/05/27 05:42:19  mast
-Various new preferences, added macro
-
-Revision 1.17  2008/02/03 18:38:25  mast
-Added option to swap left/middle in model view
-
-Revision 1.16  2008/01/25 20:22:58  mast
-Changes for new scale bar
-
-Revision 1.15  2008/01/13 22:58:35  mast
-Changes for multi-Z window
-
-Revision 1.14  2007/11/13 19:14:08  mast
-Added settings to control slicer speedup
-
-Revision 1.13  2007/07/08 16:03:49  mast
-Added hot slider active function
-
-Revision 1.12  2007/05/31 16:25:33  mast
-Added members for classic slicer
-
-Revision 1.11  2006/10/05 15:41:32  mast
-Provided for primary and second non-TIFF snapshot format
-
-Revision 1.10  2004/11/29 19:25:21  mast
-Changes to do QImage instead of RGB snapshots
-
-Revision 1.9  2004/11/04 23:30:55  mast
-Changes for rounded button style
-
-Revision 1.8  2004/11/02 20:17:00  mast
-Added color settings
-
-Revision 1.7  2004/06/23 03:33:10  mast
-New functions for generic settings
-
-Revision 1.6  2003/09/24 17:35:54  mast
-Change to routine that sets info window geometry directly
-
-Revision 1.5  2003/09/18 05:57:29  mast
-Add members for autocontrast targets
-
-Revision 1.4  2003/09/17 04:47:24  mast
-Added members for remembering window geometry
-
-Revision 1.3  2003/03/26 23:06:42  mast
-only check status of a style once
-
-Revision 1.2  2003/03/26 22:49:09  mast
-Change style handling to use list of styles
-
-Revision 1.1  2003/03/24 17:56:59  mast
-Initial creation
-
-*/
