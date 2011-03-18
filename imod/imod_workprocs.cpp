318a934ae7d92b6e905b1638c904bf3b8a02f67b
mastcu <mast@colorado.edu>
2011-03-18 04:41:49 +0000

    Made time movie call ivwTime

--- imod/imod_workprocs.cpp
+++ imod/imod_workprocs.cpp
@@ -1,38 +1,16 @@
-/*  IMOD VERSION 2.7.9
- *
+/*
  *  imod_workprocs.cpp -- imod timer and background processing functions.
  *
  *  Original author: James Kremer
  *  Revised by: David Mastronarde   email: mast@colorado.edu
+ *
+ *  Copyright (C) 1995-2011 by Boulder Laboratory for 3-Dimensional Electron
+ *  Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
+ *  Colorado.  See dist/COPYRIGHT for full copyright notice.
+ *
+ *  $Id$
+ *  Log at end of file
  */
-
-/*****************************************************************************
- *   Copyright (C) 1995-2003 by Boulder Laboratory for 3-Dimensional Fine    *
- *   Structure ("BL3DFS") and the Regents of the University of Colorado.     *
- *                                                                           *
- *   BL3DFS reserves the exclusive rights of preparing derivative works,     *
- *   distributing copies for sale, lease or lending and displaying this      *
- *   software and documentation.                                             *
- *   Users may reproduce the software and documentation as long as the       *
- *   copyright notice and other notices are preserved.                       *
- *   Neither the software nor the documentation may be distributed for       *
- *   profit, either in original form or in derivative works.                 *
- *                                                                           *
- *   THIS SOFTWARE AND/OR DOCUMENTATION IS PROVIDED WITH NO WARRANTY,        *
- *   EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, WARRANTY OF          *
- *   MERCHANTABILITY AND WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE.       *
- *                                                                           *
- *   This work is supported by NIH biotechnology grant #RR00592,             *
- *   for the Boulder Laboratory for 3-Dimensional Fine Structure.            *
- *   University of Colorado, MCDB Box 347, Boulder, CO 80309                 *
- *****************************************************************************/
-/*  $Author$
-
-$Date$
-
-$Revision$
-Log at end of file
-*/
 
 #include <stdlib.h>
 #include <qapplication.h>
@@ -210,10 +188,11 @@ void ImodWorkproc::movieProc()
 
   timetmp = vi->ct;
   movie_inc(vi, &timetmp, &vi->tmovie, 3, &show);
-  vi->ct = (int)timetmp;
+  // vi->ct = (int)timetmp;
 
   if (vi->tmovie != 0){
-    vi->hdr = vi->image = &vi->imageList[vi->ct-1];
+    // vi->hdr = vi->image = &vi->imageList[vi->ct-1];
+    ivwSetTime(vi, (int)timetmp);
     drawflag |= IMOD_DRAW_IMAGE;
   }
      
@@ -290,6 +269,9 @@ int imodMovieXYZT(struct ViewInfo *vi, int x, int y, int z, int t)
 
 /*
 $Log$
+Revision 4.4  2004/05/31 23:35:26  mast
+Switched to new standard error functions for all debug and user output
+
 Revision 4.3  2003/03/24 17:58:09  mast
 Changes for new preferences capability
 
