6066a4f67628a8ffcb4dd98a2f219aab8f73eb7c
SueHeld <sueh@colorado.edu>
2011-04-04 17:00:13 +0000

    bug# 1416 File filter for sirt output files (.srecdd and .sintdd).

---
+++ Etomo/src/etomo/storage/SirtOutputFileFilter.java
@@ -0,0 +1,96 @@
+package etomo.storage;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import javax.swing.filechooser.FileFilter;
+
+import etomo.BaseManager;
+import etomo.type.AxisID;
+import etomo.type.FileType;
+
+/**
+* <p>Description: Gets files of two types:</p>
+* <p>{dataset}{axis letter}.srec{int}</p>
+* <p>If includeScaledOutput is true:  {dataset}{axis letter}.sint{int}</p>
+* <p>The {int} must be a valid integer for a file name to be accepted.</p>
+* 
+* <p>Copyright: Copyright 2011</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public final class SirtOutputFileFilter extends FileFilter implements java.io.FileFilter,
+    FilenameFilter {
+  public static final String rcsid = "$Id$";
+
+  private final BaseManager manager;
+  private final AxisID axisID;
+  private final boolean includeScaledOutput;
+
+  public SirtOutputFileFilter(final BaseManager manager, final AxisID axisID,
+      final boolean includeScaledOutput) {
+    this.manager = manager;
+    this.axisID = axisID;
+    this.includeScaledOutput = includeScaledOutput;
+  }
+
+  @Override
+  /**
+   * Return true if the file name contains a SIRT output template followed by a valid
+   * integer.
+   */
+  public boolean accept(final File file) {
+    return accept(null, file.getName());
+  }
+
+  public boolean accept(final File dir, final String fileName) {
+    if (fileName.endsWith("~")) {
+      return false;
+    }
+    String template = FileType.SIRT_OUTPUT_TEMPLATE.getFileName(manager, axisID);
+    if (fileName.startsWith(template)) {
+      return acceptTemplate(fileName, template);
+    }
+    if (includeScaledOutput) {
+      template = FileType.SIRT_SCALED_OUTPUT_TEMPLATE.getFileName(manager, axisID);
+      if (fileName.startsWith(template)) {
+        return acceptTemplate(fileName, template);
+      }
+    }
+    return false;
+  }
+
+  /**
+   * The file name should be shorter then the template.  The part of the file name that
+   * extends beyond the template should be a valid integer. 
+   * @param fileName
+   * @param template
+   * @return
+   */
+  private boolean acceptTemplate(final String fileName, final String template) {
+    int templateLength = template.length();
+    if (fileName.length() <= templateLength) {
+      return false;
+    }
+    try {
+      Integer.getInteger(fileName.substring(templateLength));
+    }
+    catch (NumberFormatException e) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String getDescription() {
+    return "SIRT Iteration Files";
+  }
+}
