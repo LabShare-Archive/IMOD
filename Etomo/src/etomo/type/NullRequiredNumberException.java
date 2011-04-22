4a6f7ca3d69bd435b861b04b3802114c0b161c57
SueHeld <sueh@colorado.edu>
2011-04-22 02:16:29 +0000

    bug# 1474 Exception for a number when is null when it can't be.

---
+++ Etomo/src/etomo/type/NullRequiredNumberException.java
@@ -0,0 +1,24 @@
+package etomo.type;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2011</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public class NullRequiredNumberException extends Exception {
+  public static final String rcsid = "$Id$";
+  
+  public NullRequiredNumberException(String message) {
+    super(message);
+  }
+}
