8905b783f46a7cdc651767deb4989bbf5702b0c5
SueHeld <sueh@colorado.edu>
2011-05-03 03:21:07 +0000

    bug# 1416 A interface for a class which reacts to field and button changes in another class.

---
+++ Etomo/src/etomo/ui/swing/FieldObserver.java
@@ -0,0 +1,22 @@
+package etomo.ui.swing;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2011</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+interface FieldObserver {
+  public static final String rcsid = "$Id$";
+
+  public void msgFieldChanged(final boolean differentFromCheckpoint);
+}
