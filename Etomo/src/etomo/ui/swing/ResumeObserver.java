e393232b51d36c648959d279776d3ebf73aa9226
SueHeld <sueh@colorado.edu>
2011-05-03 03:32:01 +0000

    bug# 1416 A interface for a class which reacts to changes in resume field states in another class.

---
+++ Etomo/src/etomo/ui/swing/ResumeObserver.java
@@ -0,0 +1,22 @@
+package etomo.ui.swing;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2011</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+interface ResumeObserver {
+  public static final String rcsid = "$Id$";
+
+  public void msgResumeChanged(boolean resume);
+}
