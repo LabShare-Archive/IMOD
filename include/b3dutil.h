7cc4c3a08c618a5ddfccc623c7725d1fe04057f2
mastcu <mast@colorado.edu>
2011-05-13 17:35:57 +0000

    Add determ3

--- include/b3dutil.h
+++ include/b3dutil.h
@@ -25,6 +25,10 @@
 #define B3DFREE(a) if (a) free(a)
 #define B3DMALLOC(a,b) (a *)malloc((b) * sizeof(a))
 
+/* Determinant of 3x3 matrix */
+#define determ3(a1,a2,a3,b1,b2,b3,c1,c2,c3) ((a1)*(b2)*(c3) - (a1)*(b3)*(c2) +\
+  (a2)*(b3)*(c1) - (a2)*(b1)*(c3) + (a3)*(b1)*(c2) - (a3)*(b2)*(c1))
+
 #ifdef __cplusplus
 extern "C" {
 #endif
