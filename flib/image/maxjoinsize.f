92317c84cb12af4f9a8759ed10864a2efcf081bf
mastcu <mast@colorado.edu>
2011-06-17 05:43:40 +0000

    Modified for warping

--- flib/image/maxjoinsize.f
+++ flib/image/maxjoinsize.f
@@ -4,6 +4,9 @@ c
 c       $Id$
 c       
 c       $Log$
+c       Revision 3.3  2008/12/03 21:50:02  mast
+c       Increased field width in outputs, -1000 shift in Y was unparseable
+c
 c       Revision 3.2  2008/11/21 20:42:09  mast
 c       Increased character limits
 c
@@ -13,15 +16,22 @@ c
 c       
       implicit none
       integer maxfiles
-      parameter (maxfiles = 999)
-      real*4 f(2,3,maxfiles), dmin, dmax, dmean
+      parameter (maxfiles = 100000)
+      real*4 f(2,3,maxfiles), dmin, dmax, dmean, deltaFirst(3)
       integer*4 nx(maxfiles), ny(maxfiles), nxyz(3), mxyz(3), mode
       integer*4 numF, numFiles, i, lineSkip, maxx, maxy, idirx, idiry
       real*4 xcen, ycen, xhalf, yhalf, xmin, xmax, ymin, ymax, xcorn, ycorn
-      integer*4 ixofs, iyofs, newx, newy
-      character*320 rootname,filename
-      integer*4 iargc, lnblnk
+      integer*4 ixofs, iyofs, newx, newy, ierr, ifControl, nxwarp, nywarp, ibinning
+      integer*4 iflags, maxNxg, maxNyg, nxGrid, nyGrid
+      real*4 warpScale, xGridStrt, yGridStrt, xGridIntrv, yGridIntrv, dx, dy, pixelSize
+      integer*4, allocatable :: nControl(:)
+      real*4, allocatable :: dxGrid(:,:), dyGrid(:,:)
+      logical*4 warping, hasWarp
+      character*320 rootname,filename, errString
+      integer*4 iargc, readCheckWarpFile, findMaxGridSize, getLinearTransform
+      integer*4 getSizeAdjustedGrid
 c	
+      ifControl = 0
       if (iargc() .eq. 0) then
         print *,'Usage: maxjoinsize number_of_sections ',
      &      'lines_to_skip root_name'
@@ -46,18 +56,27 @@ c
 c       Get the rootname, open tomoxg and read transforms
 c       
       call getarg(3, rootname)
-      filename = rootname(1:lnblnk(rootname))//'.tomoxg'
-      open(3,file=filename,form='formatted',status='old' ,err=92)
+      filename = trim(rootname)//'.tomoxg'
+      ierr = readCheckWarpFile(filename, 0, 1, nxwarp, nywarp, numF,
+     &    ibinning, pixelSize, iflags, errString)
+      if (ierr .lt. -1) call exitError(errString)
+      if (ierr .ge. 0) then
+        warping = .true.
+        if (mod(iflags / 2, 2) .ne. 0) ifControl = 1
+      else
+        open(3,file=filename,form='formatted',status='old' ,err=92)
 
-      call xfrdall(3, f, numF, *93)
-      if (numF .ne. numFiles) call exitError(
-     &    'WRONG NUMBER OF TRANSFORMS IN FILE')
-      close(3)
+        call xfrdall2(3, f, numF, maxfiles, ierr)
+        if (ierr .eq. 2) call exitError('READING TRANSFORM FILE')
+        if (ierr .eq. 1) call exitError('TOO MANY TRANSFORMS IN FILE FOR TRANSFORM ARRAY')
+        close(3)
+      endif
+      if (numF .ne. numFiles) call exitError('WRONG NUMBER OF TRANSFORMS IN FILE')
 c       
 c       open the info file and skip lines
 c       
 
-      filename = rootname(1:lnblnk(rootname))//'.info'
+      filename = trim(rootname)//'.info'
       open(3,file=filename,form='formatted',status='old' ,err=94)
       do i = 1, lineSkip
         read(3, '(a)', err=94, end=94)filename
@@ -72,6 +91,7 @@ c
         read(3, '(a)', err=95, end=95)filename
         call imopen(1, filename, 'ro')
         call irdhdr(1, nxyz, mxyz, mode, dmin, dmax, dmean)
+        if (i .eq. 1) call irtdel(1, deltaFirst)
         call imclose(1)
         nx(i) = nxyz(1)
         ny(i) = nxyz(2)
@@ -79,6 +99,20 @@ c
         maxy = max(maxy, ny(i))
       enddo
       close(3)
+c       
+c       Set up for warping
+      if (warping) then
+        warpScale = pixelSize / deltaFirst(1)
+        allocate(nControl(numFiles), stat = ierr)
+        call memoryError(ierr, 'ARRAY FOR NUMBER OF CONTROL POINTS')
+        if (findMaxGridSize(nxwarp, nywarp, numFiles, ifControl, maxx /warpScale,
+     &      maxy / warpScale, nControl, maxNxg, maxNyg, errString) .ne. 0)
+     &      call exitError(errString)
+        if (maxNxg * maxNyg .gt. 0) then
+          allocate(dxGrid(maxNxg, maxNyg), dyGrid(maxNxg, maxNyg), stat = ierr)
+          call memoryError(ierr, 'ARRAYS FOR WARPING FIELDS')
+        endif
+      endif
 c       
 c       transform the 4 corners in the coordinate system of the maximum size
 c       
@@ -91,10 +125,28 @@ c
       do i = 1, numFiles
         xhalf = nx(i) / 2.
         yhalf = ny(i) / 2.
+        hasWarp = .false.
+        if (warping) then
+          if (getLinearTransform(i, f(1,1,i)) .ne. 0) call exitError(
+     &        'GETTING LINEAR TRANSFORM FROM WARP FILE')
+          hasWarp = nControl(i) .gt. 2
+          if (hasWarp) then
+            if (getSizeAdjustedGrid(i, nxwarp, nywarp, ifControl, maxx / warpScale,
+     &          maxy / warpScale, warpScale, 1, nxGrid, nyGrid, xGridStrt, yGridStrt,
+     &          xGridIntrv, yGridIntrv, dxGrid, dyGrid, maxNxg, maxNyg, errString) .ne. 0)
+     &          call exitError(errString)
+          endif
+        endif
         do idirx = -1, 1, 2
           do idiry = -1, 1, 2
             call xfapply(f(1,1,i), xcen, ycen, xcen + idirx * xhalf,
      &          ycen +idiry * yhalf, xcorn, ycorn)
+            if (hasWarp) then
+c              print *,i,idirx,idiry,xcorn,ycorn
+              call findInversePoint(xcorn, ycorn, dxGrid, dyGrid, maxNxg, nxgrid, nyGrid,
+     &            xGridStrt, yGridIntrv, yGridStrt, yGridIntrv, xcorn, ycorn, dx,dy)
+c              print *,xcorn,ycorn, dx, dy
+            endif
             xmin = min(xmin, xcorn)
             ymin = min(ymin, ycorn)
             xmax = max(xmax, xcorn)
