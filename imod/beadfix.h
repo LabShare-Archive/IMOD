e8c96f7686901078ddcad07431b58eff95532f2c
mastcu <mast@colorado.edu>
2011-08-09 05:39:07 +0000

    Variable for last log error

--- imod/beadfix.h
+++ imod/beadfix.h
@@ -5,7 +5,6 @@
  *   Colorado.  See implementation file for full copyright notice.
  *
  * $Id$
- * Log at end of file
  */                                                                           
 
 #ifndef BEADFIX_H
@@ -224,91 +223,7 @@ class BeadFixer : public DialogFrame
   QProcess *mAlignProcess;
   int mPeakMin, mPeakMax;
   int mLastThresh;
+  QString mLastLogError;
 };
 
 #endif
-/*
-
-$Log$
-Revision 1.27  2009/11/17 05:28:46  mast
-New items for progress line and button to move in all areas
-
-Revision 1.26  2009/01/16 06:06:59  mast
-FUnction to fix the size in Qt 4
-
-Revision 1.25  2009/01/15 16:33:17  mast
-Qt 4 port
-
-Revision 1.24  2008/11/29 22:08:50  mast
-Added skip list, extra object variables
-
-Revision 1.23  2008/11/12 00:38:31  mast
-Added variable to keep track of threshold slider value sent out
-
-Revision 1.22  2008/07/17 05:01:59  mast
-delete in all objects added
-
-Revision 1.21  2008/01/17 22:36:32  mast
-Update function, turn off checkbock
-
-Revision 1.20  2007/11/03 05:04:41  mast
-Added threshold slider
-
-Revision 1.19  2006/10/18 21:23:56  mast
-Removed seedmode variable
-
-Revision 1.18  2006/07/18 04:17:47  mast
-Removed up down arrow box
-
-Revision 1.17  2006/07/05 04:17:54  mast
-Enhanced overlay mode
-
-Revision 1.16  2006/07/04 03:51:04  mast
-Switched running align from a thread to a QProcess
-
-Revision 1.15  2006/07/03 04:12:19  mast
-Overlay mode, operating mode, new gap fizing functions
-
-Revision 1.14  2006/03/01 18:17:50  mast
-Added variables to keep track of moving all in local area
-
-Revision 1.13  2006/02/13 05:14:50  mast
-Added autocentering and seed mode
-
-Revision 1.12  2005/04/12 18:58:00  mast
-Added move all in local area
-
-Revision 1.11  2005/02/19 01:29:50  mast
-Removed function to clear extra object
-
-Revision 1.10  2004/11/04 23:30:55  mast
-Changes for rounded button style
-
-Revision 1.9  2004/09/24 18:00:36  mast
-Added message capability
-
-Revision 1.8  2004/06/25 20:06:09  mast
-Made plug variables be class members instead to eliminate plug->
-
-Revision 1.7  2004/06/24 15:34:37  mast
-Changes for read and analyze at once
-
-Revision 1.6  2004/06/12 00:58:11  mast
-Switched to reading in whole file at once
-
-Revision 1.5  2004/05/28 18:57:42  mast
-Enable fixer to run align if thread support exists
-
-Revision 1.4  2004/05/07 22:14:26  mast
-Defined a variable instead of QT_THREAD_SUPPORT for the the Run Align button
-
-Revision 1.3  2004/05/03 19:17:20  mast
-Added thread class to run tiltalign
-
-Revision 1.2  2004/04/29 00:28:51  mast
-Added button to keep window on top
-
-Revision 1.1  2003/10/01 05:10:58  mast
-Incorporation as internal module in 3dmod
-
-*/
