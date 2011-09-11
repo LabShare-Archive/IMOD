e0aa7af5963076e817d53274d8976b26acb5b956
mastcu <mast@colorado.edu>
2011-09-11 04:38:06 +0000

    Added message for setting Zap HQ mode

--- imod/imod_client_message.cpp
+++ imod/imod_client_message.cpp
@@ -4,7 +4,6 @@
  *
  *  Original author: David Mastronarde   email: mast@colorado.edu
  *  $Id$
- *  Log at end of file
  */
 
 #include <stdio.h>
@@ -36,6 +35,7 @@
 #include "imod_workprocs.h"
 #include "control.h"
 #include "xzap.h"
+#include "zap_classes.h"
 #include "sslice.h"
 
 //  Module variables
@@ -283,6 +283,7 @@ bool ImodClipboard::executeMessage()
   int succeeded = 1;
   QString convName;
   QDir *curdir;
+  ZapFuncs *zap;
   int movieVal, xaxis, yaxis, zaxis, taxis;
   int objNum, type, symbol, symSize, ptSize, mode, mask, interval;
   bool props1;
@@ -527,6 +528,16 @@ bool ImodClipboard::executeMessage()
         imodDraw(App->cvi, IMOD_DRAW_MOD);
         break;
         
+      case MESSAGE_ZAP_HQ_MODE:
+        mode = messageStrings[++arg].toInt() != 0 ? 1 : 0;
+        zap = getTopZapWindow(false);
+        if (zap) {
+          zap->stateToggled(ZAP_TOGGLE_RESOL, mode);
+          zap->mQtWindow->setToggleState(ZAP_TOGGLE_RESOL, mode);
+        } else
+          zapSetNextOpenHQstate(mode);
+        break;
+
       case MESSAGE_PLUGIN_EXECUTE:
         arg++;
         if (imodPlugMessage(App->cvi, &messageStrings, &arg)) {
@@ -637,144 +648,3 @@ static int readLine(char *line)
     line[strlen(line) - 1] = 0x00;
   return strlen(line);
 }
-
-
-/*
-$Log$
-Revision 4.31  2009/01/16 05:12:21  mast
-Stop deleting tier in handler and switch to single-shot static for mClipTimer
-
-Revision 4.30  2009/01/15 16:33:17  mast
-Qt 4 port
-
-Revision 4.29  2008/07/24 17:22:01  mast
-Fixed reading of object properties 2 to not swallow another number
-
-Revision 4.28  2008/07/16 04:29:08  mast
-Added new messages for some more object properties
-
-Revision 4.27  2008/02/06 20:28:37  mast
-Added flag to keep track of whether stderr was disconnected
-
-Revision 4.26  2008/01/13 22:58:35  mast
-Changes for multi-Z window
-
-Revision 4.25  2006/07/03 19:55:29  mast
-Replaced destructor with explicit calls to request a disconnect signal
-from the source of standard input and to wait until thread exits
-
-Revision 4.24  2006/06/20 17:26:53  mast
-Changed stdin listening to use select function instead of thread
-except in windows, because of problems killing thread in Linux
-
-Revision 4.23  2006/06/19 05:30:17  mast
-Added ability to use standard input instead of clipboard
-
-Revision 4.22  2005/10/14 22:04:39  mast
-Changes for Model reload capability
-
-Revision 4.21  2004/09/24 18:08:59  mast
-Added message for passing message to plugins
-
-Revision 4.20  2004/08/12 17:15:02  mast
-Added message to get slicer angles
-
-Revision 4.19  2004/06/22 23:59:28  mast
-Made it redo checksum after changing object type of blank model
-
-Revision 4.18  2004/06/05 00:17:43  mast
-Converted to allowing multiple actions in one message
-
-Revision 4.17  2004/06/04 03:21:27  mast
-Simplified code for keeping black/white level by just keeping the level
-the same in the open function - seemed to help crashing problem on Linux
-
-Revision 4.16  2004/05/31 23:35:26  mast
-Switched to new standard error functions for all debug and user output
-
-Revision 4.15  2004/05/31 02:14:58  mast
-Added message to set object properties (type, symbol, 3D point)
-
-Revision 4.14  2004/05/28 23:29:42  mast
-Do not open a zap window until the event loop has started
-
-Revision 4.13  2004/05/05 17:32:44  mast
-Added message to get rubberband coordinates
-
-Revision 4.12  2004/04/28 23:51:44  mast
-Added ability to open zap if none are open
-
-Revision 4.11  2004/01/07 01:54:51  mast
-Add capability to start movie on any axis with negative value to 6
-
-Revision 4.10  2003/11/12 18:48:37  mast
-Added ability to process messages in imodv
-
-Revision 4.9  2003/10/02 01:30:09  mast
-Added message to open bead fixer
-
-Revision 4.8  2003/09/24 16:20:05  mast
-Remove multiple spaces in message before processing, and keep track of
-and send last response if a message is repeated
-
-Revision 4.7  2003/09/24 15:07:11  mast
-Improved debug mode, added a repeated response to same message
-
-Revision 4.6  2003/08/01 05:52:38  mast
-Allowed model mode message to set back to movie mode, added message to open
-file and keep black/white levels.
-
-Revision 4.5  2003/06/04 23:42:33  mast
-Add message to go to model mode
-
-Revision 4.4  2003/05/23 02:45:29  mast
-Add message to raise windows, make open model raise windows also
-
-Revision 4.3  2003/02/27 20:57:42  mast
-Adjusting timing on quit for the SGI
-
-Revision 4.2  2003/02/27 19:22:16  mast
-Qt version that works on windows
-
-Revision 4.1  2003/02/10 20:28:59  mast
-autox.cpp
-
-Revision 1.1.2.3  2003/01/29 01:43:23  mast
-Have it return true at end
-
-Revision 1.1.2.2  2003/01/27 00:30:07  mast
-Pure Qt version and general cleanup
-
-Revision 1.1.2.1  2003/01/23 23:05:57  mast
-conversion to cpp
-
-Revision 3.3.2.4  2003/01/23 19:54:09  mast
-add include of imod_io
-
-Revision 3.3.2.3  2003/01/13 01:15:42  mast
-changes for Qt version of info window
-
-Revision 3.3.2.2  2002/12/17 18:35:03  mast
-Removing argument to call to open imodv
-
-Revision 3.3.2.1  2002/12/05 16:23:52  mast
-No changes - CVS detected as modified in branch
-
-Revision 3.4  2002/12/03 15:53:57  mast
-Make it set the forbid level before any action that can produce file
-dialog box.  Have it distinguish cancel from error in error reports
-
-Revision 3.3  2002/12/01 16:51:34  mast
-Changes to eliminate warnings on SGI
-
-Revision 3.2  2002/10/22 22:38:02  mast
-Added include of imod_client_message.h, removed some obviously redundant
-or unneeded includes carried over from imod_menu.c
-
-Revision 3.1  2002/10/15 21:18:09  rickg
-executeMessage() call moved so that it get called for all messages.
-
-Revision 3.0  2002/09/27 20:30:06  rickg
-Initital version of code moved from imod_menu_cb.c
-
-*/
