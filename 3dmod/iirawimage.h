71802338a9712daf3d08cb68c71623dd2ccc1dfe
mastcu <mast@colorado.edu>
2011-09-18 17:26:58 -0600

    Function to set inverted flag

--- 3dmod/iirawimage.h
+++ 3dmod/iirawimage.h
@@ -3,19 +3,9 @@
  *   Copyright (C) 1995-2004 by Boulder Laboratory for 3-Dimensional Electron
  *   Microscopy of Cells ("BL3DEMC") and the Regents of the University of 
  *   Colorado.  See dist/COPYRIGHT for full copyright notice.
+ *
+ *  $Id$
  */                                                                           
-
-/*  $Author$
-
-$Date$
-
-$Revision$
-
-$Log$
-Revision 4.1  2004/12/02 21:39:51  mast
-Addition to program
-
-*/
 
 #ifndef IIRAWIMAGE_H
 #define IIRAWIMAGE_H
@@ -30,6 +20,7 @@ int iiRawScan(ImodImageFile *inFile);
 void iiRawSetSize(int nx, int ny, int nz);
 int iiRawSetMode(int mode);
 void iiRawSetSwap();
+void iiRawSetInverted();
 void iiRawSetHeaderSize(int size);
 void iiRawSetScale(float smin, float smax);
 
