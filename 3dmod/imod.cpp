407463453bd5b30ee9f2b910fd64fa7f495532fa
mastcu <mast@colorado.edu>
2011-09-18 17:30:28 -0600

    Add raw signed byte and inverted in Y options, made it not fork if printing help

--- 3dmod/imod.cpp
+++ 3dmod/imod.cpp
@@ -9,7 +9,6 @@
  *  Colorado.  See dist/COPYRIGHT for full copyright notice.
  *
  *  $Id$
- *  Log at end of file
  */
 
 #include <limits.h>
@@ -108,8 +107,9 @@ void imod_usage(char *name)
   qstr += "   -f    Load as frames even if image file has piece "
     "coordinates.\n";
   qstr += "   -r nx,ny,nz  Dimensions for raw image files.\n";
-  qstr += "   -t #  Mode for raw files: 0=byte, 1=int; 2=float, "
-    "4=complex, 16=RGB.\n";
+  qstr += "   -ri   Raw file is inverted in Y.\n";
+  qstr += "   -t #  Mode for raw files: -1/0=byte, 1/6=int, 2=float, "
+    "4=complex, 16=RGB,\n";
   qstr += "   -H #  Header size in bytes in raw image files.\n";
   qstr += "   -w    Swap bytes from raw image files.\n";
   qstr += "   -R    Read image data from standard input, not from file."
@@ -191,14 +191,14 @@ int main( int argc, char *argv[])
   App->ghost        = IMOD_GHOST;
 
   /*DNM: prescan for debug, ci and style flags before the display_init */
-  /* Cancel forking on debug or -W output, or -L or -R */
+  /* Cancel forking on debug or -W output, or -L or -R or -h */
   for (i = 1; i < argc; i++){
     if (!strncmp("-D", argv[i], 2)) {
       Imod_debug = TRUE;
       debugKeys = strdup(argv[i] + 2);
       doFork = 0;
     }
-    if (!strcmp("-W", argv[i]) || !strcmp("-R", argv[i]))
+    if (!strcmp("-W", argv[i]) || !strcmp("-R", argv[i]) || !strcmp("-h", argv[i]))
       doFork = 0;
 
     if (!strcmp("-L", argv[i])) {
@@ -471,8 +471,12 @@ int main( int argc, char *argv[])
           break;
         
         case 'r':
-          sscanf(argv[++i], "%d,%d,%d", &nx, &ny, &nz);
-          iiRawSetSize(nx, ny, nz);
+          if (argv[i][2] == 'i') {
+            iiRawSetInverted();
+          } else {
+            sscanf(argv[++i], "%d,%d,%d", &nx, &ny, &nz);
+            iiRawSetSize(nx, ny, nz);
+          }
           rawSet = 1;
           break;
 
@@ -978,332 +982,3 @@ bool imodDebug(char key)
 {
   return (Imod_debug && debugKeys && (strchr(debugKeys, key) != NULL));
 }
-
-/*
-
-$Log$
-Revision 4.78  2011/03/14 23:39:13  mast
-Changes for ushort loading
-
-Revision 4.77  2011/02/26 17:20:37  mast
-Added equal scaling option
-
-Revision 4.76  2010/08/31 22:08:22  mast
-Added opetion to use metadata file for piece list
-
-Revision 4.75  2009/03/31 23:48:10  mast
-Filter out tiff warnings on unrecognized tags
-
-Revision 4.74  2009/03/30 18:26:03  mast
-Raise all windows after starting on the Mac
-
-Revision 4.73  2009/03/22 21:37:26  mast
-Set flag when start to close windows for Mac
-
-Revision 4.72  2009/03/22 03:17:48  mast
-Just like in imodv, only create clip handler if needed
-
-Revision 4.71  2009/02/27 23:46:55  mast
-Set listening flag if started with -W or -L
-
-Revision 4.70  2009/01/15 16:33:17  mast
-Qt 4 port
-
-Revision 4.69  2008/12/04 06:50:58  mast
-Turn model on when loading
-
-Revision 4.68  2008/12/01 15:42:01  mast
-Changes for undo/redo and selection in 3dmodv standalone
-
-Revision 4.67  2008/05/27 05:38:30  mast
-Added angle file option, middle section and autocontrasting at startup
-
-Revision 4.66  2008/04/02 04:39:40  mast
-Fixed test for image files entered with -R
-
-Revision 4.65  2008/04/02 04:23:36  mast
-Changes for reading from standard input
-
-Revision 4.64  2008/03/06 00:01:18  mast
-Made windowKeys static and used a key to suppress model save query
-
-Revision 4.63  2008/01/25 20:22:58  mast
-Changes for new scale bar
-
-Revision 4.62  2008/01/13 22:58:35  mast
-Changes for multi-Z window
-
-Revision 4.61  2007/12/04 22:03:03  mast
-Chnages for rearrangement of utilities
-
-Revision 4.60  2006/09/21 22:26:26  mast
-Go to raw check first if any raw options are entered
-
-Revision 4.59  2006/09/03 21:29:22  mast
-Moved check function adding to after plugin loading
-
-Revision 4.58  2006/08/28 05:17:25  mast
-Added option to read in custom color map
-
-Revision 4.57  2006/08/16 23:48:15  mast
-Added loading time debug output
-
-Revision 4.56  2006/07/03 19:51:51  mast
-Request a disconnect of the message handler on exit
-
-Revision 4.55  2006/06/20 17:27:26  mast
-Changed test for using -L - threads required on Windows only
-
-Revision 4.54  2006/06/19 05:29:14  mast
-Added -L option to use stdin for messages; delete clipboard object on exit
-
-Revision 4.53  2006/01/14 18:15:18  mast
-Call new function for model initialization
-
-Revision 4.52  2005/10/14 22:02:50  mast
-Call new function to set Imod_filename
-
-Revision 4.51  2004/12/24 02:18:52  mast
-Removed absolute argument from call to show help page
-
-Revision 4.50  2004/12/06 04:39:54  mast
-Changed call to ImodAssistant constructor
-
-Revision 4.49  2004/12/04 02:10:31  mast
-Moved declaration of ImodHelp into here
-
-Revision 4.48  2004/12/02 21:42:23  mast
-Changes for raw image loading
-
-Revision 4.47  2004/11/30 03:39:42  mast
-Added call to allow reading via QImage
-
-Revision 4.46  2004/11/24 18:29:38  mast
-Start the assistant with an adp file to get customized appearance
-
-Revision 4.45  2004/11/22 00:23:22  mast
-Added creation and convenience call to new help system
-
-Revision 4.44  2004/11/21 05:58:29  mast
-Added call to open imodv windows, and -DT to throw away model
-
-Revision 4.43  2004/11/20 05:05:27  mast
-Changes for undo/redo capability
-
-Revision 4.42  2004/11/04 23:52:19  mast
-Undoing erroreous checking of test version
-
-
-Revision 4.40  2004/11/04 17:01:31  mast
-Changes for loading FFTs with internal mirroring
-
-Revision 4.39  2004/11/02 20:15:54  mast
-Initialized color indices for named colors here
-
-Revision 4.38  2004/07/07 19:25:29  mast
-Changed exit(-1) to exit(3) for Cygwin
-
-Revision 4.37  2004/06/23 03:34:54  mast
-Change in exiting sequence to allow generic settings to be saved
-
-Revision 4.36  2004/06/05 00:10:24  mast
-Prevented accessing vi.image with no image loaded - crashed on FC1
-
-Revision 4.35  2004/05/31 23:27:19  mast
-Added functions for printing to standard error, with flush on windows
-
-Revision 4.34  2004/05/28 23:30:13  mast
-Add function to report in whether event loop has started
-
-Revision 4.33  2004/03/25 21:06:00  mast
-Prevented accessing vi.hdr when no image was loaded
-
-Revision 4.32  2004/01/09 15:54:12  mast
-Turn keepcachefull back on if user enter -F and doesn't limit cache
-
-Revision 4.31  2004/01/07 01:53:59  mast
-Needed to reopen image file to read piece list from it
-
-Revision 4.30  2004/01/06 16:55:01  mast
-Added new option to open startup page, passing options to it
-
-Revision 4.29  2004/01/05 17:21:09  mast
-Added binning option and did major cleanup of image file loading
-
-Revision 4.28  2003/12/30 06:36:44  mast
-Add option for multifile Z display
-
-Revision 4.27  2003/11/24 16:47:39  mast
-needed to cast argument to printInfo to char *
-
-Revision 4.26  2003/11/01 18:12:16  mast
-changed to put out virtually all error messages to a window
-
-Revision 4.25  2003/10/01 05:13:56  mast
-Added functions for rationalizing plugin compilation
-
-Revision 4.24  2003/09/24 17:33:09  mast
-Remove setting of info window geometry from here - wanted it sooner
-
-Revision 4.23  2003/09/24 15:08:37  mast
-Put window ID to info window in debug mode
-
-Revision 4.22  2003/09/24 00:50:11  mast
-Switched from keeping track of geometry to keeping track of pos() and
-size() when saving and restoring positions and sizes
-
-Revision 4.21  2003/09/17 04:48:43  mast
-Added call to set info window geometry and made settings get saved before
-exit
-
-Revision 4.20  2003/09/13 04:32:33  mast
-Changed to protect the model filename array from overflow
-
-Revision 4.19  2003/06/27 19:24:13  mast
-initialize views when start a new model
-
-Revision 4.18  2003/05/18 22:59:00  mast
-Create icon pixmap here to be able to set it for startup dialog
-
-Revision 4.17  2003/05/18 22:07:37  mast
-changes for new startup dialog
-
-Revision 4.16  2003/05/12 22:07:39  mast
-had to flush stderr to get window ID to etomo on Windows
-
-Revision 4.15  2003/04/30 23:54:41  mast
-Comment out printing window id to info window
-
-Revision 4.14  2003/04/25 00:13:33  mast
-Added cache filling option and make program name changes
-
-Revision 4.13  2003/04/18 20:15:40  mast
-Set flag when program is exiting
-
-Revision 4.12  2003/04/17 21:53:05  mast
-Fix simplification
-
-Revision 4.11  2003/04/17 21:48:44  mast
-simplify -imodv option processing
-
-Revision 4.10  2003/04/11 18:15:59  mast
-Fix exiting logic to not exit after calling Qt exit
-
-Revision 4.9  2003/03/26 23:23:15  mast
-switched from hotslider.h to preferences.h
-
-Revision 4.8  2003/03/24 17:58:09  mast
-Changes for new preferences capability
-
-Revision 4.7  2003/03/12 20:50:32  mast
-make starting with no arguments allow file selection in Windows
-
-Revision 4.6  2003/02/28 01:31:08  mast
-fixing include fiddles
-
-Revision 4.5  2003/02/27 23:07:13  mast
-fiddling with includes some more
-
-Revision 4.4  2003/02/27 19:23:51  mast
-Changes for windows version
-
-Revision 4.3  2003/02/22 00:00:44  mast
-Open file in binary mode
-
-Revision 4.2  2003/02/20 15:58:57  mast
-Add -V and -Z options, rationalize them with -S and -xyz options
-
-Revision 4.1  2003/02/10 20:28:59  mast
-autox.cpp
-
-Revision 1.1.2.17  2003/02/04 19:10:16  mast
-Set default style to windows everywhere
-
-Revision 1.1.2.16  2003/01/29 17:49:20  mast
-Fork at top of program before doing any Qt stuff, and don't fork with -W
-
-Revision 1.1.2.15  2003/01/29 01:31:24  mast
-change -rgb to -ci, close windows on exit
-
-Revision 1.1.2.14  2003/01/27 00:30:07  mast
-Pure Qt version and general cleanup
-
-Revision 1.1.2.13  2003/01/23 20:14:09  mast
-Add include of imod_io
-
-Revision 1.1.2.12  2003/01/13 01:15:42  mast
-changes for Qt version of info window
-
-Revision 1.1.2.11  2003/01/06 15:41:02  mast
-Add imodCaption function
-
-Revision 1.1.2.10  2002/12/23 04:52:58  mast
-Add option to get different font size
-
-Revision 1.1.2.9  2002/12/19 04:37:13  mast
-Cleanup of unused global variables and defines
-
-Revision 1.1.2.8  2002/12/17 18:40:24  mast
-Changes and new includes with Qt version of imodv
-
-Revision 1.1.2.7  2002/12/14 17:53:04  mast
-*** empty log message ***
-
-Revision 1.1.2.6  2002/12/14 05:40:43  mast
-new visual-assessing code
-
-Revision 1.1.2.5  2002/12/13 06:09:09  mast
-include file changes
-
-Revision 1.1.2.4  2002/12/09 17:49:19  mast
-changes to get Zap as a Qt window
-
-Revision 1.1.2.3  2002/12/07 01:23:23  mast
-Improved window title code
-
-Revision 1.1.2.2  2002/12/06 21:58:35  mast
-*** empty log message ***
-
-Revision 1.1.2.1  2002/12/05 16:24:46  mast
-Open a Qxt application
-
-Revision 3.11  2002/12/03 15:45:08  mast
-Call SaveModel instead of SaveModelQuit when quitting, to give user a chance
-to set the filename to save to
-
-Revision 3.10  2002/12/01 16:51:34  mast
-Changes to eliminate warnings on SGI
-
-Revision 3.9  2002/12/01 15:34:41  mast
-Changes to get clean compilation with g++
-
-Revision 3.8  2002/09/27 19:46:26  rickg
-Reverted LoadModel call due to changes in imod_io
-Added error string to SaveModelQuit call
-Removed redudant function declarations at begging of file.
-
-Revision 3.7  2002/09/18 22:56:48  rickg
-Print out process ID when printing out window ID.
-
-Revision 3.6  2002/09/18 02:51:35  mast
-Started event handler right after the fork, so it can receive events during
-the image load.
-
-Revision 3.5  2002/09/17 18:40:33  mast
-Moved the report to window ID to before fork and data loading
-
-Revision 3.4  2002/09/14 00:13:11  mast
-Set declarations and use of event handler right to make SGI compiler happy
-
-Revision 3.3  2002/09/13 21:05:39  mast
-Set up event handler for client messages, added option to output window ID
-
-Revision 3.2  2002/07/21 20:28:52  mast
-Changed imodwfname to return a string with number of image files when
-multiple files are loaded.
-
-Revision 3.1  2002/05/20 15:32:39  mast
-Added -S option to open slicer first; made it set a new model so that time
-index modeling is the default if multiple files are opened.
-
-*/
