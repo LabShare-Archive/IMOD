2483e4359b0676d257c6a8379dc631a184685f2e
SueHeld <sueh@colorado.edu>
2011-10-27 18:09:57 -0600

    Bug# 1562 Removed unnecessary import.

--- Etomo/src/etomo/ui/swing/FileTextField.java
+++ Etomo/src/etomo/ui/swing/FileTextField.java
@@ -10,7 +10,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
