5695291e41aa34c64d37af4605597ad1c1a1fe2c
SueHeld <sueh@colorado.edu>
2011-11-15 14:07:12 -0700

    Bug# 1561 A place for a file button to set a file.

---
+++ Etomo/src/etomo/ui/swing/ButtonTarget.java
@@ -0,0 +1,24 @@
+package etomo.ui.swing;
+
+import java.io.File;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2011</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+interface ButtonTarget {
+  public static final String rcsid = "$Id:$";
+
+  public void setFile(File file);
+}
