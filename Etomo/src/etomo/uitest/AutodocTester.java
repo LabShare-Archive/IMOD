3524fc8660da0b752615e7b17bda03124c238672
SueHeld <sueh@colorado.edu>
2011-11-18 10:08:21 -0700

    Increasing format wait time.

--- Etomo/src/etomo/uitest/AutodocTester.java
+++ Etomo/src/etomo/uitest/AutodocTester.java
@@ -361,7 +361,7 @@ final class AutodocTester extends Assert implements VariableList {
 
   private static final int REDRAW_WAIT = 40;
   private static final int MAX_FORMAT = 5;
-  private static final int FORMAT_WAIT = 690;
+  private static final int FORMAT_WAIT = 700;
 
   private final ReadOnlyAutodoc autodoc;
   private final JFCTestHelper helper;
