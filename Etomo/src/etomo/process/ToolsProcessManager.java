012e892653202d50597dcc41858d711cb107acde
SueHeld <sueh@colorado.edu>
2012-02-28 15:04:28 -0700

    Bug# 1525 Added gputilttest with post- and error-processing.

--- Etomo/src/etomo/process/ToolsProcessManager.java
+++ Etomo/src/etomo/process/ToolsProcessManager.java
@@ -3,10 +3,12 @@ package etomo.process;
 import etomo.BaseManager;
 import etomo.ToolsManager;
 import etomo.comscript.FlattenWarpParam;
+import etomo.comscript.GpuTiltTestParam;
 import etomo.comscript.WarpVolParam;
 import etomo.type.AxisID;
 import etomo.type.ConstProcessSeries;
 import etomo.type.FileType;
+import etomo.type.ProcessName;
 import etomo.type.ProcessResultDisplay;
 
 /**
@@ -45,18 +47,28 @@ public final class ToolsProcessManager extends BaseProcessManager {
   }
 
   /**
+   * Run gpuTiltTest
+   */
+  public String gpuTiltTest(GpuTiltTestParam param, AxisID axisID)
+      throws SystemProcessException {
+    BackgroundProcess backgroundProcess = startBackgroundProcess(param.getCommand(),
+        axisID, ProcessName.GPU_TILT_TEST, null);
+    return backgroundProcess.getName();
+  }
+
+  /**
    * Run the appropriate flatten com file for the given axis ID
    */
   public String flatten(final WarpVolParam param, final AxisID axisID,
       final ProcessResultDisplay processResultDisplay,
       final ConstProcessSeries processSeries, final FileType fileType)
       throws SystemProcessException {
-    //  Create the required tilt command
+    // Create the required tilt command
     String command = fileType.getFileName(manager, AxisID.ONLY);
-    //  Instantiate the process monitor
+    // Instantiate the process monitor
     Matchvol1ProcessMonitor monitor = Matchvol1ProcessMonitor.getFlattenInstance(manager,
         axisID, fileType);
-    //  Start the com script in the background
+    // Start the com script in the background
     ComScriptProcess comScriptProcess = startComScript(command, monitor, axisID,
         processResultDisplay, param, processSeries, fileType);
     return comScriptProcess.getName();
@@ -72,9 +84,30 @@ public final class ToolsProcessManager extends BaseProcessManager {
   }
 
   void postProcess(final BackgroundProcess process) {
+    super.postProcess(process);
+    if (process.getProcessName() == ProcessName.GPU_TILT_TEST) {
+      String[] output = process.getStdOutput();
+      manager.logMessageWithKeyword(output, GpuTiltTestParam.OUTPUT_KEYWORD,
+          ProcessName.GPU_TILT_TEST.toString() + ":", process.getAxisID());
+      manager.logMessageWithKeyword(process.getStdError(),
+          GpuTiltTestParam.OUTPUT_KEYWORD, ProcessName.GPU_TILT_TEST.toString() + ":",
+          process.getAxisID());
+      manager.gpuTiltTestSuceeded(output, process.getAxisID());
+    }
   }
 
   void errorProcess(final BackgroundProcess process) {
+    if (process.getProcessName() == ProcessName.GPU_TILT_TEST) {
+      manager.logMessageWithKeyword(process.getStdOutput(),
+          GpuTiltTestParam.OUTPUT_KEYWORD, ProcessName.GPU_TILT_TEST.toString() + ":",
+          process.getAxisID());
+      manager.logMessageWithKeyword(process.getStdError(),
+          GpuTiltTestParam.OUTPUT_KEYWORD, ProcessName.GPU_TILT_TEST.toString() + ":",
+          process.getAxisID());
+      manager.logMessageWithKeyword(FileType.GPU_TEST_LOG,
+          GpuTiltTestParam.OUTPUT_KEYWORD, ProcessName.GPU_TILT_TEST.toString() + ":",
+          process.getAxisID());
+    }
   }
 
   void errorProcess(final ComScriptProcess process) {
