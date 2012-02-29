0ea1dc1ad77b48b135f37328a9c9fd1eaee50bbd
mastcu <mast@colorado.edu>
2012-02-28 19:00:59 -0700

    Got rid of fixed size temporary arrays

--- flib/blend/smoothgrid.f
+++ flib/blend/smoothgrid.f
@@ -1,11 +1,6 @@
-c       $Author$
 c       
-c       $Date$
+c       $Id$
 c       
-c       $Revision$
-c       
-c       $Log$
-
 c       SMOOTHGRID smooths the 2 dimension arrays in DXGRID, DYGRID and
 c       DDENGRID, based on values in D[XY]GRID and the array SDGRID.  IXGDIM,
 c       IYGDIM are the dimensions of the arrays.  NXGRID, NYGRID are the
@@ -29,19 +24,29 @@ c       Each point is closest to the center of some polynomial fitting area;
 c       DXGRID, DYGRID, and DDENGRID for that point are replaced by the value
 c       calculated from that polynomial for that position.
 c       
-      subroutine smoothgrid(dxgrid,dygrid,sdgrid,ddengrid,ixgdim,
-     &    iygdim,nxgrid,nygrid,sdcrit, devcrit,nxfit,nyfit,norder,
-     &    nxskip,nyskip)
+      subroutine smoothgrid(dxgrid,dygrid,sdgrid,ddengrid,ixgdim, iygdim,nxgrid,nygrid,
+     &    sdcrit, devcrit,nxfit,nyfit,norder, nxskip,nyskip)
+      implicit none
+      integer idim
+      integer*4 ixgdim,iygdim,nxgrid,nygrid,nxfit,nyfit,norder,nxskip,nyskip
 c       
       real*4 dxgrid(ixgdim,iygdim),dygrid(ixgdim,iygdim)
       real*4 sdgrid(ixgdim,iygdim),ddengrid(ixgdim,iygdim)
+      real*4 sdcrit, devcrit
       parameter (idim=100)
       include 'statsize.inc'
-      real*4 xr(msiz,idim), sx(msiz), xm(msiz), sd(msiz)
-     &    , ss(msiz,msiz), ssd(msiz,msiz), d(msiz,msiz), r(msiz,msiz)
-     &    , b(msiz), b1(msiz)
-      real*4 dxnew(100,100),dynew(100,100),denew(100,100),
-     &    vect(msiz),b2(msiz),b3(msiz)
+      real*4 xr(msiz,idim), sx(msiz), xm(msiz), sd(msiz) , ss(msiz,msiz), ssd(msiz, msiz)
+      real*4 d(msiz,msiz), r(msiz,msiz) , b(msiz), b1(msiz), vect(msiz),b2(msiz),b3(msiz)
+      real*4 dxnew(nxgrid,nygrid), dynew(nxgrid,nygrid), denew(nxgrid,nygrid)
+      real*4 sdmax, sdsum,sdsq,devsum,devsqsum, dxmean ,dymean,denmean,dev, devvary, bint
+      integer*4 ix, iy, nnear, length, nfitpt, ixy, ihi, ilo, nfit, iord, i
+      integer*4 nxftl, nyftl, ixsolvsub, ixsolvadd, ixsolvstr, ixsolvend, ixsolspan
+      integer*4 nxsolve, ixfitsub,ixfitadd,iysolvsub,iysolvadd,iysolvstr,iysolvend
+      integer*4 iysolspan, nysolve, iyfitsub, iyfitadd, nindep, idepen1, idepen2, idepen3
+      integer*4 ixfitend, ixsol, ixcen, ixfitstr,ixst, ixnd, iyfitend, iysol
+      integer*4 iycen, iyfitstr, iyst, iynd, npnts, ind,nsum
+      real*4 xsum,c1,c2,c3, ysum, dsum, devsq, devsd, fra, devmean, rsq, sdmean, sdsd
+      real*4 sdvary
 c       
 c       intercept the case of no overlap
 c       
@@ -128,6 +133,7 @@ c             &           dygrid(ix,iy), sdgrid(ix,iy),dxmean,dymean
             ix=ixy
           endif
           call polyfit(b,b1,nfit,iord,vect,bint)
+          ind = ix + (iy-1) * nxgrid
           dxnew(ix,iy)=bint
           call polyfit(b,b2,nfit,iord,vect,bint)
           dynew(ix,iy)=bint
@@ -231,6 +237,7 @@ c             replace proper grid points with fit values
                   ysum=ysum+b2(i)*vect(i)
                   dsum=dsum+b3(i)*vect(i)
                 enddo
+                ind = ix + (iy-1) * nxgrid
                 dxnew(ix,iy)=xsum
                 dynew(ix,iy)=ysum
                 denew(ix,iy)=dsum
@@ -244,6 +251,7 @@ c       replace new values into dxgrid and dygrid arrays
 c       
       do ix=1,nxgrid
         do iy=1,nygrid
+          ind = ix + (iy-1) * nxgrid
           dxgrid(ix,iy)=dxnew(ix,iy)
           dygrid(ix,iy)=dynew(ix,iy)
           ddengrid(ix,iy)=denew(ix,iy)
