07f32910996237bcf5c6c45f4c383d08cd33c089
mastcu <mast@colorado.edu>
2012-03-07 22:45:24 -0700

    Made a new date routine to replace DATE that generates warnings

--- flib/subrs/compat/datetime.f
+++ flib/subrs/compat/datetime.f
@@ -1,14 +1,7 @@
       subroutine date(dat)
       implicit none
       character*(*) dat
-      character*8 packdate
-      integer*4 ivals(8)
-      character*3 months(12)
-      data months /'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug',
-     &    'Sep','Oct','Nov','Dec'/
-      call date_and_time(DATE=packdate, VALUES=ivals)
-      write(dat,101)ivals(3),months(ivals(2)),packdate(3:4)
-101   format(i2,'-',a3,'-',a2)
+      call b3ddate(dat)
       return
       end
 
