18e4e01452260ba0ddc81b74e37e0a5765c2b28e
mastcu <mast@colorado.edu>
2012-06-21 11:56:45 -0600

    Enhance robust output with local summary, more digits allowed

--- flib/tiltalign/tiltalign.f90
+++ flib/tiltalign/tiltalign.f90
@@ -79,6 +79,7 @@ program tiltalign
   integer*4 numWgtTotal, numWgtZero, numWgt1, numWgt2, numWgt5, maxDelWgtBelowCrit
   integer*4 maxRobustOneCycle, numOneCycle, numTotCycles, maxTotCycles, numBelowCrit
   real*4 robustTotCycleFac, delWgtMeanCrit, delWgtMaxCrit, rmsScale
+  real*4 wgtErrMean, wgtErrSumLocal, wgtErrLocalMin, wgtErrLocalMax
   character*20 message
   character*320 concat
   !
@@ -151,7 +152,7 @@ program tiltalign
   delWgtMaxCrit = 0.01
   maxDelWgtBelowCrit = 4
   maxRobustOneCycle = 10
-  robustTotCycleFac = 1.5
+  robustTotCycleFac = 3.
   ifDoRobust = 0
   incrGmag = 0
   incrDmag = 0
@@ -306,7 +307,6 @@ program tiltalign
     xx(i) = xx(i) / scaleXY
     yy(i) = yy(i) / scaleXY
   enddo
-  rmsScale = scaleXY**2 / nprojpt
   !
   ! Get the h array big enough for the solution and for temporary arrays in solveXyzd
   maxvar = nvarSearch + 3 * nrealPt
@@ -456,11 +456,13 @@ program tiltalign
         write(*,'(/,a)')'Summary of robust weighting in local areas:'
         write(*,321)numWgtTotal, numWgtZero, numWgt1, numWgt2, numWgt5,  &
             (100. * numWgt5) / numWgtTotal
-321     format(i6,' weights: ',i4,' are 0, ',i3,' are < .1, ',i3,' are < .2, ',i4, &
+321     format(i6,' weights:',i5,' are 0,',i5,' are < .1,',i5,' are < .2,',i6, &
             ' (',f4.1,'%) are < .5')
       endif
       write(*,119) errsumLocal / (numPatchX * numPatchY), errLocalMin, errLocalMax
 119   format(/,' Residual error local mean:  ',f9.3,'    range', f8.3, ' to',f8.3)
+      if (ifDoRobust > 0 )write(*,1191) wgtErrsumLocal / (numPatchX * numPatchY), wgtErrLocalMin, wgtErrLocalMax
+1191   format(/,' Weighted error local mean:  ',f9.3,'    range', f8.3, ' to',f8.3)
     endif
   endif
   close(7)
@@ -500,6 +502,7 @@ CONTAINS
     !
     ! Do beam tilt search only for global alignment
     robustWeights = .false.
+    rmsScale = scaleXY**2 / nprojpt
     if (ifBTSearch == 0 .or. ifLocal > 0) then
       call runMetro(nvarSearch, var, varErr, grad, h, ifLocal, facm, ncycle, 0, &
           rmsScale, fFinal, i, metroError)
@@ -573,7 +576,7 @@ CONTAINS
           numTotCycles,' cycles without meeting convergence criteria'
       write(*,'(a,2f8.4)')' Final mean and max weight change',errMean, errSd
       write(*,321) nprojpt, index, ipt, iv, jpt, (100. * jpt) / nprojpt
-321     format(i6,' weights: ',i4,' are 0, ',i3,' are < .1, ',i3,' are < .2, ',i4, &
+321     format(i6,' weights:',i5,' are 0,',i5,' are < .1,',i5,' are < .2,',i6, &
             ' (',f4.1,'%) are < .5',/)
       if (ifLocal .ne. 0) then
         numWgtTotal = numWgtTotal + nprojpt
@@ -1190,11 +1193,16 @@ CONTAINS
       if (ifDoRobust .ne. 0) write(*,1181) wgtErrSum / wgtSum, '(Global)'
     else
       write(*,118) errMean, errSd, '(Local area', ipatchX, ipatchy, ')'
-      if (ifDoRobust .ne. 0) write(*,1181) wgtErrSum / wgtSum, '(Local area', ipatchX,  &
-          ipatchy, ')'
       errsumLocal = errsumLocal + errMean
       errLocalMin = min(errLocalMin, errMean)
       errLocalMax = max(errLocalMax, errMean)
+      if (ifDoRobust .ne. 0) then
+        wgtErrMean = wgtErrSum / wgtSum
+        write(*,1181) wgtErrMean, '(Local area', ipatchX, ipatchy, ')'
+        wgtErrsumLocal = wgtErrsumLocal + wgtErrMean
+        wgtErrLocalMin = min(wgtErrLocalMin, wgtErrMean)
+        wgtErrLocalMax = max(wgtErrLocalMax, wgtErrMean)
+      endif
     endif
     if (ifResOut > 0) then
       message = ' '
@@ -1284,6 +1292,9 @@ CONTAINS
     errsumLocal = 0.
     errLocalMin = 1.e10
     errLocalMax = -10.
+    wgtErrSumLocal = 0.
+    wgtErrLocalMin = 1.e10
+    wgtErrLocalMax = -10.
     ifVarOut = 0
     ifResOut = 0
     ifXyzOut = 0
