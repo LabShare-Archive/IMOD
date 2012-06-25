92650af569545a48e5eebd803260c3eaf6b928a1
SueHeld <sueh@colorado.edu>
2012-06-25 13:33:48 -0600

    Bug# 1611 Added remove.

--- Etomo/src/etomo/type/Transform.java
+++ Etomo/src/etomo/type/Transform.java
@@ -35,7 +35,7 @@ public class Transform {
   public static final Transform ROTATION_TRANSLATION = new Transform(
       ROTATION_TRANSLATION_VALUE);
   public static final Transform TRANSLATION = new Transform(TRANSLATION_VALUE);
-  
+
   public static final Transform DEFAULT = FULL_LINEAR_TRANSFORMATION;
 
   private static final String KEY = "Transform";
@@ -48,6 +48,11 @@ public class Transform {
       props.remove(prepend + '.' + key);
     }
     props.setProperty(prepend + '.' + key, transform.toString());
+  }
+
+  public static void remove(Properties props, String prepend, String key) {
+    prepend = createPrepend(prepend);
+    props.remove(prepend + '.' + key);
   }
 
   public static Transform load(Properties props, String prepend, String key,
