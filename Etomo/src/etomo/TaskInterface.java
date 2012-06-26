4d5c575d7853c7508f25fa6fd0d332570ae1ada6
SueHeld <sueh@colorado.edu>
2012-06-26 12:25:04 -0600

    Bug# 1611 TaskInterface is an interface for task when can be passed to ProcessSeries.

---
+++ Etomo/src/etomo/TaskInterface.java
@@ -0,0 +1,19 @@
+package etomo;
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2012</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+interface TaskInterface {
+  public static  final String  rcsid =  "$Id:$";
+}
