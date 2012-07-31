8040cf58b3d8486b63c77b51d0180c1f220c3ae3
SueHeld <sueh@colorado.edu>
2012-07-31 16:28:05 -0600

    Bug# 1611 Added setInvalidEdgeFunctions, postProcess(ComScriptProcess), and errorProcess(ComScriptProcess).

--- Etomo/src/etomo/process/SerialSectionsProcessManager.java
+++ Etomo/src/etomo/process/SerialSectionsProcessManager.java
@@ -1,14 +1,19 @@
 package etomo.process;
 
-import etomo.BaseManager;
+import etomo.SerialSectionsManager;
 import etomo.comscript.BlendmontParam;
+import etomo.comscript.Command;
+import etomo.comscript.CommandDetails;
 import etomo.comscript.ConstNewstParam;
 import etomo.comscript.ExtractpiecesParam;
 import etomo.comscript.MidasParam;
+import etomo.comscript.ProcessDetails;
 import etomo.comscript.XftoxgParam;
 import etomo.type.AxisID;
 import etomo.type.ConstProcessSeries;
 import etomo.type.ProcessName;
+import etomo.type.SerialSectionsState;
+import etomo.type.ViewType;
 
 /**
 * <p>Description: </p>
@@ -28,9 +33,9 @@ import etomo.type.ProcessName;
 public final class SerialSectionsProcessManager extends BaseProcessManager {
   public static final String rcsid = "$Id:$";
 
-  private final BaseManager manager;
+  private final SerialSectionsManager manager;
 
-  public SerialSectionsProcessManager(final BaseManager manager) {
+  public SerialSectionsProcessManager(final SerialSectionsManager manager) {
     super(manager);
     this.manager = manager;
   }
@@ -96,5 +101,51 @@ public final class SerialSectionsProcessManager extends BaseProcessManager {
     ComScriptProcess comScriptProcess = startComScript(newstParam, newstProcessMonitor,
         axisID, null, processSeries);
     return comScriptProcess.getName();
+  }
+
+  private void setInvalidEdgeFunctions(Command command, boolean succeeded) {
+    if (manager.getViewType() == ViewType.MONTAGE
+        && command.getCommandName().equals(BlendmontParam.COMMAND_NAME)
+        && (command.getCommandMode() == BlendmontParam.Mode.SERIAL_SECTION_PREBLEND || command
+            .getCommandMode() == BlendmontParam.Mode.SERIAL_SECTION_BLEND)) {
+      manager.getState().setInvalidEdgeFunctions(!succeeded);
+    }
+  }
+
+  void postProcess(ComScriptProcess script) {
+    try {
+      // Script specific post processing
+      ProcessName processName = script.getProcessName();
+      ProcessDetails processDetails = script.getProcessDetails();
+      CommandDetails commandDetails = script.getCommandDetails();
+      Command command = script.getCommand();
+      SerialSectionsState state = manager.getState();
+      AxisID axisID = script.getAxisID();
+      if (processName == ProcessName.PREBLEND) {
+        setInvalidEdgeFunctions(script.getCommand(), true);
+      }
+      else if (processName == ProcessName.BLEND) {
+        setInvalidEdgeFunctions(script.getCommand(), true);
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      System.err.println("ERROR:  Unable to record state.");
+    }
+  }
+  void errorProcess(ComScriptProcess script) {
+    try {
+      ProcessName processName = script.getProcessName();
+      if (processName == ProcessName.BLEND) {
+        setInvalidEdgeFunctions(script.getCommand(), false);
+      }
+      else if (processName == ProcessName.PREBLEND) {
+        setInvalidEdgeFunctions(script.getCommand(), false);
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      System.err.println("ERROR:  Unable to record state.");
+    }
   }
 }
