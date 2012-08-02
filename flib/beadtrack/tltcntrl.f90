63c04b747bfe48210f6f3a4d62e5026571494874
mastcu <mast@colorado.edu>
2012-08-02 13:23:09 -0600

    Add izexclude and numExclude

--- flib/beadtrack/tltcntrl.f90
+++ flib/beadtrack/tltcntrl.f90
@@ -6,7 +6,7 @@ module tltcntrl
   use alivar
   ! allocated to limview or maxInArea
   real*4, allocatable :: tiltOrig(:), gmagOrig(:), rotOrig(:), dxySave(:,:), tiltAll(:)
-  integer*4, allocatable :: iobjAli(:), ivsepIn(:,:)
+  integer*4, allocatable :: iobjAli(:), ivsepIn(:,:), izExclude(:)
   integer*4 nmapSpecMag(MAXGRP), ivSpecStrMag(MAXGRP), ivSpecEndMag(MAXGRP)
   integer*4 nmapSpecRot(MAXGRP), ivSpecStrRot(MAXGRP), ivSpecEndRot(MAXGRP)
   integer*4 nmapSpecTilt(MAXGRP), ivSpecStrTilt(MAXGRP)
@@ -15,7 +15,7 @@ module tltcntrl
   integer*4 nviewAll, minTiltInd, minInView, minViewsTiltAli, initXyzDone
   real*4 rangeDoAxis, rangeDoTilt, scaleXY, xcen, ycen, xorig, yorig
   real*4 xdelt, ydelt, facMetro, eps, cgx, cgy
-  integer*4 nCycle, nviewLocal, numObjDo, maxH
+  integer*4 nCycle, nviewLocal, numObjDo, maxH, numExclude
   integer*4 nmapMag, nmapRot, nmapTilt, nRanSpecMag, nRanSpecRot, nRanSpecTilt
   ! Allocated to maxAllReal, and (3, maxAllReal)
   integer*4, allocatable :: iobjSeq(:)
