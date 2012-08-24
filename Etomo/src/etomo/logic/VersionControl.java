703cf7c9fc63720cb64223b4f1b19a1589dc3b39
SueHeld <sueh@colorado.edu>
2012-08-24 16:30:09 -0600

    timestamping

--- Etomo/src/etomo/logic/VersionControl.java
+++ Etomo/src/etomo/logic/VersionControl.java
@@ -33,7 +33,7 @@ import etomo.util.EnvironmentVariable;
 public final class VersionControl {
   public final String rcsid = "$$Id$$";
 
-  public static final String TIME_STAMP = "8/24/2012 16:29";
+  public static final String TIME_STAMP = "8/24/2012 16:30";
   
   private static final String DEPENDANT_IMOD_VERSION = "4.4.4";
   private static final String DEPENDANT_PEET_VERSION = "1.8.0";
