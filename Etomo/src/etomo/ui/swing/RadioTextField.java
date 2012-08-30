babc42f7fc1bcfae24f8c54fa4f1f2a3d050caeb
SueHeld <sueh@colorado.edu>
2012-08-30 15:30:48 -0600

    Bug# 1614 Added setText(long) to prevent longs from being converted into doubles.

--- Etomo/src/etomo/ui/swing/RadioTextField.java
+++ Etomo/src/etomo/ui/swing/RadioTextField.java
@@ -128,8 +128,12 @@ final class RadioTextField implements RadioButtonInterface {
   Container getContainer() {
     return rootPanel;
   }
-  
+
   void setText(final int value) {
+    textField.setText(String.valueOf(value));
+  }
+
+  void setText(final long value) {
     textField.setText(String.valueOf(value));
   }
 
