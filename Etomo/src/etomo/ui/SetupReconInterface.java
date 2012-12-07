b66120d3ad9dacd6be6b41d248106c43ebc45154
SueHeld <sueh@colorado.edu>
2012-12-06 21:42:29 -0700

    Bug# 1662 Interface for SetupDialog and DirectivesFile.

---
+++ Etomo/src/etomo/ui/SetupReconInterface.java
@@ -0,0 +1,67 @@
+package etomo.ui;
+
+import etomo.type.AxisID;
+import etomo.type.TiltAngleSpec;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2012</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface SetupReconInterface {
+  public static final String rcsid = "$Id:$";
+
+  public void setBinning(int input);
+
+  public void setImageRotation(String input);
+
+  public void setPixelSize(double input);
+
+  public String getDataset();
+
+  public boolean isDualAxisSelected();
+
+  public String getDistortionFile();
+
+  public String getMagGradientFile();
+
+  public boolean validateTiltAngle(AxisID axisID, String errorTitle);
+
+  public boolean isSingleViewSelected();
+
+  public String getBackupDirectory();
+
+  public String getBinning();
+
+  public String getExcludeList(AxisID axisID, boolean doValidation)
+      throws FieldValidationFailedException;
+
+  public String getFiducialDiameter(boolean doValidation)
+      throws FieldValidationFailedException;
+
+  public String getImageRotation(AxisID axisID, boolean doValidation)
+      throws FieldValidationFailedException;
+
+  public String getPixelSize(boolean doValidation) throws FieldValidationFailedException;
+
+  public boolean isAdjustedFocusSelected(AxisID axisID);
+
+  public boolean isSingleAxisSelected();
+
+  public boolean isGpuProcessingSelected();
+
+  public boolean isParallelProcessSelected();
+
+  public boolean getTiltAngleFields(AxisID axisID, TiltAngleSpec tiltAngleSpec,
+      boolean doValidation);
+}
