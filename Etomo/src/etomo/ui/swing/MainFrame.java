060b459bbbe7a9744820eb5ab6dec6f93c224924
SueHeld <sueh@colorado.edu>
2013-02-03 12:25:55 -0700

    Bug# 1682 Temp prints.

--- Etomo/src/etomo/ui/swing/MainFrame.java
+++ Etomo/src/etomo/ui/swing/MainFrame.java
@@ -578,11 +578,15 @@ public final class MainFrame extends EtomoFrame implements ContextMenu {
     else {
       System.err.println("1682 FBANDBJDCBDB");
       mainPanel = currentManager.getMainPanel();
-      title = currentManager.getName() + " - " + ETOMO_TITLE;
-      rootPanel.add(windowSwitch.getPanel(managerKey));
-      logFrame.setPanel(currentManager.getLogPanel(), newWindow);
-      toFront();
       System.err.println("1682 FBANDBJDCBDC");
+      title = currentManager.getName() + " - " + ETOMO_TITLE;
+      System.err.println("1682 FBANDBJDCBDD");
+      rootPanel.add(windowSwitch.getPanel(managerKey));
+      System.err.println("1682 FBANDBJDCBDE");
+      logFrame.setPanel(currentManager.getLogPanel(), newWindow);
+      System.err.println("1682 FBANDBJDCBDF");
+      toFront();
+      System.err.println("1682 FBANDBJDCBDG");
       mainPanel.addMouseListener(mouseAdapter);
       mainPanel.repaint();
 
