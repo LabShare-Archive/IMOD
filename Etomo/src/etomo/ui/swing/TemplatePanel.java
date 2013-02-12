924e78c27d7b6df04e7e0a2fc9ab8c9b104e1071
SueHeld <sueh@colorado.edu>
2013-02-12 15:43:03 -0700

    Bug# 1677 Added getParameters, getTemplateFile, isAppearanceSettingChanged, setParameters, and setTemplate.

--- Etomo/src/etomo/ui/swing/TemplatePanel.java
+++ Etomo/src/etomo/ui/swing/TemplatePanel.java
@@ -3,17 +3,21 @@ package etomo.ui.swing;
 import java.awt.Component;
 import java.io.File;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 import etomo.BaseManager;
 import etomo.logic.ConfigTool;
 import etomo.storage.DirectiveFile;
 import etomo.type.AxisID;
+import etomo.type.UserConfiguration;
 
 /**
 * <p>Description: </p>
 * 
-* <p>Copyright: Copyright 2012</p>
+* <p>Copyright: Copyright 2013</p>
 *
 * <p>Organization:
 * Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
@@ -29,9 +33,9 @@ final class TemplatePanel {
   public static final String rcsid = "$Id:$";
 
   private final JPanel pnlRoot = new JPanel();
-  private final ComboBox cmbScopeTemplate = ComboBox.getInstance("Scope Template");
-  private final ComboBox cmbSystemTemplate = ComboBox.getInstance("System Template");
-  private final ComboBox cmbUserTemplate = ComboBox.getInstance("User Template");
+  private final ComboBox cmbScopeTemplate = ComboBox.getInstance("Scope Template:");
+  private final ComboBox cmbSystemTemplate = ComboBox.getInstance("System Template:");
+  private final ComboBox cmbUserTemplate = ComboBox.getInstance("User Template:");
 
   private File[] scopeTemplateFileList = null;
   private File[] systemTemplateFileList = null;
@@ -76,9 +80,15 @@ final class TemplatePanel {
         cmbUserTemplate.addItem(userTemplateFileList[i].getName());
       }
     }
+    pnlRoot.setLayout(new BoxLayout(pnlRoot, BoxLayout.Y_AXIS));
+    pnlRoot.setBorder(BorderFactory.createEtchedBorder());
+    pnlRoot.add(Box.createRigidArea(FixedDim.x0_y2));
     pnlRoot.add(cmbScopeTemplate.getComponent());
+    pnlRoot.add(Box.createRigidArea(FixedDim.x0_y3));
     pnlRoot.add(cmbSystemTemplate.getComponent());
+    pnlRoot.add(Box.createRigidArea(FixedDim.x0_y3));
     pnlRoot.add(cmbUserTemplate.getComponent());
+    pnlRoot.add(Box.createRigidArea(FixedDim.x0_y2));
   }
 
   private void addListeners() {
@@ -91,56 +101,96 @@ final class TemplatePanel {
     return pnlRoot;
   }
 
-  File getScopeTemplateFile() {
-    int i = cmbScopeTemplate.getSelectedIndex();
-    if (i != -1) {
-      return scopeTemplateFileList[i];
+  private File getTemplateFile(final ComboBox cmbTemplate, final File[] templateFileList) {
+    int i = cmbTemplate.getSelectedIndex();
+    if (i != -1 && templateFileList != null) {
+      return templateFileList[i];
     }
     return null;
   }
 
-  File getSystemTemplateFile() {
-    int i = cmbSystemTemplate.getSelectedIndex();
-    if (i != -1) {
-      return systemTemplateFileList[i];
+  private void setTemplate(final String templateAbsPath, final File[] templateFileList,
+      final ComboBox cmbTemplate) {
+    if (templateFileList == null) {
+      return;
     }
-    return null;
+    // If templateAbsPath doesn't match something in templateFileList, nothing will be
+    // selected in the combobox.
+    for (int i = 0; i < templateFileList.length; i++) {
+      if (templateFileList[i].getAbsolutePath().equals(templateAbsPath)) {
+        cmbTemplate.setSelectedIndex(i);
+        break;
+      }
+    }
   }
 
-  File getUserTemplateFile() {
-    int i = cmbUserTemplate.getSelectedIndex();
-    if (i != -1) {
-      return userTemplateFileList[i];
-    }
-    return null;
-  }
-
-  DirectiveFile getScopeTemplate() {
-    int i = cmbScopeTemplate.getSelectedIndex();
-    if (i != -1) {
-      return DirectiveFile.getInstance(manager, axisID, scopeTemplateFileList[i]);
-    }
-    return null;
-  }
-  
-  DirectiveFile getSystemTemplate() {
-    int i = cmbSystemTemplate.getSelectedIndex();
-    if (i != -1) {
-      return DirectiveFile.getInstance(manager, axisID, systemTemplateFileList[i]);
-    }
-    return null;
-  }
-  DirectiveFile getUserTemplate() {
-    int i = cmbUserTemplate.getSelectedIndex();
-    if (i != -1) {
-      return DirectiveFile.getInstance(manager, axisID, userTemplateFileList[i]);
-    }
-    return null;
-  }
-  
   boolean equalsActionCommand(final String actionCommand) {
     return actionCommand.equals(cmbScopeTemplate.getActionCommand())
         || actionCommand.equals(cmbSystemTemplate.getActionCommand())
         || actionCommand.equals(cmbUserTemplate.getActionCommand());
+  }
+
+  void getParameters(final UserConfiguration userConfig) {
+    userConfig.setScopeTemplate(getScopeTemplateFile());
+    userConfig.setSystemTemplate(getSystemTemplateFile());
+    userConfig.setUserTemplate(getUserTemplateFile());
+  }
+
+  DirectiveFile getScopeTemplate() {
+    File templateFile = getScopeTemplateFile();
+    if (templateFile != null) {
+      return DirectiveFile.getInstance(manager, axisID, templateFile);
+    }
+    return null;
+  }
+
+ private File getScopeTemplateFile() {
+    return getTemplateFile(cmbScopeTemplate, scopeTemplateFileList);
+  }
+
+  DirectiveFile getSystemTemplate() {
+    File templateFile = getSystemTemplateFile();
+    if (templateFile != null) {
+      return DirectiveFile.getInstance(manager, axisID, templateFile);
+    }
+    return null;
+  }
+
+ private File getSystemTemplateFile() {
+    return getTemplateFile(cmbSystemTemplate, systemTemplateFileList);
+  }
+
+  DirectiveFile getUserTemplate() {
+    File templateFile = getUserTemplateFile();
+    if (templateFile != null) {
+      return DirectiveFile.getInstance(manager, axisID, templateFile);
+    }
+    return null;
+  }
+
+ private File getUserTemplateFile() {
+    return getTemplateFile(cmbUserTemplate, userTemplateFileList);
+  }
+
+  boolean isAppearanceSettingChanged(final UserConfiguration userConfig) {
+    if (!userConfig.equalsScopeTemplate(getScopeTemplateFile())
+        || !userConfig.equalsSystemTemplate(getSystemTemplateFile())
+        || !userConfig.equalsUserTemplate(getUserTemplateFile())) {
+      return true;
+    }
+    return false;
+  }
+
+  void setParameters(final UserConfiguration userConfig) {
+    if (userConfig.isScopeTemplateSet()) {
+      setTemplate(userConfig.getScopeTemplate(), scopeTemplateFileList, cmbScopeTemplate);
+    }
+    if (userConfig.isSystemTemplateSet()) {
+      setTemplate(userConfig.getSystemTemplate(), systemTemplateFileList,
+          cmbSystemTemplate);
+    }
+    if (userConfig.isUserTemplateSet()) {
+      setTemplate(userConfig.getUserTemplate(), userTemplateFileList, cmbUserTemplate);
+    }
   }
 }
