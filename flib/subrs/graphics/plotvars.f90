a7d3bf593046ec6e606872592ec5b1eafce0689d
mastcu <mast@colorado.edu>
2013-03-14 17:55:17 -0600

    increase LIM_KEYS to 10, they fit

--- flib/subrs/graphics/plotvars.f90
+++ flib/subrs/graphics/plotvars.f90
@@ -1,6 +1,6 @@
 module plotvars
   integer LIM_KEYS, LIM_COLORS
-  parameter (LIM_KEYS = 8, LIM_COLORS = 40)
+  parameter (LIM_KEYS = 10, LIM_COLORS = 40)
   character*80 keys(LIM_KEYS), xaxisLabel/' '/
   integer*4 igenPltType/0/, ifNoTerm/0/, ifConnect/0/, numKeys/0/, numColors/0/
   integer*4 icolors(4, LIM_COLORS)
