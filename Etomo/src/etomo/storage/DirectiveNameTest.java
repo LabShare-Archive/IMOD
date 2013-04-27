6cb700b08a298de54f3685e6ea3d1dc7603c0dbe
SueHeld <sueh@colorado.edu>
2013-04-26 20:35:33 -0600

    Bug# 1685 Replaced DirectiveName.set with setKey.

--- Etomo/src/etomo/storage/DirectiveNameTest.java
+++ Etomo/src/etomo/storage/DirectiveNameTest.java
@@ -64,98 +64,94 @@ public class DirectiveNameTest extends TestCase {
     assertFalse("returns false when nothing is passed", directiveName.equals(null));
     assertFalse("returns false when no value",
         directiveName.equals(DirectiveType.COM_PARAM));
-    directiveName.set((String) null);
+    directiveName.setKey((String) null);
     assertFalse("returns false when nothing is passed", directiveName.equals(null));
     assertFalse("returns false when no value",
         directiveName.equals(DirectiveType.COM_PARAM));
 
-    directiveName.set(SETUP_SET_COPY_ARG_NAME);
+    directiveName.setKey(SETUP_SET_COPY_ARG_NAME);
     assertFalse("returns false when no type", directiveName.equals(null));
-    directiveName.set(SETUP_SET_COPY_ARG_NAME);
+    directiveName.setKey(SETUP_SET_COPY_ARG_NAME);
     assertTrue("recognizes setupset", directiveName.equals(DirectiveType.SETUP_SET));
-    directiveName.set(SETUP_SET_NAME);
+    directiveName.setKey(SETUP_SET_NAME);
     assertTrue("recognizes setupset", directiveName.equals(DirectiveType.SETUP_SET));
-    directiveName.set(RUNTIME_NAME);
+    directiveName.setKey(RUNTIME_NAME);
     assertTrue("recognizes runtime", directiveName.equals(DirectiveType.RUNTIME));
-    directiveName.set(COM_PARAM_NAME);
+    directiveName.setKey(COM_PARAM_NAME);
     assertTrue("recognizes comparam", directiveName.equals(DirectiveType.COM_PARAM));
   }
 
   public void testGetComFile() {
     assertNull("returns null with empty data", directiveName.getComFileName());
-    directiveName.set((String) null);
+    directiveName.setKey((String) null);
     assertNull("returns null with empty data", directiveName.getComFileName());
-    directiveName.set(RUNTIME_NAME);
+    directiveName.setKey(RUNTIME_NAME);
     assertNull("returns null when not a comparam", directiveName.getComFileName());
-    directiveName.set("comparam");
+    directiveName.setKey("comparam");
     assertNull("returns null when name doesn't contain a com file name",
         directiveName.getComFileName());
 
-    directiveName.set(COM_PARAM_NAME);
+    directiveName.setKey(COM_PARAM_NAME);
     assertEquals("returns com file name", "eraser", directiveName.getComFileName());
-    directiveName.set(COM_PARAM_A_NAME);
+    directiveName.setKey(COM_PARAM_A_NAME);
     assertEquals("strips the axis letter", "eraser", directiveName.getComFileName());
-    directiveName.set(COM_PARAM_B_NAME);
+    directiveName.setKey(COM_PARAM_B_NAME);
     assertEquals("strips the axis letter", "eraser", directiveName.getComFileName());
   }
   
   public void testGetProgramName() {
     assertNull("returns null with empty data", directiveName.getProgramName());
-    directiveName.set((String) null);
+    directiveName.setKey((String) null);
     assertNull("returns null with empty data", directiveName.getProgramName());
-    directiveName.set(RUNTIME_NAME);
+    directiveName.setKey(RUNTIME_NAME);
     assertNull("returns null when not a comparam", directiveName.getProgramName());
-    directiveName.set("comparam.eraser");
+    directiveName.setKey("comparam.eraser");
     assertNull("returns null when name doesn't contain a com file name",
         directiveName.getProgramName());
 
-    directiveName.set(COM_PARAM_NAME);
+    directiveName.setKey(COM_PARAM_NAME);
     assertEquals("returns com file name", "ccderaser", directiveName.getProgramName());
-    directiveName.set(COM_PARAM_A_NAME);
+    directiveName.setKey(COM_PARAM_A_NAME);
     assertEquals("strips the axis letter", "ccderaser", directiveName.getProgramName());
-    directiveName.set(COM_PARAM_B_NAME);
+    directiveName.setKey(COM_PARAM_B_NAME);
     assertEquals("strips the axis letter", "ccderaser", directiveName.getProgramName());
   }
 
   public void testRemoveAxisID() {
     // Setting a null string does not cause an exception
-    directiveName.set((String) null);
+    directiveName.setKey((String) null);
     // Setting an empty string does not cause an exception
-    directiveName.set("");
+    directiveName.setKey("");
 
     String name = "setupset.erasera.ccderaser.PeakCriterion";
-    directiveName.set(name);
-    assertEquals("setupset names are never changed", directiveName.get(), name);
+    directiveName.setKey(name);
+    assertEquals("setupset names are never changed", directiveName.getKey(), name);
     name = "setupset.Preprocessing.b.removeXrays";
-    directiveName.set(name);
-    assertEquals("setupset names are never changed", directiveName.get(), name);
+    directiveName.setKey(name);
+    assertEquals("setupset names are never changed", directiveName.getKey(), name);
 
-    directiveName.set(RUNTIME_NAME);
-    assertEquals("runtime name without an axis is unchanged", directiveName.get(),
+    directiveName.setKey(RUNTIME_NAME);
+    assertEquals("runtime name without an axis is unchanged", directiveName.getKey(),
         RUNTIME_NAME);
     name = "runtime.a.Preprocessing.removeXrays";
-    directiveName.set(name);
-    assertEquals("only an axis letter in correct place is removed", directiveName.get(),
+    directiveName.setKey(name);
+    assertEquals("only an axis letter in correct place is removed", directiveName.getKey(),
         name);
-    directiveName.set(RUNTIME_A_NAME);
-    assertEquals("axis a is changed to 'any'", directiveName.get(), RUNTIME_NAME);
-    directiveName.set(RUNTIME_B_NAME);
-    assertEquals("axis b is changed to 'any'", directiveName.get(), RUNTIME_NAME);
+    directiveName.setKey(RUNTIME_A_NAME);
+    assertEquals("axis a is changed to 'any'", directiveName.getKey(), RUNTIME_NAME);
+    directiveName.setKey(RUNTIME_B_NAME);
+    assertEquals("axis b is changed to 'any'", directiveName.getKey(), RUNTIME_NAME);
 
-    directiveName.set(COM_PARAM_NAME);
-    assertEquals("comparam name without an axis is unchanged", directiveName.get(),
+    directiveName.setKey(COM_PARAM_NAME);
+    assertEquals("comparam name without an axis is unchanged", directiveName.getKey(),
         COM_PARAM_NAME);
     name = "comparam.eraser.ccderaserb.PeakCriterion";
-    directiveName.set(name);
-    assertEquals("only an axis letter in correct place is removed", directiveName.get(),
+    directiveName.setKey(name);
+    assertEquals("only an axis letter in correct place is removed", directiveName.getKey(),
         name);
-    directiveName.set(COM_PARAM_A_NAME);
-    assertEquals("axis a is removed", directiveName.get(), COM_PARAM_NAME);
-    directiveName.set(COM_PARAM_B_NAME);
-    assertEquals("axis b is removed", directiveName.get(), COM_PARAM_NAME);
-
-    assertEquals("axis b is changed to 'any'",
-        DirectiveName.removeAxisID(RUNTIME_B_NAME), RUNTIME_NAME);
-    assertEquals("axis a is removed", COM_PARAM_NAME,DirectiveName.removeAxisID(COM_PARAM_A_NAME));
+    directiveName.setKey(COM_PARAM_A_NAME);
+    assertEquals("axis a is removed", directiveName.getKey(), COM_PARAM_NAME);
+    directiveName.setKey(COM_PARAM_B_NAME);
+    assertEquals("axis b is removed", directiveName.getKey(), COM_PARAM_NAME);
   }
 }
