e18b605617ac0bae82efccbe314efbbd1a22824b
SueHeld <sueh@colorado.edu>
2013-04-27 08:24:46 -0600

    Bug# 1685 Fixed error in getInstance.  Added getColumns.

--- Etomo/src/etomo/storage/DirectiveValueType.java
+++ Etomo/src/etomo/storage/DirectiveValueType.java
@@ -1,4 +1,7 @@
 package etomo.storage;
+
+import etomo.ui.FieldType;
+
 /**
 * <p>Description: </p>
 * 
@@ -14,21 +17,26 @@ package etomo.storage;
 * 
 * <p> $Log$ </p>
 */
- final class DirectiveValueType {
-  public static  final String  rcsid =  "$Id:$";
-  
-  static final DirectiveValueType BOOLEAN = new DirectiveValueType("Bool");
-  static final DirectiveValueType FLOATING_POINT = new DirectiveValueType("Float");
-  static final DirectiveValueType FLOATING_POINT_PAIR = new DirectiveValueType("2 Float");
-  static final DirectiveValueType INTEGER = new DirectiveValueType("Int");
-  static final DirectiveValueType INTEGER_PAIR = new DirectiveValueType("2 Int");
-  static final DirectiveValueType LIST = new DirectiveValueType("List");
-  static final DirectiveValueType STRING = new DirectiveValueType("String");
+public final class DirectiveValueType {
+  public static final String rcsid = "$Id:$";
+
+  public static final DirectiveValueType BOOLEAN = new DirectiveValueType("Bool", 3);
+  public static final DirectiveValueType FLOATING_POINT = new DirectiveValueType("Float",
+      5);
+  public static final DirectiveValueType FLOATING_POINT_PAIR = new DirectiveValueType(
+      "2 Float", 10);
+  public static final DirectiveValueType INTEGER = new DirectiveValueType("Int", 5);
+  public static final DirectiveValueType INTEGER_PAIR = new DirectiveValueType("2 Int",
+      10);
+  public static final DirectiveValueType LIST = new DirectiveValueType("List", 15);
+  public static final DirectiveValueType STRING = new DirectiveValueType("String", 15);
 
   private final String tag;
+  private final int columns;
 
-  private DirectiveValueType(final String tag) {
+  private DirectiveValueType(final String tag, final int columns) {
     this.tag = tag;
+    this.columns = columns;
   }
 
   static DirectiveValueType getInstance(final String input) {
@@ -41,7 +49,7 @@ package etomo.storage;
     if (input.equals(FLOATING_POINT.tag)) {
       return FLOATING_POINT;
     }
-    if (input.equals(BOOLEAN.tag)) {
+    if (input.equals(FLOATING_POINT_PAIR.tag)) {
       return FLOATING_POINT_PAIR;
     }
     if (input.equals(INTEGER.tag)) {
@@ -57,5 +65,38 @@ package etomo.storage;
       return STRING;
     }
     return null;
+  }
+
+  public int getColumns() {
+    return columns;
+  }
+
+  public FieldType toFieldType() {
+    if (this == BOOLEAN) {
+      return null;
+    }
+    if (this == FLOATING_POINT) {
+      return FieldType.FLOATING_POINT;
+    }
+    if (this == FLOATING_POINT_PAIR) {
+      return FieldType.FLOATING_POINT_PAIR;
+    }
+    if (this == INTEGER) {
+      return FieldType.INTEGER;
+    }
+    if (this == INTEGER_PAIR) {
+      return FieldType.INTEGER_PAIR;
+    }
+    if (this == LIST) {
+      return FieldType.INTEGER_LIST;
+    }
+    if (this == STRING) {
+      return FieldType.STRING;
+    }
+    return null;
+  }
+
+  public String toString() {
+    return tag;
   }
 }
