20aa5764fb62b2eadaed7925253b3f03f108f4f8
mastcu <mast@colorado.edu>
2013-06-09 17:52:18 -0600

    Changed robustRegress to handle problems with actual use by xftoxg, added weighted polynomial fit when I thought that I needed it, but didn't test it.

--- libcfshr/regression.c
+++ libcfshr/regression.c
@@ -22,11 +22,13 @@
 #define multregress MULTREGRESS
 #define robustregress ROBUSTREGRESS
 #define polynomialfit POLYNOMIALFIT
+#define weightedpolyfit WEIGHTEDPOLYFIT
 #else
 #define statmatrices statmatrices_
 #define multregress multregress_
 #define robustregress robustregress_
 #define polynomialfit polynomialfit_
+#define weightedpolyfit weightedpolyfit_
 #endif
 
 /*!
@@ -51,8 +53,8 @@
  * [sd]      - Array for the standard deviations of the [m] data columns  ^
  * The output matrices will be treated as having leading dimension [msize], so they must 
  * be at least [msize] x [m] in size.  The data element at row i, column j would
- * be accessed as x\[j + i * xsize\] or x\[i\]\[j\] from C, or at x(j,i) from Fortran if
- * [colFast] is 0, or as x\[i + j * xsize\], x\[j\]\[i\], or x(i,j) if [colFast] is 
+ * be accessed as x\[i + j * xsize\] or x\[j\]\[i\] from C, or as x(i,j) from Fortran if
+ * [colFast] is 0, or as x\[j + i * xsize\], x\[i\]\[j\], or x(j,i) if [colFast] is 
  * nonzero.  When  weighting is used, the returned values are weighted means, and other 
  * statistics with the weights incorporated.
  */
@@ -155,12 +157,15 @@ void statmatrices(float *x, int *xsize, int *colFast, int *m, int *msize, int *n
  * (m + nbcol) * (m + nbcol)  floating point elements  ^
  * Outputs:  ^
  * [b]       - Matrix to receive the [nbcol] sets of [m] coefficients.  Each set is 
- * placed in a column of [b], where the column dimension is the fastest progressing one ^
+ * placed in a column of [b], where the row dimension is the fastest progressing one ^
  * [c]       - Array to receive the [m] constant terms of the fits  ^
  * [xm]      - Array for the means of the [m] plus [nbcol] data columns  ^
  * [sd]      - Array for the standard deviations of the [m] plus [nbcol] data columns  ^
  * The independent variables should be placed in the first [m] columns of [x] and the
- * the dependent variables in the next [nbcol] columns.
+ * the dependent variables in the next [nbcol] columns. The data element at row i,
+ * column j would be accessed as x\[i + j * xsize\] or x\[j\]\[i\] from C, or as x(i,j) 
+ * from Fortran if [colFast] is 0, or as x\[j + i * xsize\], x\[i\]\[j\], or x(j,i) if 
+ * [colFast] is nonzero.  ^
  * The return value is 1 if [wgtcol] has an inappropriate value, or 3 if @gaussj returns
  * with an error.
  */
@@ -229,7 +234,7 @@ int multRegress(float *x, int xsize, int colFast, int m, int ndata, int nbcol,
         /* printf("sds %d %d %f %f  den %f\n", i, j, sd[i], sd[j], den);
            fflush(stdout); */
         work[j * mp + i] = 1.;
-    } else
+      } else
         work[j * mp + i] /= den * (fndata-1.);
       if (j < m)
         work[i * mp + j] = work[j * mp + i];
@@ -243,21 +248,24 @@ int multRegress(float *x, int xsize, int colFast, int m, int ndata, int nbcol,
   
   /* The matrix to be solved is now completely filled and symmetric so row/column
      doesn't matter, but to call gaussj we need to transpose the final columns into b */
-  for (j = 0; j < nbcol; j++)
+  for (j = 0; j < nbcol; j++) {
     for (i = 0; i < m; i++)
       b[j + i * nbcol] = work[(j + m) * mp + i];
-  /*printf("\nb:");
-  for (i = 0; i < m; i++)
-    printf("    %g", b[i]);
-    printf("\nb sol:"); */
+    /*printf("\nb:");
+    for (i = 0; i < m; i++)
+    printf("    %g", b[j + i * nbcol]); */
+  }
 
   if (gaussj(work, m, mp, b, nbcol, nbcol))
     return 3;
   
-  /*for (i = 0; i < m; i++)
-    printf("    %g", b[i]);
+  /*for (j = 0; j < nbcol; j++) {
+    printf("\nb sol:");
+    for (i = 0; i < m; i++)
+      printf("    %g", b[j + i * nbcol]);
+  }
   printf("\n");
-  fflush(stdout); */
+  fflush(stdout);*/
 
   /* Scale the coefficients and transpose them back; get constant terms */
   memcpy(work, b, m * nbcol * sizeof(float));
@@ -274,6 +282,7 @@ int multRegress(float *x, int xsize, int colFast, int m, int ndata, int nbcol,
   return 0;
 }
 
+/* Fortran wrapper */
 int multregress(float *x, int *xsize, int *colFast, int *m, int *ndata, int *nbcol,
                 int *wgtcol, float *b, int *bsize, float *c, float *xm, float *sd,
                 float *work)
@@ -309,7 +318,9 @@ Also, a bigger work array is needed not just for doubles but also for dsysv.
  * constant term in [intcpt].  The equation fit is:  ^
  * Y = intcpt + slopes[0] * X + slopes[1] * X**2 + ...  ^
  * [work] is an array whose size must be at least ([order] + 1) * ([order] + 3 + [ndata]).
- * The return value is the value returned by @@multRegress@.
+ * The return value is the value returned by @@multRegress@.  Note that a Fortran 
+ * function polyfit in libhvem takes care of allocating [work] to the needed size and 
+ * calling the Fortran wrapper to this function.
  */
 int polynomialFit(float *x, float *y, int ndata, int order, float *slopes, float *intcpt,
                   float *work)
@@ -330,14 +341,48 @@ int polynomialFit(float *x, float *y, int ndata, int order, float *slopes, float
                       mwork));
 }
 
-/*
- * Fortran wrapper to @@polynomialFit@.  Note that a Fortran function polyfit takes care
- * of allocating [work] to the needed size and calling this wrapper.
- */
+/* Fortran wrapper */
 int polynomialfit(float *x, float *y, int *ndata, int *order, float *slopes, 
                   float *intcpt, float *work)
 {
   return polynomialFit(x, y, *ndata, *order, slopes, intcpt, work);
+}
+
+/*!
+ * Uses multiple linear regression to fit a polynomial of order
+ * [order] to [ndata] points whose (x,y) coordinates are in the arrays [x] and [y].
+ * It returns the coefficient of x to the i power in the array [slopes] and a
+ * constant term in [intcpt].  The equation fit is:  ^
+ * Y = intcpt + slopes[0] * X + slopes[1] * X**2 + ...  ^
+ * [work] is an array whose size must be at least ([order] + 2) * [ndata] +
+ * ([order] + 1) * ([order] + 3)).  ^
+ * The return value is the value returned by @@multRegress@.  This function is untested.
+ */
+int weightedPolyFit(float *x, float *y, float *weight, int ndata, int order,
+                    float *slopes, float *intcpt, float *work)
+{
+  int wdim = order + 1;
+  int i, j;
+  float *xm = work + (order + 2) * ndata;
+  float *sd = xm + wdim;
+  float *mwork = sd + wdim;
+  if (!order)
+    return 1;
+  for (i = 0; i < ndata; i++) {
+    for (j = 0; j < order; j++)
+      work[i + j * ndata] = (float)pow((double)x[i], j + 1.);
+    work[i + order * ndata] = y[i];
+    work[i + (order + 1) * ndata] = weight[i];
+  }
+  return (multRegress(work, ndata, 0, order, ndata, 1, order + 1, slopes, ndata, intcpt,
+                      xm, sd, mwork));
+}
+
+/* Fortran wrapper */
+int weightedpolyfit(float *x, float *y, float *weight, int *ndata, int *order,
+                    float *slopes, float *intcpt, float *work)
+{
+  return weightedPolyFit(x, y, weight, *ndata, *order, slopes, intcpt, work);
 }
 
 /*!
@@ -369,43 +414,60 @@ int polynomialfit(float *x, float *y, int *ndata, int *order, float *slopes,
  * output on each iteration.  20-50 is probably adequate.  With a negative value, the
  * program outputs the mean and maximum change in weighting and the number of points
  * with weights of 0, between 0 and 0.1, between 0.1 and 0.2, and less than 0.5.   ^
- * [maxChange] - Maximum change in weights from one iteration to the next; the iterations
- * are terminated when the biggest change in weights is less than this value.  The entry
- * should not be smaller than 0.01. ^
+ * [maxZeroWgt] - Maximum number of points allowed to have zero weights.  When this number
+ * is exceeded on an iteration, the deviations are sorted and the criterion deviation is 
+ * permanently changed to midway 
+ * between the deviations for the last point to be retained and first one to be 
+ * eliminated.  ^
+ * [maxChange]  - Maximum change in weights from one iteration to the next or across two 
+ * iterations; the entry should not be smaller than 0.01.  ^
+ * [maxOscill]  - Maximum change in weights from one iteration to the next, even if 
+ * oscillating. The iterations are terminated when the biggest change in weights between 
+ * iterations is less than [maxChange], or when it is less than [maxOscill] and the 
+ * biggest change across two iterations is less than [maxChange].
  * Outputs:  ^
  * [b]       - Matrix to receive the [nbcol] sets of [m] coefficients.  Each set is 
- * placed in a column of [b], where the column dimension is the fastest progressing one ^
+ * placed in a column of [b], where the row dimension is the fastest progressing one ^
  * [c]       - Array to receive the [m] constant terms of the fits  ^
  * [xm]      - Array for the means of the [m] plus [nbcol] data columns  ^
  * [sd]      - Array for the standard deviations of the [m] plus [nbcol] data columns  ^
- * [numIter] - Number of iterations
+ * [numIter] - Number of iterations  ^
  * The independent variables should be placed in the first [m] columns of [x] and the
- * the dependent variables in the next [nbcol] columns.
- * Final weights are returned in the column of [x] after the dependent variables, so [x]
- * must have at least [m] + [nbcol] + 1 columns.
+ * the dependent variables in the next [nbcol] columns (see @@multRegress@).
+ * Final weights are returned in the column of [x] after the dependent variables, and the
+ * column after that is used for weights on the previous iteration, so [x]
+ * must have at least [m] + [nbcol] + 2 columns.  ^
+ * The return value is 1 if there are not enough columns for the weights (detectable 
+ * only when [colFast] is nonzero), 3 if @gaussj returns with an error, or -1 if the
+ * weights do not converge.
  */
 int robustRegress(float *x, int xsize, int colFast, int m, int ndata, int nbcol, float *b,
                   int bsize, float *c, float *xm, float *sd, float *work, float kfactor,
-                  int *numIter, int maxIter, float maxChange)
+                  int *numIter, int maxIter, int maxZeroWgt, float maxChange,
+                  float maxOscill)
 {
   int i, j, numOut, ierr, k, iter, num1, num2, num5, report = 0;
   int wgtcol = m + nbcol;
+  int prevCol = wgtcol + 1;
   int colStride = colFast ? 1 : xsize;
   int rowStride = colFast ? xsize : 1;
+  int splitLastTime = 0;
   float diff, diffsum, diffmax, median, MADN, dev, weight, ressum, colres;
+  float prev, prevmax, criterion;
   if (maxIter < 0) {
     report = 1;
     maxIter = - maxIter;
   }
 
-  if (!colFast && wgtcol >= xsize)
+  if (colFast && prevCol >= xsize)
     return 1;
 
   /* Initialize weights to 1. */
   for (j = 0; j < ndata; j++)
-    XRC(j, wgtcol) = 1.;
+    XRC(j, wgtcol) = XRC(j, prevCol) = 1.;
 
   /* Iterate */
+  criterion = kfactor * MADN;
   for (iter = 0; iter < maxIter; iter++) {
 
     /* Get regression solution */
@@ -427,15 +489,18 @@ int robustRegress(float *x, int xsize, int colFast, int m, int ndata, int nbcol,
           colres = c[k] - XRC(j, (m + k));
           for (i = 0; i < m; i++)
             colres += XRC(j, i) * b[i + k * bsize];
+          /* printf("   %g", colres); */
           ressum += colres * colres;
         }
         work[j] = (float)sqrt(ressum);
+        /* printf("   resid  %g\n", work[j]); */
       }
     }
 
     /* Get the median and MADN */
     rsMedian(work, ndata, &work[ndata], &median);
     rsMADN(work, ndata, median, &work[ndata], &MADN);
+    /* printf("Median %g   MADN %g\n", median, MADN); */
 
     /* Get the new weights and evaluate change from last time */
     diffsum = 0.;
@@ -444,15 +509,46 @@ int robustRegress(float *x, int xsize, int colFast, int m, int ndata, int nbcol,
     num1 = 0;
     num2 = 0;
     num5 = 0;
+    prevmax = 0.;
+    
+    /* Convert to the deviations that will be compared to criterion and count zero's */
     for (j = 0; j < ndata; j++) {
-      dev = (work[j] - median) / (kfactor * MADN);
+      work[j] -= median;
+
+      /* For multiple columns, negative deviations are like no deviation */
       if (nbcol > 1)
-        dev = B3DMAX(0., dev);
-      if (dev >= 1.) {
+        work[j] = B3DMAX(0., work[j]);
+      else if (work[j] < 0)
+        work[j] = -work[j];
+      if (work[j] > criterion)
+        numOut++;
+    }
+
+    /* If there are too many out, need to adjust criterion to limit that number */
+    if (numOut > maxZeroWgt) {
+      for (j = 0; j < ndata; j++)
+        work[ndata + j] = work[j];
+      rsSortFloats(&work[ndata], ndata);
+      diff = criterion;
+      criterion = (work[2 * ndata - maxZeroWgt] + work[2 * ndata - maxZeroWgt - 1]) / 2.;
+      if (report)
+        printf("%d with zero weight, revising criterion from %g to %g\n", numOut,
+               diff, criterion);
+    }
+
+    numOut = 0;
+    for (j = 0; j < ndata; j++) {
+
+      /* Tests to handle case of MADN zero; avoid division by 0 */
+      if (work[j] > criterion) {
         weight = 0.;
         numOut++;
-      } else
+      } else if (work[j] <= 1.e-6 * criterion) {
+        weight = 1.;
+      } else {
+        dev = work[j] / criterion;
         weight = (1 - dev * dev) * (1 - dev * dev);
+      }
       if (report) {
         if (weight > 0 && weight <= 0.1)
           num1++;
@@ -461,27 +557,55 @@ int robustRegress(float *x, int xsize, int colFast, int m, int ndata, int nbcol,
         if (weight < 0.5)
           num5++;
       }
+
+      /* Get differences from last time and from time before that */
       diff = fabs(weight - XRC(j, wgtcol));
       diffsum += diff;
       diffmax = B3DMAX(diffmax, diff);
+      prev = fabs(weight - XRC(j, prevCol));
+      prevmax = B3DMAX(prevmax, prev);
+      XRC(j, prevCol) = XRC(j, wgtcol);
       XRC(j, wgtcol) = weight;
     }
-    if (report)
-      printf("Iter %d  change mean %f  max %f   # 0, 0.1, 0.2, <0.5: %d %d %d %d\n", iter,
-             diffsum / ndata, diffmax, numOut, num1, num2, num5);
-    if (diffmax < maxChange)
+    if (report) {
+      printf("Iter %3d del mean %.4f max %.4f prev %.4f # 0, 0.1, 0.2, <0.5: %d %d %d "
+             "%d\n", iter, diffsum / ndata, diffmax, prevmax, numOut, num1, num2, num5);
+      fflush(stdout);
+    }
+
+    /* Quit if difference from last time is below limit, or if it is below the oscillation
+       limit and difference from the previous time is below limit */
+    if (!splitLastTime && (diffmax < maxChange || 
+                           (diffmax < maxOscill && prevmax < maxChange)))
       break;
+
+    /* But if we are oscillating, try half-way between and defer testing for 2 rounds */
+    if (!splitLastTime && prevmax < maxChange / 2.) {
+      splitLastTime = 1;
+      if (report) {
+        printf("        Oscillation detected: averaging previous weights\n");
+        fflush(stdout);
+      }
+      for (j = 0; j < ndata; j++)
+        XRC(j, wgtcol) = (XRC(j, wgtcol) + XRC(j, prevCol)) / 2.;
+    } else if (splitLastTime == 1)
+      splitLastTime = 2;
+    else
+      splitLastTime = 0;
   }
+
   *numIter = iter;
   return (*numIter < maxIter ? 0 : -1);
 }
 
+/* Fortran wrapper */
 int robustregress(float *x, int *xsize, int *colFast, int *m, int *ndata, int *nbcol, 
                   float *b, int *bsize, float *c, float *xm, float *sd, float *work,
-                  float *kfactor, int *numIter, int *maxIter, float *maxChange)
+                  float *kfactor, int *numIter, int *maxIter, int *maxZeroWgt, 
+                  float *maxChange, float *maxOscillate)
 {
   return robustRegress(x, *xsize, *colFast, *m, *ndata, *nbcol, b,
                        *bsize, c, xm, sd, work, *kfactor, numIter, 
-                       *maxIter, *maxChange);
+                       *maxIter, *maxZeroWgt, *maxChange, *maxOscillate);
 }
 
