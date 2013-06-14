f66e12ce139b88422a02a24576f66e168a57873e
SueHeld <sueh@colorado.edu>
2013-06-13 23:07:34 -0600

    Bug# 1486 Replaces UIComponent in ui.swing.

---
+++ Etomo/src/etomo/ui/swing/SwingComponent.java
@@ -0,0 +1,24 @@
+package etomo.ui.swing;
+
+import java.awt.Component;
+
+/**
+* <p>Description: </p>
+* 
+* <p>Copyright: Copyright 2013</p>
+*
+* <p>Organization:
+* Boulder Laboratory for 3-Dimensional Electron Microscopy of Cells (BL3DEMC),
+* University of Colorado</p>
+* 
+* @author $Author$
+* 
+* @version $Revision$
+* 
+* <p> $Log$ </p>
+*/
+public interface SwingComponent {
+  public static final String rcsid = "$Id:$";
+
+  public Component getComponent();
+}
