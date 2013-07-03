efc036e777d9639f05cfd8a3c667fc92992bba82
SueHeld <sueh@colorado.edu>
2013-07-03 17:47:42 -0600

    BUg# 1671 In executeField increated wait for panel.

--- Etomo/src/etomo/uitest/AutodocTester.java
+++ Etomo/src/etomo/uitest/AutodocTester.java
@@ -2126,7 +2126,7 @@ final class AutodocTester extends Assert implements VariableList {
     else if (fieldType == UITestFieldType.PANEL) {
       assertNull("value not valid in a panel command (" + command + ")", value);
       try {
-        Thread.sleep(408);
+        Thread.sleep(410);
       }
       catch (InterruptedException e) {
       }
