1a4b72e6e61141c0a5ca7351d5e63460c6d7d1f1
mastcu <mast@colorado.edu>
2013-07-16 13:00:48 -0600

    Added empty functions so def file would be stable

--- libfft/fftw_wrap.c
+++ libfft/fftw_wrap.c
@@ -179,3 +179,33 @@ void todfftc(float *array, int nx, int ny, int idir)
 {
   todfft(array, &nx, &ny, &idir);
 }
+
+void thrdfftc(float *array, float *brray, int nx, int ny, int nz, int idir)
+{
+  thrdfft(array, brray, &nx, &ny, &nz, &idir);
+}
+
+/* These have to be here to have a Windows def file that doesn't depend on whether
+   FFT is available */
+void srfp (int pts, int pmax, int twogrp, int *factor, int *sym, int *psym,
+	   int *unsym, int *error){}
+void realft (float *even, float *odd, int n, int *dim){}
+void cmplft (float *x, float *y, int n, int *d){}
+void hermft(float *x, float *y, int n, int *dim){}
+void diprp (int pts, int *sym, int psym, int *unsym, int *dim,
+	    float *x, float *y){}
+void mdftkd (int n, int *factor, int *dim, float *x, float *y){}
+void r2cftk (int n, int m, float *x0, float *y0, float *x1, float *y1,
+	     int *dim){}
+void r3cftk (int n, int m, float *x0, float *y0, float *x1, float *y1, 
+	     float *x2, float *y2, int *dim){}
+void r4cftk (int n, int m, float *x0, float *y0, float *x1, float *y1, 
+	     float *x2, float *y2, float *x3, float *y3, int *dim){}
+void r5cftk (int n, int m, float *x0, float *y0, float *x1, float *y1,
+	     float *x2, float *y2, float *x3, float *y3, float *x4, float *y4,
+	     int *dim){}
+void r8cftk (int n, int m, float *x0, float *y0, float *x1, float *y1,
+	     float *x2, float *y2, float *x3, float *y3, float *x4,
+	     float *y4, float *x5, float *y5, float *x6, float *y6,
+	     float *x7, float *y7, int *dim){}
+void rpcftk (int n, int m, int p, int r, float *x, float *y, int *dim){}
