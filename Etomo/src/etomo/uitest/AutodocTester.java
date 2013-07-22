4eb9dfa380f72a5761c422c15ff1b3283a69578e
SueHeld <sueh@colorado.edu>
2013-07-22 16:54:37 -0600

    Bug# 1724 Increased wait for panel.

--- Etomo/src/etomo/uitest/AutodocTester.java
+++ Etomo/src/etomo/uitest/AutodocTester.java
@@ -2126,7 +2126,7 @@ final class AutodocTester extends Assert implements VariableList {
     else if (fieldType == UITestFieldType.PANEL) {
       assertNull("value not valid in a panel command (" + command + ")", value);
       try {
-        Thread.sleep(508);
+        Thread.sleep(509);
       }
       catch (InterruptedException e) {
       }
