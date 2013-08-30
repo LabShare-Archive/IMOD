beb9336feb9a6e0a446bdf411f6cfdea662b1dc8
SueHeld <sueh@colorado.edu>
2013-08-30 14:15:43 -0600

    Bug# 1731 Made the button group a member variable.  Added cbSearch.  Made the root panel a member variable and renamed it pnlRoot.  Added getComponent.  Added get...Instance static functions.  Change get/set to getTransform/setTransform.  Added updateDisplay.

--- Etomo/src/etomo/ui/swing/TransformChooserPanel.java
+++ Etomo/src/etomo/ui/swing/TransformChooserPanel.java
@@ -1,7 +1,8 @@
 package etomo.ui.swing;
 
 import java.awt.Component;
-import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -28,55 +29,126 @@ import etomo.type.Transform;
 */
 final class TransformChooserPanel {
   public static final String rcsid = "$Id:$";
-  
+
+  private static final String SEARCH_LABEL = "Search For:";
+
+  private final JPanel pnlRoot = new JPanel();
+  private final ButtonGroup bgTransform = new ButtonGroup();
   private final RadioButton rbFullLinearTransformation = new RadioButton(
-      "Full linear transformation");
+      "Full linear transformation", bgTransform);
   private final RadioButton rbRotationTranslationMagnification = new RadioButton(
-      "Rotation/translation/magnification");
+      "Rotation/translation/magnification", bgTransform);
   private final RadioButton rbRotationTranslation = new RadioButton(
-      "Rotation/translation");
-  private final RadioButton rbTranslation = new RadioButton("Translation");
+      "Rotation/translation", bgTransform);
 
-  private JPanel pnlTranslationChooser = null;
-  private boolean includeTranslation = false;
+  private final RadioButton rbTranslation;
+  private final CheckBox cbSearch;
 
-  Container getContainer() {
-    if (pnlTranslationChooser == null) {
-      pnlTranslationChooser = new JPanel();
-      pnlTranslationChooser.setLayout(new BoxLayout(pnlTranslationChooser,
-          BoxLayout.X_AXIS));
-      JPanel ipnlTranslationChooser = new JPanel();
-      pnlTranslationChooser.add(ipnlTranslationChooser);
-      pnlTranslationChooser.add(Box.createHorizontalGlue());
-      ipnlTranslationChooser.setLayout(new BoxLayout(ipnlTranslationChooser,
-          BoxLayout.Y_AXIS));
-      ipnlTranslationChooser.setAlignmentX(Component.CENTER_ALIGNMENT);
-      ipnlTranslationChooser.add(new JLabel("Search For:"));
-      ButtonGroup group = new ButtonGroup();
-      group.add(rbFullLinearTransformation.getAbstractButton());
-      group.add(rbRotationTranslationMagnification.getAbstractButton());
-      group.add(rbRotationTranslation.getAbstractButton());
-      ipnlTranslationChooser.add(rbFullLinearTransformation.getComponent());
-      ipnlTranslationChooser.add(rbRotationTranslationMagnification.getComponent());
-      ipnlTranslationChooser.add(rbRotationTranslation.getComponent());
-      // set default
-      set(null);
-      rbFullLinearTransformation
-          .setToolTipText("Use rotation, translation, magnification, and stretching to align images.");
-      rbRotationTranslationMagnification
-          .setToolTipText("Use translation, rotation, and magnification to align images.");
-      rbRotationTranslation
-          .setToolTipText("Use translation and rotation to align images.");
-      if (includeTranslation) {
-        group.add(rbTranslation.getAbstractButton());
-        ipnlTranslationChooser.add(rbTranslation.getComponent());
-        rbTranslation.setToolTipText("Use translation to align images.");
-      }
+  private TransformChooserPanel(final boolean allowTranslationsAlone,
+      final boolean optionalSearch) {
+    if (allowTranslationsAlone) {
+      rbTranslation = new RadioButton("Translation", bgTransform);
     }
-    return pnlTranslationChooser;
+    else {
+      rbTranslation = null;
+    }
+    if (optionalSearch) {
+      cbSearch = new CheckBox(SEARCH_LABEL);
+    }
+    else {
+      cbSearch = null;
+    }
   }
 
-  Transform get() {
+  static TransformChooserPanel getJoinModelInstance() {
+    TransformChooserPanel instance = new TransformChooserPanel(true, false);
+    instance.createPanel();
+    instance.setTooltips();
+    instance.addListeners();
+    return instance;
+  }
+
+  static TransformChooserPanel getJoinAlignInstance() {
+    TransformChooserPanel instance = new TransformChooserPanel(false, false);
+    instance.createPanel();
+    instance.setTooltips();
+    instance.addListeners();
+    return instance;
+  }
+
+  static TransformChooserPanel getSerialSectionsInstance() {
+    TransformChooserPanel instance = new TransformChooserPanel(false, true);
+    instance.createPanel();
+    instance.setTooltips();
+    instance.addListeners();
+    return instance;
+  }
+
+  private void createPanel() {
+    // init
+    setTransform(null);
+    // root panel
+    pnlRoot.setLayout(new BoxLayout(pnlRoot, BoxLayout.X_AXIS));
+    JPanel pnlChooser = new JPanel();
+    pnlRoot.add(pnlChooser);
+    pnlRoot.add(Box.createHorizontalGlue());
+    // choooser panel
+    pnlChooser.setLayout(new BoxLayout(pnlChooser, BoxLayout.Y_AXIS));
+    pnlChooser.setAlignmentX(Component.CENTER_ALIGNMENT);
+    if (cbSearch != null) {
+      pnlChooser.add(cbSearch);
+    }
+    else {
+      pnlChooser.add(new JLabel(SEARCH_LABEL));
+    }
+    pnlChooser.add(rbFullLinearTransformation.getComponent());
+    pnlChooser.add(rbRotationTranslationMagnification.getComponent());
+    pnlChooser.add(rbRotationTranslation.getComponent());
+    if (rbTranslation != null) {
+      pnlChooser.add(rbTranslation.getComponent());
+    }
+  }
+
+  private void addListeners() {
+    if (cbSearch != null) {
+      cbSearch.addActionListener(new TransformChooserListener(this));
+    }
+  }
+
+  public Component getComponent() {
+    return pnlRoot;
+  }
+
+  private void action() {
+    updateDisplay();
+  }
+
+  private void updateDisplay() {
+    boolean enable = cbSearch.isSelected();
+
+    rbFullLinearTransformation.setEnabled(enable);
+    rbRotationTranslationMagnification.setEnabled(enable);
+    rbRotationTranslation.setEnabled(enable);
+    if (rbTranslation != null) {
+      rbTranslation.setEnabled(enable);
+    }
+  }
+
+  private void setTooltips() {
+    rbFullLinearTransformation
+        .setToolTipText("Use rotation, translation, magnification, and stretching to align images.");
+    rbRotationTranslationMagnification
+        .setToolTipText("Use translation, rotation, and magnification to align images.");
+    rbRotationTranslation.setToolTipText("Use translation and rotation to align images.");
+    if (rbTranslation != null) {
+      rbTranslation.setToolTipText("Use translation to align images.");
+    }
+  }
+
+  Transform getTransform() {
+    if (cbSearch != null && !cbSearch.isSelected()) {
+      return Transform.SKIP_SEARCH;
+    }
     if (rbFullLinearTransformation.isSelected()) {
       return Transform.FULL_LINEAR_TRANSFORMATION;
     }
@@ -92,25 +164,44 @@ final class TransformChooserPanel {
     return Transform.DEFAULT;
   }
 
-  void set(Transform transform) {
-    if (transform == null) {
-      transform = Transform.DEFAULT;
+  void setTransform(Transform transform) {
+    if (cbSearch != null) {
+      if (transform == Transform.SKIP_SEARCH) {
+        cbSearch.setSelected(false);
+      }
+      else {
+        cbSearch.setSelected(true);
+      }
+      updateDisplay();
     }
-    if (transform == Transform.FULL_LINEAR_TRANSFORMATION) {
-      rbFullLinearTransformation.setSelected(true);
-    }
-    else if (transform == Transform.ROTATION_TRANSLATION_MAGNIFICATION) {
-      rbRotationTranslationMagnification.setSelected(true);
-    }
-    else if (transform == Transform.ROTATION_TRANSLATION) {
-      rbRotationTranslation.setSelected(true);
-    }
-    else if (transform == Transform.TRANSLATION) {
-      rbTranslation.setSelected(true);
+    if (transform != Transform.SKIP_SEARCH) {
+      if (transform == null) {
+        transform = Transform.DEFAULT;
+      }
+      if (transform == Transform.FULL_LINEAR_TRANSFORMATION) {
+        rbFullLinearTransformation.setSelected(true);
+      }
+      else if (transform == Transform.ROTATION_TRANSLATION_MAGNIFICATION) {
+        rbRotationTranslationMagnification.setSelected(true);
+      }
+      else if (transform == Transform.ROTATION_TRANSLATION) {
+        rbRotationTranslation.setSelected(true);
+      }
+      else if (transform == Transform.TRANSLATION) {
+        rbTranslation.setSelected(true);
+      }
     }
   }
 
-  void includeTranslation() {
-    includeTranslation = true;
+  private static final class TransformChooserListener implements ActionListener {
+    private final TransformChooserPanel panel;
+
+    private TransformChooserListener(final TransformChooserPanel panel) {
+      this.panel = panel;
+    }
+
+    public void actionPerformed(final ActionEvent event) {
+      panel.action();
+    }
   }
 }
