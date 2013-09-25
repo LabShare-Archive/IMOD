00e0495808556409ced4f2c6dd133c51cc512929
SueHeld <sueh@colorado.edu>
2013-09-25 14:41:28 -0600

    Bug# 1707 Added align tasks.

--- Etomo/src/etomo/comscript/TomodataplotsParam.java
+++ Etomo/src/etomo/comscript/TomodataplotsParam.java
@@ -62,8 +62,14 @@ public final class TomodataplotsParam {
   }
 
   private static final class TypeOfDataToPlot {
-    private static final TypeOfDataToPlot STATS_MIN_MAX = new TypeOfDataToPlot("13");
-    private static final TypeOfDataToPlot BLEND_MEAN_MAX = new TypeOfDataToPlot("4");
+    private static final TypeOfDataToPlot MEAN_MAX = new TypeOfDataToPlot("4");
+    private static final TypeOfDataToPlot ROTATION = new TypeOfDataToPlot("5");
+    private static final TypeOfDataToPlot TILT_SKEW = new TypeOfDataToPlot("6");
+    private static final TypeOfDataToPlot MAG = new TypeOfDataToPlot("7");
+    private static final TypeOfDataToPlot XSTRETCH = new TypeOfDataToPlot("8");
+    private static final TypeOfDataToPlot RESID = new TypeOfDataToPlot("9");
+    private static final TypeOfDataToPlot AVER_RESID = new TypeOfDataToPlot("10");
+    private static final TypeOfDataToPlot MIN_MAX = new TypeOfDataToPlot("13");
 
     private final String value;
 
@@ -73,20 +79,41 @@ public final class TomodataplotsParam {
   }
 
   public static final class Task implements TaskInterface {
-    public static final Task STATS_MIN_MAX = new Task("Plot min/max",
-        TomodataplotsParam.TypeOfDataToPlot.STATS_MIN_MAX, FileType.STATS_LOG,
+    public static final Task MIN_MAX = new Task("Plot min/max",
+        TomodataplotsParam.TypeOfDataToPlot.MIN_MAX, FileType.STATS_LOG,
         "View number in raw stack ");
-    public static final Task STATS_FIXED_MIN_MAX = new Task("Plot fixed min/max",
-        TomodataplotsParam.TypeOfDataToPlot.STATS_MIN_MAX, FileType.FIXED_STATS_LOG,
+    public static final Task FIXED_MIN_MAX = new Task("Plot fixed min/max",
+        TomodataplotsParam.TypeOfDataToPlot.MIN_MAX, FileType.FIXED_STATS_LOG,
         "View number in fixed stack ");
-    public static final Task BLEND_MEAN_MAX = new Task("plot edge errors",
-        TomodataplotsParam.TypeOfDataToPlot.BLEND_MEAN_MAX, FileType.CROSS_CORRELATION_LOG,
-        "View number in coarse aligned stack ");
+    public static final Task MEAN_MAX = new Task("Plot edge errors",
+        TomodataplotsParam.TypeOfDataToPlot.MEAN_MAX, FileType.CROSS_CORRELATION_LOG);
+    public static final Task ROTATION = new Task("Plot rotation",
+        TomodataplotsParam.TypeOfDataToPlot.ROTATION, FileType.ALIGN_SOLUTIONS_LOG);
+    public static final Task TILT_SKEW = new Task("Plot delta tilt and skew",
+        TomodataplotsParam.TypeOfDataToPlot.TILT_SKEW, FileType.ALIGN_SOLUTIONS_LOG);
+    public static final Task MAG = new Task("Plot magnification",
+        TomodataplotsParam.TypeOfDataToPlot.MAG, FileType.ALIGN_SOLUTIONS_LOG);
+    public static final Task XSTRETCH = new Task("Plot X-stretch (dmag)",
+        TomodataplotsParam.TypeOfDataToPlot.XSTRETCH, FileType.ALIGN_SOLUTIONS_LOG);
+    public static final Task RESID = new Task("Plot global mean residual",
+        TomodataplotsParam.TypeOfDataToPlot.RESID, FileType.ALIGN_SOLUTIONS_LOG);
+    public static final Task AVER_RESID = new Task(
+        "Plot the average of the mean residual",
+        TomodataplotsParam.TypeOfDataToPlot.AVER_RESID, FileType.ALIGN_SOLUTIONS_LOG);
 
     private final String label;
     private final TomodataplotsParam.TypeOfDataToPlot typeOfDataToPlot;
     private final FileType inputFile;
     private final String xaxisLabel;
+
+    private Task(final String label,
+        final TomodataplotsParam.TypeOfDataToPlot typeOfDataToPlot,
+        final FileType inputFile) {
+      this.label = label;
+      this.typeOfDataToPlot = typeOfDataToPlot;
+      this.inputFile = inputFile;
+      this.xaxisLabel = null;
+    }
 
     private Task(final String label,
         final TomodataplotsParam.TypeOfDataToPlot typeOfDataToPlot,
