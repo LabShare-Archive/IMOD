013f874f2b5a6f841aa5ade5bc95e8dcd5937d58
mastcu <mast@colorado.edu>
2014-01-02 16:12:21 -0700

    Removed due to replacement with C code

--- flib/subrs/imsubs/imunit.f
+++
@@ -1,9 +0,0 @@
-C       *IMUNIT
-C       
-C       Returns the lower-level compatible unit number (Function call)
-C       
-      FUNCTION IMUNIT(ISTREAM)
-      use imsubs
-      IMUNIT = LSTREAM(ISTREAM)
-      RETURN
-      END
