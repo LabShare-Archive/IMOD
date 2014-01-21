6d3169677d4e27fe6ab3a2b039e33d55c66b5d12
mastcu <mast@colorado.edu>
2014-01-20 22:41:05 -0700

    After some renaming and formatting

--- flib/subrs/hvem/get_tilt_angles.f90
+++ flib/subrs/hvem/get_tilt_angles.f90
@@ -1,156 +1,139 @@
-c       !
-c       GET_TILT_ANGLES will read in tilt angles by the user's method of
-c       choice.  
-c       ^ [nview] should be the number of views, if known, or 0 if it
-c       not known, in which case the routine will return the number in this
-c       variable.  
-c       ^ [nunit] should contain the number of a free logical unit.
-c       ^ Tilt angles are returned in [tilt].
-c       ^ [limtilt] should contain the dimensions of [tilt].
-c       ^ [ifpip] should be set to 0 for interactive input, or nonzero for
-c       input via PIP.  In the later case, the program must define the
-c       options FirstTiltAngle, TiltIncrement, TiltFile, and TiltAngles.
-c       If a TiltFile is entered, or if TiltAngles are entered, they 
-c       supercede entries of FirstTiltAngle and TiltIncrement.
-c       !       
-c       $Id$
-c       $Log$
-c       Revision 3.4  2009/08/24 22:10:25  mast
-c       Upgrade filename length
-c
-c       Revision 3.3  2006/01/26 15:11:43  mast
-c       Added function to read from file
-c
-c       Revision 3.2  2003/10/24 03:35:42  mast
-c       fix problem with reading set of angles with PIP
-c	
-c       Revision 3.1  2003/06/21 00:45:49  mast
-c       New version to use PIP as an option
-c       
-      subroutine get_tilt_angles(nview,nunit,tilt, limtilt, ifpip)
-      implicit none
-      integer*4 nview, nunit, ifpip, limtilt
-      real*4 tilt(*)
-      character*320 filename
-      integer*4 nvin, i, ierr, ierr2, numLines, index, ninLine
-      real*4 tiltstr, tiltinc
-      logical startIncOK
-      integer*4 PipGetFloat,PipGetString, PipNumberOfEntries
-      integer*4 PipGetFloatArray
-c       
-      if (ifpip .ne. 0) then
-        ierr = PipGetFloat('FirstTiltAngle', tiltstr)
-        ierr2 = PipGetFloat('TiltIncrement', tiltinc)
-        startIncOK = ierr .eq. 0 .and. ierr2 .eq. 0
+! !
+! GET_TILT_ANGLES will read in tilt angles by the user's method of
+! choice.
+! ^ [numViews] should be the number of views, if known, or 0 if it
+! not known, in which case the routine will return the number in this
+! variable.
+! ^ [nunit] should contain the number of a free logical unit.
+! ^ Tilt angles are returned in [tilt].
+! ^ [limTilt] should contain the dimensions of [tilt].
+! ^ [ifpip] should be set to 0 for interactive input, or nonzero for
+! input via PIP.  In the later case, the program must define the
+! options FirstTiltAngle, TiltIncrement, TiltFile, and TiltAngles.
+! If a TiltFile is entered, or if TiltAngles are entered, they
+! supercede entries of FirstTiltAngle and TiltIncrement.
+! !
+! $Id$
+!
+subroutine get_tilt_angles(numViews, nunit, tilt, limTilt, ifpip)
+  implicit none
+  integer*4 numViews, nunit, ifpip, limTilt
+  real*4 tilt(*)
+  character*320 filename
+  integer*4 nViewIn, i, ierr, ierr2, numLines, index, ninLine
+  real*4 tiltStart, tiltInc
+  logical startIncOK
+  integer*4 PipGetFloat, PipGetString, PipNumberOfEntries
+  integer*4 PipGetFloatArray
+  !
+  if (ifpip .ne. 0) then
+    ierr = PipGetFloat('FirstTiltAngle', tiltStart)
+    ierr2 = PipGetFloat('TiltIncrement', tiltInc)
+    startIncOK = ierr == 0 .and. ierr2 == 0
 
-        ierr = PipGetString('TiltFile', filename)
-        ierr2 = PipNumberOfEntries('TiltAngles', numLines)
+    ierr = PipGetString('TiltFile', filename)
+    ierr2 = PipNumberOfEntries('TiltAngles', numLines)
 
-        if (ierr .gt. 0 .and. numLines .eq. 0) then
-          if (.not. startIncOK) call gta_errorexit('NO TILT ANGLES'//
-     &        ' SPECIFIED BY START AND INCREMENT, FILE, OR '//
-     &        'INDIVIDUAL VALUES')
-          do i=1,nview
-            tilt(i)=tiltstr+(i-1)*tiltinc
-          enddo
-          return
-        endif
-
-        if (numLines .gt. 0) then
-          if (ierr .eq. 0) call gta_errorexit('YOU CANNOT SPECIFY '//
-     &        'BOTH A TILT ANGLE FILE AND INDIVIDUAL ENTRIES')
-          index = 0
-          do i = 1, numLines
-            ninLine = 0
-            ierr = PipGetFloatArray('TiltAngles', tilt(index + 1), ninLine,
-     &          limtilt - index)
-            index = index + ninLine
-          enddo
-          if (nview .eq. 0) nview = index
-          if (index .ne. nview) then 
-            print *
-            print *,'ERROR: GET_TILT_ANGLES -',nview,
-     &          ' ANGLES EXPECTED BUT ONLY', index,' ENTERED'
-            call exit(1)
-          endif
-          return
-        endif
-      else
-        if(nview.eq.0)then
-          write(*,'(1x,a,/,a,/,a,$)')'Enter the # of tilt angles to '//
-     &        'specify them by starting and increment angle,',
-     &        '     - the # of angles to specify each individual value,'
-     &        ,'    or 0 to read angles from a file: '
-          read(5,*)nvin
-          nview = abs(nvin)
-        else
-          write(*,'(1x,a,/,a,/,a,$)')'Enter 1 to '//
-     &        'specify starting and increment angle,',
-     &        '     -1 to specify each individual value,'
-     &        ,'    or 0 to read angles from a file: '
-          read(5,*)nvin
-        endif
-        if (nview .gt. limtilt) call gta_errorexit(
-     &      'TOO MANY VIEWS FOR TILT ANGLE ARRAY')
-        if(nvin.gt.0)then
-          write(*,'(1x,a,$)')'Starting and increment angle: '
-          read(5,*)tiltstr,tiltinc
-          do i=1,nview
-            tilt(i)=tiltstr+(i-1)*tiltinc
-          enddo
-          return
-        elseif(nvin.lt.0)then
-          print *,'Enter all tilt angles'
-          read(5,*,err=40,end=30)(tilt(i),i=1,nview)
-          return
-        else
-          write(*,'(1x,a,$)')'Name of file with tilt angles: '
-          read(5,'(a)')filename
-        endif
-      endif
-
-      call read_tilt_file(nview, nunit, filename, tilt, limtilt)
+    if (ierr > 0 .and. numLines == 0) then
+      if (.not. startIncOK) call gta_errorexit('NO TILT ANGLES'// &
+          ' SPECIFIED BY START AND INCREMENT, FILE, OR INDIVIDUAL VALUES')
+      do i = 1, numViews
+        tilt(i) = tiltStart + (i - 1) * tiltInc
+      enddo
       return
-30    call gta_errorexit('end of file or input reached reading tilt angles')
-40    call gta_errorexit('error reading tilt angles')
-      end
+    endif
 
-
-c       !
-c       READ_TILT_FILE will read in tilt angles from a file.
-c       ^ [nview] should be the number of views, if known, or 0 if it
-c       not known, in which case the routine will return the number in this
-c       variable.  
-c       ^ [nunit] should contain the number of a free logical unit.
-c       ^ [filename] should contain the filename.
-c       ^ Tilt angles are returned in [tilt].
-c       ^ [limtilt] should contain the dimensions of [tilt].
-c       !       
-      subroutine read_tilt_file(nview, nunit, filename, tilt, limtilt)
-      implicit none
-      integer*4 nview, nunit, limtilt, i
-      real*4 tilt(*)
-      character*(*) filename
-      call dopen(nunit,filename,'ro','f')
-      if(nview.eq.0)then
-10      read(nunit,*,end=20,err=40)tilt(nview+1)
-        nview=nview+1
-        if (nview .gt. limtilt) call gta_errorexit(
-     &      'TOO MANY VIEWS FOR TILT ANGLE ARRAY')
-        go to 10
-      else
-        read(nunit,*,err=40,end=30)(tilt(i),i=1,nview)
+    if (numLines > 0) then
+      if (ierr == 0) call gta_errorexit('YOU CANNOT SPECIFY '// &
+          'BOTH A TILT ANGLE FILE AND INDIVIDUAL ENTRIES')
+      index = 0
+      do i = 1, numLines
+        ninLine = 0
+        ierr = PipGetFloatArray('TiltAngles', tilt(index + 1), ninLine, limTilt - index)
+        index = index + ninLine
+      enddo
+      if (numViews == 0) numViews = index
+      if (index .ne. numViews) then
+        print *
+        print *,'ERROR: GET_TILT_ANGLES -', numViews, &
+            ' ANGLES EXPECTED BUT ONLY', index, ' ENTERED'
+        call exit(1)
       endif
-20    close(nunit)
       return
-30    call gta_errorexit('end of file reached reading tilt angles')
-40    call gta_errorexit('error reading tilt angles')
-      end
-      
+    endif
+  else
+    if (numViews == 0) then
+      write(*,'(1x,a,/,a,/,a,$)') 'Enter the # of tilt angles to '// &
+          'specify them by starting and increment angle,', &
+          '     - the # of angles to specify each individual value,' &
+          , '    or 0 to read angles from a file: '
+      read(5,*) nViewIn
+      numViews = abs(nViewIn)
+    else
+      write(*,'(1x,a,/,a,/,a,$)') 'Enter 1 to '// &
+          'specify starting and increment angle,', &
+          '     -1 to specify each individual value,' &
+          , '    or 0 to read angles from a file: '
+      read(5,*) nViewIn
+    endif
+    if (numViews > limTilt) call gta_errorexit('TOO MANY VIEWS FOR TILT ANGLE ARRAY')
+    if (nViewIn > 0) then
+      write(*,'(1x,a,$)') 'Starting and increment angle: '
+      read(5,*) tiltStart, tiltInc
+      do i = 1, numViews
+        tilt(i) = tiltStart + (i - 1) * tiltInc
+      enddo
+      return
+    elseif (nViewIn < 0) then
+      print *,'Enter all tilt angles'
+      read(5,*,err = 40, end = 30) (tilt(i), i = 1, numViews)
+      return
+    else
+      write(*,'(1x,a,$)') 'Name of file with tilt angles: '
+      read(5, '(a)') filename
+    endif
+  endif
 
-      subroutine gta_errorexit(message)
-      character*(*) message
-      write(*,'(/,a,a)')'ERROR: GET_TILT_ANGLES - ',message
-      call exit(1)
-      end
-      
+  call read_tilt_file(numViews, nunit, filename, tilt, limTilt)
+  return
+30 call gta_errorexit('end of input reached reading tilt angles')
+40 call gta_errorexit('error reading tilt angles')
+end subroutine get_tilt_angles
+
+
+! !
+! READ_TILT_FILE will read in tilt angles from a file.
+! ^ [numViews] should be the number of views, if known, or 0 if it
+! not known, in which case the routine will return the number in this
+! variable.
+! ^ [nunit] should contain the number of a free logical unit.
+! ^ [filename] should contain the filename.
+! ^ Tilt angles are returned in [tilt].
+! ^ [limTilt] should contain the dimensions of [tilt].
+! !
+subroutine read_tilt_file(numViews, nunit, filename, tilt, limTilt)
+  implicit none
+  integer*4 numViews, nunit, limTilt, i
+  real*4 tilt(*)
+  character*(*) filename
+  call dopen(nunit, filename, 'ro', 'f')
+  if (numViews == 0) then
+10  read(nunit,*,end = 20, err = 40) tilt(numViews + 1)
+    numViews = numViews + 1
+    if (numViews > limTilt) call gta_errorexit('TOO MANY VIEWS FOR TILT ANGLE ARRAY')
+    go to 10
+  else
+    read(nunit,*,err = 40, end = 30) (tilt(i), i = 1, numViews)
+  endif
+20 close(nunit)
+  return
+30 call gta_errorexit('end of file reached reading tilt angles')
+40 call gta_errorexit('error reading tilt angles')
+end subroutine read_tilt_file
+
+
+subroutine gta_errorexit(message)
+  character*(*) message
+  write(*,'(/,a,a)') 'ERROR: GET_TILT_ANGLES - ', message
+  call exit(1)
+end subroutine gta_errorexit
