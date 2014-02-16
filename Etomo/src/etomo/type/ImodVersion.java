1ed431a298c70f00c9b27b8ca367fc7a512160f1
mastcu <mast@colorado.edu>
2014-02-15 23:31:04 -0700

    new version

--- Etomo/src/etomo/type/ImodVersion.java
+++ Etomo/src/etomo/type/ImodVersion.java
@@ -17,5 +17,5 @@ package etomo.type;
 public class ImodVersion {
   public static  final String  rcsid =  "$Id$";
   
-  public static final String CURRENT_VERSION = "4.6.40";
+  public static final String CURRENT_VERSION = "4.6.41";
 }
